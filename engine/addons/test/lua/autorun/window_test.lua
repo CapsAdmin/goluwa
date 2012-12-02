7dbaed51958073524756dc598c481517dc7034cd
CapsAdmin <eliashogstvedt@gmail.com>
2012-12-02 13:45:21 +0100

    Revert "objects!"
    This reverts commit d4888d1a691bab68828eb12218b9bf32ed60d6a1 [formerly f842b5fd0ec3fbb8d40976491e951b7590985e7c].
    
    
    Former-commit-id: 1a2f93841f41f72bef9dcd32ae923b54c1d95e15

--- engine/addons/test/lua/autorun/window_test.lua
+++ engine/addons/test/lua/autorun/window_test.lua
@@ -1,54 +1,61 @@
+
+local sfw = sfml.window
+local sfg = sfml.graphics
+local sfs = sfml.system
+local sfw = sfml.window
+local sfa = sfml.audio
+
 local mode = ffi.new("sfVideoMode")
 mode.width = 800
 mode.height = 600
 mode.bitsPerPixel = 32
 
 -- Create the main window
-local window = RenderWindow(mode, "SFML window", bit.bor(sfml.libraries.window.sfResize, sfml.libraries.window.sfClose), ffi.new("sfContextSettings"))
+local window = sfg.sfRenderWindow_create(mode, "SFML window", bit.bor(sfw.sfResize, sfw.sfClose), nil)
 
 -- Load a sprite to display
 local rect = ffi.new("sfIntRect", 0, 0, 100, 100)
 
-local texture = Texture("file", "../textures/cute_image.jpg", rect)
-local sprite = Sprite()
-sprite:SetTexture(texture, 1)
+local texture = sfg.sfTexture_createFromFile("../textures/cute_image.jpg", rect)
+local sprite = sfg.sfSprite_create()
+sfg.sfSprite_setTexture(sprite, texture, 1)
 
 -- Create a graphical text to display
-local font = Font("file", "../fonts/arial.ttf")
-local text = Text()
-text:SetString("Hello SFML")
-text:SetFont(font)
-text:SetCharacterSize(50)
+local font = sfg.sfFont_createFromFile("../fonts/arial.ttf")
+local text = sfg.sfText_create()
+sfg.sfText_setString(text, "Hello SFML")
+sfg.sfText_setFont(text, font)
+sfg.sfText_setCharacterSize(text, 50)
 
 -- Load a music to play
-local music = Music("file", "../sound/nice_music.ogg")
+local music = sfa.sfMusic_createFromFile("../sound/nice_music.ogg")
 
 -- Play the music
-music:Play()
+sfa.sfMusic_play(music)
 
 local event = ffi.new("sfEvent")
 
 -- Start the game loop
 hook.Add("OnUpdate", "test", function()
-	if window:IsOpen() then
+	if sfg.sfRenderWindow_isOpen(window) then
 	-- Process events
-		if window:PollEvent(event) then
+		if sfg.sfRenderWindow_pollEvent(window, event) then
 			-- Close window : exit
-			if event.type == sfml.libraries.window.sfEvtClosed then
-				window:Close()
+			if event.type == sfw.sfEvtClosed then
+				sfg.sfRenderWindow_close(window)
 			end
  		end
 
 		-- Clear the screen
-		window:Clear(sfml.libraries.graphics.sfBlack)
+		sfg.sfRenderWindow_clear(window, sfg.sfBlack)
 
 		-- Draw the sprite
-		window:DrawSprite(sprite, nil)
+		sfg.sfRenderWindow_drawSprite(window, sprite, nil)
 
 		-- Draw the text
-		window:DrawText(text, nil)
+		sfg.sfRenderWindow_drawText(window, text, nil)
 
 		-- Update the window
-		window:Display()
+		sfg.sfRenderWindow_display(window)
 	end
 end)
