2b007caa6b3c21eae3a1a139484809a68dc048be
CapsAdmin <eliashogstvedt@gmail.com>
2013-06-08 02:47:29 +0200

    added networkable keys

--- addons/online/lua/online/players.lua
+++ addons/online/lua/online/players.lua
@@ -56,6 +56,42 @@ do -- player meta
 		self.IsValid = function() return false end
 		timer.Simple(0, function() utilities.MakeNULL(self) end)
 	end	
+	
+	do -- networked input
+		local function add_event(name, check)	
+			input.SetupAccessorFunctions(META, name)
+			
+			if CLIENT then
+				event.AddListener("On" .. name .. "Input", "player_" .. name .. "_event", function(key, press)
+					local ply = players.GetLocalPlayer()
+					
+					if ply:IsValid() then						
+						if check and not check[key] then return end
+						
+						input.CallOnTable(ply, name, key, press, nil, nil, true)
+						message.Send("Player" .. name .. "Event", key, press)
+						
+						return event.Call("Player" .. name .. "Event", ply, key, press)
+					end
+				end, print)
+			end
+			
+			if SERVER then
+				message.AddListener("Player" .. name .. "Event", function(ply, key, press)
+					if ply:IsValid() then
+						if check and not check[key] then return end
+
+						input.CallOnTable(ply, name, key, press, nil, nil, true)
+					
+						event.Call("Player" .. name .. "Event", ply, key, press)
+					end
+				end, print)
+			end
+		end
+				
+		add_event("Key")
+		add_event("Char")
+	end
 		
 	players.player_meta = META
 
