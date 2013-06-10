14e3ad3b47247c1b3e673ecf0c8d2e4f44dec5da
CapsAdmin <eliashogstvedt@gmail.com>
2013-06-10 04:45:18 +0200

    added todo improved threedee
    Former-commit-id: 34d6eabf8cc9bd65631843bf31a14614737d7f6d

--- addons/sfml tests/lua/threedee.lua
+++ addons/sfml tests/lua/threedee.lua
@@ -62,8 +62,7 @@ do -- model
 	function META:Draw()
 		if not self.obj then return end
 
-		gl.MatrixMode(e.GL_MODELVIEW)
-		gl.LoadIdentity()
+		gl.PushMatrix()
 
 		gl.Translated(self.pos.x, self.pos.y, self.pos.z)
 		gl.Rotated(self.ang.p, 1, 0, 0)
@@ -88,6 +87,8 @@ do -- model
 				gl.Vertex3d(data[3].x, data[3].y, data[3].z)
 			gl.End()
 		end
+
+		gl.PopMatrix()
 	end
 
 	function Model(path)
@@ -105,73 +106,87 @@ do -- model
 	end
 end
    
-local function calc_camera()
+  
+local last_x = 0
+local last_y = 0
+   
+local function calc_camera(window)
+	local pos = mouse.GetPosition(ffi.cast("sfWindow * ", window))
+	
+	local dx = pos.x - last_x
+	local dy = pos.y - last_y
+	
+	cam_ang.p = math.clamp(cam_ang.p + dy * 0.01, -math.pi / 2, math.pi / 2)
+	cam_ang.y = (cam_ang.y + dx * 0.01)%(math.pi*2)
+		
+	last_x = pos.x 
+	last_y = pos.y
+		
+	--local size = window:GetSize()
+	--mouse.SetPosition(Vector2i(size.x/2, size.y/2), ffi.cast("sfWindow * ", window))
+	 
+
 	if input.IsKeyDown("space") then
-		cam_pos = cam_pos - (cam_ang:GetUp() * 1)
-	elseif input.IsKeyDown("lcontrol") then
-		cam_pos = cam_pos + (cam_ang:GetUp() * 1)
+		cam_pos = cam_pos + cam_ang:GetUp()
+	elseif input.IsKeyDown("l_control") then
+		cam_pos = cam_pos - cam_ang:GetUp()
 	end
 
 	if input.IsKeyDown("w") then
-		cam_pos = cam_pos + (cam_ang:GetForward() * 1)
+		cam_pos = cam_pos + cam_ang:GetForward()
 	elseif input.IsKeyDown("s") then
-		cam_pos = cam_pos - (cam_ang:GetForward() * 1)
+		cam_pos = cam_pos - cam_ang:GetForward()
 	end
 
 	if input.IsKeyDown("a") then
-		cam_pos = cam_pos + (cam_ang:GetRight() * 1)
+		cam_pos = cam_pos - cam_ang:GetRight()
 	elseif input.IsKeyDown("d") then
-		cam_pos = cam_pos - (cam_ang:GetRight() * 1)
+		cam_pos = cam_pos + cam_ang:GetRight()
 	end
 
 	if input.IsKeyDown("up") then
-		cam_ang.p = math.clamp(cam_ang.p - 4, -90, 90)
+		cam_ang.p = math.clamp(cam_ang.p + 0.1, -math.pi / 2, math.pi / 2)
 	elseif input.IsKeyDown("down") then
-		cam_ang.p = math.clamp(cam_ang.p + 4, -90, 90)
+		cam_ang.p = math.clamp(cam_ang.p - 0.1, -math.pi / 2, math.pi / 2)
 	end
 
 	if input.IsKeyDown("left") then
-		cam_ang.y = (cam_ang.y - 4)%360
+		cam_ang.y = (cam_ang.y + 0.1)%(math.pi*2)
 	elseif input.IsKeyDown("right") then
-		cam_ang.y = (cam_ang.y + 4)%360
+		cam_ang.y = (cam_ang.y - 0.1)%(math.pi*2)
 	end
-end
+	
+	local a = cam_ang:GetDeg() 
+	
+	gl.Rotatef(a.p , 1.0, 0.0, 0.0);
+	gl.Rotatef(a.y , 0.0, 1.0, 0.0);
+	gl.Rotatef(a.r , 0.0, 0.0, 1.0);
+	gl.Translatef(-cam_pos.x, -cam_pos.z, -cam_pos.y)
+end 
 
-input.debug = false  
-
-gl.ClearColor(0, 0.25, 0.5, 1)
-gl.LineWidth(1.5)
-gl.Enable(e.GL_LINE_SMOOTH)
-gl.Enable(e.GL_POINT_SMOOTH)
-gl.PointSize(10)
+gl.ClearColor(0.0, 0.25, 0.5, 1)
 gl.Enable(e.GL_BLEND)
 gl.BlendFunc(e.GL_SRC_ALPHA, e.GL_ONE_MINUS_SRC_ALPHA)
+gl.Enable(e.GL_DEPTH_TEST)
 
 event.AddListener("OnDraw", "gl", function(dt, window)
 	local size = window:GetSize()
 	frame = os.clock()*10
-	calc_camera()
- 
+	
 	gl.Viewport(0, 0, size.x, size.y)
+	gl.Clear(e.GL_COLOR_BUFFER_BIT + e.GL_DEPTH_BUFFER_BIT)
 
-	gl.ClearColor(0, 0, 0, 1)
-	gl.Clear(e.GL_COLOR_BUFFER_BIT)
-
-	gl.MatrixMode(e.GL_PROJECTION)
+	
 	gl.LoadIdentity()
-	glu.Perspective(70, size.x/size.y, 0.1, 10000)
-	
-	gl.Rotated(cam_ang.p, 1, 0, 0)
-	gl.Rotated(cam_ang.y, 0, 1, 0)
-	
-	gl.Translated(cam_pos.x, cam_pos.y, cam_pos.z)
-
+	calc_camera(window) 
+ 
 	for key, obj in pairs(active_models) do
 		obj:Draw()
 	end
+
+	gl.Flush()
 end)
 
 local obj = Model()
 obj:SetModel("teapot.obj")
 obj:SetSize(0.12)
-obj:SetAngles(Ang3(10,0,0))
