e0a067933d53e3cdb446328ac73008884207fbda
CapsAdmin <eliashogstvedt@gmail.com>
2013-06-11 22:07:09 +0200

    fixed camera movement in threedee
    Former-commit-id: e70052ff7e4931c98811d9704558e39313f47942

--- addons/ffi_structs/lua/structs/Ang3.lua
+++ addons/ffi_structs/lua/structs/Ang3.lua
@@ -34,6 +34,85 @@ function META.GetForward(a)
 	)
 end
 
+if false then
+	--[[
+	
+	GMOD
+	
+	======90 0 0======
+	FORWARD = -1 -0 -1
+	RIGHT = 0 -1 0
+	UP = 1 0 -1
+	==================
+	
+	======0 90 0======
+	FORWARD = -1 1 -0
+	RIGHT = 1 0 -0
+	UP = 0 0 1
+	==================
+	
+	======0 0 90======
+	FORWARD = 1 0 -0
+	RIGHT = -0 0 -1
+	UP = 0 -1 -1
+	==================		
+	]]
+	
+	--[[
+	
+	ASDFML
+	
+	======90 0 0======
+	FORWARD = 0 -1 1
+	RIGHT = 1 0 -0
+	UP = 0 -1 -1
+	==================
+
+
+	======0 90 0======
+	FORWARD = 0 1 -0
+	RIGHT = -1 -0 -1
+	UP = 1 0 -1
+	==================
+
+
+	======0 0 90======
+	FORWARD = -1 -1 0
+	RIGHT = -1 1 -0
+	UP = 0 0 1
+	==================
+
+
+	]]
+
+	local msg = gmod and MsgN or logn
+
+	local printv = function(str, v)
+		msg(str .. " = " .. math.floor(v.x) .. " " .. math.floor(v.y) .. " " .. math.floor(v.z)) 
+	end
+
+	function PrintDirectons(p, y, r) 
+		local ang = gmod and Angle(p,y,r) or Ang3(math.rad(p),math.rad(y),math.rad(r)) 
+
+		msg("\n======" .. p .. " " .. y .. " " .. r .. "======")
+		if gmod then 		
+			printv("FORWARD", ang:Forward()) 
+			printv("RIGHT", ang:Right()) 
+			printv("UP", ang:Up()) 
+		else 
+			printv("FORWARD", ang:GetForward()) 
+			printv("RIGHT", ang:GetRight()) 
+			printv("UP", ang:GetUp()) 
+		end
+		msg("==================\n")
+	end
+
+	PrintDirectons(0, 0, 0)
+	PrintDirectons(180, 0, 0)
+	PrintDirectons(0, 0, 180)
+end
+
+
 local PI1 = math.pi
 local PI2 = math.pi * 2
 local function normalize(a, b)
