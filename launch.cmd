5821bcd10b2c1e130c6ec263990b16da7290ab2d
CapsAdmin <eliashogstvedt@gmail.com>
2013-08-30 21:56:56 +0200

    added a hacky (for now?) ability to pass args to launch.cmd
    Former-commit-id: 182c46113c883ed3d7fa00799c529f03a5388bfa

--- launch.cmd
+++ launch.cmd
@@ -1,7 +1,8 @@
+cd %~dp0
 cd bin/windows/x86/
 mode con:cols=140 lines=50
 
 :LOL:
-luajit.exe -e PLATFORM='glw'dofile('../../../lua/init.lua')
+luajit.exe -e PLATFORM='glw'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')
 pause
 goto LOL
