911586e2d370e1f9aa7e7c4ae882b1de0b7cbf95
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-01 01:00:08 +0200

    added wip debug.stepin()

--- lua/platforms/standard/extensions/debug.lua
+++ lua/platforms/standard/extensions/debug.lua
@@ -26,3 +26,87 @@ function debug.getparams(func)
 
     return params
 end
+
+function debug.getparamsx(func)
+    local params = {}
+	
+	for i = 1, math.huge do
+		local key, val = debug.getlocal(func, i)
+		if key then
+			table.insert(params, {key = key, val = val})
+		else
+			break
+		end
+	end
+
+    return params
+end
+
+function debug.stepin()
+	if not curses then return end
+	
+	local first_time = true
+
+	local step
+	
+	step = function()
+		system.SetWindowTitle("DEBUG SETHOOK |space = exit | enter = return | down = line | pagedown = call|")
+		curses.Clear()
+		
+		local info = debug.getinfo(2)
+		local script = vfs.Read(info.short_src)
+		
+		logn(info.short_src)
+		
+		if script then
+			local lines = script:explode("\n")
+			
+			for i = -10, 10 do
+				local line = lines[info.currentline + i]
+				
+				if i == 0 then
+					line = (">"):rep(string.len(info.currentline)) .. ":\t" ..  line
+				else
+					line = (info.currentline + i) .. ": " .. line
+				end
+				
+				curses.ColorPrint(line .. "\n")
+			end
+		else
+			logn(info.short_src)
+		end
+		
+		logn("")
+		logn("LOCALS: ")
+		for _, data in pairs(debug.getparamsx(3)) do
+			if not data.key:find("(",nil,true) then
+				logf("%s = %s", data.key, luadata.ToString(data.val))
+			end
+		end
+		
+		while true do
+			if first_time then
+				first_time = false
+				break
+			end
+			
+			local key = curses.GetActiveKey()
+			
+			if key == "KEY_ENTER" then
+				debug.sethook(step, "r")
+				break
+			elseif key == "KEY_DOWN" then
+				debug.sethook(step, "l")
+				break
+			elseif key == "KEY_NPAGE" then
+				debug.sethook(step, "c")
+				break
+			elseif key == "KEY_SPACE" then
+				debug.sethook()
+				break
+			end
+		end
+	end
+	
+	debug.sethook(step, "l")
+end
