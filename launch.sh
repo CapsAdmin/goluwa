d37205d64de65475e1fc20838206bba828e09604
Morten Erlandsen <animorten@gmail.com>
2013-09-15 20:48:38 +0200

    Fixed 32 bit Linux

--- launch.sh
+++ launch.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-pushd bin/linux/x64
+ARCH=$(getconf LONG_BIT)
+
+if [ $ARCH -eq "64" ]; then
+	cd bin/linux/x64
+else
+	cd bin/linux/x86
+fi
+
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
 while true; do
-	./luajit -e "PLATFORM='glw'dofile('../../../lua/init.lua')"
+	./luajit -e 'PLATFORM = "glw" dofile("../../../lua/init.lua")'
 	if [ $? -eq 0 ]; then break; fi
 	sleep 1
 done
-popd
