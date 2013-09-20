bdbfc7f040977bca6522d3f02702971d889536ae
Morten Erlandsen <animorten@gmail.com>
2013-09-20 03:51:12 +0200

    renamed sound library to audio added libsnd bindings audio library can now handle file types libsnd can handle added openal extensions for effects and such

--- addons/tests/lua/openal.lua
+++ addons/tests/lua/openal.lua
@@ -1,58 +1,23 @@
-local size = 2 ^ 16
+
+local size = 2048*4
 local data = ffi.new("unsigned char[?]", size)
 
-for i = 1, size do
-	data[i-1] = math.random(255)
-end
+ 
+local effect = audio.CreateEffect(e.AL_EFFECT_EAXREVERB)
+effect:SetParam(e.AL_EAXREVERB_DECAY_TIME, 10) 
+effect:SetParam(e.AL_EAXREVERB_DECAY_TIME, 10) 
+effect:BindToChannel(1)
 
-local sound = Sound(data, size)
-sound:Play()
-sound:SetPitch(0.1)
-timer.Simple(1, function() sound:Stop() end)
-
-do return end
-
-al.debug = true
-
-local device = alc.OpenDevice(nil)
-
--- needed to provide debugging info for alc
-alc.device = device 
-alc.debug = true
-
-local context = alc.CreateContext(device, nil)
-alc.MakeContextCurrent(context)
-
-al.Listenerfv(e.AL_POSITION, ffi.new("float[3]",0, 0, 0))
-al.Listenerfv(e.AL_VELOCITY, ffi.new("float[3]",0, 0, 0))
-al.Listenerfv(e.AL_ORIENTATION, ffi.new("float[3]", 0, 0, -1))
-
-local source = al.GenSource()
-
-al.Sourcef(source, e.AL_PITCH, 1)
-al.Sourcef(source, e.AL_GAIN, 1)
-al.Source3f(source, e.AL_POSITION, 0, 0, 0)
-al.Source3f(source, e.AL_VELOCITY, 0, 0, 0)
-al.Sourcei(source, e.AL_LOOPING, e.AL_FALSE)
-
-local buffer = al.GenBuffer()
-
-local size = 2 ^ 16
-local data = ffi.new("unsigned char[?]", size)
-
-for i = 1, size do
-	data[i-1] = math.random(255)
-end
-
-al.BufferData(buffer, e.AL_FORMAT_MONO8, data, size, 1024)
-al.Sourcei(source, e.AL_BUFFER, buffer)
-
-al.SourcePlay(source)
-
-timer.Create("um", 0,0,function()
-	local offset = ffi.new("int[1]")
-	al.GetSourcei(source, e.AL_BYTE_OFFSET, offset)	
-	offset = offset[0]
-end)
-
-LOL_SOURCE = source
+timer.Create("um", 1,0, function()
+	local um = math.randomf(-5,5) 
+	for i = 0, size-1 do
+		data[i] = ((128 + (math.sin((i/size)*500 + ((i/size)^um*100))*100)) * (i/size)) * 0.5
+	end     
+	
+	local sound = Sound(data, size)
+	sound:SetEffectChannel(1) 
+	sound:SetPitch(1) 
+	sound:Play()  
+	LOL_SOUND = sound 
+end) 
+ 
