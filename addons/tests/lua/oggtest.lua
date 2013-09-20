7b3b6d38659eff70f4a1e80ff07240867adaa5e3
Morten Erlandsen <animorten@gmail.com>
2013-09-20 04:00:25 +0200

    updated ogg example

--- addons/tests/lua/oggtest.lua
+++ addons/tests/lua/oggtest.lua
@@ -1,20 +1,28 @@
 local effect = audio.CreateEffect(e.AL_EFFECT_EAXREVERB)
 effect:SetParam(e.AL_EAXREVERB_DECAY_TIME, 10) 
-effect:BindToChannel(1)
 
-local snd = utilities.RemoveOldObject(Sound("sounds/cantina.ogg"))
-table.print(snd.decode_info)
-snd:Play() 
-snd:SetChannel(1)
-snd:SetLooping(true)
+
+local music = utilities.RemoveOldObject(Sound("sounds/cantina.ogg"))
+table.print(music.decode_info)
+music:Play() 
+music:SetChannel(1)
+music:SetLooping(true)
  
-local snd = utilities.RemoveOldObject(Sound("sounds/what a shame.ogg"))
-snd:Play() 
-snd:SetGain(0.25)
-snd:SetChannel(1)
+local voice = utilities.RemoveOldObject(Sound("sounds/what a shame.ogg"))
+voice:Play() 
+voice:SetGain(0.25)
+voice:SetChannel(1)
 
 timer.Create("shame", 1, 0, function()
-	snd:Play()
+	voice:Play()
 
-	snd:SetPosition(math.sin(glfw.GetTime()), math.cos(glfw.GetTime()),0)
+	voice:SetPosition(math.sin(glfw.GetTime()), math.cos(glfw.GetTime()),0)
 end) 
+
+timer.Create("pitchy",0,0,function()
+	music:SetPitch(1 + math.sin(glfw.GetTime()*10)/30)
+	local gain = math.abs(math.sin(os.clock()/10))
+	
+	effect:SetParam(e.AL_EAXREVERB_GAIN, gain)
+	effect:BindToChannel(1)
+end)
