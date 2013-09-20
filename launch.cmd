ac614dd360f940d17912cd0cad0990914b5e3d71
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-20 15:57:15 +0200

    cmd files will now run the appropriate version depending on if you're on 64 or 32 bit windows

--- launch.cmd
+++ launch.cmd
@@ -1,5 +1,4 @@
 cd %~dp0
-cd bin/windows/x86/
-mode con:cols=140 lines=100
-
-start "" "luajit.exe" "-e PLATFORM='glw'PDCURSES='yes'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
+ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
+cd bin/windows/%arch%/
+start "" "luajit.exe" "-e PLATFORM='glw'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
--- launch_x86.cmd
+++ launch_x86.cmd
@@ -1,5 +1,3 @@
 cd %~dp0
-cd bin/windows/x64/
-mode con:cols=140 lines=100
-
+cd bin/windows/x86/
 start "" "luajit.exe" "-e PLATFORM='glw'PDCURSES='yes'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
