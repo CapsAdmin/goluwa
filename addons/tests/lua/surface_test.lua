0be5265176171f59ee1b98a7b6c3bf347775a7d1
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-22 20:50:17 +0200

    changed

--- addons/tests/lua/surface_test.lua
+++ addons/tests/lua/surface_test.lua
@@ -1,22 +1,13 @@
-local window = glw.OpenWindow(1280, 720)
-      
-local font = surface.CreateFont("test", {
-	size = 12,
-	path = "fonts/unifont.ttf",
-})	 
-   
-event.AddListener("OnDraw2D", "gl", function(dt)		 
+local window = glw.OpenWindow(500, 500)
 
-	surface.SetFont(font)
-	for i= 1, 1000  do
-		local c = HSVToColor(glfw.GetTime(),1,1)
-		surface.Color(c.r,c.g,c.b, 1)
-		surface.SetTextPos(i,i)
-		surface.DrawText("æøå|ops汉语/漢語")
-	end
-	
-			
+event.AddListener("OnDraw2D", "lol", function()
+	surface.Color(1,1,1,1)
 	surface.SetWhiteTexture()
-	surface.Color(1,1,1, 1)
-	surface.DrawRect(400 + math.sin(glfw.GetTime()) * 100, 400 + math.cos(glfw.GetTime()) * 100, 50, 50)
-end) 
+
+	surface.DrawRect(90, 50, 100, 100)
+	
+	for i =1, 50 do
+		surface.SetTextPos(0,0)
+		surface.DrawText("LOLASUIODHAS HduAHSDIuhASPudh ASDuASd")
+	end
+end)
