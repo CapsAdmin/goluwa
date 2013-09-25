1a2992f2ee579e1cea63646cb9e8a8bb775e9afd
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-25 06:11:10 +0200

    added 2d stress test

---
+++ addons/tests/lua/surface_stress.lua
@@ -0,0 +1,20 @@
+window.Open(500, 500)
+
+local cache = {}
+
+event.AddListener("OnDraw2D", "lol", function()
+	surface.SetWhiteTexture()
+	local time = glfw.GetTime()
+	
+	for x = 1, 512 do
+	for y = 1, 512 do
+		
+		local h =  math.ceil((time + (x/y))* 100)
+		local c = cache[h] or HSVToColor(h/100,1,1)
+		cache[h] = c
+		
+		surface.Color(c.r,c.g,c.b, 0.5)
+		surface.DrawRect(x, y, 1, 1)
+	end
+	end	
+end)
