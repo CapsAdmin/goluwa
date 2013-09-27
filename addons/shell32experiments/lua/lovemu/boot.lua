f375e6436dc80684acd12abb18f75339bd1bfc56
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-27 02:15:20 +0200

    updated lovemu

--- addons/shell32experiments/lua/lovemu/boot.lua
+++ addons/shell32experiments/lua/lovemu/boot.lua
@@ -6,6 +6,16 @@ local hasWindow=false
 
 local getn=table.getn
 
+local function run()
+	love.update(lovemu.delta)
+	love.draw()
+	lovemu.translate_x=0
+	lovemu.translate_y=0
+	lovemu.scale_x=1
+	lovemu.scale_y=1
+	lovemu.angle=0
+end
+
 function lovemu.boot(folder)
 	--reload modules
 	for i=1,getn(lovemu.modules) do
@@ -52,14 +62,15 @@ function lovemu.boot(folder)
 	end
 	
 	lovemu.conf={}
-	lovemu.conf.screen={}
 	function love.conf(t) --partial
 		t.screen={}
+		t.modules={}
 		t.screen.height = 600      
 		t.screen.width = 800  
 		t.title = "LovEmu - The Love2D for GOLUWA"      
 		t.author = "Shell32"
 	end
+	love.conf(lovemu.conf)
 
 	if vfs.Exists(e.ABSOLUTE_BASE_FOLDER.."addons/shell32experiments/lovers/"..lovemu.demoname.."/conf.lua")==true then
 		print("LOADING CONF.LUA")
@@ -77,4 +88,22 @@ function lovemu.boot(folder)
 	
 	include("lovers/"..folder.."/main.lua")
 	love.load()
+	
+	event.AddListener("OnDraw2D", "lovemu", function(dt)
+		love.graphics.clear()
+		lovemu.delta=dt
+		surface.SetTexture()
+		if lovemu.errored==false then
+			local err,msg=pcall(run)
+			if err==false then
+				print("\nERROR\n")
+				print(msg)
+				lovemu.errored=true
+				lovemu.error_msg=msg
+				love.errhand(lovemu.error_msg)
+			end
+		else
+			love.errhand(lovemu.error_msg)
+		end
+	end)
 end
