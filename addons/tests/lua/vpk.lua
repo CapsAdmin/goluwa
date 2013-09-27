c8c1ae751fcc7fde0ef6d4fc540488c296c452af
Morten Erlandsen <animorten@gmail.com>
2013-09-27 10:48:52 +0200

    Added VPK test script

---
+++ addons/tests/lua/vpk.lua
@@ -0,0 +1,29 @@
+local prefix = steam.GetInstallPath() .. "/SteamApps/common"
+local packs = {}
+
+print(string.format("Looking for VPKs in %q", prefix))
+
+vfs.Traverse(prefix, function(path, mode, level)
+	if mode == "file" and path:find("_dir%.vpk$") then
+		packs[#packs + 1] = path
+	end
+end)
+
+print("Found " .. #packs .. " VPKs")
+
+local files = {}
+
+for k, v in ipairs(packs) do
+	local data, error_message = vpk.Read(v)
+
+	if not data then
+		print(error_message)
+	end
+
+	for k, v in ipairs(data.tree) do
+		files[v.Path] = files[v.Path] or {}
+		files[v.Path][#files[v.Path] + 1] = v
+	end
+end
+
+FILES = files
