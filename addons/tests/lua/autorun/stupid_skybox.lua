cc00eb968c9a311ec33879b27326429047b24f43
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-28 19:43:44 +0200

    prevented skybox from loading before the gl context has been created

--- addons/tests/lua/autorun/stupid_skybox.lua
+++ addons/tests/lua/autorun/stupid_skybox.lua
@@ -1,18 +1,21 @@
-local tex = Texture(1, 255):Fill(function(x, y)
-	local frac = y/255
-	local frac2 = -frac+1
-	
-	local c = HSVToColor(frac2/5, frac^0.5, 1)
-	
-	return c.r * 255, c.g * 255, c.b * 255, 255
-end)     
+event.AddListener("RenderContextInitialized", "skybox", function()
+	local cam_pos = Vec3(0, 0, -10)
+	local tex = Texture(1, 255):Fill(function(x, y)
+		local frac = y/255
+		local frac2 = -frac+1
+		
+		local c = HSVToColor(frac2/5, frac^0.5, 1)
+		
+		return c.r * 255, c.g * 255, c.b * 255, 255
+	end)     
 
-local stretch = math.pi
+	local stretch = math.pi
 
-event.AddListener("PreDisplay", "sky", function()
-	local h = window.GetSize().h
-	local y = render.GetCamAng().p/90
-	y = (1 + y) / 2
-	surface.SetTexture(tex)
-	surface.DrawRect(0,y*-h*stretch/2, 1024, h*stretch)	
+	event.AddListener("PreDisplay", "sky", function()
+		local h = window.GetSize().h
+		local y = render.GetCamAng().p/90
+		y = (1 + y) / 2
+		surface.SetTexture(tex)
+		surface.DrawRect(0,y*-h*stretch/2, 1024, h*stretch)	
+	end)
 end)
