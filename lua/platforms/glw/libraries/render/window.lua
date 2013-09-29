a2f88e41be975d3e976193c48253dadd6de16af4
CapsAdmin <eliashogstvedt@gmail.com>
2013-09-30 00:43:04 +0200

    changed rendering from forward to deffered

--- lua/platforms/glw/libraries/render/window.lua
+++ lua/platforms/glw/libraries/render/window.lua
@@ -101,14 +101,23 @@ do -- window meta
 
 		render.Clear(e.GL_COLOR_BUFFER_BIT, e.GL_DEPTH_BUFFER_BIT)
 
-		render.Start(self)		
+		render.Start(self)
 			event.Call("PreDisplay", dt)
 			
+			if render.gbuffer then
+				render.gbuffer:Begin()
+			end
+
 			render.Start3D()
 			event.Call("OnDraw3D", dt)
 			
 			render.Start2D()
 			event.Call("OnDraw2D", dt)
+			
+			if render.gbuffer then
+				render.gbuffer:End()
+				render.DrawDeffered(self:GetSize():Unpack())			
+			end
 			
 			event.Call("PostDisplay", dt)
 		render.End()
