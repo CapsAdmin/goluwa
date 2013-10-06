0e7e8a9d7857c055efc9b91ae535b5ca8d567efd
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-06 13:21:59 +0200

    fixed startup error if steamapi service list wasn't generated
    Former-commit-id: 6ec205795c944f4be3402311fa03fbd32aad88b1

--- lua/platforms/standard/libraries/steamapi.lua
+++ lua/platforms/standard/libraries/steamapi.lua
@@ -44,85 +44,85 @@ function steamapi.Initialize()
 		logn("steamapi key is not set (run steamapi_key *key*)")
 	end
 
-	if not steamapi.supported.apilist then
-		steamapi.UpdateSupported()
-	end
-	
 	steamapi.services = {}
-
-	for key, interface in pairs(steamapi.supported.apilist.interfaces) do
-		local functions = {}
-		
-		for key, info in pairs(interface.methods) do
-			local parameters = {}
+	
+	if steamapi.supported.apilist then	
+		for key, interface in pairs(steamapi.supported.apilist.interfaces) do
+			local functions = {}
 			
-			for key, val in pairs(info.parameters) do
-				if val.optional == false then
-					parameters[val.name] = val
+			for key, info in pairs(interface.methods) do
+				local parameters = {}
+				
+				for key, val in pairs(info.parameters) do
+					if val.optional == false then
+						parameters[val.name] = val
+					end
 				end
-			end
-			
-			functions[info.name] = function(data, callback)
-				if not steamapi.httpmethods[info.httpmethod] then
-					errorf("http method %s is not supported", 2, info.httpmethod)
-				end
-			
-				callback = callback or table.print
+				
+				functions[info.name] = function(data, callback)
+					if not steamapi.httpmethods[info.httpmethod] then
+						errorf("http method %s is not supported", 2, info.httpmethod)
+					end
+				
+					callback = callback or table.print
 
-				if parameters.key and not data.key then
-					data.key = steamapi.key
-				end
-							
-				-- check and convert parameters
-				for key, info in pairs(parameters) do
-					local t = type(data[key])
-					local expected = type_translate[info.type] or info.type
-					
-					if t == "stringnumber" then
-						local num = tonumber(data[key])
-						if not num then
-							errorf("field %q is not a valid type (expected string got %s)", 2, key, type(data[key]))
-						end
+					if parameters.key and not data.key then
+						data.key = steamapi.key
 					end
-
-					if t == "number" then
-						local num = tonumber(data[key])
-						
-						if not num then
-							errorf("field %q is not a valid type (expected number got %s)", 2, key, type(data[key]))
-						end
-						
-						data[key] = num 
-					end
-					
-					if t == "boolean" then
-						data[key] = data[key] and "1" or "0"
-					end
-					
-					if t == "string" and expected ~= "stringnumber" and key ~= "key" then
-						data[key] = base64.encode(data[key])
-					end
-					
-					if t ~= expected and expected ~= "stringnumber" and not info.optional then
-						errorf("field %q is not a valid type (expected %s got %s)", 2, key, expected, type(data[key]))
-					end
-				end
 								
-				local url = ("http://api.steampowered.com/%s/%s/v%.4d/?"):format(interface.name, info.name, data.version or 1)
-				
-				if steamapi.debug then	
-					logf("[steamapi] http url: %s", url)
-				end				
-				
-				steamapi.httpmethods[info.httpmethod](interface, info, data, url, callback)
+					-- check and convert parameters
+					for key, info in pairs(parameters) do
+						local t = type(data[key])
+						local expected = type_translate[info.type] or info.type
+						
+						if t == "stringnumber" then
+							local num = tonumber(data[key])
+							if not num then
+								errorf("field %q is not a valid type (expected string got %s)", 2, key, type(data[key]))
+							end
+						end
+
+						if t == "number" then
+							local num = tonumber(data[key])
+							
+							if not num then
+								errorf("field %q is not a valid type (expected number got %s)", 2, key, type(data[key]))
+							end
+							
+							data[key] = num 
+						end
+						
+						if t == "boolean" then
+							data[key] = data[key] and "1" or "0"
+						end
+						
+						if t == "string" and expected ~= "stringnumber" and key ~= "key" then
+							data[key] = base64.encode(data[key])
+						end
+						
+						if t ~= expected and expected ~= "stringnumber" and not info.optional then
+							errorf("field %q is not a valid type (expected %s got %s)", 2, key, expected, type(data[key]))
+						end
+					end
+									
+					local url = ("http://api.steampowered.com/%s/%s/v%.4d/?"):format(interface.name, info.name, data.version or 1)
+					
+					if steamapi.debug then	
+						logf("[steamapi] http url: %s", url)
+					end				
+					
+					steamapi.httpmethods[info.httpmethod](interface, info, data, url, callback)
+				end
 			end
+			
+			steamapi.services[interface.name] = functions
 		end
-		
-		steamapi.services[interface.name] = functions
+	else
+		steamapi.UpdateSupported(steamapi.Initialize)
 	end
 end
 
-function steamapi.UpdateSupported()
+function steamapi.UpdateSupported(callback)
 	logn("[steamapi] fetching supported api..")
 	
 	luasocket.Get("http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v0001/?key=" .. steamapi.key, function(data)
@@ -131,6 +131,8 @@ function steamapi.UpdateSupported()
 		luadata.WriteFile("steamapi/supported.lua", tbl)
 		
 		logn("[steamapi] supported api updated")
+		
+		callback()
 	end)
 end
 
