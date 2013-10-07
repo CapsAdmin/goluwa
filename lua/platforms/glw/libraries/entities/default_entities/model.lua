c0ca8a9639ed92ec48ab377621732354608d8234
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-07 21:44:10 +0200

    added DrawManual to model entities
    Former-commit-id: aeb4b48548f38bc2b4e186c12b69670463027dd9

--- lua/platforms/glw/libraries/entities/default_entities/model.lua
+++ lua/platforms/glw/libraries/entities/default_entities/model.lua
@@ -4,6 +4,7 @@ META.ClassName = "model"
 
 class.GetSet(META, "Model", NULL)
 class.GetSet(META, "ModelPath", "")
+class.GetSet(META, "DrawManual", false)
 
 function META:SetModelPath(path)
 	self.ModelPath = path
@@ -13,14 +14,14 @@ end
 function META:SetScale(v) self.Scale = v self.temp_scale = v * self.Size end
 function META:SetSize(v) self.Size = v self.temp_scale = v * self.Scale end
 
-function META:Draw()		
+function META:Draw()
 	render.PushMatrix(self.Pos, self.Angles, self.temp_scale)
 		if self.Model:IsValid() then		
 			self.Model:Draw()
 		end
 		
 		for _, ent in pairs(self.Children) do
-			if ent.Draw then
+			if ent.Draw and not ent.DrawManual then
 				ent:Draw()
 			end
 		end
