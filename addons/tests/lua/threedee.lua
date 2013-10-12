12607b7473d503981a2b745138069796ad7ca77f
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-13 00:26:44 +0200

    changed example
    Former-commit-id: b9e5fdb944b8d8118992d5ff0c4c635bfa4bba61

--- addons/tests/lua/threedee.lua
+++ addons/tests/lua/threedee.lua
@@ -2,8 +2,20 @@ window.Open(1280, 720)
   
 entities.world_entity:RemoveChildren() 
 
-local obj = Entity("model")
-obj:SetPos(Vec3(5,0,0))
-obj:SetModelPath("models/spider.obj")
+for i = 1, 4 do 
+for i2 = 1, 4 do 
+	local obj = Entity("model")
+	obj:SetPos(Vec3(30*i,30*i2,0))
+	obj:SetModelPath("models/volcano.obj") 
+end
+end
 
-window.SetMouseTrapped(true)
+local obj = Entity("model")
+obj:SetPos(Vec3(0,50,0))
+obj:SetModelPath("models/bird_nest.obj") 
+
+local obj = Entity("model")
+obj:SetPos(Vec3(-50,-50,0))
+obj:SetModelPath("models/spider.obj") 
+
+window.SetMouseTrapped(true)  
