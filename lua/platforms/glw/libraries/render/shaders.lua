94a7959c3ff5ca3db481dd9729c09d4cb2869a53
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-14 21:50:57 +0200

    moved the cached uniform stuff to render.lua for proper initialization VAOs are now used (ops)

--- lua/platforms/glw/libraries/render/shaders.lua
+++ lua/platforms/glw/libraries/render/shaders.lua
@@ -71,60 +71,11 @@ function render.BindArrayBuffer(id)
 	end
 end
 
-local last_prog, last_loc, last_x, last_y, last_z, last_w
+local last
 
-local function SETUP_CACHED_UNIFORM(name, func, arg_count)	
-	local lua = [[
-	local func = ...
-	local last_program, __LAST_LOCALS__
-
-	function render.__NAME__(__ARGUMENTS__)
-
-		if 
-			render.current_program == last_program and 
-			__COMPARE__ 
-		then return end
-		
-		func(__ARGUMENTS__)
-		
-		last_program = render.current_program
-		__ASSIGN__
+function render.BindVertexArray(id)
+	if last ~= id then
+		gl.BindVertexArray(id)
+		last = id
 	end
-	]]
-		
-	local last_locals = ""
-	local arguments = ""
-	local compare = ""
-	local assign = ""
-		
-	for i = 1, arg_count do
-		last_locals =  last_locals .. "last_" .. i
-		arguments = arguments .. "_" .. i
-		compare = compare .. "_" .. i .. " == last_" .. i
-		assign = assign .. "last_" .. i .. " = _" .. i .. "\n"
-		
-		if i ~= arg_count then
-			last_locals = last_locals .. ", "
-			arguments = arguments .. ", "
-			compare = compare .. " and \n"
-		end
-	end
-	
-	lua = lua:gsub("__LAST_LOCALS__", last_locals)
-	lua = lua:gsub("__ARGUMENTS__", arguments)
-	lua = lua:gsub("__COMPARE__", compare)
-	lua = lua:gsub("__NAME__", name)
-	lua = lua:gsub("__ASSIGN__", assign)
-	
-	assert(loadstring(lua))(func)
 end
-
-event.AddListener("RenderContextInitialized", "uniform_wrappers", function()
-	SETUP_CACHED_UNIFORM("Uniform4f", gl.Uniform4f, 5)
-	SETUP_CACHED_UNIFORM("Uniform3f", gl.Uniform3f, 4)
-	SETUP_CACHED_UNIFORM("Uniform2f", gl.Uniform2f, 3)
-	SETUP_CACHED_UNIFORM("Uniform1f", gl.Uniform1f, 2)
-	SETUP_CACHED_UNIFORM("UniformMatrix4fv", gl.UniformMatrix4fv, 4)
-end)
-
---render.Uniform4f = gl.Uniform4f
