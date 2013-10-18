e5a760a9e3ead408f9f74bb62a2977e719d01ddb
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-18 15:27:48 +0200

    cleanup
    Former-commit-id: d53a50b85d39bc2cd950ccbbaf4af9c82b4b3504

--- launch.sh
+++ launch.sh
@@ -9,6 +9,6 @@ fi
 
 while true; do
 	$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit -e 'PLATFORM = "glw" dofile("../../../lua/init.lua")'
-	if [ $? -eq 0 ]; then break; fi
+	# BROKE (when using screen) ==> if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
 	sleep 1
 done
--- launch_x86.cmd
+++ launch_x86.cmd
@@ -1,3 +1,3 @@
 cd %~dp0
 cd bin/windows/x86/
-start "" "luajit.exe" "-e PLATFORM='glw'PDCURSES='yes'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
+start "" "luajit.exe" "-e PLATFORM='glw'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
