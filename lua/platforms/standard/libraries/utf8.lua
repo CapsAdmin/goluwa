bb21c656b6ac27dd6ee13cb1b900d2bbbc105502
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-20 21:02:58 +0200

    added utf8.sub added utf8.upper added utf8.lower

--- lua/platforms/standard/libraries/utf8.lua
+++ lua/platforms/standard/libraries/utf8.lua
@@ -4,6 +4,7 @@ local math_floor = math.floor
 
 -- a lot of this was taken from 
 -- http://cakesaddons.googlecode.com/svn/trunk/glib/lua/glib/unicode/utf8.lua
+-- and http://www.curse.com/addons/wow/utf8/546587
 
 function utf8.byte(char, offset)
 	if char == "" then return -1 end
@@ -67,6 +68,72 @@ function utf8.char(byte)
 	end
 	
 	return utf8
+end
+
+function utf8.sub(str, i, j)
+	j = j or -1
+
+	local pos = 1
+	local bytes = #str
+	local length = 0
+
+	-- only set l if i or j is negative
+	local l = (i >= 0 and j >= 0) or utf8.length(str)
+	local start_char = (i >= 0) and i or l + i + 1
+	local end_char   = (j >= 0) and j or l + j + 1
+
+	-- can't have start before end!
+	if start_char > end_char then
+		return ""
+	end
+
+	-- byte offsets to pass to string.sub
+	local start_byte, end_byte = 1, bytes
+
+	while pos <= bytes do
+		length = length + 1
+
+		if length == start_char then
+			start_byte = pos
+		end
+
+		pos = pos + select(2, utf8.byte(str, pos))
+
+		if length == end_char then
+			end_byte = pos - 1
+			break
+		end
+	end
+
+	return str:sub(start_byte, end_byte)
+end
+
+local function utf8replace(str, mapping)
+	local pos = 1
+	local bytes = str:len()
+	local char_bytes
+	local new_str = ""
+
+	while pos <= bytes do
+		char_bytes = select(2, utf8.byte(str, pos))
+		local c = str:sub(pos, pos + char_bytes - 1)
+
+		new_str = new_str .. (mapping[c] or c)
+
+		pos = pos + char_bytes
+	end
+
+	return new_str
+end
+
+local upper, lower = include("utf8data.lua")
+
+function utf8.upper(str)
+	return utf8replace(str, upper)
+end
+
+function utf8.lower(str)
+	return utf8replace(str, lower)
 end
 
 function utf8.length(str)
