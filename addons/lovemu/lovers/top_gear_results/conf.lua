124ba233ba57806a15b349bbfeb996fee7ae02a8
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-29 00:41:11 +0100

    misc fixes and improvements

--- addons/lovemu/lovers/top_gear_results/conf.lua
+++ addons/lovemu/lovers/top_gear_results/conf.lua
@@ -1,7 +1,13 @@
 function love.conf(t) --partial
-	t.window.width = 1280
-	t.window.height = 720
-	t.window.vsync = false
+	if t.screen then
+		t.screen.width = 1280
+		t.screen.height = 720
+		t.screen.vsync = false
+	else
+		t.window.width = 1280
+		t.window.height = 720
+		t.window.vsync = false
+	end
 	t.title = "LovEmu - The Love2D for GOLUWA"      
 	t.author = "Shell32"
 end
