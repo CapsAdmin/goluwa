aa2a6164b7c51c7082b900756f8aec17d6b1c947
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-01 21:55:54 +0100

    cleanup

--- addons/tests/lua/textbox.lua
+++ addons/tests/lua/textbox.lua
@@ -1,24 +1,19 @@
 window.Open(1000, 1000)
 
-surface.CreateFont("lol", {
-	path = "fonts/unifont.ttf",
-	size = 13,
-	smoothness = 0,
-})
-
 local frame = utilities.RemoveOldObject(aahh.Create("frame"), "lol")
 	frame:SetSize(Vec2()+1000)
 	frame:Center()
 	frame:SetTitle("")
 
 	local edit = aahh.Create("text_input", frame)
-		edit:SetFont("lol")
-		edit:SetText(vfs.Read("lua/textbox.lua"))
+		edit:SetFont("default")
 		edit:Dock("fill")
-		edit:SetWrap(true)
+		edit:SetWrap(false)
 		edit:SetLineNumbers(true)
 		edit:SetMultiLine(true)
+		edit:SetText(vfs.Read("lua/textbox.lua"))
 		edit:MakeActivePanel()
 	frame:RequestLayout(true)
 	
 	LOL = edit
+ 
