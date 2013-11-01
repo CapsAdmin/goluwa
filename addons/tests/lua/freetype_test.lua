c6933d730152b02dc2127d4fac7fb2ad670df7ce
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-01 21:57:21 +0100

    added text test

---
+++ addons/tests/lua/freetype_test.lua
@@ -0,0 +1,9 @@
+window.Open(500, 500)
+surface.debug = false
+
+local str = "ᗢᖇᓮᘐᓰﬡᗩᒪ(강남스타일)Morshmellow🗽eee333222"
+event.AddListener("OnDraw2D", "lol", function()
+	surface.SetTextPos(50.3 ,50)
+	surface.DrawText(str)
+
+end)                        
