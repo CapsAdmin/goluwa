442295812461c525048f5b8fd4eaf27517cdba1a
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-06 21:15:47 +0100

    refactoring

--- addons/lovemu/lua/lovemu/love/system.lua
+++ addons/lovemu/lua/lovemu/love/system.lua
@@ -7,9 +7,9 @@ local glfw=glfw
 local window=window
 
 function love.system.getClipboardText()
-	return ffi.string(glfw.GetClipboardString(window.render_window.__ptr))
+	return system.GetClipboard()
 end
 
 function love.system.setClipboardText(str)
-	glfw.SetClipboardString(window.render_window.__ptr,str)
+	system.SetClipboard(str)
 end
