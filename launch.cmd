b83369c1f3fc42d86054f3134e5dec955aa65a3d
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-22 22:21:01 +0100

    replaced cmd and sh files with a cross platform bat file

--- launch.cmd
+++
@@ -1,4 +0,0 @@
-cd %~dp0
-ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
-cd bin/windows/%arch%/
-start "" "luajit.exe" "-e PLATFORM='glw'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
--- launch_server.cmd
+++
@@ -1,4 +0,0 @@
-cd %~dp0
-ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
-cd bin/windows/%arch%/
-start "" "luajit.exe" "-e PLATFORM='glw'SERVER=true;dofile('../../../lua/init.lua')"
