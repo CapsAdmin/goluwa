ba71852b1d46c7bff23eb5dd5d9884e253ffc363
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-22 22:21:01 +0100

    replaced cmd and sh files with a cross platform bat file
    Former-commit-id: b83369c1f3fc42d86054f3134e5dec955aa65a3d

---
+++ launch.bat
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if %OS% == Windows_NT goto WINDOWS
+then
+:
+## Hack to make a cross-OS compatible script
+fi 2> /dev/null
+
+# ------------------------------------------------------------------------------
+# Unix execution
+# ------------------------------------------------------------------------------
+
+ARCH=$(getconf LONG_BIT)
+
+if [ $ARCH -eq "64" ]; then
+	cd bin/linux/x64
+else
+	cd bin/linux/x86
+fi
+
+while true; do
+	$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit -e 'PLATFORM = "glw" dofile("../../../lua/init.lua")'
+	# BROKE (when using screen) ==> if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
+	sleep 1
+done
+
+# ------------------------------------------------------------------------------
+# Windows execution
+# ------------------------------------------------------------------------------
+
+:WINDOWS
+
+cd %~dp0
+ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
+cd bin/windows/%arch%/
+start "" "luajit.exe" "-e PLATFORM='glw'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
