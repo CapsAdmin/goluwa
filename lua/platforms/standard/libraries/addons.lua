06996962c689192a28bf01614e88d6279b3dd426
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-25 01:11:34 +0100

    addons can now load autorun on events

--- lua/platforms/standard/libraries/addons.lua
+++ lua/platforms/standard/libraries/addons.lua
@@ -12,27 +12,39 @@ local function autorun_addon(folder, info)
 	if info.load ~= false then			
 		_G.INFO = info
 			
-			if info.startup then
-				if not info.startup_launched then
-					-- we want to make sure the addon loads the correct startup file (or do we???)
-					-- update:
-					-- if we use include in the startup file, it won't work.. we need to
-					-- include it instead so it pushes the path to the include stack
-					include(e.BASE_FOLDER .. info.path .. "lua/" .. info.startup)
-											
-					info.startup_launched = true
+			local function run()
+				if info.startup then
+					if not info.startup_launched then
+						-- we want to make sure the addon loads the correct startup file (or do we???)
+						-- update:
+						-- if we use include in the startup file, it won't work.. we need to
+						-- include it instead so it pushes the path to the include stack
+						include(e.BASE_FOLDER .. info.path .. "lua/" .. info.startup)
+												
+						info.startup_launched = true
+					end
 				end
-			end
-			
-			-- autorun folders			
-			for path in vfs.Iterate(info.path .. "lua/autorun/" .. folder, nil, true) do
-				if path:find("%.lua") then
-					local ok, err = xpcall(include, mmyy.OnError, path)
-					if not ok then
-						logn(err)
+				
+				-- autorun folders			
+				for path in vfs.Iterate(info.path .. "lua/autorun/" .. folder, nil, true) do
+					if path:find("%.lua") then
+						local ok, err = xpcall(include, mmyy.OnError, path)
+						if not ok then
+							logn(err)
+						end
 					end
 				end
 			end
+			
+			if info.event then
+				event.AddListener(info.event, "addon_" .. folder, function() 
+					run() 
+					return e.EVENT_DESTROY 
+				end)
+			else
+				run()
+			end
+			
 		_G.INFO = nil	
 	else
 		--logf("the addon %q does not want to be loaded", addon)
