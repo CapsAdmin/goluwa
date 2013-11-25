8b7af127dbc8b1b8cc4d3888c439c900846fc5b5
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-25 01:14:52 +0100

    added launch_x86.bat
    Former-commit-id: c80e5c3635e47f21597a2bf75d3bf9b7d46114e1

---
+++ launch_x86.bat
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if %OS% == Windows_NT goto WINDOWS
+then
+:
+## Hack to make a cross-OS compatible script
+fi 2> /dev/null
+
+# ------------------------------------------------------------------------------
+# Unix execution
+# ------------------------------------------------------------------------------
+
+ARCH=$(getconf LONG_BIT)
+
+cd bin/linux/x86
+
+fi
+
+while true; do
+	$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit -e 'PLATFORM = "glw" dofile("../../../lua/init.lua")'
+	# BROKE (when using screen) ==> if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
+	sleep 1
+done
+
+# ------------------------------------------------------------------------------
+# Windows execution
+# ------------------------------------------------------------------------------
+
+:WINDOWS
+
+cd %~dp0
+cd bin/windows/x86/
+start "" "luajit.exe" "-e PLATFORM='glw'ARGS={'%1','%2','%3','%3','%5','%6','%7','%8','%9'}dofile('../../../lua/init.lua')"
