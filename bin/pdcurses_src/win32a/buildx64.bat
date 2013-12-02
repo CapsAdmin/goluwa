317854ac53b5aa8be9c368a1a9069e0f8372e57e
CapsAdmin <eliashogstvedt@gmail.com>
2013-12-02 02:36:49 +0100

    improved build batch files for pdcurses
    Former-commit-id: a2bbeaabb2a1dd55afc12e91479dc62ab21dd4fc

--- bin/pdcurses_src/win32a/buildx64.bat
+++ bin/pdcurses_src/win32a/buildx64.bat
@@ -1,8 +1,9 @@
+mingw32-make -f mingwin32.mak DEBUG=N DLL=Y WIDE=Y UTF8=Y all
+copy pdcurses.dll ..\..\windows\x64
 del *.o
 del *.lik
 del *.a
 del *.def
 del none
-
-mingw32-make -f mingwin32.mak DEBUG=N DLL=Y WIDE=Y UTF8=Y all
+del *.exe
 pause
