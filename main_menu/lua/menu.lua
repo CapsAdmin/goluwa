dd2b6fae8d4041af04abcbc5626eea4244892793
CapsAdmin <eliashogstvedt@gmail.com>
2014-01-25 17:04:00 +0100

    main menu cleanup and tweaking
    Former-commit-id: f4b63b7abb05d0eb9702a2860e5e844ee3da9021

--- main_menu/lua/menu.lua
+++ main_menu/lua/menu.lua
@@ -1,18 +1,66 @@
-input.Bind("escape", "toggle_menu")
-
-console.AddCommand("toggle_menu", function()
-	menu.Toggle()
-end)
-
 menu = menu or {}
 menu.buttons = menu.buttons or {}
 
-menu.visible = false
+do -- open close  
 
-if menu.Toggle then
-	menu.Toggle()
-	menu.Toggle()
-end 
+	-- remake the menu on reload 
+	if menu.Remake then 
+		menu.Remake() 
+	end
+
+	function menu.Open()
+		if menu.visible then return end
+		window.SetMouseTrapped(false) 
+		menu.MakeButtons()
+		event.AddListener("PreDrawMenu", "StartupMenu", menu.RenderBackground)
+		menu.visible = true
+	end
+
+	function menu.Close()
+		if not menu.visible then return end
+		window.SetMouseTrapped(true) 
+		for k,v in ipairs(menu.buttons)do
+			if type(v) == "table" and v.Remove then 
+				v:Remove() 
+			end
+		end
+		menu.buttons = {}
+		event.RemoveListener("PreDrawMenu", "StartupMenu")
+		menu.visible = false
+	end
+
+	function menu.Toggle()
+		if menu.visible then
+			menu.Close()
+		else
+			menu.Open()
+		end
+	end
+
+	function menu.Remake()
+		menu.Toggle()
+		menu.Toggle()
+	end
+
+	input.Bind("escape", "toggle_menu")
+
+	console.AddCommand("toggle_menu", function()
+		menu.Toggle()
+	end)
+
+	function menu.FadeIn()
+		local i = 1 
+		event.AddListener("PostDrawMenu", "StartupMenu", function()
+			i = i - (i*1.5) * FT * 5
+			surface.Color(0,0,0,i)
+			surface.DraRect(0,0,surface.GetScreenSize())
+			if i < 0 then
+				return HOOK_DESTROY
+			end
+		end)
+	end
+
+end
 
 function menu.RenderBackground()	
 	local scrw, scrh = render.GetScreenSize()
@@ -43,100 +91,6 @@ function menu.RenderBackground()
 		surface.Color(r*f, g*f, b*f, alpha)
 		surface.DrawRect(scrw*fract, 0, scrw/steps, scrh)
 	end
-end
-
-function menu.FadeIn()
-	local i = 1 
-	event.AddListener("PostDrawMenu", "StartupMenu", function()
-		i = i - (i*1.5) * FT * 5
-		surface.Color(0,0,0,i)
-		surface.DraRect(0,0,surface.GetScreenSize())
-		if i < 0 then
-			return HOOK_DESTROY
-		end
-	end)
-end
-
-function menu.Toggle()
-	if menu.visible then
-		menu.Close()
-	else
-		menu.Open()
-	end
-end
-
-function menu.Open()
-	if menu.visible then return end
-	window.SetMouseTrapped(false) 
-	menu.MakeButtons()
-	event.AddListener("PreDrawMenu", "StartupMenu", menu.RenderBackground)
-	menu.visible = true
-end
-
-function menu.Close()
-	if not menu.visible then return end
-	window.SetMouseTrapped(true) 
-	for k,v in ipairs(menu.buttons)do
-		if type(v) == "table" and v.Remove then 
-			v:Remove() 
-		end
-	end
-	menu.buttons = {}
-	event.RemoveListener("PreDrawMenu", "StartupMenu")
-	menu.visible = false
-end
-
-function menu.Remake()
-	menu.Toggle()
-	menu.Toggle()
-end
-
-function menu.AddButton(name, func)
-
-	local pnl = aahh.Create("label")
-		pnl:SetSkinColor("text", "light")
-		pnl:SetSkinColor("shadow", Color(0,0,0,0.1)) 
-		pnl:SetFont("impact.ttf")
-		pnl:SetText(name)
-		pnl:SetCursor(e.IDC_HAND)
-		
-		--pnl:SetShadowDir(Vec2())
-		--pnl:SetShadowSize(18)
-		
-		pnl:SetIgnoreMouse(false)
-		pnl:SetSize(Vec2(100, 18))	
-		function pnl:OnMouseInput(key, press)
-			if key == "button_1" and press then
-				func()
-			end
-		end
-	
-	menu.buttons[#menu.buttons+1] = pnl
-end 
-
-function menu.AddButtonSpace()
-	menu.buttons[#menu.buttons+1] = true
-end
-
-function menu.SetupButtons()
-	local sw, sh = render.GetScreenSize()
-	
-	local margin = 50
-	local x = sw/2
-	local y = sh/1.5
-	
-	for i=1, #menu.buttons do
-		local b = menu.buttons[#menu.buttons-i+1]
-
-		if b == true then
-			y = y - (margin / 2)
-		else
-			b:RequestLayout(true)
-			b:SetPos(Vec2(x - b:GetWidth() / 2, y-b:GetHeight() * 2)) 
-			y = y - (margin / 1.25)
-		end
-	end
-	
 end
 
 function menu.MakeButtons()
@@ -186,10 +140,11 @@ function menu.MakeButtons()
 		
 		local grid = aahh.Create("grid", frame)
 		grid:Dock("fill")
+		grid:SetSizeToContent(true)
 		grid:SetSizeToWidth(true)
 		grid:SetStackRight(false)
 		
-		grid:SetItemSize(Vec2()+20)
+		grid:SetItemSize(Vec2()+25)
 
 		local function populate(dir)
 			frame:SetTitle(dir)
@@ -205,18 +160,16 @@ function menu.MakeButtons()
 					
 				grid:AddChild(btn)
 			end
-			for name in vfs.Iterate(dir, false, true) do
+			for name in vfs.Iterate(dir) do 
 				if name ~= "." and name ~= ".." then
 					local btn = aahh.Create("textbutton")
 					btn:SetText(name)
-					
+
 					if name:find(".lua", nil, true) then
 						function btn:OnPress()
-							easylua.Start(players.GetLocalPlayer())
 							tester.Begin(name)
 								include(dir .. name)
 							tester.End()
-							easylua.End()
 							frame:Remove()
 						end
 					elseif not name:find("%.") then
@@ -230,16 +183,18 @@ function menu.MakeButtons()
 						end
 					end	
 					
-					grid:AddChild(btn)
+					grid:AddChild(btn)  
 				end
-			end
-			
+			end 
+			  			
 			grid:RequestLayout(true)
-			frame:SetHeight(grid:GetCurrentSize().h + 33)
+			frame:SetHeight(grid:GetHeight() + 33)
 			frame:RequestLayout(true)
 		end
 		
 		populate("lua/")
+				
+		LOL = grid
 	end)
 	
 	menu.AddButtonSpace() 
@@ -247,5 +202,53 @@ function menu.MakeButtons()
 	menu.AddButton("Exit", function() os.exit() end)
 	
 	menu.SetupButtons()
+end
+
+function menu.AddButton(name, func)
+
+	local pnl = aahh.Create("label")
+		pnl:SetSkinColor("text", "light")
+		pnl:SetSkinColor("shadow", Color(0,0,0,0.1)) 
+		pnl:SetFont("impact.ttf")
+		pnl:SetText(name)
+		pnl:SetCursor(e.IDC_HAND)
+		
+		--pnl:SetShadowDir(Vec2())
+		--pnl:SetShadowSize(18)
+		
+		pnl:SetIgnoreMouse(false)
+		pnl:SetSize(Vec2(100, 18))	
+		function pnl:OnMouseInput(key, press)
+			if key == "button_1" and press then
+				func()
+			end
+		end
+	
+	menu.buttons[#menu.buttons+1] = pnl
+end 
+
+function menu.AddButtonSpace()
+	menu.buttons[#menu.buttons+1] = true
+end
+
+function menu.SetupButtons()
+	local sw, sh = render.GetScreenSize()
+	
+	local margin = 50
+	local x = sw/2
+	local y = sh/1.5
+	
+	for i=1, #menu.buttons do
+		local b = menu.buttons[#menu.buttons-i+1]
+
+		if b == true then
+			y = y - (margin / 2)
+		else
+			b:RequestLayout(true)
+			b:SetPos(Vec2(x - b:GetWidth() / 2, y-b:GetHeight() * 2)) 
+			y = y - (margin / 1.25)
+		end
+	end
+	
 end
  
