46aa175e2f9923fae0d64e8989e308b00e2d3eaa
CapsAdmin <eliashogstvedt@gmail.com>
2014-01-29 05:31:31 +0100

    improved join_announcer.lua

--- online/lua/autorun/client/join_announcer.lua
+++ online/lua/autorun/client/join_announcer.lua
@@ -1,5 +1,11 @@
 event.AddListener("PlayerSpawned", "join_announcer", function(ply)
 	if chathud then
-		chathud.AddText(ply, " spawned!")
+		chathud.AddText(Color(0,1,0), ply:GetName(), Color(1,1,1), " spawned!")
+	end
+end)
+
+event.AddListener("PlayerLeft", "join_announcer", function(ply, uid, reason)
+	if chathud then
+		chathud.AddText(Color(1,0,0), ply, Color(1,1,1), " left! (", reason ,")")
 	end
 end)
