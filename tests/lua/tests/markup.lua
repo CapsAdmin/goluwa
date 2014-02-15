1d59c014be6c854c42cc285c618c359cd5299cbd
CapsAdmin <eliashogstvedt@gmail.com>
2014-02-15 15:03:42 +0100

    changed where and how markup is initialized
    Former-commit-id: 2dd20b39d211a00c86c51eac52711458272a2787

--- tests/lua/tests/markup.lua
+++ tests/lua/tests/markup.lua
@@ -205,7 +205,10 @@ local frame = aahh.Create("frame")
 local panel = frame:CreatePanel("panel")
 
 panel:Dock("fill")
+window.SetSize(Vec2(1680, 1050))
+timer.Delay(0.1, function()
 frame:SetSize(1000, 1000)
+end)
 panel:MakeActivePanel()
 frame:RequestLayout(true) 
 
