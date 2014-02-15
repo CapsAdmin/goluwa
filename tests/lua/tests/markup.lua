ff62b019b16da1f2dc16dbd266800bc0f1a12046
CapsAdmin <eliashogstvedt@gmail.com>
2014-02-15 05:10:21 +0100

    more work on markup

--- tests/lua/tests/markup.lua
+++ tests/lua/tests/markup.lua
@@ -1,95 +1,141 @@
-local lex_setup = require("langtoolkit.lexer")
-local reader = require("langtoolkit.reader")
- 
-local colors = {
-	default = Color(255, 255, 255),
-	keyword = Color(127, 159, 191),
-	identifier = Color(223, 223, 223),
-	string = Color(191, 127, 127),
-	number = Color(127, 191, 127),
-	operator = Color(191, 191, 159),
-	ccomment = Color(159, 159, 159),
-	cmulticomment = Color(159, 159, 159),
-	comment = Color(159, 159, 159),
-	multicomment = Color(159, 159, 159),
-}
+local syntax_process
 
-local translate = {
-	TK_ge = colors.operator, 
-	TK_le = colors.operator, 
-	TK_concat = colors.operator, 
-	TK_eq = colors.operator,
-	TK_label = colors.operator,
-	
-	TK_dots = colors.operator,
-	TK_number = colors.number,
-	TK_string = colors.string,
-	TK_name = colors.default,
-	
-	--TK_local = colors.comment,
-	
-	-- rest is keyword ??
-}
+do
+	local lex_setup = require("langtoolkit.lexer")
+	local reader = require("langtoolkit.reader")
+	 
+	local colors = {
+		default = Color(255, 255, 255),
+		keyword = Color(127, 159, 191),
+		identifier = Color(223, 223, 223),
+		string = Color(191, 127, 127),
+		number = Color(127, 191, 127),
+		operator = Color(191, 191, 159),
+		ccomment = Color(159, 159, 159),
+		cmulticomment = Color(159, 159, 159),
+		comment = Color(159, 159, 159),
+		multicomment = Color(159, 159, 159),
+	}
 
-local function syntax_process(str)
-	reader.string_init(str)
-	local ls = lex_setup(reader.string, str)
-
-	local last_pos = 1
-	local out = {}
+	local translate = {
+		TK_ge = colors.operator, 
+		TK_le = colors.operator, 
+		TK_concat = colors.operator, 
+		TK_eq = colors.operator,
+		TK_label = colors.operator,
 		
-	for i = 1, 10000 do
-		local ok, msg = pcall(ls.next, ls)
+		TK_dots = colors.operator,
+		TK_number = colors.number,
+		TK_string = colors.string,
+		TK_name = colors.default,
 		
-		if not ok then
-			local tbl = msg:explode("\n")
-			table.insert(out, str:sub(-ls.p))
-			break
-		end
+		--TK_local = colors.comment,
+		
+		-- rest is keyword ??
+	}
+
+	function syntax_process(str, markup)
+		reader.string_init(str)
+		local ls = lex_setup(reader.string, str)
+
+		local last_pos = 1
 			
-		if #ls.token == 1 then
-			table.insert(out, colors.operator)
-		else
-			table.insert(out, translate[ls.token] or colors.keyword)
-		end
-		table.insert(out, str:sub(last_pos-1, ls.p-2))
-		
-		last_pos = ls.p 
+		for i = 1, 10000 do
+			local ok, msg = pcall(ls.next, ls)
+			
+			if not ok then
+				local tbl = msg:explode("\n")
+				markup:AddString(str:sub(-ls.p))
+				break
+			end
 				
-		if ls.token == "TK_eof" then break end
+			if #ls.token == 1 then
+				markup:AddColor(colors.operator)
+			else
+				markup:AddColor(translate[ls.token] or colors.keyword)
+			end
+			markup:AddString(str:sub(last_pos-1, ls.p-2))
+			
+			last_pos = ls.p 
+					
+			if ls.token == "TK_eof" then break end
+		end
+		
+		markup:AddString(str:sub(last_pos-1, last_pos))
+		
+		--table.print(syntax.)
+		
+		return out
+	end  
+end
+
+local markup = Markup()
+
+do	  
+	markup:AddString("Hello markup test!\n\n有一些中國\nそして、いくつかの日本の\nكيف حول بعض عربية")
+
+	local small_font = surface.CreateFont("small", {size = 8, read_speed = 100})
+
+	markup:AddFont(small_font)
+	markup:AddString("\nhere's some small unicode:\n我寫了這個在谷歌翻譯，所以我可以測試我的標記語言使用Unicode正確。它似乎做工精細！\n\n")
+
+	markup:AddFont("default")
+	markup:AddString("back to normal!\n\n") 
+
+	do	
+		local icons = vfs.Find("textures/silkicons/.")
+		local tags = ""
+		for i = 1, 32 do
+			local path = table.random(icons)
+			tags = tags .. ("<texture=textures/silkicons/%s,2>%s"):format(path, i%16 == 0 and "\n" or "")
+		end
+		
+		markup:AddString(tags, true) 
+		markup:AddString("\n\n")
+	end	
+
+	syntax_process(
+[[if CLIENT then
+	if window and #window.GetSize() > 5 then 
+		timer.Delay(0, function()
+			include("tests/markup.lua")
+		end)
 	end
+end
+
+]], markup)
 	
-	out[#out] = out[#out] .. str:sub(last_pos-1, last_pos)
+	local big_font = surface.CreateFont("big", {size = 30, read_speed = 100})
 	
-	--table.print(syntax.)
+	markup:AddFont(big_font)
+	markup:AddString("<hsv=[t()]>", true)
+	markup:AddString("This font is huge and flashing for some reason!\n\n")
+	markup:AddFont("default") 
 	
-	return out
-end  
+	markup:AddColor(Color(255, 255, 255, 255))
+	markup:AddString("rotated grin<rotate=90>:D</rotate>\n", true)	
+	markup:AddString("that's <wrong>WRONG</wrong>\n", true)
+	markup:AddString("<mark>did you forget the <mark>eggs</mark>", true)
+end
+
+timer.Delay(0.1, function() markup:Invalidate() end)
+
+if markup_frame and markup_frame:IsValid() then 
+	markup_frame:Remove() 
+end 
   
-local str = vfs.Read("lua/goluwa/libraries/markup.lua"):sub(0, 500)
-  
-if markup_frame and markup_frame:IsValid()
- then markup_frame:Remove() end 
-  
+markup.chunk_fix = true
+M = markup
+
 local frame = aahh.Create("frame")
 local panel = frame:CreatePanel("panel")
+
 panel:Dock("fill")
+frame:SetSize(700, 700)
+panel:MakeActivePanel()
+frame:RequestLayout(true) 
 
-frame:SetSize(500, 500)
- 
-local markup = Markup()
-markup.chunk_fix = true
-markup:SetTable(syntax_process(str))
-
-markup.OnTextChanged = function(self, str)
-	if not markup.chunk_fix then
-		self:SetTable(syntax_process(str))
-	end
-end
-
-markup.OnInvalidate = function()
-	--markup:SetTable()
-end
+markup_frame = frame
 
 function panel:OnDraw(size)
 	local w,h = size:Unpack()
@@ -131,8 +177,3 @@ function panel:OnCharInput(char)
 	markup:OnCharInput(char)
 end
 
-panel:MakeActivePanel()
-frame:RequestLayout(true) 
-
-M = markup
-markup_frame = frame
