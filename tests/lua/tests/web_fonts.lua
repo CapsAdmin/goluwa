aef93b275edd54d99837bc12bd208321b8085e03
CapsAdmin <eliashogstvedt@gmail.com>
2014-02-27 03:47:30 +0100

    updated tests to reflect changes and removed some redundant tests

--- tests/lua/tests/web_fonts.lua
+++ tests/lua/tests/web_fonts.lua
@@ -1,16 +1,14 @@
-window.Open()
+ן»¿window.Open()
 
-local font = surface.CreateFont("test", {
-	path = "http://googlefontdirectory.googlecode.com/hg/ofl/aladin/Aladin-Regular.ttf",
+local font = surface.CreateFont("lol", {
+	path = "Aladin",
 	size = 50,
-})
-  
+})  
+   
 event.AddListener("OnDraw2D", "lol", function()
 	surface.Color(1,1,1,1)
 
 	surface.SetFont(font)
-	surface.SetTextPos(100, 100)
-	surface.DrawText("Aladin זר ורז רזרו")
-end)    
-
-     
+	surface.SetTextPos(10, 10)
+	surface.DrawText("Aladin ד…ו‹ו‹ד„´ד…‡ ד…ד…ד…‡ >> נ‘¾נ‘¾ << no aliens in unifont :(") 
+end)        
