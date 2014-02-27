ac91b7bf15c5a52a19b9a9b17084c1f3b959d817
CapsAdmin <eliashogstvedt@gmail.com>
2014-02-27 03:43:33 +0100

    _G.Gif now uses vfs.ReadAsync so it can read from urls

--- .base/lua/goluwa/libraries/gif.lua
+++ .base/lua/goluwa/libraries/gif.lua
@@ -1,48 +1,62 @@
 function Gif(path)
 	local self = utilities.CreateBaseObject("gif")	
-
-	local data = vfs.Read(path, "rb")
-	local frames = freeimage.LoadMultiPageImage(data)
-	
-	local w, h = 0, 0
-	for i, frame in pairs(frames) do
-		if frame.w > w then w = frame.w end
-		if frame.h > h then h = frame.h end
 		
-		frames[i] = Texture(frame.w, frame.h, frame.data)
-		frames[i].x = frame.x
-		frames[i].y = frame.y
-		frames[i].ms = frame.ms
-	end
+	self.frames = {}
+	self.frame_count = 1
+	self.frame_speed = 15
 	
-	local frame_count = #frames
-	local frame_speed = 15
+	self.loading = true
+
+	vfs.ReadAsync(path, function(data)
+		local frames = freeimage.LoadMultiPageImage(data)
+		
+		local w, h = 0, 0
+		for i, frame in pairs(frames) do
+			if frame.w > w then w = frame.w end
+			if frame.h > h then h = frame.h end
+			
+			frames[i] = Texture(frame.w, frame.h, frame.data)
+			frames[i].x = frame.x
+			frames[i].y = frame.y
+			frames[i].ms = frame.ms
+		end
+		
+		self.frames = frames
+		self.frame_count = #frames
+		self.loading = false
+		
+		if self.frame_count == 0 then
+			self.error = true
+		end
+	end)
 	
 	function self:GetFrameCount()
-		return frame_count
+		return self.frame_count
 	end
 	
 	function self:SetFPS(fps)
-		frame_speed = fps
+		self.frame_speed = fps
 	end
 	
 	function self:GetTexture(num)
-		return frames[math.ceil(math.clamp(num%frame_count, 1, frame_count))]
+		if self.error then return render.GetErrorTexture() end
+		if self.loading then return render.GetLoadingTexture() end
+		return self.frames[math.ceil(math.clamp(num%self.frame_count, 1, self.frame_count))]
 	end
 	
 	function self:GetTextures()
-		return frames
+		return self.frames
 	end
 	
 	function self:Draw(x, y)
-		local tex = self:GetTexture(os.clock() * frame_speed)
+		local tex = self:GetTexture(os.clock() * self.frame_speed)
 		surface.SetTexture(tex)
 
 		surface.DrawRect(x, y, tex.w, tex.h)
 	end
 	
 	function self:OnRemove()
-		for _, tex in pairs(frames) do
+		for _, tex in pairs(self.frames) do
 			if tex:IsValid() then -- might've been garbage collected
 				tex:Remove()
 			end
