6f58d3999fd584575dc0642e566ec81e351be3c1
CapsAdmin <eliashogstvedt@gmail.com>
2014-02-27 03:46:44 +0100

    when a font is finished downloading it will layout panels using it properly

--- aahh/lua/aahh/panels/label.lua
+++ aahh/lua/aahh/panels/label.lua
@@ -14,13 +14,23 @@ aahh.GetSet(PANEL, "AlignNormal", e.ALIGN_CENTERY)
 aahh.GetSet(PANEL, "ResizeTextWithPanel", true)
 aahh.GetSet(PANEL, "IgnoreMouse", true)
 
-aahh.GetSet(PANEL, "Font")
+aahh.GetSet(PANEL, "Font", "default")
+
+function PANEL:SetFont(font)
+	self.Font = font
+	if self.ResizeTextWithPanel then
+		self:SizeToText()
+	end
+end
 
 function PANEL:SetText(str)
 	self.Text = str
 end
 
 function PANEL:SizeToText()
+	if self:HasParent() then
+		self.Parent:RequestLayout()
+	end
 	self:LayoutHook("LabelLayout")
 end
 
@@ -38,5 +48,15 @@ end
 function PANEL:OnRequestLayout()
 	self:LayoutHook("LabelLayout")
 end
+
+event.AddListener("FontChanged", "aahh_font_change", function(name, info)
+	for k,v in pairs(aahh.active_panels) do
+		if v.Font == name then
+			aahh.World:RequestLayout()
+			print("huh") 
+			break
+		end
+	end
+end)
 
 aahh.RegisterPanel(PANEL)
