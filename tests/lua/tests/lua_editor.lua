1f98040c61b2def98051f8d2e3039810a06f6959
CapsAdmin <eliashogstvedt@gmail.com>
2014-03-29 13:50:47 +0100

    added some tests
    Former-commit-id: daa149d4e69f2a8f1f4c5e6568cecfdaf6811c24

---
+++ tests/lua/tests/lua_editor.lua
@@ -0,0 +1,143 @@
+local syntax_process
+
+do       
+	local lex_setup = require("langtoolkit.lexer")
+	local reader = require("langtoolkit.reader")
+	 
+	local colors = {
+		default = Color(255, 255, 255),
+		keyword = Color(127, 159, 191),
+		identifier = Color(223, 223, 223),
+		string = Color(191, 127, 127),
+		number = Color(127, 191, 127),
+		operator = Color(191, 191, 159),
+		ccomment = Color(159, 159, 159),
+		cmulticomment = Color(159, 159, 159),
+		
+		comment = Color(159, 159, 159),
+		multicomment = Color(159, 159, 159),
+	}
+
+	local translate = {
+		TK_ge = colors.operator, 
+		TK_le = colors.operator, 
+		TK_concat = colors.operator, 
+		TK_eq = colors.operator,
+		TK_label = colors.operator,
+		["#"] = colors.operator,
+		["]"] = colors.operator,
+		[">"] = colors.operator,
+		["/"] = colors.operator,
+		["{"] = colors.operator,
+		["}"] = colors.operator,
+		[":"] = colors.operator,
+		["*"] = colors.operator,
+		["["] = colors.operator,
+		["("] = colors.operator,
+		[")"] = colors.operator,
+		["+"] = colors.operator,
+		[","] = colors.operator,
+		["="] = colors.operator,
+		["."] = colors.operator,
+		["<"] = colors.operator,
+		["-"] = colors.operator,
+		[""] = colors.operator,
+		TK_dots = colors.operator,
+				
+			
+		TK_else = colors.keyword,
+		TK_goto = colors.keyword,
+		TK_if = colors.keyword,
+		TK_nil = colors.keyword,
+		TK_end = colors.keyword,
+		TK_or = colors.keyword,
+		TK_return = colors.keyword,
+		TK_true = colors.keyword,
+		TK_elseif = colors.keyword,
+		TK_function = colors.keyword,
+		TK_while = colors.keyword,
+		TK_and = colors.keyword,
+		TK_then = colors.keyword,
+		TK_in = colors.keyword,
+		TK_for = colors.keyword,
+		TK_do = colors.keyword,
+		TK_for = colors.keyword,
+		TK_false = colors.keyword,
+		TK_break = colors.keyword,
+		TK_not = colors.keyword,
+
+		TK_local = colors.keyword,
+
+		TK_ne = colors.keyword,
+		["/37"] = colors.keyword,
+			
+		TK_number = colors.number,
+		TK_string = colors.string,
+		TK_name = colors.default,
+	} 
+  
+  
+	function syntax_process(str, markup)
+		reader.string_init(str)
+		local ls = lex_setup(reader.string, str)
+
+		local last_pos = 1
+		local last_color
+			
+		for i = 1, 100 do
+			local ok, msg = pcall(ls.next, ls)
+			
+			if not ok then
+				local tbl = msg:explode("\n")
+				markup:AddString(str:sub(-ls.p))
+				break
+			end
+					
+			if #ls.token == 1 then
+				local color = colors.operator
+				if color ~= last_color then   
+					markup:AddColor(color)
+					last_color = color
+				end
+			else
+				local color = translate[ls.token] or colors.comment
+				if color ~= last_color then
+					markup:AddColor(color)
+					last_color = color
+				end
+			end
+						
+			markup:AddString(str:sub(last_pos-1, ls.p-2))
+			
+			last_pos = ls.p 
+			
+			table.print(ls)
+					
+			if ls.token == "TK_eof" then break end
+		end
+		
+		markup:AddString(str:sub(last_pos-1, last_pos-2))
+		  		  
+		return out
+	end  
+end 
+   
+local frame = utilities.RemoveOldObject(aahh.Create("frame"), "markup")
+frame:SetSize(1000, 1000)
+frame:RequestLayout(true) 
+
+local scroll = aahh.Create("scrollable", frame)
+scroll:Dock("fill")
+
+local markup = aahh.Create("text_input")  
+markup:SetMultiLine(true)
+markup:SetEditorMode(true)
+scroll:SetPanel(markup)  
+
+function markup:OnTextChanged()
+	self:SizeToContents()
+end 
+ 
+syntax_process("local hello = ''\n asdasdasd = 1234\n --[[it's a comment]] local test \n --it's really powerful\n", markup.markup)  
+
+window.SetSize(Vec2(1680, 1050))
---
+++ tests/lua/tests/paint.lua
@@ -0,0 +1,57 @@
+window.Open()
+
+local fb = render.CreateFrameBuffer(512, 512, {
+	attach = e.GL_COLOR_ATTACHMENT1,
+	texture_format = {
+		internal_format = e.GL_RGB32F,
+	}
+})
+
+local brush = Texture(128, 128):Fill(function(x, y) 
+	x = x / 128
+	y = y / 128
+	
+	x = x - 1
+	y = y - 1.5
+	
+	x = x * math.pi
+	y = y * math.pi
+		
+	local a = math.sin(x) * math.cos(y)
+	
+	a = a ^ 32
+		
+	return 255, 255, 255, a * 128
+end)
+
+local size = 16 
+   
+timer.Create("fb_update", 0, 0, function()
+	fb:Begin()
+		surface.Start(0, 0, 512, 512)			
+			if input.IsMouseDown("button_1") then
+				surface.SetTexture(brush)
+				surface.Color(1, 1, 1, 1)
+				local x, y = surface.GetMousePos()
+				local vx, vy = surface.GetMouseVel()
+				vx = vx * 100
+				vy = vy * 100
+				if vx ~= 0 and vy ~= 0 then
+					local len = Vec2(vx, vy):GetLength() / 100
+					local deg = math.deg(math.atan2(vx, -vy))
+					surface.Color(HSVToColor(timer.clock(), 1, len):Unpack())
+					for i = 1, 12 do
+						local size = size * i / 8
+						surface.DrawRect(x + math.randomf(-size, size), y + math.randomf(-size, size), size, size + 100, deg, size/2, size/2 + 50)
+					end
+				end
+			end
+		surface.End()
+	fb:End()
+end)
+
+event.AddListener("OnDraw2D", "fb", function()
+	surface.SetTexture(fb:GetTexture())
+	surface.Color(1,1,1,1)
+	surface.DrawRect(0, 0, 512, 512) 
+end)   
