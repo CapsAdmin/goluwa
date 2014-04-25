409f87a697dfdf5d07774814a211ec5dc7dbfa67
CapsAdmin <eliashogstvedt@gmail.com>
2014-04-25 23:06:21 +0200

    improved 2d camera movement
    Former-commit-id: 78a2d9f5c4a2467d16dbf3738b7359dfe27f2bb1

--- .base/lua/autorun/fly_camera.lua
+++ .base/lua/autorun/fly_camera.lua
@@ -4,6 +4,10 @@ local cam_fov = 90
 
 local pos_2d = Vec2(0, 0)
 local zoom = 1
+local zoom2d = 1
+
+local max_zoom2d = 100
+local min_zoom2d = 0.01
  
 event.AddListener("OnUpdate", "fly_camera", function(dt)
 	if not window.IsOpen() then return end
@@ -18,16 +22,25 @@ event.AddListener("OnUpdate", "fly_camera", function(dt)
 		cam_ang.r = 0
 		cam_fov = 90
 		zoom = 1
+		zoom2d = 1
 		pos_2d = Vec2(0,0)
 	end
 	
 	if input.IsMouseDown("button_2") then
-		cam_fov = math.clamp(cam_fov + delta.y * 100, 0.1, 120)
-		cam_ang.r = cam_ang.r - delta.x
+		cam_ang.r = cam_ang.r - delta.x / 2
+		
 		zoom = math.clamp(zoom + delta.y, 0.1, 5)
+		zoom2d = math.clamp(zoom2d + delta.y * 2, min_zoom2d, max_zoom2d)
+		cam_fov = math.clamp(cam_fov + delta.y * 100, 0.1, 120)
 	else
 		cam_ang.p = math.clamp(cam_ang.p + delta.y, -math.pi/2, math.pi/2)
 		cam_ang.y = cam_ang.y - delta.x
+	end
+	
+	if input.IsMouseDown("mwheel_up") then
+		zoom2d = math.clamp(zoom2d - 0.25, min_zoom2d, max_zoom2d)
+	elseif input.IsMouseDown("mwheel_down") then
+		zoom2d = math.clamp(zoom2d + 0.25, min_zoom2d, max_zoom2d)
 	end
 
 	if input.IsKeyDown("left_shift") then
@@ -60,6 +73,11 @@ event.AddListener("OnUpdate", "fly_camera", function(dt)
 		pos_2d.x = pos_2d.x - speed
 	end
 
-	render.SetupView3D(cam_pos, cam_ang:GetDeg(), cam_fov)
-	--render.SetupView2D(pos_2d, 0, zoom)
-end) 
+	if input.IsKeyDown("left_alt") then
+		cam_ang.r = math.round(cam_ang.r / math.rad(45)) * math.rad(45)
+	end
+	
+	local deg = cam_ang:GetDeg()
+	render.SetupView3D(cam_pos, deg, cam_fov)
+	render.SetupView2D(pos_2d, deg.r, 1/zoom2d)
+end)  
