8069236b27e73b701c812f7233d59839848b2114
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-02 03:57:22 +0200

    more linux binaries
    Former-commit-id: fe0f3135c06c8ed2b45333ae5b86b9aed5653f58

--- launch.sh
+++ launch.sh
@@ -9,7 +9,8 @@ else
 fi
 
 while true; do
-	$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit ../../../lua/init.lua
+	#$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit ../../../lua/init.lua
+	env LD_LIBRARY_PATH=.:../../../../ffmpeg/bin/linux/x64 ./luajit ../../../lua/init.lua
 	# BROKE (when using screen) ==> if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
 	sleep 1
 done
