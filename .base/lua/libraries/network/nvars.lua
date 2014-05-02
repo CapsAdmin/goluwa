006532f7ee5854d59f1cea2aa1f59c9d36dfdc79
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-02 03:52:23 +0200

    added nvars.IsSet(tbl, name, def, cvar)
    Former-commit-id: 7f1e027a9858e0dfb11946b16104684015ab798a

--- .base/lua/libraries/network/nvars.lua
+++ .base/lua/libraries/network/nvars.lua
@@ -3,8 +3,11 @@ nvars = _G.nvars or {}
 nvars.Environments = nvars.Environments or {} 
 nvars.added_cvars = nvars.added_cvars or {}
 
-function nvars.GetSet(tbl, name, def, cvar)
-
+local function get_is_set(is, tbl, name, def, cvar)
+	
+	local get = is and "Is" or "Get"
+	local set = "Set"
+	
 	if cvar then
 		cvar = "cl_" .. name:lower()
 		nvars.added_cvars[cvar] = name
@@ -17,17 +20,20 @@ function nvars.GetSet(tbl, name, def, cvar)
 	end
 
     if type(def) == "number" then
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self.nv[name] = tonumber(var) end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tonumber(self.nv[name]) or def end
+		tbl[set .. name] = tbl[set .. name] or function(self, var) self.nv[name] = tonumber(var) end
+		tbl[get .. name] = tbl[get .. name] or function(self, var) return tonumber(self.nv[name]) or def end
 	elseif type(def) == "string" then
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self.nv[name] = tostring(var) end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tostring(self.nv[name]) end
+		tbl[set .. name] = tbl[set .. name] or function(self, var) self.nv[name] = tostring(var) end
+		tbl[get .. name] = tbl[get .. name] or function(self, var) return tostring(self.nv[name]) end
 	else
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self.nv[name] = var end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return self.nv[name] or def end
+		tbl[set .. name] = tbl[set .. name] or function(self, var) self.nv[name] = var end
+		tbl[get .. name] = tbl[get .. name] or function(self, var) return self.nv[name] or def end
 	end
 	
 end
+
+nvars.GetSet = function(...) return get_is_set(false, ...) end
+nvars.IsSet = function(...) return get_is_set(true, ...) end
 
 if CLIENT then
 	message.AddListener("nv", function(env, key, value)
