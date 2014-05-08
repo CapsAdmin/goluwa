1a5e69c0d58be2a37a4a7f90c4f963b64519b5ee
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-08 03:54:04 +0200

    fixed nvars/class.GetSet not working properly with booleans fixed networking false values not working nvars environments are now removed when the player is removed
    Former-commit-id: 6b113b930308cc1a314d80ab5507e75060a84356

--- .base/lua/libraries/low_level/class.lua
+++ .base/lua/libraries/low_level/class.lua
@@ -26,14 +26,14 @@ end
 function class.GetSet(tbl, name, def)
 
     if type(def) == "number" then
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tonumber(self[name]) end
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) or def end
+		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tonumber(self[name]) or def end
 	elseif type(def) == "string" then
 		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tostring(var) end
 		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tostring(self[name]) end
 	else
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = var end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return self[name] end
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) if var == nil then var = def end self[name] = var end
+		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) if self[name] ~= nil then return self[name] end return def end
 	end
 		
     tbl[name] = def
@@ -51,10 +51,9 @@ function class.IsSet(tbl, name, def)
 	else
 		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = var end
 	end
-    tbl["Is" .. name] = tbl["Is" .. name] or function(self, var) return self[name] end
+    tbl["Is" .. name] = tbl["Is" .. name] or function(self, var) if self[name] ~= nil then return self[name] end return def end
 
     tbl[name] = def
-	
 	
 	if __store then
 		tbl.StorableProperties = tbl.StorableProperties or {}
