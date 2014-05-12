3a77ee214ba35ae2b465831d1126512dbb194bea
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-13 01:10:27 +0200

    better linux support

--- launch.sh
+++ launch.sh
@@ -10,7 +10,7 @@ fi
 
 while true; do
 	#$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit ../../../lua/init.lua
-	env LD_LIBRARY_PATH=.:../../../../ffmpeg/bin/linux/x64 ./luajit ../../../lua/init.lua
+	env - USER=$USER DISPLAY=$DISPLAY HOME=$HOME PATH=$PATH TERM=$TERM LD_LIBRARY_PATH=.:../../../../ffmpeg/bin/linux/x64 ./luajit ../../../lua/init.lua
 	# BROKE (when using screen) ==> if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
 	sleep 1
 done
