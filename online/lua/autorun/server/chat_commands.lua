fdf4e16981a48003e7724baaa860ebde5f4a9de6
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-12 03:35:29 +0200

    added google translate command

--- online/lua/autorun/server/chat_commands.lua
+++ online/lua/autorun/server/chat_commands.lua
@@ -23,6 +23,29 @@ console.AddCommand("lm", function(line)
 	ply:SendLua(line)
 end)
 
+local base_url = "http://translate.google.com/translate_a/t?client=t&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&q=%s"
+
+console.AddCommand("t", function(line, from, to, str)
+	local ply = console.GetServerPlayer()
+	local url = base_url:format(from, to, luasocket.EscapeURL(str))
+	
+	luasocket.Get(url, function(data)
+		local out = {translated = "", transliteration = "", from = ""}
+		local content = data.content:match(".-%[(%b[])"):sub(2, -2)
+		
+		for part in content:gmatch("(%b[])") do
+			local to, from, trl = part:match("%[(%b\"\"),(%b\"\"),(%b\"\")")
+			out.translated = out.translated .. to:sub(2,-2)
+			out.from = out.from .. from:sub(2,-2)
+			out.transliteration = out.transliteration .. trl:sub(2,-2)
+		end
+		
+		if ply:IsValid() then
+			chat.PlayerSay(ply, out.translated)
+		end
+	end)
+end)
+
 local prefix = "[!|/|%.]" 
 
 event.AddListener("OnPlayerChat", "chat_commands", function(ply, txt) 
