289458c8745a036317f8a751a09795e22df5b7d8
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-13 01:08:54 +0200

    updated examples
    Former-commit-id: 8a1c0b26f74425a56a32684d3666133bde1323bc

---
+++ tests/lua/tests/steam_translate.lua
@@ -0,0 +1,13 @@
+event.AddListener("SteamFriendsMessage", "steam_translate", function(sender_steam_id, txt, receiver_steam_id)
+	if txt:sub(1, 2) == ">>" then return end
+
+	local ply = players.GetByUniqueID(sender_steam_id)
+	
+	if sender_steam_id == steam.GetClientSteamID() then
+		sender_steam_id = receiver_steam_id
+	end
+	
+	translation.GoogleTranslate("auto", "en", txt, function(data)
+		steam.SendChatMessage(sender_steam_id, ">> " ..data.translated)
+	end)	
+end)
