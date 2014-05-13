9d7b81bf794856dd1ca404419c92104db9ea556f
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-13 19:50:38 +0200

    removed ffmpeg addon

--- ffmpeg/lua/audio_decoders/ffmpeg.lua
+++
@@ -1,8 +0,0 @@
-include("libraries/decoder.lua")
-
-audio.AddDecoder("ffmpeg", function(data, length, path_hint)
-	local ext = path_hint:match(".+%.(.+)")
-	local decoder = assert(ffmpeg.Open(data, {audio_only = true, file_ext = ext})) 
-	local data = decoder:ReadAll()
-	return data.buffer, data.length, decoder:GetInfo().audio
-end)
