d4953d4c862fda20a181f7f3bcdc338e2b9843d5
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-13 19:51:14 +0200

    test sorting

---
+++ tests/lua/tests/ffmpeg/audio.lua
@@ -0,0 +1,12 @@
+local path = R"videos/casiopea.webm"
+local decoder = assert(ffmpeg.Open(path, {audio_only = true}))
+local sound = audio.CreateSource()
+
+sound:SetBufferFormat(e.AL_FORMAT_STEREO16)
+sound:SetBufferCount(4)
+ 
+local audio_data = decoder:Read(math.huge)
+table.print(audio_data)
+sound:FeedBuffer(audio_data[1].buffer, audio_data[1].length)
+
+sound:Play()
