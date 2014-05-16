3bee7d83ed2a8351579ee94d4529960d6b97d9fa
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-16 14:36:59 +0200

    updated to work with buffer:Read/WriteStructure

--- .base/lua/libraries/steam.lua
+++ .base/lua/libraries/steam.lua
@@ -250,137 +250,91 @@ do -- server query
 	local queries = {
 		info = {
 			request = {
-				{val = 0x54, type = "byte"},
-				{val = "Source Engine Query", type = "string"},
+				{"byte", 0x54},
+				{"string", "Source Engine Query"},
 			},
 			response = {
-				{key = "Header", type = "byte", assert = 0x49}, -- Always equal to 'I' (0x49.)
-				{key = "Protocol", type = "byte"}, -- Protocol version used by the server.
-				{key = "Name", type = "string"}, -- Name of the server.
-				{key = "Map", type = "string"}, -- Map the server has currently loaded.
-				{key = "Folder", type = "string"}, -- Name of the folder containing the game files.
-				{key = "Game", type = "string"}, -- Full name of the game.
-				{key = "ID", type = "short"}, -- Steam Application ID of game.
-				{key = "Players", type = "byte"}, -- Number of players on the server.
-				{key = "MaxPlayers", type = "byte"}, -- Maximum number of players the server reports it can hold.
-				{key = "Bots", type = "byte"}, -- Number of bots on the server.
-				{key = "ServerType", type = "byte", translate = {d = "dedicated server", l = "non-dedicated server", p = "SourceTV relay (proxy)"}}, -- Indicates the type of server
-				{key = "Environment", type = "byte", translate = {l = "Linux", w = "Windows", m = "Mac", o = "Mac"}}, -- Indicates the operating system of the server
-				{key = "Visibility", type = "boolean"}, -- Indicates whether the server requires a password
-				{key = "VAC", type = "boolean"}, -- Specifies whether the server uses VAC
+				{"byte", "Header", assert = 0x49}, -- Always equal to 'I' (0x49.)
+				{"byte", "Protocol"}, -- Protocol version used by the server.
+				{"string", "Name"}, -- Name of the server.
+				{"string", "Map"}, -- Map the server has currently loaded.
+				{"string", "Folder"}, -- Name of the folder containing the game files.
+				{"string", "Game"}, -- Full name of the game.
+				{"short", "ID"}, -- Steam Application ID of game.
+				{"byte", "Players"}, -- Number of players on the server.
+				{"byte", "MaxPlayers"}, -- Maximum number of players the server reports it can hold.
+				{"byte", "Bots"}, -- Number of bots on the server.
+				{"char", "ServerType", translate = {d = "dedicated server", l = "non-dedicated server", p = "SourceTV relay (proxy)"}}, -- Indicates the type of server
+				{"char", "Environment", translate = {l = "Linux", w = "Windows", m = "Mac", o = "Mac"}}, -- Indicates the operating system of the server
+				{"boolean", "Visibility"}, -- Indicates whether the server requires a password
+				{"boolean", "VAC"}, -- Specifies whether the server uses VAC
 				
 				-- the ship
-				{match = {ID = 2400}, key = "Mode", type = "byte", translate =  {[0] = "Hunt", [1] = "Elimination", [2] = "Duel", [3] = "Deathmatch", [4] = "VIP Team", [5] = "Team Ellimination"}},
-				{match = {ID = 2400}, key = "Witnesses", type = "byte"}, -- The number of witnesses necessary to have a player arrested.
-				{match = {ID = 2400}, key = "Duration", type = "byte"}, -- Time (in seconds) before a player is arrested while being witnessed
+				{"byte", "Mode", match = {ID = 2400}, translate =  {[0] = "Hunt", [1] = "Elimination", [2] = "Duel", [3] = "Deathmatch", [4] = "VIP Team", [5] = "Team Ellimination"}},
+				{"byte", "Witnesses", match = {ID = 2400}, }, -- The number of witnesses necessary to have a player arrested.
+				{"byte", "Duration", match = {ID = 2400}}, -- Time (in seconds) before a player is arrested while being witnessed
 				
-				{key = "Version", type = "string"}, -- Version of the game installed on the server.
-				{key = "ExtraDataFlag", type = "byte"}, -- If present, this specifies which additional data fields will be included.
+				{"string", "Version"}, -- Version of the game installed on the server.
+				{"byte", "ExtraDataFlag"}, -- If present, this specifies which additional data fields will be included.
 				
-				{match = {ExtraDataFlag = function(num) return bit.band(num, 0x80) end}, key = "Port", type = "short"}, -- The server's game port number.
-				{match = {ExtraDataFlag = function(num) return bit.band(num, 0x10) end}, key = "SteamID", type = "long"}, -- Server's SteamID.
+				{"short", "Port", match = {ExtraDataFlag = function(num) return bit.band(num, 0x80) end}}, -- The server's game port number.
+				{"long", "SteamID", match = {ExtraDataFlag = function(num) return bit.band(num, 0x10) end}}, -- Server's SteamID.
 
-				{match = {ExtraDataFlag = function(num) return bit.band(num, 0x40) end}, key = "Port", type = "short"}, -- Spectator port number for SourceTV.
-				{match = {ExtraDataFlag = function(num) return bit.band(num, 0x20) end}, key = "Name", type = "string"}, -- Name of the spectator server for SourceTV.
+				{"short", "Port", match = {ExtraDataFlag = function(num) return bit.band(num, 0x40) end}}, -- Spectator port number for SourceTV.
+				{"string", "Name", match = {ExtraDataFlag = function(num) return bit.band(num, 0x20) end}}, -- Name of the spectator server for SourceTV.
 				
-				{match = {ExtraDataFlag = function(num) return bit.band(num, 0x01) end}, key = "Name", type = "long"}, -- The server's 64-bit GameID. If this is present, a more accurate AppID is present in the low 24 bits. The earlier AppID could have been truncated as it was forced into 16-bit storage.
+				{"long", "Name", match = {ExtraDataFlag = function(num) return bit.band(num, 0x01) end}}, -- The server's 64-bit GameID. If this is present, a more accurate AppID is present in the low 24 bits. The earlier AppID could have been truncated as it was forced into 16-bit storage.
 			}
 		},
 		players = {
 			challenge = true,
 			request = {
-				{val = 0x55, type = "byte"},
-				{val = 0xFFFFFFFF, type = "long"},
+				{"byte", 0x55},
+				{"long", 0xFFFFFFFF},
 			},
 			response = {
-				{key = "Header", type = "byte", assert = 0x44}, -- Always equal to 'D' (0x44.)
-				{key = "Players", type = "byte", response = {
-					{key = "Index", type = "byte"}, -- Index of player chunk starting from 0.
-					{key = "Name", type = "string"}, -- Name of the player.
-					{key = "Score", type = "long"}, -- Player's score (usually "frags" or "kills".)
-					{key = "Duration", type = "float"}, -- Time (in seconds) player has been connected to the server.
+				{"byte", "Header", assert = 0x44}, -- Always equal to 'D' (0x44.)
+				{"byte", "Players", {
+					{"byte", "Index"}, -- Index of player chunk starting from 0.
+					{"string", "Name"}, -- Name of the player.
+					{"long", "Score"}, -- Player's score (usually "frags" or "kills".)
+					{"float", "Duration"}, -- Time (in seconds) player has been connected to the server.
 				}}, -- Number of players whose information was gathered.
 			}
 		},
 		rules = {
 			challenge = true,
 			request = {
-				{val = 0x56, type = "byte"},
-				{val = 0xFFFFFFFF, type = "long"},
+				{"byte", 0x56},
+				{"long", 0xFFFFFFFF},
 			},
 			response = {			
-				{key = "Header", type = "byte", assert = 0x45}, -- Always equal to 'E' (0x45.)
-				{key = "Rules", type = "short", response = {
-					{key = "Name", type = "string"}, -- Name of the rule
-					{key = "Value", type = "string"}, -- Value of the rule.
+				{"byte", "Header", assert = 0x45}, -- Always equal to 'E' (0x45.)
+				{"short", "Rules", {
+					{"string", "Name"},  -- Name of the rule
+					{"string", "Value"},  -- Value of the rule.
 				}}, -- Number of rules in the response.
 			}
 		},
 		ping = {
 			request = {
-				{val = 0x69, type = "byte"},
+				{"byte", 0x69},
 			},
 			response = {
-				{key = "Heading", type = "byte", assert = 0x6A}, -- Always equal to 'j' (0x6A)
-				{key = "Payload", type = "string"}, -- '00000000000000'
+				{"byte", "Heading", assert = 0x6A}, -- Always equal to 'j' (0x6A)
+				{"string", "Payload"}, -- '00000000000000'
 			}
 		},
 	}
 
 	local split_query = {
 		response = {
-			{key = "ID", type = "long"}, -- Same as the Goldsource server meaning. However, if the most significant bit is 1, then the response was compressed with bzip2 before being cut and sent. Refer to compression procedure below.
-			{key = "Total", type = "byte"}, -- The total number of packets in the response.
-			{key = "Number", type = "byte"}, -- The number of the packet. Starts at 0.
-			{key = "Size", type = "short"}, -- (Orange Box Engine and above only.) Maximum size of packet before packet switching occurs. The default value is 1248 bytes (0x04E0), but the server administrator can decrease this. For older engine versions: the maximum and minimum size of the packet was unchangeable. AppIDs which are known not to contain this field: 215, 17550, 17700, and 240 when protocol = 7.
+			{"long", "ID"}, -- Same as the Goldsource server meaning. However, if the most significant bit is 1, then the response was compressed with bzip2 before being cut and sent. Refer to compression procedure below.
+			{"byte", "Total"}, -- The total number of packets in the response.
+			{"byte", "Number"}, -- The number of the packet. Starts at 0.
+			{"short", "Size"}, -- (Orange Box Engine and above only.) Maximum size of packet before packet switching occurs. The default value is 1248 bytes (0x04E0), but the server administrator can decrease this. For older engine versions: the maximum and minimum size of the packet was unchangeable. AppIDs which are known not to contain this field: 215, 17550, 17700, and 240 when protocol = 7.
 		}
 	}
-
-	local function write_request(buffer, request)
-		for i, data in ipairs(request) do
-			buffer:WriteType(data.val, data.type)
-		end
-	end
-	 
-	local function read_response(buffer, response)
-		local out = {}
-			
-		for i, data in ipairs(response) do
-		
-			if data.match then
-				local key, val = next(data.match)
-				if (type(val) == "function" and not val(out[key])) or out[key] ~= val then
-					goto continue
-				end
-			end
-			
-			local val = buffer:ReadType(data.type) 
-			
-			if data.assert then
-				if val ~= data.assert then
-					error("error in header, expected " .. data.type .. " " .. ("%X"):format(data.assert) .. " got " .. (type(val) == "number" and ("%X"):format(val) or type(val)))
-				end
-			end
-	
-			if data.translate then
-				val = data.translate[string.char(val)] or val
-			end			
-						
-			out[data.key] = val or "nil"
-				
-			if data.response then
-				local tbl = {}
-				out[data.key] = tbl			
-				for i = 1, val do
-					table.insert(tbl, read_response(buffer, data.response))
-				end
-			end
-			
-			::continue::
-		end
-		
-		return out
-	end
 
 	local function query_server(ip, port, query, callback)
 		callback = callback or table.print
@@ -393,7 +347,7 @@ do -- server query
 		function socket:OnConnect()
 			local buffer = Buffer()
 			buffer:WriteLong(0xFFFFFFFF)
-			write_request(buffer, query.request)
+			buffer:WriteStructure(query.request)
 			
 			if steam.debug then
 				logf("sending %s to %s %i", buffer:GetDebugString(), ip, port)
@@ -413,7 +367,7 @@ do -- server query
 
 			-- packet is split up
 			if header == -2 then
-				local info = read_response(buffer, split_query.response)
+				local info = buffer:ReadStructure(split_query.response)
 				
 				if not self.buffer_chunks then
 					self.buffer_size = buffer:ReadLong()
@@ -425,7 +379,7 @@ do -- server query
 				self.buffer_chunks[info.Number + 1] = buffer:ReadRest()
 				
 				if table.count(self.buffer_chunks) - 1 == info.Total then
-					callback(read_response(Buffer(table.concat(self.buffer_chunks)), query.response))
+					callback(Buffer(table.concat(self.buffer_chunks)):ReadStructure(query.response))
 				end
 			elseif header == -1 then
 				if query.challenge and not self.challenge then
@@ -433,10 +387,10 @@ do -- server query
 							
 					if type == 0x41 then
 						local challenge = buffer:ReadLong()
-									
+					
 						local buffer = Buffer()
 						buffer:WriteLong(0xFFFFFFFF)
-						buffer:WriteByte(query.request[1].val)
+						buffer:WriteByte(query.request[1][2])
 						buffer:WriteLong(challenge)
 						
 						if steam.debug then
@@ -448,7 +402,7 @@ do -- server query
 						self.challenge = challenge
 					end
 				else
-					callback(read_response(buffer, query.response))
+					callback(buffer:ReadStructure(query.response))
 				end
 			else
 				error("received unknown header " .. header)
