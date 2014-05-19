7ed6ef37d9b868446f73d9bdcf0436c41b2c973b
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-19 16:14:25 +0200

    fixed steam friends not being used

--- .base/bin/windows/x86/launch_server.bat
+++ .base/bin/windows/x86/launch_server.bat
@@ -1,3 +1,3 @@
 :again
-luajit.exe -e ARGS={'start_server','host','open\32steam_friends','timer.Delay(0,function()vfs.MonitorEverything(false)end)'}dofile('../../../lua/init.lua')
+luajit.exe -e ARGS={'start_server','host','open\32steam/steam_friends','timer.Delay(0,function()vfs.MonitorEverything(false)end)'}dofile('../../../lua/init.lua')
 goto again
