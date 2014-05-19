6deaf1b851cd351af6f2d364d746d0b5a98d8788
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-20 01:26:20 +0200

    more linux support
    Former-commit-id: e6551d8833da7346df669b83e6c82b1938fa79f8

--- launch.sh
+++ launch.sh
@@ -1,16 +1,20 @@
-ARCH=$(getconf LONG_BIT)
+has_command() {
+	command -v $1 2>&1 >/dev/null
+	return $?
+}
 
-if [ $ARCH -eq "64" ]; then
-	cd .base/bin/linux/x64
-	echo x64
-else
-	cd .base/bin/linux/x86
-	echo x86
-fi
+is_arch() {
+	if [ $(getconf LONG_BIT) = "$1" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+cd .base/bin/linux/$(is_arch 64 && echo x64 || echo x86)
 
 while true; do
-	#$(hash screen 2> /dev/null && echo "screen") env LD_LIBRARY_PATH=. ./luajit ../../../lua/init.lua
-	env - USER=$USER DISPLAY=$DISPLAY HOME=$HOME PATH=$PATH TERM=$TERM LD_LIBRARY_PATH=.:../../../../ffmpeg/bin/linux/x64 ./luajit ../../../lua/init.lua
-	# BROKE (when using screen) ==> if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
+	tmux attach-session -t goluwa || tmux new-session -s goluwa 'env - PATH=$PWD LD_LIBRARY_PATH=$PWD TERM=$TERM USER=$USER HOME=$HOME DISPLAY=$DISPLAY ./luajit ../../../lua/init.lua'
+	if [ $? -eq 0 ] || [ $? -ge 128 ]; then echo "im outta here"; break; fi
 	sleep 1
 done
