3f793d59f3601b0f4adbd271551ff88cffa45b6b
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-22 21:34:37 +0200

    fixed system.SetWindowTitle on linux
    Former-commit-id: 4d67722d8778756585cabdb3b2c327b5d87c0506

--- .base/lua/libraries/system.lua
+++ .base/lua/libraries/system.lua
@@ -127,8 +127,9 @@ do -- title
 	end
 
 	if LINUX then
+		local iowrite = _OLD_G.io.write
 		set_title = function(str)
-			return io.old_write and io.old_write('\27]0;', str, '\7') or nil
+			return iowrite and iowrite('\27]0;', str, '\7') or nil
 		end
 	end
 	
