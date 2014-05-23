b0f0dfd68fac32dd8b3b58c109c64f024248e943
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-23 14:28:50 +0200

    networked cursors
    Former-commit-id: 915e73141836f77f8b9ec60c0ba36e853e91b511

---
+++ online/lua/autorun/client/cursors.lua
@@ -0,0 +1,14 @@
+event.AddListener("Draw2D", "cursors", function()
+	surface.Color(1,1,1,1)
+	surface.SetFont("default")
+	
+	for _, ply in pairs(players.GetAll()) do
+		if not ply:IsBot() then
+			local cmd = ply:GetCurrentCommand()
+			surface.SetTextPos(cmd.cursor.x, cmd.cursor.y)
+			surface.DrawText(ply:GetNick())
+		end
+	end
+	
+	surface.SetAlphaMultiplier(1)
+end)
