14f30f6248291c08e350eed2368aa835e44b66bf
CapsAdmin <eliashogstvedt@gmail.com>
2014-05-26 18:17:40 +0200

    updated tests to use poly

--- test/lua/tests/particles.lua
+++ test/lua/tests/particles.lua
@@ -1,8 +1,14 @@
-local W, H = 1024, 768 
+local glow_size = 32
+local glow_brightness = 4
+local glow_alpha = 1
+local random_force = 0.5
+local particle_count = 5000
+local gravity = 0.25
 
-window.Open(W, H) 
+local trail_tex = Texture(1, 255):Fill(function(x, y) 
+	return 255, 255, 255, y 
+end)
 
-local trail_tex = Texture(1, 255):Fill(function(x, y) return 255, 255, 255, y end)
 local head_tex = Texture(128, 128):Fill(function(x, y) 
 	x = x / 128
 	y = y / 128
@@ -20,29 +26,27 @@ local head_tex = Texture(128, 128):Fill(function(x, y)
 	return 255, 255, 255, a * 128
 end)
 
-local glow_size = 32
-local glow_brightness = 4
-local glow_alpha = 1
-local random_force = 0.5
-local max_sand = 1024*2
-local gravity = 0.25
-local sand = {}
+local particles = {}
+
+local poly_head = surface.CreatePoly(particle_count)
+local poly_tail = surface.CreatePoly(particle_count)
+local W, H = surface.GetScreenSize()
  
-for i = 1, max_sand do
-	sand[i] = {
+for i = 1, particle_count do
+	particles[i] = {
 		px = math.random(W),
 		py = math.random(H),
 		
 		vx = 0,
 		vy = 0,
 		
-		drag = math.clamp((i/max_sand) ^ 0.1, 0.98, 0.99),
+		drag = math.clamp((i/particle_count) ^ 0.1, 0.99, 0.999),
 		
-		size = 1,
+		size = (-(i/particle_count)+1) * 3,
 		
-		r = i/max_sand,
-		g = math.sin(i/(max_sand/3)),
-		b = math.tan(i/(max_sand/5)),
+		r = i/particle_count,
+		g = math.sin(i/(particle_count/3)),
+		b = math.tan(i/(particle_count/5)),
 	}
 end
 	
@@ -78,17 +82,20 @@ end
 
 render.SetClearColor(0,0,0,0)
 
-event.AddListener("Draw2D", "sand", function(dt)
+event.AddListener("Draw2D", "particles", function(dt)
 	dt = dt  * 25
 	
 	render.SetBlendMode("additive")
 	
 	W,H = surface.GetScreenSize()
+	surface.SetTexture(head_tex)
 			
-	local ext_vel_x, ext_vel_y = (window.GetMouseDelta()*0.1):Unpack()
+	local ext_vel_x, ext_vel_y = surface.GetMouseVel()
+	ext_vel_x = ext_vel_x * 0.1
+	ext_vel_y = ext_vel_y * 0.1
 	
-	for i = 1, max_sand do
-		local p = sand[i]
+	for i = 1, particle_count do
+		local p = particles[i]
 		
 		p.vx = p.vx + math.randomf(-random_force, random_force) + ext_vel_x
         p.vy = p.vy + math.randomf(-random_force, random_force) + gravity + ext_vel_y
@@ -100,32 +107,26 @@ event.AddListener("Draw2D", "sand", function(dt)
         p.vy = p.vy * p.drag
 		
 		calc_collision(p)
-
-		surface.Color(p.r, p.g, p.b, 1)
-		surface.SetTexture(tex)
-		surface.DrawLine(
-			p.px, 
-			p.py, 
-			
-			p.px + (p.vx*-2), 
-			p.py + (p.vy*-2), 
-			
-			p.size, true
-		)
+      
+		poly_tail:SetColor(i, p.r, p.g, p.b, 1)
+		poly_tail:DrawLine(i, p.px, p.py, p.px + (p.vx*-2), p.py + (p.vy*-2), p.size)
 		
-		
-		surface.Color(p.r*glow_brightness, p.g*glow_brightness, p.b*glow_brightness, glow_alpha)
-		surface.SetTexture(head_tex)
+		poly_head:SetColor(p.r*glow_brightness, p.g*glow_brightness, p.b*glow_brightness, glow_alpha)
 		
 		local size = p.size*glow_size
-		
-		surface.DrawRect(
-			p.px - (size*0.5), 
-			p.py - (size*0.5), 
-			
-			size, size
-		)		
+		if math.random() > 0.5 then
+			size = size + math.random()
+		end
+		poly_head:SetRect(i, p.px - (size*0.5), p.py - (size*0.5), size, size)
 	end
+	
+	surface.Color(1,1,1,1)
+	
+	surface.SetTexture(trail_tex)
+	poly_tail:Draw()
+	
+	surface.SetTexture(head_tex)
+	poly_head:Draw()
 	
 	render.SetBlendMode("alpha")
 	
