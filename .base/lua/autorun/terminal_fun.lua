49a0859a6486d5498f0ad95ff0af3c240ed2e326
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-01 21:36:26 +0200

    fun
    Former-commit-id: e6b07b6b58180546e16a49a4cab3d678c2085e46

--- .base/lua/autorun/terminal_fun.lua
+++ .base/lua/autorun/terminal_fun.lua
@@ -1,31 +1,32 @@
-local cd = "%ROOT%"
+local cd = ""
 
 console.AddCommand("ls", function()
 	local files = vfs.Find(cd)
 	table.print(files)
 end)
 
+console.AddCommand("cat", function(line, file)
+	if vfs.Exists(cd .. file) then
+		log(vfs.Read(file))
+	end
+end, nil, function(arg, args)
+	if #args > 1 then return end
+	
+	return vfs.Find(cd)
+end)
 console.AddCommand("cd", function(line, folder)
 	if not folder then
 		logn(cd)
 	elseif folder == ".." then
 		cd = cd:match("(.+)/")
 	elseif vfs.IsDir(cd .. "/" .. folder) then
-		cd = cd .. "/" .. folder
+		cd = cd .. "/" .. folder .. "/"
 	end
 end, nil, function(arg, args)
 	if #args > 1 then return end
 	print(#args)
 	
-	local out = {".."}
-	
-	for folder in vfs.Iterate(cd) do
-		if vfs.IsDir(folder) then
-			table.insert(out, folder)
-		end
-	end
-	
-	return out
+	return vfs.Find(cd)
 end)
 
 console.AddCommand("quit", function()
@@ -38,4 +39,4 @@ end)
 
 console.AddCommand("restart", function()
 	system.Restart()
-end)
+end) 
