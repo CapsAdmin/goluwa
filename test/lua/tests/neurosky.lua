11e7805bf2ff5ebea86f9ce58a9321ea2bc44746
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-01 15:03:15 +0200

    neurosky mindwave example
    Former-commit-id: 52b7913612df4fb5198e678cd8aeeb61cb44efd2

---
+++ test/lua/tests/neurosky.lua
@@ -0,0 +1,75 @@
+local eeg_power = {}
+local esense = {attention = 50, meditation = 50}
+
+local W, H = surface.GetScreenSize()
+
+local last_x = 0
+local last_y = 0
+
+local fb = render.CreateFrameBuffer(W, H, {
+	attach = "color1",
+	texture_format = {
+		internal_format = "RGB32F",
+	}
+})
+
+-- last argument stores this socket with the specified id and destroys previous socket with that id if called again
+local client = sockets.CreateClient("tcp", "localhost", 13854, "neurosky") 
+
+local function update_rt(eeg)
+	fb:Begin()
+		surface.Start(0, 0, W, H)
+			surface.SetWhiteTexture()
+			surface.Color(1, 1, 1, 1)
+			
+			local x, y = timer.GetSystemTime() * 100 % W, H / 2 + eeg / 10
+			
+			surface.DrawLine(x, y, last_x, last_y)
+			
+			last_x = x
+			last_y = y
+			
+			if x < 5 then 
+				fb:Clear() 
+			end
+			
+		surface.End()
+	fb:End()
+end
+
+event.AddListener("Draw2D", "mindwave", function()
+	surface.SetTexture(fb:GetTexture())
+	surface.DrawRect(0,0, W,H)
+	
+	surface.SetFont("default")
+	surface.SetColor(1,1,1,1)
+		
+	local x, y = 5, 5
+	
+	for k,v in pairs(eeg_power) do
+		surface.SetTextPos(x, y)
+		surface.DrawText(k .. " = " .. v)
+		y = y + 20
+	end
+end)
+
+client:Send(serializer.Encode("json", {
+	appName = "NodeThinkGear", 
+	appKey = "0fc4141b4b45c675cc8d3a765b8d71c5bde9390", 
+	format = "Json", 
+	enableRawOutput = true
+}))
+
+function client:OnReceive(data)
+	data = serializer.Decode("json", data)
+	
+	if data.rawEeg then			
+		update_rt(data.rawEeg)
+	elseif data.eegPower then
+		eeg_power = data.eegPower
+	elseif data.eSense then
+		esense = data.eSense
+	else
+		table.print(data)
+	end
+end
