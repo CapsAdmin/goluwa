019ea44f1eac3e06d27b97dded6ab1029188681a
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-02 16:36:19 +0200

    moved chat above head stuff to cursors

--- .base/lua/autorun/client/cursors.lua
+++ .base/lua/autorun/client/cursors.lua
@@ -6,7 +6,14 @@ event.AddListener("DrawHUD", "cursors", function()
 		if not ply:IsBot() then
 			local cmd = ply:GetCurrentCommand()
 			surface.SetTextPos(cmd.cursor.x, cmd.cursor.y)
-			surface.DrawText(ply:GetNick())
+			local str = ply:GetNick()
+			local coh = ply:GetChatAboveHead()
+			
+			if #coh > 0 then
+				str = str .. ": " .. coh
+			end
+			
+			surface.DrawText(str)
 		end
 	end
 	
--- .base/lua/autorun/client/scoreboard.lua
+++ .base/lua/autorun/client/scoreboard.lua
@@ -1,16 +1,16 @@
-function goluwa.scoreboard.DrawHUD()
+event.AddListener("DrawHUD", "scoreboard", function()
+	if not input.IsKeyDown("tab") then return end
+	
 	surface.Color(1,1,1,1)
 	surface.SetFont("default")
+	
 	local i = 0
+	local w, h = surface.GetScreenSize()
 	for _, ply in pairs(players.GetAll()) do
-		if ply ~= players.GetLocalPlayer() and not ply:IsBot() then
-			surface.SetTextPos(10, i * 20 + 5)
-			surface.DrawText(ply.score_str)
+		if not ply:IsBot() then
+			surface.SetTextPos(w/2, h/2 + i * 20 + 5)
+			surface.DrawText(ply:GetNick())
 			i = i + 1
 		end
 	end
-end
-
-function goluwa.scoreboard.OnChatAboveHead(ply, str)
-	ply.score_str = ply:GetNick() .. ": " .. str
-end
+end)
