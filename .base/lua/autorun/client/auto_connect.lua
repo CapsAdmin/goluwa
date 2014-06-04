2e52cf139e510878c4aaac30b8457ad0e060e3a0
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-04 02:17:59 +0200

    ops

--- .base/lua/autorun/client/auto_connect.lua
+++
@@ -1,9 +0,0 @@
--- delay so we don't time out while initializing all the other stuff
-event.Delay(0.25, function()
-	if CAPS or MORTEN then
-		console.RunString("connect 192.168.0.18")
-		--console.RunString("connect 192.168.0.10")
-	else
-		console.RunString("connect 109.199.217.23")
-	end
-end)
