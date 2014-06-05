187b3f473e1245f34d7cf5a134033b9a5171ac38
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-05 14:50:05 +0200

    more work on ecs
    Former-commit-id: 2317cf678b94a20006fbeddacfe4a58bc37ffbf4

--- .base/lua/libraries/ecs.lua
+++ .base/lua/libraries/ecs.lua
@@ -1,14 +1,37 @@
-local ecs = {}
+local ecs = _G.ecs or {}
 
-ecs.entities = {}
-ecs.configurations = {}
+ecs.entities = ecs.entities or {}
+ecs.configurations = ecs.configurations or {}
+
+for k,v in pairs(ecs.entities) do
+	if v:IsValid() then
+		v:Remove()
+	end
+end
 
 function ecs.GetAll()
 	return ecs.entities
 end
 
-function ecs.RegisterConfiguration(name, components)
-	ecs.configurations[name] = components
+function ecs.SetupComponents(name, components)
+	
+	local functions = {}
+	
+	for _, name in ipairs(components) do
+		for k, v in pairs(ecs.GetComponent(name)) do
+			if type(v) == "function" then			
+				functions[k] = function(ent, ...)
+					local obj = ent:GetComponent(name)
+					return obj[k](obj, ...)
+				end
+			end
+		end
+	end
+
+	ecs.configurations[name] = {
+		components = components,
+		functions = functions,
+	}
 end
 
 do -- entity
@@ -91,20 +114,12 @@ do -- entity
 		table.insert(ecs.entities, ent)
 		
 		if ecs.configurations[config] then
-			for _, name in ipairs(ecs.configurations[config]) do
+			for _, name in ipairs(ecs.configurations[config].components) do
 				ent:AddComponent(name)
-				for k, v in pairs(ecs.GetComponent(name)) do
-					if type(v) == "function" then
-						if not ent[k] then
-							ent[k] = function(ent, ...)
-								local obj = ent:GetComponent(name)
-								return obj[k](obj, ...)
-							end
-						else
---							print(k)
-						end
-					end
-				end
+			end
+			
+			for name, func in pairs(ecs.configurations[config].functions) do
+				ent[name] = ent[name] or func
 			end
 		end
 		
@@ -159,14 +174,14 @@ do -- test
 		metatable.GetSet(COMPONENT, "Matrix", Matrix44())
 		
 		metatable.StartStorable()		
-			metatable.GetSet(COMPONENT, "Pos", Vec3(0, 0, 0))
+			metatable.GetSet(COMPONENT, "Position", Vec3(0, 0, 0))
 			metatable.GetSet(COMPONENT, "Angles", Ang3(0, 0, 0))
 			metatable.GetSet(COMPONENT, "Scale", Vec3(1, 1, 1))
 			metatable.GetSet(COMPONENT, "Size", 1)
 		metatable.EndStorable()
 		
 		function COMPONENT:OnAdd(ent)
-			
+			self.temp_scale = Vec3(1, 1, 1)
 		end
 		
 		function COMPONENT:OnRemove(ent)
@@ -174,7 +189,7 @@ do -- test
 		end
 		
 		function COMPONENT:SetPos(vec3)
-			self.Pos = vec3
+			self.Position = vec3
 			self:InvalidateMatrix()
 		end
 		
@@ -203,13 +218,13 @@ do -- test
 			if self.rebuild_matrix then
 				self.Matrix:Identity()
 
-				self.Matrix:Translate(-self.Pos.x, -self.Pos.y, -self.Pos.z)
+				self.Matrix:Translate(-self.Position.x, -self.Position.y, -self.Position.z)
 
 				self.Matrix:Rotate(self.Angles.p, 0, 1, 0)
 				self.Matrix:Rotate(-self.Angles.y, 0, 0, 1)
 				self.Matrix:Rotate(-self.Angles.r, 1, 0, 0)				
 
-				self.Matrix:Scale(self.Scale.x * self.Size, self.Scale.y * self.Size, self.Scale.z * self.Size) 
+				self.Matrix:Scale(self.temp_scale.x, self.temp_scale.y, self.temp_scale.z) 
 
 				self.rebuild_matrix = false
 			end
@@ -224,6 +239,12 @@ do -- test
 		COMPONENT.Name = "visual"
 		COMPONENT.Require = {"transform"}
 		COMPONENT.Events = {"Draw2D"}
+		
+		metatable.StartStorable()		
+			metatable.GetSet(COMPONENT, "Texture", NULL)
+			metatable.GetSet(COMPONENT, "Color", Color(1, 1, 1, 1))
+			metatable.GetSet(COMPONENT, "Alpha", 1)
+		metatable.EndStorable()
 
 		function COMPONENT:OnAdd(ent)
 
@@ -236,11 +257,16 @@ do -- test
 		function COMPONENT:OnDraw2D(dt)
 			local matrix = self:GetComponent("transform"):GetMatrix()
 			
-			 render.PushWorldMatrixEx(matrix)
+			render.PushWorldMatrixEx(matrix)
 			
-				surface.SetWhiteTexture()
-				surface.SetColor(1,1,1,1)
-				surface.DrawRect(0,0,50,50)
+				if self.Texture:IsValid() then
+					surface.SetTexture(self.Texture)
+				else
+					surface.SetWhiteTexture(self.Texture)
+				end
+				
+				surface.SetColor(self.Color.r, self.Color.g, self.Color.b, self.Alpha)
+				surface.DrawRect(0,0,1,1)
 				
 			render.PopWorldMatrix()
 		end
@@ -249,11 +275,21 @@ do -- test
 	end
 	
 	do -- test
-		ecs.RegisterConfiguration("shape", {"transform", "visual"})
+		ecs.SetupComponents("shape", {"transform", "visual"})
 	
 		local ent = ecs.CreateEntity("shape")
 		
-		ent:SetPos(Vec3(-200,-234,0))
+		ent:SetTexture(Texture("textures/debug/brain.jpg"))
+		ent:SetColor(Color(1,0,1))
+		ent:SetAlpha(0.5)
+		ent:SetPosition(Vec3(-200,-134,0))
+		ent:SetAngles(Ang3(0,0,0)) 
+		ent:SetScale(Vec3(50,50,1))
+		
+		event.AddListener("Update", "test", function()
+		end)
 	end
 	
 end
+
+_G.ecs = ecs
