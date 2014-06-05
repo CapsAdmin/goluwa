a84d3bdf3e55f256295a4ea38766f6a14cbf24bb
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-05 02:35:25 +0200

    removed lower case store name

--- .base/lua/libraries/metatable/get_is_set.lua
+++ .base/lua/libraries/metatable/get_is_set.lua
@@ -12,44 +12,40 @@ end
 
 function metatable.GetSet(tbl, name, def)
 
-	local store_name = name:gsub("%u", function(l) return "_" .. l:lower() end):sub(2)
-
     if type(def) == "number" then
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[store_name] = tonumber(var) or def end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tonumber(self[store_name]) or def end
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) or def end
+		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tonumber(self[name]) or def end
 	elseif type(def) == "string" then
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[store_name] = tostring(var) end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tostring(self[store_name]) end
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tostring(var) end
+		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tostring(self[name]) end
 	else
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) if var == nil then var = def end self[store_name] = var end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) if self[store_name] ~= nil then return self[store_name] end return def end
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) if var == nil then var = def end self[name] = var end
+		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) if self[name] ~= nil then return self[name] end return def end
 	end
-
-    tbl[store_name] = def
-
-	if __store then
-		tbl.storable_variables = tbl.storable_variables or {}
-		table.insert(tbl.storable_variables, store_name)
-	end
-end
-
-function metatable.IsSet(tbl, name, def)
-	
-	local store_name = name:gsub("%u", function(l) return "_" .. l:lower() end):sub(2)
-
-	if type(def) == "number" then
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[store_name] = tonumber(var) end
-	else
-		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[store_name] = var end
-	end
-	
-    tbl["Is" .. name] = tbl["Is" .. name] or function(self, var) if self[store_name] ~= nil then return self[store_name] end return def end
 
     tbl[name] = def
 
 	if __store then
 		tbl.storable_variables = tbl.storable_variables or {}
-		table.insert(tbl.storable_variables, store_name)
+		table.insert(tbl.storable_variables, name)
+	end
+end
+
+function metatable.IsSet(tbl, name, def)
+	
+	if type(def) == "number" then
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) end
+	else
+		tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = var end
+	end
+	
+    tbl["Is" .. name] = tbl["Is" .. name] or function(self, var) if self[name] ~= nil then return self[name] end return def end
+
+    tbl[name] = def
+
+	if __store then
+		tbl.storable_variables = tbl.storable_variables or {}
+		table.insert(tbl.storable_variables, name)
 	end
 end
 
