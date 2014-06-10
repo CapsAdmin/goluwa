004d92e4b39d4f7a2c171560e42c993f2a4bd196
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-10 06:32:53 +0200

    ops

---
+++ .base/lua/libraries/filesystem/files/zip.lua
@@ -0,0 +1,218 @@
+local vfs2 = (...) or _G.vfs2
+
+local zip2 = require("zip") -- GRRRR
+local zip = require("minizip")
+
+local CONTEXT = {}
+
+CONTEXT.Name = "zip"
+
+local valid_names = {
+	"a", -- Hellhog XP (*.A)
+	"abz", -- Alpha Black Zero (*.ABZ)
+	"arf", -- Packmania 2 (*.ARF)
+	"arh", -- El Airplane (*.ARH)
+	"bnd", -- Neighbours From Hell (*.BND)|Neighbours From Hell 2 (*.BND)
+	"bos", -- Fallout Tactics (*.BOS)
+	"bot", -- Team Factor (*.BOT)
+	"box", -- Cellblock Squadrons (*.BOX)
+	"bin", -- X-Men Legends 2 (*.BIN)|XPand Rally (*.BIN)
+	"cab", -- Microsoft Flight Simulator 2004 (*.CAB)
+	"crf", -- System Shock 2 (*.CRF)|Thief (*.CRF)|Thief 2 (*.CRF)
+	"csc", -- 18 Wheels Of Steel Pedal To The Metal (*.CSC)
+	"ctp", -- Call To Power (*.CTP)
+	"dat", -- Against Rome (*.DAT)|Defiance (*.DAT)|Ricochet Lost Worlds Recharged (*.DAT)|Ricochet Xtreme (*.DAT)|Star Wolves (*.DAT)|Uplink (*.DAT)
+	"dlu", -- Dirty Little Helper 98(*.DLU)
+	"fbz", -- Shadowgrounds (*.FBZ)
+	"ff", -- Freedom Force (*.FF)|Freedom Force vs The 3rd Reich (*.FF)
+	"flmod", -- Freelancer (*.FLMOD)
+	"gro", -- Serious Sam (*.GRO)|Serious Sam 2 (*.GRO)
+	"iwd", -- Call of Duty 2 (*.IWD)|Call of Duty 3 (*.IWD)|Call of Duty 4: Modern Warfare (*.IWD)|Call of Duty: World at War (*.IWD)
+	"lzp", -- Law And Order 3 Justice Is Served (*.LZP)
+	"mgz", -- Metal Gear Solid (*.MGZ)
+	"mob", -- Master of Orion 3 (*.MOB)
+	"nob", -- Vampire: The Masquerade (*.NOB)|Vampire The Masquerade Redemption (*.NOB)
+	"pac", -- Desperados: Wanted Dead or Alive (*.PAC)
+	"pak", -- Blitzkrieg (*.PAK)|Blitzkrieg Burning Horizon (*.PAK)|Blitzkrieg Rolling Thunder (*.PAK)|Brothers Pilots 4 (*.PAK)|Call of Juarez (*.PAK)|Far Cry (*.PAK)|Heroes of Might & Magi " XV" -- PAK)|Monte Cristo (*.PAK)|Outfront (*.PAK)|Crysis 1-3 (*.PAK)
+	"pk1", -- PK2','XS Mark (*.PK1;*.PK2)
+	"pk3", -- Call of Duty (*.PK3)|Quake 3 Arena (*.PK3)|Medal of Honor: Allied Assault (*.PK3)|American McGee Alice (*.PK3)|Jedi Knight 2: Jedi Outcast (*.PK3)|Heavy Metal: F.A.K.K.2 (*.PK3)
+	"pk4", -- Doom 3 (*.PK4)|Quake 4 (*.PK4)|Doom 3 Resurrection Of Evil (*.PK4)
+	"pod", -- Hoyle Games 2005 (*.POD)|Terminator 3 (*.POD)
+	"psh", -- Itch (*.PSH)|Pusher (*.PSH)
+	"rbz", -- Richard Burns Rally (*.RBZ)
+	"res", -- Swat 3 Close Quarters Battle (*.RES)
+	"rod", -- Hot Rod American Street Drag (*.ROD)
+	"rvi", -- RVM;*.RVR','Revenant (*.RVI;*.RVM;*.RVR)
+	"sab", -- Sabotain (*.SAB)
+	"scs", -- Hunting Unlimited 3 (*.SCS)
+	"sxt", -- Singles Flirt Up Your Life (*.SXT)
+	"texturepack", -- DATA','Arena Wars (*.TEXTUREPACK;*.DATA)
+	"vl2", -- Tribes 2 (*.VL2)
+	"za", -- Elite Warriors (*.ZA)|Line of Sight: Vietnam (*.ZA)|Deadly Dozen (*.ZA)|Deadly Dozen 2 Pacific Theater (*.ZA)
+	"zip", -- Dethkarz (*.ZIP)|Battlefield 2 (*.ZIP)|Empire Earth 2 (*.ZIP)|Falcon 4 (*.ZIP)|Fire Starter (*.ZIP)|Freedom Fighters (*.ZIP)|Hitman Contracts (*.ZIP)|Hitman Bloodmoney (*.ZIP)|Hitma Silent "Slave" --  (*.ZIP)
+	"zipfs", -- 18 Wheels Of Steel Across America (*.ZIPFS)|Duke Nukem - Manhattan Project (*.ZIPFS)
+	"ztd", -- Dinosaur Digs (*.ZTD)|Marine Mania (*.ZTD)
+}
+
+local function split_path(path_info)
+	
+	local archive_path, relative
+	
+	for i, ext in ipairs(valid_names) do
+		archive_path, relative = path_info.full_path:match("(.-%."..ext..")/(.*)")
+		if archive_path and relative then
+			break
+		end
+	end
+		
+	if not archive_path and not relative then
+		error("not a valid archive path", 2)
+	end
+
+	local temp = io.open(archive_path, "rb")
+	local signature = temp:read(4)
+	if signature ~= "\x50\x4b\x03\x04" then 
+		logn(signature:dumphex())
+		error("not a valid zip file: expected signature '\x50\x4b\x03\x04' got " .. signature)
+	end
+	
+	return archive_path, relative
+end
+
+function CONTEXT:IsFile(path_info)
+	local archive_path, relative = split_path(path_info)
+	
+	local archive = zip.open(archive_path, "r")
+
+	if archive:locate_file(relative) then
+		archive:close()
+		return true
+	end
+	
+	archive:close()
+end
+
+function CONTEXT:IsFolder(path_info)
+	
+	-- [zipak] files are folders
+	if path_info.full_path:find("^.+%.[zipak]$") then
+		return true
+	end
+
+	local archive = zip.open(archive_path, "r")
+
+	if archive:locate_file(relative) and archive:get_file_info().crc == 0 then
+		archive:close()
+		return true
+	end
+	
+	archive:close()
+end
+
+function CONTEXT:GetFiles(path_info)
+	local archive_path, relative = split_path(path_info)
+	
+	local out = {}
+	
+	local archive = zip.open(archive_path, "r")
+	
+	relative = relative .. "."
+	
+	local dir = relative:match("(.*/)")
+	local done = {}
+	
+	for info in archive:files() do
+		local path = info.filename
+		if path:find(relative) and (not dir or path:match("(.*/)") == dir) then
+			-- path is just . so it needs to be handled a bit different
+			if not dir then
+				if not done[path] then
+					path = path:match("(.-)/") or path
+					table.insert(out, path)
+					done[path] = true
+				end
+			else
+				table.insert(out, path:match(".+/(.+)") or path)
+			end
+		end 
+	end
+	
+	archive:close()
+	
+	return out
+end
+
+function CONTEXT:Open(path_info, mode, ...)	
+	local archive_path, relative = split_path(path_info)
+	local file
+		
+	if self:GetMode() == "read" then		
+		if zip2 then
+			local archive = zip2.open(archive_path, "r")
+			local file = assert(archive:open(relative))
+			
+			self.file = file
+		else
+			local archive = zip2.open(archive_path, "r")
+			if not archive:locate_file(relative) then
+				archive:close()
+				error("file not found in archive")
+			end
+			self.archive = archive
+		end
+		
+	elseif self:GetMode() == "write" then
+		error("not implemented")
+	end
+end
+
+if zip2 then
+	function CONTEXT:ReadBytes(bytes)
+		return self.file:read(bytes)
+	end
+
+	function CONTEXT:SetPos(pos)
+		self.file:seek("set", pos)
+	end
+
+	function CONTEXT:GetPos()
+		return self.file:seek()
+	end
+
+	function CONTEXT:Close()
+		self.file:close()
+	end
+	
+	function CONTEXT:GetSize()
+		local old = self:GetPos()
+		local size = self.file:seek("end")
+		self:SetPos(old)
+		return size
+	end
+else
+	function CONTEXT:ReadBytes(bytes)
+		return self.archive:read(bytes)
+	end
+
+	function CONTEXT:SetPos(pos)
+		self.archive:set_offset(pos)
+	end
+
+	function CONTEXT:GetPos()
+		return self.archive:tell()
+	end
+
+	function CONTEXT:Close()
+		self.archive:close()
+	end
+	
+	function CONTEXT:GetSize()
+		return self.archive:get_file_info().compressed_size
+	end
+
+	function CONTEXT:GetLastModified()
+		return self.archive:get_file_info().dosDate
+	end
+end
+
+vfs2.RegisterFileSystem(CONTEXT) 
--- test/lua/tests/bsp.lua
+++ test/lua/tests/bsp.lua
@@ -4,20 +4,28 @@ long version; // BSP file version
 ]]
 
 
-vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/")
-vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_misc_dir.vpk")
-vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_textures_dir.vpk") 
-local buffer = Buffer(io.open(R"maps/d2_prison_03.bsp", "rb"))
+--[[vfs.Mount(steam.GetGamePath("Counter-Strike Global Offensive") .. "csgo/")
+vfs.Mount(steam.GetGamePath("Counter-Strike Global Offensive") .. "csgo/pak01_dir.vpk")
+local buffer = Buffer(io.open(R"maps/cs_agency.bsp", "rb"))]]
 
---[[vfs.Mount(steam.GetGamePath("Half-Life 2") .. "ep2/")
-vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_misc_dir.vpk")
-vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_textures_dir.vpk")
-vfs.Mount(steam.GetGamePath("Half-Life 2") .. "ep2/ep2_pak_dir.vpk")
-local buffer = Buffer(io.open(R"maps/ep2_outland_12.bsp", "rb"))]]
+local buffer
 
---[[vfs.Mount(steam.GetGamePath("Left 4 Dead 2") .. "left4dead2/")
-vfs.Mount(steam.GetGamePath("Left 4 Dead 2") .. "left4dead2/pak01_dir.vpk")
-local buffer = Buffer(io.open(R"maps/c3m1_plankcountry.bsp", "rb"))]]
+if true then
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/")
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_misc_dir.vpk")
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_textures_dir.vpk") 
+	buffer = Buffer(vfs.GetFile("freespace06_v2-1.bsp", "rb")) or Buffer(io.open(R"maps/d2_prison_03.bsp", "rb"))
+elseif false then
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "ep2/")
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_misc_dir.vpk")
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "hl2/hl2_textures_dir.vpk")
+	vfs.Mount(steam.GetGamePath("Half-Life 2") .. "ep2/ep2_pak_dir.vpk")
+	buffer = Buffer(vfs.GetFile("maps/ep2_outland_12.bsp", "rb"))
+else
+	vfs.Mount(steam.GetGamePath("Left 4 Dead 2") .. "left4dead2/")
+	vfs.Mount(steam.GetGamePath("Left 4 Dead 2") .. "left4dead2/pak01_dir.vpk")
+	buffer = Buffer(vfs.GetFile("maps/c3m1_plankcountry.bsp", "rb"))
+end
 
 local header = buffer:ReadStructure(vtf_header_structure)
 
@@ -55,6 +63,8 @@ local lump21_struct = [[
 	int	filelen;	// length of lump (bytes)
 	char fourCC[4];	// lump ident code
 ]]
+
+
 
 header.lumps = {}
 
@@ -198,6 +208,123 @@ do
 	end
 end
 
+local displacements = {}
+
+do
+	local structure = [[
+		vec3			startPosition;		// start position used for orientation
+		int			DispVertStart;		// Index into LUMP_DISP_VERTS.
+		int			DispTriStart;		// Index into LUMP_DISP_TRIS.
+		int			power;			// power - indicates size of surface (2^power	1)
+		int			minTess;		// minimum tesselation allowed
+		float			smoothingAngle;		// lighting smoothing angle
+		int			contents;		// surface contents
+		unsigned short		MapFace;		// Which map face this displacement comes from.
+		char asdf[2];
+		int			LightmapAlphaStart;	// Index into ddisplightmapalpha.
+		int			LightmapSamplePositionStart;	// Index into LUMP_DISP_LIGHTMAP_SAMPLE_POSITIONS.
+		
+		
+		//CDispNeighbor		EdgeNeighbors[4];	// Indexed by NEIGHBOREDGE_ defines.
+		//CDispCornerNeighbors	CornerNeighbors[4];	// Indexed by CORNER_ defines.
+		//unsigned int		AllowedVerts[10];	// active verticies
+	]]
+	
+	local edge_neighbor = [[
+		unsigned short 	m_iNeighbor;
+		unsigned char 	m_NeighborOrientation;
+		unsigned char 	m_Span;
+		unsigned char 	m_NeighborSpan;
+		char llol;
+	]]
+	
+	local corner_neighbors = [[
+		unsigned short m_Neighbors[4]; // indices of neighbors.
+		unsigned char m_nNeighbors;
+		char llol;
+	]]
+	
+	local lump = header.lumps[27]
+	local length = lump.filelen / 176 
+	buffer:SetPos(lump.fileofs)
+	
+	local function get_face_vertex(face_index, i)
+		local face = faces[1 + face_index]
+
+		if i < 0 or i >= face.numedges then
+			return
+		end
+
+		local surfedge = surfedges[1 + face.firstedge + (i - 1)]
+		local edge = edges[1 + math.abs(surfedge)]
+		local vertex = edge[1 + (surfedge < 0 and 1 or 0)]
+
+		return vertex
+	end
+
+	local function lerp(x, y, a)
+		return x * (1 - a) + y * a
+	end
+
+	local function lerpvec(a, b, alpha)
+		return {lerp(a[1], b[1], alpha), lerp(a[2], b[2], alpha), lerp(a[3], b[3], alpha)}
+	end
+	
+	local function bilerpvec(a, b, c, d, alpha1, alpha2)
+		return lerpvec(lerpvec(a, b, alpha1), lerpvec(c, d, alpha1), alpha2)
+	end
+	
+	for i = 1, length do
+		local lol = buffer:GetPos()
+		local data = buffer:ReadStructure(structure)
+		
+		do -- http://fal.xrea.jp/plugin/SourceSDK/bspfile_8h-source.html				
+			data.EdgeNeighbors = {}
+			
+			for i = 1, 4 do
+				data.EdgeNeighbors[i] = {m_SubNeighbors = {buffer:ReadStructure(edge_neighbor), buffer:ReadStructure(edge_neighbor)}}
+			end
+			
+			data.CornerNeighbors = {}
+			
+			for i = 1, 4 do
+				data.CornerNeighbors[i] = buffer:ReadStructure(corner_neighbors)
+			end
+			
+			data.AllowedVerts = {}
+			
+			for i = 1, 10 do
+				data.AllowedVerts[i] = buffer:ReadLong()
+			end
+		end
+		
+		local old_pos = buffer:GetPos()
+		
+		local lump = header.lumps[34]
+		local length = lump.filelen / 20
+		buffer:SetPos(lump.fileofs + (data.DispVertStart * 20))
+		
+		local DispVertLength = ((2 ^ data.power) + 1) ^ 2
+		
+		data.vertex_info = {}
+
+		for i = 1, DispVertLength do
+
+			local x = buffer:ReadFloat()
+			local y = buffer:ReadFloat()
+			local z = buffer:ReadFloat()
+			local dist = buffer:ReadFloat()
+			local alpha = buffer:ReadFloat()
+
+			data.vertex_info[i] = {vertex = {x, y, z}, dist = dist, alpha = alpha}
+		end
+
+		buffer:SetPos(old_pos)
+		
+		displacements[i] = data
+	end
+end
+
 local models = {}
 
 do -- model
@@ -232,6 +359,32 @@ local function add_vertex(model, texinfo, texdata, x, y, z)
 	})
 end
 
+local function get_face_vertex(face_index, i)
+	local face = faces[1 + face_index]
+
+	if i < 0 or i >= face.numedges then
+		return
+	end
+
+	local surfedge = surfedges[1 + face.firstedge + (i - 1)]
+	local edge = edges[1 + math.abs(surfedge)]
+	local vertex = edge[1 + (surfedge < 0 and 1 or 0)]
+
+	return vertex
+end
+
+local function lerp(x, y, a)
+	return x * (1 - a) + y * a
+end
+
+local function lerpvec(a, b, alpha)
+	return {lerp(a[1], b[1], alpha), lerp(a[2], b[2], alpha), lerp(a[3], b[3], alpha)}
+end
+
+local function bilerpvec(a, b, c, d, alpha1, alpha2)
+	return lerpvec(lerpvec(a, b, alpha1), lerpvec(c, d, alpha1), alpha2)
+end
+
 local bsp_mesh = {sub_models = {}}
 
 local meshes = {}
@@ -243,7 +396,7 @@ for model_index = 1, #models do
 		local face = faces[models[model_index].firstface + i]
 		local texinfo = texinfos[1 + face.texinfo]
 		local texdata = texinfo and texdatas[1 + texinfo.texdata] or nil
-		
+
 		-- split the world up into sub models by texture
 		if model_index == 1 then
 			local texname = texdatastringdata[1 + texdata.nameStringTableID]:lower()
@@ -307,27 +460,94 @@ for model_index = 1, #models do
 
 		local first, previous, current
 
-		for j = 1, edge_count do
-			local surfedge = surfedges[edge_first + j]
-			local edge = edges[1 + math.abs(surfedge)]
-			
-			local current = edge[surfedge < 0 and 2 or 1] + 1
+		if face.dispinfo < 0 then
+			for j = 1, edge_count do
+				local surfedge = surfedges[edge_first + j]
+				local edge = edges[1 + math.abs(surfedge)]
+				
+				local current = edge[surfedge < 0 and 2 or 1] + 1
 
-			if j >= 3 then
-				local p1 = vertices[first]
-				local p2 = vertices[current]
-				local p3 = vertices[previous]
+				if j >= 3 then
+					local p1 = vertices[first]
+					local p2 = vertices[current]
+					local p3 = vertices[previous]
 
-				if p1 and p2 and p3 then
-					add_vertex(sub_model, texinfo, texdata, unpack(p1))
-					add_vertex(sub_model, texinfo, texdata, unpack(p2))
-					add_vertex(sub_model, texinfo, texdata, unpack(p3))
+					if p1 and p2 and p3 then
+						add_vertex(sub_model, texinfo, texdata, unpack(p1))
+						add_vertex(sub_model, texinfo, texdata, unpack(p2))
+						add_vertex(sub_model, texinfo, texdata, unpack(p3))
+					end
+				elseif j == 1 then
+					first = current
 				end
-			elseif j == 1 then
-				first = current
+
+				previous = current
+			end
+		else
+			local dispinfo = displacements[face.dispinfo + 1]
+			local size = 2 ^ dispinfo.power + 1
+			local count = size ^ 2
+			
+			local start_corner_dist = math.huge
+			local start_corner = 0
+			local corners = {}
+
+			local function dist(a, b)
+				local x = a[1] - b.x
+				local y = a[2] - b.y
+				local z = a[3] - b.z
+				return math.sqrt(x * x + y * y + z * z)
+			end
+			
+			for i = 1, 4 do
+				corners[i] = vertices[1 + get_face_vertex(dispinfo.MapFace, i - 1)]
+				local cough = dist(corners[i], dispinfo.startPosition)
+				if cough < start_corner_dist then
+					start_corner_dist = cough
+					start_corner = i - 1
+				end
 			end
 
-			previous = current
+			local dims = 2 ^ dispinfo.power + 1
+
+			local function asdf(x, y)
+				return bilerpvec(corners[1 + (start_corner + 0) % 4], corners[1 + (start_corner + 1) % 4], corners[1 + (start_corner + 3) % 4], corners[1 + (start_corner + 2) % 4], (y - 1) / (dims - 1), (x - 1) / (dims - 1))
+			end
+
+			local function addVectors(a, b)
+				return {a[1] + b[1], a[2] + b[2], a[3] + b[3]}
+			end
+			
+			local function fdsa(a, b)
+				return {a[1] * b, a[2] * b, a[3] * b}
+			end
+			
+			local function qwerty(x, y)
+				local index = (y - 1) * dims + x
+				local data = dispinfo.vertex_info[index]
+				return addVectors(asdf(x, y), fdsa(data.vertex, data.dist))
+			end
+		
+			for x = 1, dims - 1 do
+				for y = 1, dims - 1 do
+					local index = (y - 1) * dims + x
+					local data = dispinfo.vertex_info[index]
+
+					add_vertex(sub_model, texinfo, texdata, unpack(qwerty(x, y)))
+					add_vertex(sub_model, texinfo, texdata, unpack(qwerty(x + 1, y + 1)))
+					add_vertex(sub_model, texinfo, texdata, unpack(qwerty(x, y + 1)))
+					
+					add_vertex(sub_model, texinfo, texdata, unpack(qwerty(x, y)))
+					add_vertex(sub_model, texinfo, texdata, unpack(qwerty(x + 1, y)))
+					add_vertex(sub_model, texinfo, texdata, unpack(qwerty(x + 1, y + 1)))
+
+					--local xx = initial[1] + data.vertex[1] * data.dist
+				--	local yy = initial[2] + data.vertex[2] * data.dist
+					--local zz = initial[3] + data.vertex[3] * data.dist
+
+					--add_vertex(sub_model, texinfo, texdata, xx, yy, zz)
+				end
+			end
 		end
 	end
 	
