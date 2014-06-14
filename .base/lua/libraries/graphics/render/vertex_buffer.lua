54136b65df423a4208f273dd30ef2aea429803e3
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-14 07:09:46 +0200

    added a way to provide an index buffer when creating meshes

--- .base/lua/libraries/graphics/render/vertex_buffer.lua
+++ .base/lua/libraries/graphics/render/vertex_buffer.lua
@@ -3,20 +3,18 @@ local render = (...) or _G.render
 
 local META = metatable.CreateTemplate("vertex_buffer")
 
-function render.CreateVertexBuffer(buffer, vertex_attributes)
-	check(buffer, "cdata")
+function render.CreateVertexBuffer(vertex_attributes, vertices, indices)
 	check(vertex_attributes, "table")
-	
-	local size = ffi.sizeof(buffer)
+	check(vertices, "cdata")
+	check(indices, "cdata")
 	
 	local self = META:New()
-	self.size = size
-	self.buffer = buffer
-	self.id = gl.GenBuffer()
+	self.vertices_id = gl.GenBuffer()
+	self.indices_id = gl.GenBuffer()
 	self.vao_id = gl.GenVertexArray()
 	self.vertex_attributes = vertex_attributes
 	
-	self:UpdateBuffer()
+	self:UpdateBuffer(vertices, indices)
 
 	return self
 end 
@@ -27,28 +25,36 @@ end
 
 function META:Draw()
 	render.BindVertexArray(self.vao_id)
-	render.BindArrayBuffer(self.id)	
-	gl.DrawArrays(gl.e.GL_TRIANGLES, 0, self.size)
+	--render.BindArrayBuffer(self.vertices_id)	
+	gl.BindBuffer(gl.e.GL_ELEMENT_ARRAY_BUFFER, self.indices_id)
+	gl.DrawElements(gl.e.GL_TRIANGLES, self.indices_count, gl.e.GL_UNSIGNED_INT, nil)
 end
 
-function META:UpdateVertexBuffer(buffer)
-	if buffer then
-		self.buffer = buffer
-		self.size = ffi.sizeof(buffer)
+function META:UpdateBuffer(vertices, indices)
+	if vertices then
+		self.vertices = vertices
+		self.vertices_size = ffi.sizeof(vertices)
+		
+		render.BindArrayBuffer(self.vertices_id)
+		gl.BufferData(gl.e.GL_ARRAY_BUFFER, self.vertices_size, self.vertices, gl.e.GL_DYNAMIC_DRAW)
 	end
 	
-	self:UpdateBuffer()
-end
-
-function META:UpdateBuffer()
-	render.BindArrayBuffer(self.id)
-		gl.BufferData(gl.e.GL_ARRAY_BUFFER, self.size, self.buffer, gl.e.GL_DYNAMIC_DRAW)
-		render.BindVertexArray(self.vao_id)
-			for location, data in pairs(self.vertex_attributes) do
-				gl.EnableVertexAttribArray(location)
-				gl.VertexAttribPointer(location, data.arg_count, data.enum, false, data.stride, data.type_stride)
-			end
-		render.BindVertexArray(0)
+	if indices then
+		self.indices = indices
+		self.indices_size = ffi.sizeof(self.indices)
+		self.indices_count = self.indices_size / ffi.sizeof("unsigned int")
 		
+		gl.BindBuffer(gl.e.GL_ELEMENT_ARRAY_BUFFER, self.indices_id)
+		gl.BufferData(gl.e.GL_ELEMENT_ARRAY_BUFFER, self.indices_size, self.indices, gl.e.GL_DYNAMIC_DRAW)
+	end
+		
+	render.BindVertexArray(self.vao_id)		
+		for location, data in pairs(self.vertex_attributes) do
+			gl.EnableVertexAttribArray(location)
+			gl.VertexAttribPointer(location, data.arg_count, data.enum, false, data.stride, data.type_stride)
+		end
+	render.BindVertexArray(0)
+	
 	render.BindArrayBuffer(0)
+	gl.BindBuffer(gl.e.GL_ELEMENT_ARRAY_BUFFER, 0)
 end
