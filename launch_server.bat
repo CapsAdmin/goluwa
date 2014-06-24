f135fa0aac44363a2f50ae6f86405b5eb1426950
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-24 17:12:44 +0200

    corrected path to steam friends
    Former-commit-id: 4194f078f853bcfc3a4dc189a668e878566fd937

--- launch_server.bat
+++ launch_server.bat
@@ -3,7 +3,7 @@ ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
 
 SET CLIENT=0
 SET SERVER=1
-SET ARGS={'host','open steam/steam_friends'}
+SET ARGS={'host','open steam/friends'}
 
 cd .base/bin/windows/%ARCH%/
 start luajit ../../../lua/init.lua
