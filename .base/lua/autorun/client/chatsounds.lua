8aaff34ec351a58de01f8d1b664ca96b743bddbd
CapsAdmin <eliashogstvedt@gmail.com>
2014-06-28 12:36:02 +0200

    look for a punctuation character at the beginning of the string and not anywhere

--- .base/lua/autorun/client/chatsounds.lua
+++ .base/lua/autorun/client/chatsounds.lua
@@ -1,7 +1,7 @@
 --chatsounds.Initialize()
 
 event.AddListener("ClientChat", "chatsounds", function(client, txt, seed)
-	if not txt:find(".-%p") then
+	if not txt:find("^%p") then
 		chatsounds.Say(client, txt, seed)
 	end
 end)
