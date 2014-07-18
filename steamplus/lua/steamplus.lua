547d3d23efed1c9799d790b3a51b3558249ab5c6
CapsAdmin <eliashogstvedt@gmail.com>
2014-07-18 11:13:38 +0200

    Steam Plus!

---
+++ steamplus/lua/steamplus.lua
@@ -0,0 +1,81 @@
+if not steam.IsSteamClientAvailible() then
+	logn("steam friends extension not available")
+return end
+
+chatsounds.Initialize()
+
+local subject
+
+event.AddListener("SteamFriendsMessage", "steam_friends", function(sender_steam_id, txt, receiver_steam_id)
+	if txt:sub(1, 2) == ">>" then return end
+
+	local ply = clients.GetByUniqueID(sender_steam_id)
+	
+	if sender_steam_id == steam.GetClientSteamID() then
+		sender_steam_id = receiver_steam_id
+	end
+	
+	subject = sender_steam_id
+	event.Delay(0.1, function() subject = nil end)
+	
+	if ply:IsValid() then
+		if txt:sub(1, 1) == "!" then
+			console.RunString(txt:sub(2),nil,nil,true)
+		end
+		
+		chat.ClientSay(ply, txt, true)
+	end
+end)
+
+event.AddListener("ConsolePrint", "steam_friends", function(line)
+	if subject then
+		steam.SendChatMessage(subject, ">> " .. line)
+	end
+end)
+
+for i, steam_id in pairs(steam.GetFriends()) do
+	local ply = clients.Create(steam_id, true)
+	ply:SetNick(steam.GetNickFromSteamID(ply:GetUniqueID()))
+end
+
+local ply = clients.Create(steam.GetClientSteamID(), true)
+ply:SetNick(steam.GetNickFromSteamID(ply:GetUniqueID()))
+
+--[==[ffi.cdef[[
+typedef struct NOTIFYICONDATA {
+  unsigned long cbSize;
+  void *hWnd;
+  unsigned int uID;
+  unsigned int uFlags;
+  unsigned int uCallbackMessage;
+  void *hIcon;
+  char szTip[64];
+  unsigned long dwState;
+  unsigned long dwStateMask;
+  char szInfo[256];
+  union {
+    unsigned int uTimeout;
+    unsigned int uVersion;
+  };
+  char szInfoTitle[64];
+  unsigned long dwInfoFlags;
+  void *guidItem;
+  void *hBalloonIcon;
+} NOTIFYICONDATA, *PNOTIFYICONDATA;
+
+int Shell_NotifyIconW(int dwMessage, NOTIFYICONDATA *lpdata);
+]]
+
+local lib = ffi.load("Shell32.dll")
+
+local struct = ffi.new("NOTIFYICONDATA[1]")
+
+ffi.fill(struct[0], 0)
+
+struct[0].cbSize = ffi.sizeof("NOTIFYICONDATA")
+struct[0].uID = 1231
+struct[0].hWnd = nil
+struct[0].szTip = "hello"
+struct[0].szInfo = "hello"
+
+lib.Shell_NotifyIconW(0, struct) ]==]
