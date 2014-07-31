d8eb5097634e9849585ebcd3ae0bf3a6544ba115
CapsAdmin <eliashogstvedt@gmail.com>
2014-07-31 05:34:09 +0200

    added and updated some tests

--- test/lua/tests/framebuffer.lua
+++ test/lua/tests/framebuffer.lua
@@ -1,27 +1,23 @@
-require("lj-opengl").logcalls = true
 local fb = render.CreateFrameBuffer(512, 512, {
-	attach = "color0",
+	attach = "color1",
 	texture_format = {
-		internal_format = gl.e.GL_RGB32F,
+		internal_format = "RGB32F",
+		min_filter = "nearest",
 	}
 })
 
-event.CreateTimer("lol", 0.2, 0, function()
-		
-	fb:Begin()   
-		fb:Clear(1, 1, 1)
-			
-		--surface.Start(0, 0, 512, 512) 
+event.CreateTimer("fb_update", 0, 0, function()
+	fb:Begin()
+		surface.Start(0, 0, 512, 512)			
 			surface.SetWhiteTexture()
 			surface.SetColor(math.randomf(), math.randomf(), math.randomf(), 1)
 			surface.DrawRect(math.random(512), math.random(512), 100, 100)
-		--surface.End()
-			
-	fb:End()  
+		surface.End()
+	fb:End()
 end)
+
 event.AddListener("Draw2D", "fb", function()
 	surface.SetTexture(fb:GetTexture())
 	surface.SetColor(1,1,1,1)
 	surface.DrawRect(128, 128, 128, 128, timer.GetSystemTime()*100, 64, 64)
-end)                    
-require("lj-opengl").logcalls = false
+end)   
