998190081f2d41d846e9fa3eaf7f9a33234f05fe
CapsAdmin <eliashogstvedt@gmail.com>
2014-08-04 19:47:08 +0200

    updated and added tests
    Former-commit-id: 92185181418445336c5ab3b3d58e6f8dfc07d6e6

--- test/lua/tests/surface/start2d.lua
+++ test/lua/tests/surface/start2d.lua
@@ -1,21 +1,30 @@
-local fb = render.CreateFrameBuffer(512, 512)
+local fb = render.CreateFrameBuffer(256, 256, {
+	attach = "color1",
+	texture_format = {
+		internal_format = "RGB32F",
+		min_filter = "nearest",
+	}
+})
 
 local tex = fb:GetTexture()
  
 event.AddListener("Draw2D", "lol", function()
+	local t = timer.GetSystemTime()
 	
-	fb:Begin()		
-		surface.SetColor(1, 0, 0, 1) 
-		if wait(0.5) then
-			fb:Clear(1,1,1,0)
-		end
-		
+	fb:Begin()				
 		surface.Start(0, 0, tex.w, tex.h)
+			surface.Translate(math.sin(t) * 100, math.cos(t) * 100)
+			surface.Rotate(t*100)
+			
 			surface.SetWhiteTexture() 
-			for i = 1, 10 do
-				surface.SetColor(math.randomf(), 0, 0, 1)
-				surface.DrawRect(math.random(tex.w), math.random(tex.h), 100, 100, math.random(360))
-			end
+			local x, y = surface.GetMousePos(50, 50)
+						
+			surface.DrawRect(x,y,5,5) 
+			surface.SetWhiteTexture(tex)
+			render.SetBlendMode("additive")
+			surface.SetColor(math.randomf(), math.randomf(), math.randomf(), 0.1)
+			surface.DrawRect(math.random(tex.w), math.random(tex.h), 100, 100, math.random(360))
+			render.SetBlendMode("alpha")
 		surface.End()
 	fb:End() 
 
@@ -35,13 +44,13 @@ event.AddListener("Draw2D", "lol", function()
 			surface.SetColor(0, 1, 0, 1) 
 			surface.DrawRect(0, 0, 100, 100)
 		surface.End()
-		
-		surface.SetTexture(tex)
-		surface.SetColor(1, 1, 1, 1)
-		surface.DrawRect(0, 0, tex.w, tex.h)
 	surface.End()
+		
+	surface.SetTexture(tex)
+	surface.SetColor(1, 1, 1, 1)
+	surface.DrawRect(50, 50, tex.w, tex.h)
 	
-
-
-
+	surface.SetTexture(tex)
+	surface.SetColor(1, 1, 1, 1)
+	surface.DrawRect(tex.w, tex.h, 50, 50, t*100, 25, 25)
 end) 
