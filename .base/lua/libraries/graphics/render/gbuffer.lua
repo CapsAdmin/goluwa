ac2d86af9c5c983e8ccb4ffbdf008963a14ff84a
CapsAdmin <eliashogstvedt@gmail.com>
2014-08-08 05:04:54 +0200

    post process effects can now easily be added
    Former-commit-id: fd4d8e7f348a63e704205981cb7e2b3c83dcde05

--- .base/lua/libraries/graphics/render/gbuffer.lua
+++ .base/lua/libraries/graphics/render/gbuffer.lua
@@ -39,19 +39,17 @@ local SHADER = {
 			//
 			//SSAO
 			//
-			float aoMultiplier= 50000.0;
-			float depthTolerance = 0.001;
+			float aoMultiplier= 99999999.0;
 			
-			float readDepth( in vec2 coord ) {
+			float readDepth(vec2 coord, vec3 normal) {
 				//return (2.0 * rt_w) / (rt_h + rt_w - texture2D( tex_position, coord ).x * (rt_h - rt_w));	
+				
 				return pow(texture(tex_depth, coord).a, 0.5);
 			}
 			 
 			float compareDepths( in float depth1, in float depth2 ) {
-				float aoCap = 1.2;
-				float aoMultiplier=20000.0;
-				float depthTolerance=0.000001;
-				//float aorange = 10.0;// units in space the AO effect extends to (this gets divided by the camera far range
+				float aoCap = 0.4;
+				float depthTolerance=0.001;
 				float diff = sqrt( clamp(1.0-(depth1-depth2) / 0.01,0.0,0.3) );
 				float ao = min(aoCap,max(0.0,depth1-depth2-depthTolerance) * aoMultiplier) * diff;
 				return ao;
@@ -59,84 +57,29 @@ local SHADER = {
 			 
 			float ssao(void)
 			{	
-				float depth = readDepth( uv );
+				vec3 normal = texture(tex_normal, uv).yxz;
+				float depth = readDepth( uv, normal );
 				float d;
 			 
 				float pw = 1.0 / rt_w;
 				float ph = 1.0 / rt_h;
 			 
-				float aoCap = 1.0;
+				float ao = 7.5;	 
 			 
-				float ao = 0.1;
-			 
-				float aoMultiplier=10000.0;
-			 
-				float depthTolerance = 0.001;
-			 
-				float aoscale=0.75;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				pw*=2.0;
-				ph*=2.0;
-				aoMultiplier/=2.0;
-				aoscale*=1.2;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				pw*=2.0;
-				ph*=2.0;
-				aoMultiplier/=2.0;
-				aoscale*=1.2;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				pw*=2.0;
-				ph*=2.0;
-				aoMultiplier/=2.0;
-				aoscale*=1.2;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y+ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x+pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
-			 
-				d=readDepth( vec2(uv.x-pw,uv.y-ph));
-				ao+=compareDepths(depth,d)/aoscale;
+				float aoscale = 0.5;
+				
+				for (int i = 1; i < 5; i++)
+				{					
+					ao += compareDepths(depth, readDepth(vec2(uv.x+pw,uv.y+ph), normal)) / aoscale;
+					ao += compareDepths(depth, readDepth(vec2(uv.x-pw,uv.y+ph), normal)) / aoscale;
+					ao += compareDepths(depth, readDepth(vec2(uv.x+pw,uv.y-ph), normal)) / aoscale;
+					ao += compareDepths(depth, readDepth(vec2(uv.x-pw,uv.y-ph), normal)) / aoscale;
+				 
+					pw *= 2.0;
+					ph *= 2.0;
+					aoMultiplier /= 2.0;
+					aoscale *= 1.2;
+				}			 
 			 
 				ao/=16.0;
 			 
@@ -146,16 +89,9 @@ local SHADER = {
 			//
 			//FOG
 			//
-			vec3 mix_fog(vec3 color, float depth)
+			vec3 mix_fog(vec3 color, float depth, float fog_intensity, vec3 fog_color)
 			{
-				//blue
-				//const vec3 ambient = vec3(0.30588236451149, 0.59607845544815, 0.88235300779343); 
-				const vec3 ambient = vec3(245.0 / 255.0, 210.0 / 255.0, 144.0 / 255.0); 
-				
-				float fog_intensity = pow(depth, 255);
-				fog_intensity = -fog_intensity + 1;
-
-				color = mix(ambient * color, color, fog_intensity);
+				color = mix( 1 - fog_color, color, clamp(1.0 - (pow(depth, fog_intensity)), 0.0, 1.0));
 				
 				return color;
 			}
@@ -169,9 +105,11 @@ local SHADER = {
 				const float method = 1;
 				float light_specular = 64;
 				float light_shininess = 64;
-				float light_radius = 1000; //light radius isnt working, its also not being used
+				float light_intensity = 250.0; //added light intensity
+				float light_radius = 1000 * light_intensity;
 				
-				vec3 final_color = light_color;
+				
+				vec3 final_color = vec3(0);
 				
 				vec3 light_vec = light_pos - position;
 				float light_dist = length(light_vec);
@@ -190,10 +128,10 @@ local SHADER = {
 					float spec_angle = max(dot(half_dir, normal), 0.0);
 					float S = pow(spec_angle, 64.0);
 					
-					final_color = vec3(0,0,0) + lambertian * diffuse + S * specular_map;
+					final_color = (lambertian * diffuse + S * specular_map) * light_color;
 				}
 						
-				return final_color / light_dist * 10;
+				return final_color / light_dist * light_intensity;
 			}
 			
 			//
@@ -217,48 +155,64 @@ local SHADER = {
 
 				float specular = texture(tex_specular, uv).x;
 		
-				out_color.rgb = diffuse * 0.1;
+		
+				//this is for BSP!!!
+				vec3 ambient_light_color = vec3(191.0 / 255.0, 205.0 / 255.0, 214.0 / 255.0) * 0.9;
+				vec3 atmosphere_color = ambient_light_color;
+				vec3 fog_color = atmosphere_color;
+				float fog_distance = 750.0;
+				
+				//vec3 ambient_light_color = vec3(0.05);
+				//vec3 atmosphere_color = ambient_light_color;
+				//vec3 fog_color = atmosphere_color;
+				//float fog_distance = 2000.0;
+				
+				//if(uv.x < 0.5) {
+					out_color.rgb = diffuse * ambient_light_color * ssao();
+				//} else {
+				//	out_color.rgb = diffuse * ambient_light_color;
+				//}
+				//out_color.rgb = diffuse * (vec3(1.0) - vec3(0.88235300779343, 0.59607845544815, 0.30588236451149) / 8) * 1.3; //0.0 = ambient color
+				
 				
 				//out_color.rgb += calc_light(vec3(0, 0, 10) + vec3(sin(time) * 10, cos(time) * 10, 0), normal, position, diffuse, specular, vec3(0,0,0));				
-				out_color.rgb += calc_light(cam_pos, normal, position, diffuse, specular, vec3(0,0,0));				
-				out_color.rgb = mix_fog(out_color.rgb, depth);
+				out_color.rgb += calc_light(vec3(70.180198669434, -182.70826721191, 122.75518035889), normal, position, diffuse, specular, vec3(1.25,1.25,1.0));			
+
+				
+				if(out_color.rgb == vec3(0))
+				{
+					out_color.rgb = atmosphere_color;
+				}
 				
 				out_color.a = 1;
-				out_color.rgb *=  vec3(ssao());
+				//out_color.rgb = vec3(ssao());
+				out_color.rgb = mix_fog(out_color.rgb, depth, fog_distance, 1-fog_color); //this fog is fucked up, needs to be redone
+				
+				//out_color.rgb = 1-vec3(pow(depth,100));  
 			}
 		]]  
 	}
 } 
 
-local PPSHADER = {
-	name = "gbuffer_post_process",
-	base = "gbuffer",
-	fragment = {
-		uniform = {
-			tex_diffuse = "sampler2D",
-			tex_depth = "sampler2D",
-			tex_normal = "sampler2D",
-			tex_position = "sampler2D",
-			rt_w = "float",
-			rt_h = "float",
-		},
-		attributes = {
-			{pos = "vec2"},
-			{uv = "vec2"},
-		},
-		source = [[			
+local EFFECTS = {
+	{
+		name = "fxaa",
+		source = [[
 			out vec4 out_color;
 					
+			//
+			//FXAA
+			//
 			float FXAA_SPAN_MAX = 8.0;
-			float FXAA_REDUCE_MUL = 1.0/8.0;
-			float FXAA_SUBPIX_SHIFT = 1.0/4.0;
+			float FXAA_REDUCE_MUL = 1.0/16.0;
+			float FXAA_SUBPIX_SHIFT = 1.0/16.0;
 
 			#define FxaaInt2 ivec2
 			#define FxaaFloat2 vec2
 			#define FxaaTexLod0(t, p) textureLod(t, p, 0.0)
 			#define FxaaTexOff(t, p, o, r) textureLodOffset(t, p, 0.0, o)
 			
-			vec2 rcpFrame = vec2(1.0/rt_w, 1.0/rt_h);
+			vec2 rcpFrame = vec2(1.0/width, 1.0/height);
 			vec4 posPos = vec4(uv, uv - (rcpFrame * (0.5 + FXAA_SUBPIX_SHIFT)));
 
 			vec3 FxaaPixelShader(vec4 posPos, sampler2D tex)
@@ -315,19 +269,117 @@ local PPSHADER = {
 
 				return rgbB; 
 			}
+			
+			void main() 
+			{ 
+				out_color.rgb = FxaaPixelShader(posPos, tex_diffuse);
+				out_color.a = 1;
+			}
+		]],
+	},
+	{
+		name = "bloom",
+		source = [[
+			out vec4 out_color;
+						
+			//BLUR
+			vec4 blur(sampler2D tex)
+			{
+				float offset = vec2(0.01, 0.01) * 0.2;
+				vec3 c = vec3(0.);
+				float weight = 0.;
+				for(float i = 0.; i <= 2.; i += 0.2) 
+				{
+					c += texture(tex, uv + i * offset).rgb;
+					weight += 1.;
+				}
+				c /= weight;
+				return vec4(c, 1.);
+			}
+			
+			//CONTRAST
+			vec4 contrast(vec4 color)
+			{
+				vec3 col = color.rgb * 1.5;
+				col *= col;
+				return vec4(col, color.a);
+			}
 				
 			void main() 
 			{ 
-			  out_color = texture(tex_diffuse, uv);
-			  //out_color.rgb = vec3(pow(ssao(), 4));
-			  out_color.rgb = FxaaPixelShader(posPos, tex_diffuse);
-			 // out_color.rgb *= vec3(ssao());
-			  out_color.a = 1;
+				out_color = texture(tex_diffuse, uv); 
+				out_color += blur(tex_diffuse) * 0.14;
+				out_color.a = 1;
 			}
-		]]  
-	}
+		]],
+	},
+	
 }
 
+render.pp_shaders = {}
+
+function render.AddPostProcessShader(name, source, priority)
+	priority = priority or #render.pp_shaders
+	
+	local width = render.GetWidth()
+	local height = render.GetHeight()
+	
+	local shader = render.CreateShader({
+		name = "gbuffer_post_process_" .. name,
+		base = "gbuffer",
+		fragment = {
+			uniform = {
+				tex_diffuse = "sampler2D",
+				width = "float",
+				height = "float",
+			},
+			attributes = {
+				{pos = "vec2"},
+				{uv = "vec2"},
+			},
+			source = source
+		}
+	})
+	
+	local buffer = render.CreateFrameBuffer(width, height, {
+		{
+			name = "diffuse",
+			attach = "color",
+			texture_format = {
+				internal_format = "RGBA16F",
+			}
+		},
+	})
+	
+	
+	shader.pvm_matrix = render.GetPVWMatrix2D
+	shader.tex_diffuse = buffer:GetTexture("diffuse")
+
+	
+	shader.width = width
+	shader.height = height
+		
+	local quad = shader:CreateVertexBuffer({
+		{pos = {0, 0}, uv = {0, 1}},
+		{pos = {0, 1}, uv = {0, 0}},
+		{pos = {1, 1}, uv = {1, 0}},
+
+		{pos = {1, 1}, uv = {1, 0}},
+		{pos = {1, 0}, uv = {1, 1}},
+		{pos = {0, 0}, uv = {0, 1}},
+	})
+
+	for k, v in pairs(render.pp_shaders) do
+		if v.name == name then
+			table.remove(render.pp_shaders, k)
+			break
+		end
+	end
+	
+	table.insert(render.pp_shaders, {shader = shader, quad = quad, buffer = buffer, name = name, priority = priority})
+	
+	table.sort(render.pp_shaders, function(a, b) return a.priority > b.priority end)
+end
  
 local sphere = NULL
 
@@ -424,40 +476,6 @@ function render.InitializeGBuffer(width, height)
 	render.gbuffer_shader = shader
 	render.gbuffer_screen_quad = vbo
 	
-	render.pp_buffer = render.CreateFrameBuffer(width, height, {
-		{
-			name = "diffuse",
-			attach = "color",
-			texture_format = {
-				internal_format = "RGBA8",
-			}
-		},
-	}) 
-
-	local shader = render.CreateShader(PPSHADER)
-	shader.pvm_matrix = render.GetPVWMatrix2D
-
-	shader.cam_pos = render.GetCamPos
-	shader.tex_diffuse = render.pp_buffer:GetTexture("diffuse")
-	shader.tex_depth = render.gbuffer:GetTexture("depth")
-	shader.tex_position = render.gbuffer:GetTexture("position") 
-	shader.tex_normal = render.gbuffer:GetTexture("normal")
-	shader.rt_w = width
-	shader.rt_h = height
-	
-	local vbo = shader:CreateVertexBuffer({
-		{pos = {0, 0}, uv = {0, 1}},
-		{pos = {0, 1}, uv = {0, 0}},
-		{pos = {1, 1}, uv = {1, 0}},
-
-		{pos = {1, 1}, uv = {1, 0}},
-		{pos = {1, 0}, uv = {1, 1}},
-		{pos = {0, 0}, uv = {0, 1}},
-	})
-	
-	render.pp_screen_shader = shader
-	render.pp_screen_quad = vbo
-	
 	event.AddListener("PreDisplay", "gbuffer", function()
 		render.gbuffer:Begin()
 		render.gbuffer:Clear()
@@ -501,6 +519,10 @@ function render.InitializeGBuffer(width, height)
 			end
 		end
 	end)
+	
+	for i, data in pairs(EFFECTS) do
+		render.AddPostProcessShader(data.name, data.source)
+	end	
 end
 
 function render.ShutdownGBuffer()
@@ -526,42 +548,47 @@ end
 local size = 4
 
 function render.DrawGBuffer(w, h)
-	render.Start3D()
-	
-	render.gbuffer:Begin()
-	
-	--gl.ActiveTextureARB(gl.e.GL_TEXTURE4)
-	gl.Disable(gl.e.GL_DEPTH_TEST)
-	gl.Disable(gl.e.GL_CULL_FACE)	
-	
-	if sphere:IsValid() then 
-		gl.DrawBuffers(1, render.gbuffer.buffers.light.draw_enum)
-		sphere:Draw()   
-	end
-	
-	gl.Enable(gl.e.GL_DEPTH_TEST)
-	gl.Enable(gl.e.GL_CULL_FACE)
-	
-	render.gbuffer:End()
-		
 	render.Start2D()
-		-- draw to the pp buffer
-		render.pp_buffer:Begin()
+		-- draw to the pp buffer		
+		local effect = render.pp_shaders[1]
+		
+		if effect then
+			
+			render.PushWorldMatrix()
+			surface.Scale(w, h)
+			
+			-- draw the gbuffer into the first effect 
+			effect.buffer:Begin()
+				render.gbuffer_shader:Bind()
+				render.gbuffer_screen_quad:Draw()
+			effect.buffer:End()
+			
+			for i = 2, #render.pp_shaders do 
+				local next = render.pp_shaders[i]
+									
+				next.buffer:Begin()
+					effect.shader:Bind()
+					effect.quad:Draw()
+				next.buffer:End()					
+				
+				effect = next
+			end
+			
+			render.PopWorldMatrix()
+			
+			-- draw the pp texture as quad
+			render.PushWorldMatrix()
+				surface.Scale(w, h)
+				effect.shader:Bind()
+				effect.quad:Draw()
+			render.PopWorldMatrix()
+		else
 			render.PushWorldMatrix()
 				surface.Scale(w, h)
 				render.gbuffer_shader:Bind()
 				render.gbuffer_screen_quad:Draw()
 			render.PopWorldMatrix()		
-		render.pp_buffer:End()
-		
-		-- draw the pp texture as quad
-		render.PushWorldMatrix()
-			surface.Scale(w, h)
-			render.pp_screen_shader:Bind()
-			render.pp_screen_quad:Draw()
-		render.PopWorldMatrix()
-		
-		
+		end		
 	render.End2D()
 end
 
