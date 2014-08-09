86c16ab40743675a408b9582f37c3365e8cb9a24
CapsAdmin <eliashogstvedt@gmail.com>
2014-08-09 20:06:32 +0200

    let there be light
    Former-commit-id: c1055b8303a96c5eba5c0110daacfe43d6a454d5

--- .base/lua/libraries/entities/components/light.lua
+++ .base/lua/libraries/entities/components/light.lua
@@ -6,12 +6,23 @@ COMPONENT.Name = "light"
 COMPONENT.Require = {"transform"}
 COMPONENT.Events = {"DrawLights"}
 
-metatable.StartStorable()		
-
+metatable.StartStorable()
+	metatable.GetSet(COMPONENT, "Color", Color(1, 1, 1))
+	
+	--metatable.GetSet(COMPONENT, "Color", Color(1,1,1,1))	
+	--metatable.GetSet(COMPONENT, "Radius", 1000),
+	--metatable.GetSet(COMPONENT, "Pos", Vec3(0,0,0))
+	
+	-- automate this!!
+	metatable.GetSet(COMPONENT, "AmbientIntensity", 0)
+	metatable.GetSet(COMPONENT, "DiffuseIntensity", 0.5)
+	metatable.GetSet(COMPONENT, "SpecularPower", 32)
+	metatable.GetSet(COMPONENT, "AttenuationConstant", 0)
+	metatable.GetSet(COMPONENT, "AttenuationLinear", 0)
+	metatable.GetSet(COMPONENT, "AttenuationExponent", 0.01)	
 metatable.EndStorable()
 
-if CLIENT then
-			
+if CLIENT then			
 	function COMPONENT:OnAdd(ent)
 		self.light_mesh = render.Create3DMesh("models/sphere.obj")
 	end
@@ -29,7 +40,17 @@ if CLIENT then
 		shader.light_pos = transform:GetPosition()
 		shader.light_radius = transform:GetSize()
 		
+		-- automate this!!
+		shader.light_color = self.Color
+		shader.light_ambient_intensity = self.AmbientIntensity
+		shader.light_diffuse_intensity = self.DiffuseIntensity
+		shader.light_specular_power = self.SpecularPower
+		shader.light_attenuation_constant = self.AttenuationConstant
+		shader.light_attenuation_linear = self.AttenuationLinear
+		shader.light_attenuation_exponent = self.AttenuationExponent
+		
 		for i, model in ipairs(self.light_mesh.sub_models) do
+			shader:Bind()
 			model.mesh:Draw()
 		end
 	end	
