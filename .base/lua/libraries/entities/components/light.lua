47ac1dff2112c750f3c825bf6aec2d0f36912396
CapsAdmin <eliashogstvedt@gmail.com>
2014-08-11 01:56:23 +0200

    wip shadow map
    Former-commit-id: 5b31f041829a86806ef9c8d8b65bb9b7ef85f956

--- .base/lua/libraries/entities/components/light.lua
+++ .base/lua/libraries/entities/components/light.lua
@@ -4,7 +4,7 @@ local COMPONENT = {}
 
 COMPONENT.Name = "light"
 COMPONENT.Require = {"transform"}
-COMPONENT.Events = {"Draw3DLights"}
+COMPONENT.Events = {"Draw3DLights", "DrawShadowMaps"}
 
 metatable.StartStorable()
 	metatable.GetSet(COMPONENT, "Color", Color(1, 1, 1))
@@ -30,6 +30,75 @@ if CLIENT then
 	function COMPONENT:OnRemove(ent)
 
 	end	
+	
+	do -- shadow map		
+		local gl = require("lj-opengl")
+		
+		function COMPONENT:SetShadow(b)
+			if b then
+				self.shadow_map = render.CreateFrameBuffer(512, 512, {
+					name = "depth",
+					attach = "depth",
+					draw_manual = true,
+					texture_format = {
+						internal_format = "DEPTH_COMPONENT32F",	 
+						depth_texture_mode = gl.e.GL_RED,
+						min_filter = "nearest",				
+					} 
+				})
+				
+				render.shadow_maps[self] = self.shadow_map
+			end
+		end
+						
+		function COMPONENT:OnDrawShadowMaps(shader)
+			if ohno or not self.shadow_map then return end
+			
+			local transform = self:GetComponent("transform")
+
+			
+			--transform:SetPosition(Vec3(1000, 1000, 1000)*0.2)
+			--transform:SetAngles(Ang3(-90, 0, 0)) 
+			
+			transform:SetPosition(Vec3(math.cos(os.clock())*5, 70, 10 + math.sin(os.clock())*5))
+			transform:SetAngles(Ang3(0, -90, 0)) 
+			
+			--local pos = Vec3(math.sin(os.clock())*1500, 0, math.cos(os.clock())*500)
+			--transform:SetPosition(pos)
+			--transform:SetAngles((pos - render.GetCamPos()):GetAng3())
+			
+			--transform:SetPosition(render.GetCamPos())
+			--transform:SetAngles(render.GetCamAng())
+			
+			do
+				local pos = transform:GetPosition()
+				local ang = transform:GetAngles()
+				local forward = ang:GetForward()
+				
+				local projection = Matrix44()
+				local cam = render.camera
+				local size = 200
+				projection:Perspective(80, cam.nearz, cam.farz, cam.ratio) 
+				--projection:Ortho(-size, size, -size, size, 200, 0) 
+
+				local view = Matrix44()
+				view:LoadIdentity()		
+
+				view:Rotate(ang.r, 0, 0, 1)
+				view:Rotate(ang.p + 90, 1, 0, 0)
+				view:Rotate(ang.y, 0, 0, 1)
+			
+				view:Translate(pos.y, pos.x, pos.z)
+						
+				self.vp_matrix = view * projection
+				
+				self.shadow_map:Begin()
+					self.shadow_map:Clear()
+					event.Call("Draw3DGeometry", shader, self.vp_matrix)
+				self.shadow_map:End()
+			end
+		end
+	end
 
 	function COMPONENT:OnDraw3DLights(shader)
 		local transform = self:GetComponent("transform")
@@ -48,6 +117,11 @@ if CLIENT then
 		shader.light_attenuation_constant = self.AttenuationConstant
 		shader.light_attenuation_linear = self.AttenuationLinear
 		shader.light_attenuation_exponent = self.AttenuationExponent
+		
+		if self.vp_matrix and self.shadow_map then
+			shader.tex_shadow_map = self.shadow_map:GetTexture("depth")
+			shader.light_vp_matrix = self.vp_matrix.m
+		end
 		
 		for i, model in ipairs(self.light_mesh.sub_models) do
 			shader:Bind()
