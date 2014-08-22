a42ff9d895a7c9800bce42e537884ca8c32ca27d
CapsAdmin <eliashogstvedt@gmail.com>
2014-08-22 19:43:57 +0200

    added drag drop better animations cleanup

--- test/lua/tests/gui2.lua
+++ test/lua/tests/gui2.lua
@@ -1,3 +1,7 @@
+-- multiple animations of the same type
+-- merge update and draw somehow for performance
+-- support rotation in TrapChildren and drag drop
+-- use stencils for multiple clip planes
 
 local gui2 = {}
 
@@ -10,16 +14,23 @@ do -- base panel
 	metatable.AddParentingTemplate(PANEL)
 
 	metatable.GetSet(PANEL, "Position", Vec2(0, 0))
-	metatable.GetSet(PANEL, "Order", 0)
-	metatable.GetSet(PANEL, "Scroll", Vec2(0, 0))
 	metatable.GetSet(PANEL, "Size", Vec2(50, 50))
 	metatable.GetSet(PANEL, "Angle", 0)
+
+	metatable.GetSet(PANEL, "Scroll", Vec2(0, 0))
+
+	metatable.GetSet(PANEL, "Order", 0)
+
+	-- these are useful for animations
+	metatable.GetSet(PANEL, "DrawSizeOffset", Vec2(0, 0))
+	metatable.GetSet(PANEL, "DrawPositionOffset", Vec2(0, 0))
+	metatable.GetSet(PANEL, "DrawAngleOffset", 0)
+
 	metatable.GetSet(PANEL, "MousePosition", Vec2(0, 0))
 	metatable.GetSet(PANEL, "Clipping", false)
 	metatable.GetSet(PANEL, "Color", Color(1,1,1,1))
-	metatable.IsSet (PANEL, "Valid", true)
 	metatable.GetSet(PANEL, "Cursor", "hand")
-	metatable.GetSet(PANEL, "CachedRendering", false)
+	metatable.GetSet(PANEL, "TrapChildren", false)
 	metatable.GetSet(PANEL, "Texture", render.GetWhiteTexture())
 
 	function PANEL:__tostring()
@@ -27,6 +38,8 @@ do -- base panel
 	end
 
 	do -- cached rendering
+		metatable.GetSet(PANEL, "CachedRendering", false)
+
 		function PANEL:SetCachedRendering(b)
 			self.CachedRendering = b
 
@@ -64,7 +77,7 @@ do -- base panel
 					self:OnDraw()
 
 					surface.Translate(-self.Scroll.x, -self.Scroll.y)
-					
+
 					for k,v in pairs(self:GetChildren()) do
 						v:Draw()
 					end
@@ -80,23 +93,33 @@ do -- base panel
 		end
 	end
 
+	function PANEL:SetPosition(pos)
+
+		if self:HasParent() and self.Parent.TrapChildren then
+			pos.x = math.clamp(pos.x, 0, self.Parent.Size.w - self.Size.w)
+			pos.y = math.clamp(pos.y, 0, self.Parent.Size.h - self.Size.h)
+		end
+
+		self.Position = pos
+	end
+
 	local sorter = function(a,b)
 		return a.Order > b.Order
 	end
 
 	function PANEL:SetOrder(pos)
 		self.Order = pos
-		
+
 		local parent = self:GetParent()
 
 		if parent:IsValid() then
 			table.sort(parent:GetChildren(), sorter)
 		end
 	end
-	
+
 	function PANEL:SetScroll(vec)
 		local size = self:GetSizeOfChildren()
-			
+
 		self.Scroll = Vec2(math.clamp(vec.x, 0, size.x - self.Size.w), math.clamp(vec.y, 0, size.y - self.Size.h))
 	end
 
@@ -104,7 +127,6 @@ do -- base panel
 		local parent = self:GetParent()
 
 		if parent:IsValid() then
-			parent:UnparentChild(self)
 			parent:AddChild(self)
 		end
 	end
@@ -116,31 +138,82 @@ do -- base panel
 	function PANEL:IsWorld()
 		return self == gui2.world
 	end
-	
+
 	function PANEL:GetSizeOfChildren()
 		local total_size = Vec2()
-		
+
 		for k, v in pairs(self:GetChildren()) do
 			local x, y = v:GetPosition():Unpack()
-		 
+
 			x = x + v.Size.x
 			y = y + v.Size.y
-			
-			if x > total_size.x then 
+
+			if x > total_size.x then
 				total_size.x = x
 			end
-			
+
 			if y > total_size.y then
 				total_size.y = y
 			end
 		end
-		
+
 		return total_size
 	end
-	
+
+	do -- drag drop
+		metatable.GetSet(PANEL, "DragDrop", false)
+
+		function PANEL:StartDragging(button)
+			--self:StopAnimations()
+
+			self.drag_pos = gui2.mouse_pos:Copy()
+			self.drag_stop_button = button
+		end
+
+		function PANEL:StopDragging()
+			self.drag_pos = nil
+			self.drag_pos2 = nil
+		end
+
+		function PANEL:CalcDragging()
+			if self.drag_pos then
+
+				if not self.drag_pos2 then
+					self.drag_pos2 = self:GetPosition()
+				end
+
+				local drag_pos = Vec2(surface.WorldToLocal(self.drag_pos:Unpack()))
+				local mouse_pos = self:GetMousePosition()
+				local world_pos = self.drag_pos2
+
+				self:SetPosition(world_pos + mouse_pos - drag_pos)
+
+				if not input.IsMouseDown(self.drag_stop_button) then
+					local panel = gui2.GetHoveringPanel(nil, self)
+
+					if panel == self then panel = self.Parent end
+
+					self:OnParentLand(panel)
+					panel:OnChildDrop(self, panel:GetMousePosition() - self:GetMousePosition())
+
+					self:StopDragging()
+				end
+			end
+		end
+
+		function PANEL:OnParentLand(parent)
+
+		end
+
+		function PANEL:OnChildDrop(child, pos)
+			self:AddChild(child)
+			child:SetPosition(pos)
+		end
+	end
+
 	do -- animations
 		PANEL.animations = {}
-		
+
 		--[[2:11 AM - Morten: ]]
 		local function lerp_values(values, alpha)
 			local tbl = {}
@@ -153,18 +226,18 @@ do -- base panel
 				end
 			end
 
-			if #tbl > 1 then 
-				return lerp_values(tbl, alpha) 
-			else 
-				return tbl[1] 
+			if #tbl > 1 then
+				return lerp_values(tbl, alpha)
+			else
+				return tbl[1]
 			end
 		end
-		
+
 		function PANEL:UpdateAnimations()
 			for key, animation in pairs(self.animations) do
-				
+
 				local pause = false
-				
+
 				for i, v in ipairs(animation.pausers) do
 					if animation.alpha >= v.alpha then
 						if v.check()  then
@@ -175,27 +248,27 @@ do -- base panel
 						end
 					end
 				end
-				
+
 				if not pause then
-				
+
 					animation.alpha = animation.alpha + timer.GetFrameTime() / animation.time
 					local alpha = animation.alpha
-					
+
 					local val
 					local from = animation.from
 					local to = animation.to
-					
+
 					if animation.pow then
 						alpha = alpha ^ animation.pow
 					end
-								
+
 					val = lerp_values(to, alpha)
-					
+
 					if val == false then return end
-							
+
 					animation.func(self, val)
-								
-					if alpha > 1 then
+
+					if alpha >= 1 then
 						if animation.callback then
 							if animation.callback(self) ~= false then
 								animation.func(self, from)
@@ -203,41 +276,56 @@ do -- base panel
 						else
 							animation.func(self, from)
 						end
-						
+
 						self.animations[key] = nil
 					end
 				end
 			end
 		end
-		
-		function PANEL:Animate(var, to, time, operator, pow) 
-		
-			-- todo: multiple animations of the same type
-			if self.animations[var] then 
+
+		function PANEL:StopAnimations()
+			for key, animation in pairs(self.animations) do
+				if animation.callback then
+					if animation.callback(self) ~= false then
+						animation.func(self, animation.from)
+					end
+				else
+					animation.func(self, animation.from)
+				end
+
+				self.animations[key] = nil
+			end
+			self:UpdateAnimations()
+		end
+
+		function PANEL:Animate(var, to, time, operator, pow)
+
+			if self.animations[var] then
 				self.animations[var].alpha = 0
-			return end
-		
-			
+				return
+			end
+
+
 			local from = type(self[var]) == "number" and self[var] or self[var]:Copy()
-			
+
 			if type(to) ~= "table" then
 				to = {to}
 			end
-			
+
 			local pausers = {}
-			
+
 			for i, v in pairs(to) do
 				if type(v) == "function" then
 					to[i] = nil
 					table.insert(pausers, {check = v, alpha = (i - 1) / (table.count(to) + #pausers)})
 				end
 			end
-			
+
 			table.fixindices(to)
-			
-			for i, v in ipairs(to) do 
-				if v == "from" then 
-					to[i] = from 
+
+			for i, v in ipairs(to) do
+				if v == "from" then
+					to[i] = from
 				else
 					if operator then
 						if operator == "+" then
@@ -252,20 +340,20 @@ do -- base panel
 							v = from / v
 						end
 					end
-					
+
 					to[i] = v
 				end
 			end
-			
+
 			table.insert(to, 1, from)
-				
+
 			self.animations[var] = {
 				operator = operator,
 				from = from,
-				to = to,			
-				time = time or 0.25, 
-				var = var, 
-				func = self["Set" .. var], 
+				to = to,
+				time = time or 0.25,
+				var = var,
+				func = self["Set" .. var],
 				start_time = timer.GetSystemTime(),
 				pow = pow,
 				callback = callback,
@@ -274,38 +362,45 @@ do -- base panel
 			}
 		end
 	end
-		
-	function PANEL:Draw()
+
+	function PANEL:Draw(no_clip)
 		surface.PushMatrix()
-			render.Translate(self.Position.x, self.Position.y, 0)
-			
-			render.Translate(self.Size.w/2, self.Size.h/2, 0)
-			render.Rotate(self.Angle, 0, 0, 1)
-			render.Translate(-self.Size.w/2, -self.Size.h/2, 0)
+			render.Translate(self.Position.x + self.DrawPositionOffset.x, self.Position.y + self.DrawPositionOffset.y, 0)
+
+			local w = (self.Size.w + self.DrawSizeOffset.w)/2
+			local h = (self.Size.h + self.DrawSizeOffset.h)/2
+
+			render.Translate(w, h, 0)
+			render.Rotate(self.Angle + self.DrawAngleOffset, 0, 0, 1)
+			render.Translate(-w, -h, 0)
 
 			if self.CachedRendering then
 				self:DrawCache()
 			else
-				if self.Clipping then
-					surface.StartClipping(0, 0, self.Size.w, self.Size.h)
+
+				local sigh = false
+				if not no_clip and self.Clipping then
+					surface.StartClipping(0, 0, self.Size.w + self.DrawSizeOffset.w, self.Size.h + self.DrawSizeOffset.h)
+					no_clip = true
+					sigh = true
 				end
 
 					self:OnDraw()
 
 					render.Translate(-self.Scroll.x, -self.Scroll.y, 0)
-					
+
 					for k,v in ipairs(self:GetChildren()) do
-						if 	
-							v.Position.x - self.Scroll.x < self.Size.w and 
+						if 	v.drag_pos or
+							(v.Position.x - self.Scroll.x < self.Size.w and
 							v.Position.y - self.Scroll.y < self.Size.h and
-							v.Position.x - self.Scroll.x > -v.Size.w and 
-							v.Position.y - self.Scroll.y > -v.Size.h
+							v.Position.x - self.Scroll.x > -v.Size.w and
+							v.Position.y - self.Scroll.y > -v.Size.h)
 						then
-							v:Draw()
+							v:Draw(no_clip)
 						end
 					end
 
-				if self.Clipping then
+				if sigh or not no_clip and self.Clipping then
 					surface.EndClipping()
 				end
 			end
@@ -313,28 +408,29 @@ do -- base panel
 	end
 
 	function PANEL:Update()
-		
+
 		self:UpdateAnimations()
-	
+
 		surface.PushMatrix()
 			render.Translate(self.Position.x, self.Position.y, 0)
-			
+
 			render.Translate(self.Size.w/2, self.Size.h/2, 0)
 			render.Rotate(self.Angle, 0, 0, 1)
 			render.Translate(-self.Size.w/2, -self.Size.h/2, 0)
 
 			self:CalcMouse()
+			self:CalcDragging()
 
 			self:OnUpdate()
-			
+
 			render.Translate(-self.Scroll.x, -self.Scroll.y, 0)
 
 			for k,v in ipairs(self:GetChildren()) do
-				if 	
-					v.Position.x - self.Scroll.x < self.Size.w and 
+				if v.drag_pos or
+					(v.Position.x - self.Scroll.x < self.Size.w and
 					v.Position.y - self.Scroll.y < self.Size.h and
-					v.Position.x - self.Scroll.x > -v.Size.w and 
-					v.Position.y - self.Scroll.y > -v.Size.h
+					v.Position.x - self.Scroll.x > -v.Size.w and
+					v.Position.y - self.Scroll.y > -v.Size.h)
 				then
 					v:Update()
 				end
@@ -352,7 +448,7 @@ do -- base panel
 
 			self.MousePosition.x = x
 			self.MousePosition.y = y
-			
+
 			local alpha = 1
 
 			if self.Texture ~= render.GetWhiteTexture() and not self.Texture:IsLoading() then
@@ -395,6 +491,7 @@ do -- base panel
 					self:OnMouseEnter(x, y)
 					self.mouse_just_entered = true
 				end
+
 				self:OnMouseMove(x, y)
 			else
 				if self.mouse_just_entered then
@@ -413,16 +510,33 @@ do -- base panel
 		surface.SetColor(self.Color:Unpack())
 		surface.SetTexture(self.Texture)
 
-		surface.DrawRect(0, 0, self.Size.w, self.Size.h)
+		surface.DrawRect(0, 0, self.Size.w + self.DrawSizeOffset.w, self.Size.h + self.DrawSizeOffset.h)
 	end
 
 	function PANEL:OnMouseEnter(x, y) self:SetColor(Color(1,1,1,1)) end
 	function PANEL:OnMouseExit(x, y) self:SetColor(self.original_color) end
 	function PANEL:OnMouseMove(x, y) self:MarkDirty() end
-	function PANEL:OnMouseInput(button, press) self:RequestFocus() self:BringToFront() self:SetClipping(not self:GetClipping()) end
+	function PANEL:OnMouseInput(button, press)
+		self:BringToFront()
+
+		if press and button == "button_2" then
+			self:SetClipping(not self:GetClipping())
+		end
+
+		if press and button == "button_1" then
+			self:StartDragging(button)
+		end
+
+		self:RequestFocus()
+
+		if button == "button_1" and press then
+			self:OnClick()
+		end
+	end
 
 	function PANEL:OnCharTyped(char) end
 	function PANEL:OnKeyPressed(key, pressed) end
+	function PANEL:OnClick(key, pressed) end
 
 	function gui2.CreatePanel(parent)
 		local self = PANEL:New()
@@ -435,32 +549,35 @@ do -- base panel
 	end
 end
 
+function gui2.GetHoveringPanel(panel, filter)
+	panel = panel or gui2.world
+	local children = panel:GetChildren()
+
+	for i = #children, 1, -1 do
+		local panel = children[i]
+		if panel.mouse_over and (not filter or panel ~= filter) then
+			if panel:HasChildren() then
+				return gui2.GetHoveringPanel(panel)
+			end
+			return panel
+		end
+	end
+
+	return panel.mouse_over and panel or gui2.world
+end
+
 function gui2.Initialize()
 	local world = gui2.CreatePanel()
 	world:SetPosition(Vec2(0, 0))
 	world:SetSize(Vec2(window.GetSize()))
 	world:SetColor(Color(1,1,1,0.1))
-	world:SetCursor'arrow'
+	world:SetCursor("arrow")
+	world:SetTrapChildren(true)
+
 	gui2.world = world
 
-	local function check_mouse(panel)
-		local children = panel:GetChildren()
-
-		for i = #children, 1, -1 do
-			local panel = children[i]
-			if panel.mouse_over then
-				if panel:HasChildren() then
-					return check_mouse(panel)
-				end
-				return panel
-			end
-		end
- 
-		return panel.mouse_over and panel
-	end
-
 	local skin = Texture("textures/aahh/DefaultSkin.png")
-	
+
 	gui2.mouse_pos = Vec2()
 
 	event.AddListener("Draw2D", "gui2", function()
@@ -469,17 +586,16 @@ function gui2.Initialize()
 
 		world:Draw()
 		world:Update()
-		
 
 		surface.SetWhiteTexture()
 		surface.SetColor(1,0,0,1)
 		local x, y = surface.WorldToLocal(gui2.mouse_pos.x, gui2.mouse_pos.y)
 		surface.DrawRect(x, y, 1, 1)
 
-		--surface.SetTexture(skin) 
-		--surface.DrawNinePatch(50, 50, 200, 200, 128, 32, 0, 0)  
-		
-		gui2.hovering_panel = check_mouse(world) or gui2.world
+		--surface.SetTexture(skin)
+		--surface.DrawNinePatch(50, 50, 200, 200, 128, 32, 0, 0)
+
+		gui2.hovering_panel = gui2.GetHoveringPanel()
 
 		if gui2.hovering_panel:IsValid() then
 			local cursor = gui2.hovering_panel:GetCursor()
@@ -495,9 +611,7 @@ function gui2.Initialize()
 		local panel = gui2.hovering_panel
 
 		if panel:IsValid() and panel:IsMouseOver() then
-			panel:MarkDirty()
 			panel:OnMouseInput(button, press)
-			panel:BringToFront()
 		end
 	end)
 end
@@ -535,13 +649,13 @@ function gui2.Test()
 	local c = Color(1,1,1,1) * 0.25
 	frame:SetColor(c)
 	frame.original_color = c
-	
+
 	frame:SetCachedRendering(true)
 	--frame.OnMouseExit = function() end
 	--frame.OnMouseEnter = function() end
 
 	local lol = {}
-	
+
 	for x = 1, 5 do
 	for y = 1, 5 do
 		math.randomseed(x*y)
@@ -553,42 +667,42 @@ function gui2.Test()
 		pnl.original_color = c
 
 		pnl.rand = math.random() > 0.5 and math.randomf(20, 100) or -math.randomf(20, 100)
-		
+
 		pnl:SetPosition(Vec2(x * math.random(30, 80), y * math.random(30, 80)))
 		pnl:SetSize(Vec2(80,80) * math.randomf(0.25, 2))
 		--pnl:SetAngle(math.random(360))
 		pnl:SetCursor("icon")
 		pnl:SetTexture(Texture("textures/aahh/gear.png"))
 
-		pnl.OnMouseInput = pnl.RequestFocus
-		
+		--pnl.OnMouseInput = pnl.RequestFocus
+
 		table.insert(lol, pnl)
 	end
 	end
-	
+
 	event.AddListener("Update", "lol", function()
 		for i, v in ipairs(lol) do
-			v:SetAngle(os.clock()*v.rand)
+		--	v:SetAngle(os.clock()*v.rand)
 		end
 	end)
-	
+
 	function frame:OnMouseMove(x, y)
-		if input.IsMouseDown("button_3") and not self.drag_pos then
-			self.drag_pos = self:GetScroll() + Vec2(x, y)
+		if input.IsMouseDown("button_3") and not self.scroll_drag_pos then
+			self.scroll_drag_pos = self:GetScroll() + Vec2(x, y)
 		end
 		self:MarkDirty()
 	end
-	
+
 	function frame:OnUpdate()
-		if self.drag_pos and input.IsMouseDown("button_3") then
-			self:SetScroll(self.drag_pos - self:GetMousePosition())
+		if self.scroll_drag_pos and input.IsMouseDown("button_3") then
+			self:SetScroll(self.scroll_drag_pos - self:GetMousePosition())
 			self:MarkDirty()
 		else
-			self.drag_pos = nil
+			self.scroll_drag_pos = nil
 		end
 	end
-	
-	
+
+
 	for x = 1, 4 do
 	for y = 1, 4 do
 		math.randomseed(x*y)
@@ -597,63 +711,55 @@ function gui2.Test()
 
 		local c = HSVToColor(math.sin(x+y), 0.65, 1)
 		pnl:SetColor(c)
-		
+
 		pnl:SetPosition(Vec2(-5, 260) + Vec2(x, y) * 55)
 		pnl:SetSize(Vec2(50, 50))
 		--pnl:SetTexture(Texture("textures/aahh/button.png"))
-		
+
 		pnl.OnMouseEnter = function() end
 		pnl.OnMouseExit = function() end
-		
+
 		if math.random() > 0.5 then
 			if math.random() > 0.5 then
 				if math.random() > 0.5 then
-					pnl.OnMouseInput = function(s, key, press) 
-						if not press then return end
-						s:Animate("Color", {Color(0,0,0,0), "from", Color(1,1,0,1), "from"}, 0.5) 
+					pnl.OnClick = function(self)
+						self:Animate("Color", {Color(0,0,0,0), "from", Color(1,1,0,1), "from"}, 0.5)
 					end
 				else
-					pnl.OnMouseInput = function(s, key, press) 
-						if not press then return end
-						s:Animate("Color", {Color(1,0,0,1), Color(0,1,0,1),  Color(0,0,1,1), "from"}, 2) 
+					pnl.OnClick = function(self)
+						self:Animate("Color", {Color(1,0,0,1), Color(0,1,0,1),  Color(0,0,1,1), "from"}, 2)
 					end
 				end
 			else
-				pnl.OnMouseInput = function(s, key, press) 
-					if not press then return end
-					
+				pnl.OnClick = function(self)
 					local duration = 0.2
-					s:Animate("Size", {Vec2(10, 10), function() return input.IsMouseDown(key) end, Vec2(0, 0)}, duration, "-") 
-					s:Animate("Position", {Vec2(10, 10) * 0.5, function() return input.IsMouseDown(key) end, Vec2(0, 0)}, duration, "+")
-										
-					--s:Animate("Position", Vec2(150, 150), 0.5, function(s) s:SetSize(Vec2(50,50)) end) 
+
+					self:Animate("DrawSizeOffset", {Vec2(10, 10), function() return input.IsMouseDown("button_1") end, Vec2(0, 0)}, duration, "-")
+					self:Animate("DrawPositionOffset", {Vec2(10, 10) * 0.5, function() return input.IsMouseDown("button_1") end, Vec2(0, 0)}, duration, "+")
+
+					--self:Animate("DrawPositionOffset", Vec2(150, 150), 0.5, function(self) self:SetSize(Vec2(50,50)) end)
 				end
 			end
 		else
 			if math.random() > 0.5 then
-				pnl.OnMouseInput = function(s, key, press) 
-					if not press then return end
-					
+				pnl.OnClick = function(self)
 					local duration = 0.2
-					s:Animate("Color", {Color(0,0,0,0), "from"}, duration) 
-					s:Animate("Angle", math.random() > 0.5 and 90 or -90, duration) 
+					self:Animate("Color", {Color(0,0,0,0), "from"}, duration)
+					self:Animate("DrawAngleOffset", math.random() > 0.5 and 90 or -90, duration)
 				end
 			else
 				if math.random() > 0.5 then
-					pnl.OnMouseInput = function(s, key, press) 
-						if not press then return end
-						
+					pnl.OnClick = function(self)
 						local pow = 1
 						local duration = 0.5
-						
-						s:Animate("Size", {Vec2(1, -1), function() return input.IsMouseDown(key) end, "from"}, duration, "*", pow) 
-						s:Animate("Position", {Vec2(0, s.Size.h), function() return input.IsMouseDown(key) end, "from"}, duration, "+", pow) 
-						s:Animate("Color", {Color(0,0,0,0), function() return input.IsMouseDown(key) end, "from"}, duration, "", pow) 
+
+						self:Animate("DrawSizeOffset", {Vec2(1, -self.Size.h*2), function() return input.IsMouseDown("button_1") end, "from"}, duration, "+", pow)
+						self:Animate("DrawPositionOffset", {Vec2(0, self.Size.h), function() return input.IsMouseDown("button_1") end, "from"}, duration, "+", pow)
+						self:Animate("Color", {Color(0,0,0,0), function() return input.IsMouseDown("button_1") end, "from"}, duration, "", pow)
 					end
 				else
-					pnl.OnMouseInput = function(s, key, press) 
-						if not press then return end
-						s:Animate("Angle", {math.randomf(-360, 360), "from"}, math.random()) 
+					pnl.OnClick = function(self)
+						self:Animate("DrawAngleOffset", {math.randomf(-360, 360), "from"}, math.random())
 					end
 				end
 			end
