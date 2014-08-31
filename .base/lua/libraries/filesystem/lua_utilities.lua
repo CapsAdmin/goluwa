b38e66b9c6ede343e0a38879e84bc13d5cd48339
CapsAdmin <eliashogstvedt@gmail.com>
2014-08-31 21:46:54 +0200

    don't use lua's loadfile so lua files can be loaded from archives
    Former-commit-id: 19ff9e2fdb2a204344ee9998354ced68a254127a

--- .base/lua/libraries/filesystem/lua_utilities.lua
+++ .base/lua/libraries/filesystem/lua_utilities.lua
@@ -26,8 +26,10 @@ function vfs.loadfile(path)
 	check(path, "string")
 	
 	local full_path = vfs.GetAbsolutePath(path)
-		
+	
 	if full_path then
+		store(full_path)
+		
 		local res, err = vfs.Read(full_path)
 		if not res then 
 			return res, err, full_path 
@@ -56,13 +58,25 @@ end
 -- although vfs will add a loader for each mount, the module folder has to be an exception for modules only
 -- this loader should support more ways of loading than just adding ".lua"
 function vfs.AddModuleDirectory(dir)
-	table.insert(package.loaders, function(path)
-		return vfs.loadfile(dir .. path)
-	end)
-	
-	table.insert(package.loaders, function(path)
-		return vfs.loadfile(dir .. path .. ".lua")
-	end)
+	do -- full path
+		table.insert(package.loaders, function(path)
+			return vfs.loadfile(path)
+		end)
+		
+		table.insert(package.loaders, function(path)
+			return vfs.loadfile(path .. ".lua")
+		end)
+	end
+		
+	do -- relative path
+		table.insert(package.loaders, function(path)
+			return vfs.loadfile(dir .. path)
+		end)
+		
+		table.insert(package.loaders, function(path)
+			return vfs.loadfile(dir .. path .. ".lua")
+		end)
+	end
 		
 	table.insert(package.loaders, function(path)
 		return vfs.loadfile(dir .. path .. "/init.lua")
