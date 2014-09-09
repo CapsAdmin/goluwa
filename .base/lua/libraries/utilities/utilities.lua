a530f4dc1a83a242a7c8255a55346edeaed845a1
CapsAdmin <eliashogstvedt@gmail.com>
2014-09-09 18:19:07 +0200

    added utilities.TableToColumns(title, tbl, columns, check, sort_key)

--- .base/lua/libraries/utilities/utilities.lua
+++ .base/lua/libraries/utilities/utilities.lua
@@ -3,6 +3,93 @@ local utilities = _G.utilities or {}
 include("mesh.lua", utilities)
 include("packed_rectangle.lua", utilities)
 
+
+function utilities.TableToColumns(title, tbl, columns, check, sort_key)
+	local top = {}
+	
+	for k, v in pairs(tbl) do
+		if not check or check(v) then
+			table.insert(top, {key = k, val = v})
+		end
+	end
+	
+	if type(sort_key) == "function" then
+		table.sort(top, function(a, b)
+			return sort_key(a.val, b.val)
+		end)
+	else
+		table.sort(top, function(a, b)
+			return a.val[sort_key] > b.val[sort_key]
+		end)
+	end
+
+	local max_lengths = {}
+	local temp = {}
+	
+	for i, column in ipairs(top) do
+		for key, data in ipairs(columns) do
+			data.tostring = data.tostring or function(...) return ... end
+			data.friendly = data.friendly or data.key
+			
+			max_lengths[data.key] = max_lengths[data.key] or 0
+			
+			local str = tostring(data.tostring(column.val[data.key], column.val, top))
+			column.str = column.str or {}
+			column.str[data.key] = str
+			
+			if #str > max_lengths[data.key] then
+				max_lengths[data.key] = #str
+			end			
+			
+			temp[key] = data
+		end
+	end
+	
+	columns = temp
+	
+	local width = 0
+		
+	for k,v in pairs(columns) do 		
+		if max_lengths[v.key] > #v.friendly then 
+			v.length = max_lengths[v.key]
+		else 
+			v.length = #v.friendly + 1
+		end 
+		width = width + #v.friendly + max_lengths[v.key] - 2 
+	end
+	
+	local out = " "
+		
+	out = out .. ("_"):rep(width - 1) .. "\n"
+	out = out .. "|" .. (" "):rep(width / 2 - math.floor(#title / 2)) .. title .. (" "):rep(math.floor(width / 2) - #title + math.floor(#title / 2)) .. "|\n"
+	out = out .. "|" .. ("_"):rep(width - 1) .. "|\n"
+
+	for k,v in ipairs(columns) do 
+		out = out .. "| " .. v.friendly .. ": " .. (" "):rep(-#v.friendly + max_lengths[v.key] - 1)  -- 2 = : + |
+	end 
+	out = out .. "|\n"
+	
+	
+	for k,v in ipairs(columns) do 
+		out = out .. "|" .. ("_"):rep(v.length + 2) 
+	end 
+	out = out .. "|\n"
+	
+	for k,v in ipairs(top) do 
+		for _,column in ipairs(columns) do 
+			out = out .. "| " .. v.str[column.key] .. (" "):rep(-#v.str[column.key] + column.length + 1) 
+		end 
+		out = out .. "|\n"
+	end 
+	
+	out = out .. "|"
+	
+	out = out .. ("_"):rep(width-1) .. "|\n"
+
+
+	return out
+end
+
 do -- tree
 	local META = metatable.CreateTemplate("tree")
 
@@ -112,21 +199,6 @@ do -- tree
 		self.list = list
 		
 		return self
-	end
-
-	if RELOAD then
-		local tree = utilities.CreateTree("/", true)
-
-		tree:SetEntry({"hello"}, "LOL")
-		tree:SetEntry("hello/world/lol.txt", "wwww")
-		tree:SetEntry("hello/world/lsdaWl.txt", "asdf")
-		print(tree:GetEntry("hello/world/lol.txt"))
-		table.print(tree:GetChildren("hello/world"))
-		table.print(tree:GetChildren("/"))
-		table.print(tree:GetChildren())
-		table.print(tree:GetChildren("hello/"))
-		
-		table.print(tree.tree)
 	end
 end
 
