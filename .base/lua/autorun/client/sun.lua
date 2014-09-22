333a301112dd5d1879e5be43083e05f69f9d5b02
CapsAdmin <eliashogstvedt@gmail.com>
2014-09-22 04:49:05 +0200

    brighter sun

--- .base/lua/autorun/client/sun.lua
+++ .base/lua/autorun/client/sun.lua
@@ -1,11 +1,12 @@
-if SUN then return end
+event.Delay(0.1, function()
 
-event.Delay(1, function()
+	if SUN and SUN:IsValid() then SUN:Remove() end  
 
-local sun = entities.CreateEntity("light")
-sun:SetPosition(Vec3(-284.77694702148, -271.65432739258, 244.03981018066)) 
-sun:SetColor(Color(1,1,0.9)) 
-sun:SetSize(1000)
-sun:SetDiffuseIntensity(0.75)
-SUN = sun
+	local sun = entities.CreateEntity("light")
+	sun:SetPosition(Vec3(-284.77694702148, -271.65432739258, 244.03981018066)) 
+	sun:SetColor(Color(1,1,1)) 
+	sun:SetSize(1000)
+	sun:SetDiffuseIntensity(2)
+	
+	SUN = sun
 end)
