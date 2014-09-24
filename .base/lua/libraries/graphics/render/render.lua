1f84156e9ce00492c8ef24ccc5b80a29b30e418c
CapsAdmin <eliashogstvedt@gmail.com>
2014-09-24 03:45:46 +0200

    don't use gl.SwapIntervalEXT on nvidia x64 windows due to freezes
    Former-commit-id: f15ffed3482102ceaa19d0fc7cd9dd8b6bee1f3d

--- .base/lua/libraries/graphics/render/render.lua
+++ .base/lua/libraries/graphics/render/render.lua
@@ -168,7 +168,7 @@ do
 	local vsync = 0
 	
 	function render.SetVSync(b)
-		if gl.SwapIntervalEXT then
+		if gl.SwapIntervalEXT and not (NVIDIA and X64 and WINDOWS) then
 			gl.SwapIntervalEXT(b == true and 1 or b == "adaptive" and -1 or 0)
 			vsync = b
 		elseif window and window.IsOpen() then
