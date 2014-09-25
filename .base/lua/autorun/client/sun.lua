f2599b655acd84f14d5c781c46e9aaea656ec993
CapsAdmin <eliashogstvedt@gmail.com>
2014-09-25 17:11:01 +0200

    tweaked sun values

--- .base/lua/autorun/client/sun.lua
+++ .base/lua/autorun/client/sun.lua
@@ -2,11 +2,19 @@ event.Delay(0.1, function()
 
 	if SUN and SUN:IsValid() then SUN:Remove() end  
 
-	local sun = entities.CreateEntity("light")
-	sun:SetPosition(Vec3(-284.77694702148, -271.65432739258, 244.03981018066)) 
-	sun:SetColor(Color(1,1,1)) 
-	sun:SetSize(1000)
-	sun:SetDiffuseIntensity(2)
+	local size = 2000
 	
-	SUN = sun
-end)
+	local sun = entities.CreateEntity("light")
+	sun:SetPosition(Vec3(-size/2, -size/2, size)) 
+	sun:SetAngles(Ang3(44.95, 44.8, 0)) 
+	sun:SetColor(Color(1,1,0.9)) 
+	sun:SetSize(size)
+	sun:SetDiffuseIntensity(1.75)
+	sun:SetSpecularIntensity(0.1)
+	
+	sun:SetShadow(false) -- doesn't work yet
+	sun:SetFOV(100)
+	sun:SetNearZ(0.1)
+
+	SUN = sun 
+end) 
