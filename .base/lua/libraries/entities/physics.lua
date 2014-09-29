8b2e9ecca9444d05250f44bfc78e546a548c3d1a
CapsAdmin <eliashogstvedt@gmail.com>
2014-09-29 12:53:41 +0200

    moved bullet code to physics library

---
+++ .base/lua/libraries/entities/physics.lua
@@ -0,0 +1,89 @@
+local physics = physics or {}
+
+local bullet = require("lj-bullet3")
+
+if not physics.bullet then
+	bullet.Initialize()
+	bullet.SetGravity(0,0,9.8) 		 
+
+	event.AddListener("Update", "bullet", function(dt)
+		bullet.Update(dt)
+	end)
+	
+	function bullet.OnCollision(body_a, body_b)
+		event.Call("PhysicsCollide", body_a.ent, body_b.ent)
+	end
+
+	physics.bullet = bullet
+end
+
+function physics.RayCast(a, b)
+	local res = bullet.RayCast(a.x, a.y, a.z, b.x, b.y, b.z)
+	
+	if res then 
+		res.body = res.body and res.body.ent or NULL 
+		res.hit_normal = Vec3(res.hit_normal[0], res.hit_normal[1], res.hit_normal[3])
+		res.hit_pos = Vec3(res.hit_pos[0], res.hit_pos[1], res.hit_pos[3])
+	end
+	
+	return res
+end
+
+function physics.SetGravity(vec)
+	bullet.SetGravity(vec:Unpack())
+end
+
+local assimp = require("lj-assimp")
+
+function physics.GetPhysicsModelsFromPath(path)
+	local meshes = {}
+	
+	if vfs.Exists(path) then
+		
+		local scene = assimp.ImportFile(R(path), assimp.e.aiProcessPreset_TargetRealtime_Quality)
+		
+		for i = 0, scene.mNumMeshes - 1 do
+			
+			if scene.mMeshes[i].mNumVertices == 0 then
+				logn("no vertices found in " .. path, 2)
+			else
+									
+				local vertices = ffi.new("float[?]", scene.mMeshes[i].mNumVertices  * 3)
+				local triangles = ffi.new("unsigned int[?]", scene.mMeshes[i].mNumFaces * 3)
+				
+				ffi.copy(vertices, scene.mMeshes[i].mVertices, ffi.sizeof(vertices))
+
+				local j = 0
+				for k = 0, scene.mMeshes[i].mNumFaces - 1 do
+					for l = 0, scene.mMeshes[i].mFaces[k].mNumIndices - 1 do
+						triangles[j] = scene.mMeshes[i].mFaces[k].mIndices[l]
+						j = j + 1 
+					end
+				end
+							
+				local mesh = {	
+					triangles = {
+						count = tonumber(scene.mMeshes[i].mNumFaces), 
+						pointer = triangles, 
+						stride = ffi.sizeof("unsigned int") * 3, 
+					},					
+					vertices = {
+						count = tonumber(scene.mMeshes[i].mNumVertices),  
+						pointer = vertices, 
+						stride = ffi.sizeof("float") * 3,
+					},
+				}
+				
+				meshes[i + 1] = mesh
+			end
+		end
+		
+		assimp.ReleaseImport(scene)
+		
+		return meshes
+	end
+	
+	return nil, "file does not exist"
+end
+
+return physics
