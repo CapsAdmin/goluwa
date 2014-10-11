dd3c07685d9c77c3b8a05d0353232fa4b3269f25
CapsAdmin <eliashogstvedt@gmail.com>
2014-10-11 03:10:49 +0200

    use assert
    Former-commit-id: 2457e5bea25be9002ede0bf86b0430438fb5b5db

--- .base/lua/libraries/audio/decoders/ffmpeg.lua
+++ .base/lua/libraries/audio/decoders/ffmpeg.lua
@@ -1,4 +1,3 @@
-do return end
 local ffmpeg = require("lj-ffmpeg")
 
 audio.AddDecoder("ffmpeg", function(data, path_hint)
@@ -7,10 +6,9 @@ audio.AddDecoder("ffmpeg", function(data, path_hint)
 	if ext == "wav" and data:sub(1, 4) ~= "RIFF" then
 		ext = "mp3" -- VALVE
 	end
+		
+	local decoder = assert(ffmpeg.Open(data, {audio_only = true, file_ext = ext}))
+	local data = assert(decoder:ReadAll())
 	
-	local decoder, err = ffmpeg.Open(data, {audio_only = true, file_ext = ext})
-	if not decoder then return nil, err end
-	local data, err = decoder:ReadAll()
-	if not data then return nil, err end
 	return data.buffer, data.length, decoder:GetInfo().audio
 end)
