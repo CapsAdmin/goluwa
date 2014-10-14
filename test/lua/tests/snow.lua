66b320878d90b7a23eab02e78ee72a5d8b308aaa
CapsAdmin <eliashogstvedt@gmail.com>
2014-10-14 05:02:44 +0200

    refactoring

--- test/lua/tests/snow.lua
+++ test/lua/tests/snow.lua
@@ -103,153 +103,172 @@ local skin = {
 	end),
 } 
 
-local function create_frame()
-	local frame = gui2.CreatePanel()
-	frame:SetDraggable(true)
-	frame:SetResizable(true) 
-	frame:SetMargin(Rect(4,10*scale+4,4,4))  
-	frame:SetupNinepatch(skin.frame, ninepatch_size, ninepatch_corner_size)
-		
-		local bar = gui2.CreatePanel(frame)
-		bar:SetObeyMargin(false)
-		bar:Dock("fill_top") 
-		bar:SetSendMouseInputToParent(true)
-		bar:SetHeight(10*scale)
-		bar:SetTexture(skin.gradient)
-		bar:SetColor(Color(120, 120, 160))
-		bar:SetClipping(true)
-				
-			local text = gui2.CreatePanel(bar)
-			text:SetHeight(bar:GetHeight())
-			text:SetParseTags(true)  
-			text:SetText("<font=snow_font><color=200,200,200>about")
-			text:CenterTextY()
-			text:SetPosition(Vec2(2*scale,0))
-			text:SetColor(Color(0,0,0,0))
-			
-			local close = gui2.CreatePanel(bar) 			
-			close:SetParseTags(true)  
-			close:SetText("<font=snow_font_noshadow><color=50,50,50>X")
-			close:SetSize(close:GetTextSize()+Vec2(3,3)*scale)
-			close:CenterText()
-			
-			close:Dock("top_right")
-			
-			close:SetupNinepatch(skin.button_rounded, ninepatch_size, ninepatch_corner_size)
-			
-			close.OnMouseInput = function(self, button, press) 
-				if button == "button_1" and not press then
-					frame:Remove()
-				end
-			end 
-	
-	frame:SetMinimumSize(Vec2(bar:GetHeight(), bar:GetHeight()))
-	
-	return frame
-end 
-
 do
-	local scroll_width = scale*8
+	local META = {}
+	META.ClassName = "frame"
 	
-	local frame = create_frame()  
-	
-	frame:SetPosition(Vec2(100, 100) )
-	frame:SetSize(Vec2(300, 300))
-	
-	local panel = gui2.CreatePanel(frame)
-	panel:Dock("fill") 
-	panel:SetColor(Color(0,0,0,0))
+	function META:Initialize()	
+		local frame = self
+		frame:SetDraggable(true)
+		frame:SetResizable(true) 
+		frame:SetMargin(Rect(4,10*scale+4,4,4))  
+		frame:SetupNinepatch(skin.frame, ninepatch_size, ninepatch_corner_size)
+			
+			local bar = gui2.CreatePanel("base", frame)
+			bar:SetObeyMargin(false)
+			bar:Dock("fill_top") 
+			bar:SetSendMouseInputToParent(true)
+			bar:SetHeight(10*scale)
+			bar:SetTexture(skin.gradient)
+			bar:SetColor(Color(120, 120, 160))
+			bar:SetClipping(true)
+					
+				local text = gui2.CreatePanel("base", bar)
+				text:SetHeight(bar:GetHeight())
+				text:SetParseTags(true)  
+				text:SetText("<font=snow_font><color=200,200,200>about")
+				text:CenterTextY()
+				text:SetPosition(Vec2(2*scale,0))
+				text:SetColor(Color(0,0,0,0))
+				
+				local close = gui2.CreatePanel("base", bar) 			
+				close:SetParseTags(true)  
+				close:SetText("<font=snow_font_noshadow><color=50,50,50>X")
+				close:SetSize(close:GetTextSize()+Vec2(3,3)*scale)
+				close:CenterText()
+				
+				close:Dock("top_right")
+				
+				close:SetupNinepatch(skin.button_rounded, ninepatch_size, ninepatch_corner_size)
+				
+				close.OnMouseInput = function(self, button, press) 
+					if button == "button_1" and not press then
+						frame:Remove()
+					end
+				end 
 		
-		local list = gui2.CreatePanel(panel)
+		frame:SetMinimumSize(Vec2(bar:GetHeight(), bar:GetHeight()))
+	end
+	
+	gui2.RegisterPanel(META)   
+end 
+	
+do	
+	local scroll_width = scale*8 
+
+	local META = {}
+	META.ClassName = "list"
+	
+	META.entries = {}
+	
+	function META:Initialize()		
+		local list = gui2.CreatePanel("base", self)
 		list:SetColor(Color(0,0,0,1))
 		list:SetClipping(true)
 		list:SetScrollable(true)
 			
-			local y_scroll = gui2.CreatePanel(panel)
-			y_scroll:SetTexture(skin.gradient2)
+		local y_scroll = gui2.CreatePanel("base", self)
+		y_scroll:SetTexture(skin.gradient2)
+
+		local up = gui2.CreatePanel("base", y_scroll)				
+		up:SetupNinepatch(skin.button_rounded, ninepatch_size, ninepatch_corner_size)
+
+		local scroll_bar = gui2.CreatePanel("base", y_scroll)
+		scroll_bar:SetDraggable(true)	
+		scroll_bar:SetupNinepatch(skin.button_inactive, ninepatch_size, ninepatch_corner_size)
+
+		local down = gui2.CreatePanel("base", y_scroll)
+		down:SetupNinepatch(skin.button_rounded, ninepatch_size, ninepatch_corner_size)
 				
-				local up = gui2.CreatePanel(y_scroll)				
-				up:SetupNinepatch(skin.button_rounded, ninepatch_size, ninepatch_corner_size)
-				
-				local scroll_bar = gui2.CreatePanel(y_scroll)
-				scroll_bar:SetDraggable(true)	
-				scroll_bar:SetupNinepatch(skin.button_inactive, ninepatch_size, ninepatch_corner_size)
-				
-				local down = gui2.CreatePanel(y_scroll)
-				down:SetupNinepatch(skin.button_rounded, ninepatch_size, ninepatch_corner_size)
 		
-	function panel:OnLayout()	
-		list:SetSize(panel:GetSize()*1) 
-		list:SetWidth(panel:GetWidth() - scroll_width)
-		
-		y_scroll:SetSize(Vec2(scroll_width, panel:GetHeight()))
-		y_scroll:SetPosition(Vec2(panel:GetWidth() - scroll_width, 0))
-		
-		down:SetSize(Vec2(scroll_width, scroll_width))
-		down:SetPosition(Vec2(0, panel:GetHeight() - down:GetHeight()))
-		
-		scroll_bar:SetSize(Vec2(scroll_width, scroll_width))
-		
-		up:SetSize(Vec2(scroll_width, scroll_width))		
-	end
-	
-	local lol
-	
-	scroll_bar.OnPositionChanged = function(self, pos)
-		if not lol then
+		scroll_bar.OnPositionChanged = function(self, pos)
 			local frac = math.clamp(pos.y / self.Parent:GetHeight(), 0, 1)
 			list:SetScrollFraction(Vec2(list:GetScrollFraction().x, frac))
+	 
+			pos.x = self.Parent:GetWidth() - self:GetWidth()
+			pos.y = math.clamp(pos.y, self:GetHeight(), self.Parent:GetHeight() - (self:GetHeight() * 2))
 		end
- 
-		pos.x = self.Parent:GetWidth() - self:GetWidth()
-		pos.y = math.clamp(pos.y, self:GetHeight(), self.Parent:GetHeight() - (self:GetHeight() * 2))
+		
+		list.OnScroll = function(self, frac)
+			scroll_bar:SetPosition(Vec2(0,frac.y * self.Parent:GetHeight()))
+		end
+		
+		up.OnMouseInput = function(self, button, press)
+			if button ~= "button_1" or not press then return end
+			if #list:GetChildren() == 0 then return end
+			
+			local h = list:GetChildren()[1]:GetHeight()
+			
+			local pos = list:GetScroll()
+			pos.y = pos.y - h
+			list:SetScroll(pos)
+		end
+		
+		down.OnMouseInput = function(self, button, press)
+			if button ~= "button_1" or not press then return end
+			if #list:GetChildren() == 0 then return end
+			
+			local h = list:GetChildren()[1]:GetHeight()
+			
+			local pos = list:GetScroll()
+			pos.y = pos.y + h
+			list:SetScroll(pos)
+		end
+		
+		self.list = list
+		self.down = down
+		self.up = up
+		self.scroll_bar = scroll_bar
+		self.y_scroll = y_scroll
 	end
 	
-	list.OnScroll = function(self, frac)
-		lol = true
-		scroll_bar:SetPosition(Vec2(0,frac.y * self.Parent:GetHeight()))
-		lol = false
+	function META:OnLayout()
+		local w = 0
+		
+		local y = 0
+		
+		for k, v in pairs(self.entries) do
+			v:SetPosition(Vec2(0, y))
+			y = y + v:GetHeight() + 2*scale
+			w = math.max(w, v:GetTextSize().w)
+		end
+		
+		for k,v in ipairs(self.list:GetChildren()) do
+			v:SetWidth(w)
+		end
+	
+		self.list:SetSize(self:GetSize()*1) 
+		self.list:SetWidth(self:GetWidth() - scroll_width)
+		
+		self.y_scroll:SetSize(Vec2(scroll_width, self:GetHeight()))
+		self.y_scroll:SetPosition(Vec2(self:GetWidth() - scroll_width, 0))
+		
+		self.down:SetSize(Vec2(scroll_width, scroll_width))
+		self.down:SetPosition(Vec2(0, self:GetHeight() - self.down:GetHeight()))
+		
+		self.scroll_bar:SetSize(Vec2(scroll_width, scroll_width))
+		
+		self.up:SetSize(Vec2(scroll_width, scroll_width))
+	
+		self.scroll_bar:SetHeight(math.max((self.list:GetHeight() / self.list:GetSizeOfChildren().h) * self.list:GetHeight(), scroll_width))
+		
+		if self.list:GetHeight() > self.list:GetSizeOfChildren().h then
+			self.scroll_bar:SetWidth(0)
+		end
 	end
 	
-	up.OnMouseInput = function(self, button, press)
-		if button ~= "button_1" or not press then return end
-		if #list:GetChildren() == 0 then return end
-		
-		local h = list:GetChildren()[1]:GetHeight()
-		
-		local pos = list:GetScroll()
-		pos.y = pos.y - h
-		list:SetScroll(pos)
-	end
-	
-	down.OnMouseInput = function(self, button, press)
-		if button ~= "button_1" or not press then return end
-		if #list:GetChildren() == 0 then return end
-		
-		local h = list:GetChildren()[1]:GetHeight()
-		
-		local pos = list:GetScroll()
-		pos.y = pos.y + h
-		list:SetScroll(pos)
-	end
-	
-	scroll_bar:SetPosition(Vec2(0, 0))
-	
-	local w = 0
-	local y = 0
-	
-	for _, file in ipairs(vfs.Find("/")) do
-		local button = gui2.CreatePanel(list)
+	function META:AddEntry(name, on_click)		
+		local button = gui2.CreatePanel("base", self.list)
 		button:SetSendMouseInputToParent(true)
 		button:SetParseTags(true)
-		button:SetText("<font=snow_font_green><color=0,255,0>" .. file)
+		button:SetText("<font=snow_font_green><color=0,255,0>" .. name)
 		button:SetWrapText(false)
 		button:SetSize(button:GetTextSize() + Vec2(4,4) * scale)
 		button:CenterTextY()
-		button:SetPosition(Vec2(0, y))
 		
-		w = math.max(w, button:GetTextSize().w)
-		
+		local last_child = self:GetChildren()[#self:GetChildren()]
+		button:SetPosition(Vec2(0, last_child:GetPosition().y + last_child:GetHeight() - 2*scale))
+			
 		button:SetColor(Color(0,0,0,0))
 		button:SetupNinepatch(skin.menu_select, ninepatch_size, ninepatch_corner_size)
 
@@ -262,134 +281,165 @@ do
 							v:SetColor(Color(0,0,0,0))
 						end
 					end
+					if on_click then on_click(button) end
 				end
 			end
 		end
 		
-		y = y + button:GetSize().h - 2*scale
-	end
+		table.insert(self.entries, button)
+	end	
 	
-	for k,v in ipairs(list:GetChildren()) do
-		if v ~= self then
-			v:SetWidth(w)
-		end
-	end
-	
-	scroll_bar:SetHeight(math.max((list:GetHeight() / list:GetSizeOfChildren().h) * list:GetHeight(), scroll_width))
-	
-	if list:GetHeight() > list:GetSizeOfChildren().h then
-		scroll_bar:SetWidth(0)
-	end
-	
-	panel:InvalidateLayout()
+	gui2.RegisterPanel(META) 
 end
-local function create_menu(pos, options, parent_menu)
-	local frame = gui2.CreatePanel()
-	frame:SetupNinepatch(skin.button_inactive, ninepatch_size, ninepatch_corner_size)
-	frame:SetPosition(pos)
-	
-	if parent_menu then
-		parent_menu:CallOnRemove(function() if frame:IsValid() then frame:Remove() end end)
-	else
-		gui2.SetActiveMenu(frame)
-	end
-	 
-	local y = 2*scale
-	local w = 0
-	
-	for i, option in ipairs(options) do
+ 
+do
+	do
+		local META = {}
+		META.ClassName = "menu"
+		META.sub_menu = NULL
 		
-		local text, callback = option[1], option[2] or print
-		
-		if text then 
-			local button = gui2.CreatePanel(frame)
-			--button:SetColor(Color(88, 92, 88))
-			--button:SetClipping(true)
-			button:SetParseTags(true)  
-			button:SetText("<font=snow_font><color=200,200,200>" .. text)
-			button:SetHeight(button:GetTextSize().h + 4*scale)
-			button:CenterTextY()
-			button:SetPosition(Vec2(2*scale, y))			
-			button:SetColor(Color(0,0,0,0))			
-			button:SetupNinepatch(skin.menu_select, ninepatch_size, ninepatch_corner_size)			
-			button:SetMargin(Rect(0,0,0,0))
-			
-			if type(callback) == "table" then
-				local icon = gui2.CreatePanel(button)
-				icon:SetIgnoreMouse(true)
-				icon:SetPadding(Rect(0,0,scale*2,0))
-				icon:Dock("right")
-				icon:SetColor(Color(0,0,0,0))
-				icon:SetParseTags(true) 
-				icon:SetText("<font=snow_font><color=200,200,200>▶")
-				icon:SetSize(icon:GetTextSize())
-
-			end
-			
-			w = math.max(w, button:GetTextSize().w)
-			 
-			button.OnMouseEnter = function()
-				button:SetColor(Color(1,1,1,1))
-				
-				if frame.sub_menu and frame.sub_menu:IsValid() and frame.sub_menu.button ~= button then
-					frame.sub_menu:Remove() 
-				end
-				
-				if type(callback) == "table" then
-					if not frame.sub_menu or not frame.sub_menu:IsValid() then
-						frame.sub_menu = create_menu(button:GetWorldPosition() + Vec2(button:GetWidth() + scale*2, 0), callback, frame)
-						frame.sub_menu.button = button
-					end
-				end
-			end
-			button.OnMouseExit = function()
-				if frame.sub_menu and frame.sub_menu:IsValid() then
-					if frame.sub_menu.button ~= button then
-						frame.sub_menu:Remove() 
-					end
-				end
-				button:SetColor(Color(1,1,1,0))
-			end
-			
-			button.OnMouseInput = function(self, button, press)
-				if button == "button_1" and press then
-					create_frame()
-				end
-			end
-			
-			--button:CenterText() 
-			y = y + button:GetSize().h + scale
-		else
-			local button = gui2.CreatePanel(frame)
-			button:SetPosition(Vec2(4, y + scale*4))
-			button:SetSize(Vec2(frame:GetWidth() - 8, 6))	
-			
-			button:SetupNinepatch(skin.button_active, ninepatch_size, ninepatch_corner_size)
-			y = y + 8*scale
+		function META:Initialize()
+			self:SetupNinepatch(skin.button_inactive, ninepatch_size, ninepatch_corner_size)
 		end
+		
+		function META:AddEntry(text, on_click)
+			local entry = gui2.CreatePanel("menu_entry", self)
+			
+			entry:SetText(text)			
+			entry.OnClick = on_click
+			
+			return entry
+		end
+		
+		function META:AddSubMenu(text, on_click)
+			local menu = self:AddEntry(text, on_click):CreateSubMenu()
+			
+			self:CallOnRemove(function() gui2.RemovePanel(menu) end)
+			self:CallOnHide(function() menu:SetVisible(false) end)
+			
+			return menu  
+		end
+		
+		function META:AddSeparator()
+			local panel = gui2.CreatePanel("base", self)
+			panel:SetupNinepatch(skin.button_active, ninepatch_size, ninepatch_corner_size)
+			panel:SetHeight(2*scale)
+			panel:SetIgnoreMouse(true)
+		end
+		
+		function META:OnLayout()
+			local w = 0
+			local y = 0
+			
+			for k, v in ipairs(self:GetChildren()) do
+				v:SetPosition(Vec2(2*scale, y))
+				w = math.max(w, v:GetTextSize().w)
+				y = y + v:GetSize().h + scale
+			end
+			
+			for k, v in ipairs(self:GetChildren()) do
+				v:SetWidth(w + 4*scale)
+			end
+			
+			self:SetSize(Vec2(w + 8*scale, y))
+		end
+		
+		gui2.RegisterPanel(META)
 	end
+
+	do
+		local META = {}
+		
+		META.ClassName = "menu_entry"
+		META.menu = NULL
+		
+		function META:Initialize()
+			self:SetParseTags(true) 			
+			self:SetColor(Color(0,0,0,0))			
+			self:SetupNinepatch(skin.menu_select, ninepatch_size, ninepatch_corner_size)			
+		end
+		
+		function META:OnMouseEnter()
+			self:SetColor(Color(1,1,1,1))
+			
+			-- close all parent menus
+			for k,v in ipairs(self.Parent:GetChildren()) do
+				if v ~= self and v.ClassName == "menu_entry" and v.menu and v.menu:IsValid() and v.menu.ClassName == "menu" then
+					v.menu:SetVisible(false)
+				end
+			end
+			
+			if self.menu:IsValid() then				
+				self.menu:SetPosition(self:GetWorldPosition() + Vec2(self:GetWidth() + scale*2, 0))
+				self.menu:SetVisible(true)							
+				self.menu:Layout()
+			end
+		end
+		
+		function META:OnMouseExit()
+			self:SetColor(Color(1,1,1,0))
+		end
+		
+		function META:SetText(str)
+			self.BaseClass.SetText(self, "<font=snow_font><color=200,200,200>" .. str)
+			self:SetHeight(self:GetTextSize().h + 4*scale)
+			self:CenterTextY()
+		end
+		
+		function META:CreateSubMenu()			
+		
+			local icon = gui2.CreatePanel("base", self)
+			icon:Dock("right")
+			icon:SetIgnoreMouse(true)
+			icon:SetPadding(Rect(0,0,scale*2,0))
+			icon:SetColor(Color(0,0,0,0))
+			icon:SetParseTags(true) 
+			icon:SetText("<font=snow_font><color=200,200,200>▶")
+			icon:SetSize(icon:GetTextSize())
+
+			self.menu = gui2.CreatePanel("menu")
+			self.menu:SetVisible(false)
+			
+			return self.menu
+		end
+				
+		function META:OnMouseInput(button, press)
+			if button == "button_1" and press then
+				self:OnClick()
+			end
+		end
+		
+		function META:OnClick() gui2.SetActiveMenu() end 
+		
+		gui2.RegisterPanel(META)
+	end
+end
+
+do	
+	local frame = gui2.CreatePanel("frame") 
 	
-	for i, panel in ipairs(frame:GetChildren()) do
-		panel:SetWidth(w + 4*scale)
-	end 
-	 
-	frame:SetSize(Vec2(w + 8*scale, y + 2*scale))
+	frame:SetPosition(Vec2(100, 100) )
+	frame:SetSize(Vec2(300, 300))
 	
-	return frame
+	local panel = gui2.CreatePanel("list", frame)
+	panel:Dock("fill") 
+	for k,v in pairs(vfs.Find("/")) do
+		panel:AddEntry(v)
+	end
 end
 
 local padding = 5 * scale
 local x = padding
 local y = 3 * scale
 
-local bar = gui2.CreatePanel() 
+local bar = gui2.CreatePanel("base") 
 bar:SetTexture(skin.gradient)
 bar:SetColor(Color(0,72,248))
 bar:SetDraggable(true)
 bar:SetResizable(true)
 
 local function create_button(text, options)
-	local button = gui2.CreatePanel(bar)
+	local button = gui2.CreatePanel("base", bar)
 	--button:SetColor(Color(88, 92, 88))
 	button:SetPosition(Vec2(x, y))
 	button:SetClipping(true)
@@ -404,7 +454,24 @@ local function create_button(text, options)
 		if press then
 			self:SetTexture(skin.button_active)
 			
-			local menu = create_menu(self:GetWorldPosition() + Vec2(0, self:GetHeight() + 2*scale), options)
+			local menu = gui2.CreatePanel("menu")
+			gui2.SetActiveMenu(menu)
+			menu:SetPosition(self:GetWorldPosition() + Vec2(0, self:GetHeight() + 2*scale), options)
+			
+			local function add_entry(menu, val)
+				for k, v in ipairs(val) do
+					if type(v[2]) == "table" then
+						add_entry(menu:AddSubMenu(v[1]), v[2])
+					elseif v[1] then
+						menu:AddEntry(v[1], v[2])
+					else
+						menu:AddSeparator()
+					end
+				end
+			end
+			
+			add_entry(menu, options)			
+			
 			menu:CallOnRemove(function() self:SetTexture(skin.button_inactive) end)
 		end
 	end
@@ -488,8 +555,6 @@ create_button("misc", {
 
 bar:SetSize(Vec2(x, 16 * scale))
 
-do return end
-
 local emitter = ParticleEmitter(800)
 emitter:SetPos(Vec3(50,50,0))
 --emitter:SetMoveResolution(0.25)  
@@ -502,7 +567,7 @@ if DX then
 	fb = render.CreateFrameBuffer(128, 128)
 end
 
-event.AddListener("PreDrawMenu", "zsnow", function(dt)
+event.AddListener("Draw2D", "zsnow", function(dt)
 	if DX then
 		fb:Begin()
 			surface.SetColor(0,0,0,0.01)
@@ -550,7 +615,7 @@ event.AddListener("PreDrawMenu", "zsnow", function(dt)
 	else
 		emitter:Draw()
 	end
-end) 
+end, {priority = math.huge}) 
 
 event.CreateTimer("zsnow", 0.01, function()
 	emitter:SetPos(Vec3(math.random((DX and 256 or render.GetWidth()) + 100) - 150, -50, 0))
