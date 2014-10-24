a79cb68c95898b20489c327591e2f0a9a584bcaa
CapsAdmin <eliashogstvedt@gmail.com>
2014-10-24 18:41:51 +0200

    use font shading

--- test/lua/tests/surface/blur_font.lua
+++ test/lua/tests/surface/blur_font.lua
@@ -1,23 +1,55 @@
-﻿local font = surface.CreateFont("lol", {
+﻿local source = [[
+	vec4 sum = vec4(0.0);
+	
+	vec2 blur = radius/size;
+
+	sum += texture(self, vec2(uv.x - 4.0*blur.x*dir.x, uv.y - 4.0*blur.y*dir.y)) * 0.0162162162;
+	sum += texture(self, vec2(uv.x - 3.0*blur.x*dir.x, uv.y - 3.0*blur.y*dir.y)) * 0.0540540541;
+	sum += texture(self, vec2(uv.x - 2.0*blur.x*dir.x, uv.y - 2.0*blur.y*dir.y)) * 0.1216216216;
+	sum += texture(self, vec2(uv.x - 1.0*blur.x*dir.x, uv.y - 1.0*blur.y*dir.y)) * 0.1945945946;
+
+//	sum += texture(self, vec2(uv.x, uv.y)) * 0.2270270270;
+
+	sum += texture(self, vec2(uv.x + 1.0*blur.x*dir.x, uv.y + 1.0*blur.y*dir.y)) * 0.1945945946;
+	sum += texture(self, vec2(uv.x + 2.0*blur.x*dir.x, uv.y + 2.0*blur.y*dir.y)) * 0.1216216216;
+	sum += texture(self, vec2(uv.x + 3.0*blur.x*dir.x, uv.y + 3.0*blur.y*dir.y)) * 0.0540540541;
+	sum += texture(self, vec2(uv.x + 4.0*blur.x*dir.x, uv.y + 4.0*blur.y*dir.y)) * 0.0162162162;
+
+	sum = pow(sum, vec4(0.5));
+	sum -= texture(self, uv);
+			
+	return sum;
+]]
+
+local radius = 0.5
+
+local font = surface.CreateFont("lol", {
 	path = "Roboto",
-	size = 50,
-	blur = {
-		size = 0.75,
-		step_size = 0.6,
-		alpha = 1,
-		color = Color(0.5, 0.5, 0.5),
+	size = 20, 
+	padding = 8, 
+	shade = {
+		{source = source, vars = {dir = Vec2(0,1), radius = radius*2}},
+		{source = source, vars = {dir = Vec2(1,0), radius = radius*2}},
+		{source = source, vars = {dir = Vec2(0,1), radius = radius}},
+		{source = source, vars = {dir = Vec2(1,0), radius = radius}},
 	},
-})   
-   
+})
+
 event.AddListener("DrawHUD", "lol", function()
 	local w, h = surface.GetScreenSize()
-	surface.SetColor(0.6,1,1,1)
-	surface.SetWhiteTexture()
-	surface.DrawRect(0,0,w*0.6,h*0.1)
 	
 	surface.SetColor(1,1,1,1)  
 
 	surface.SetFont("lol")
-	surface.SetTextPos(10, 10)
-	surface.DrawText("no yes that2" .. math.random(50))
+	surface.SetTextPos(350, 350)
+	surface.DrawText("outline blur text")
+	
+	if surface.fonts.lol.texture_atlas then
+		surface.fonts.lol.texture_atlas:DebugDraw() 
+	end
+	
+do return end	
+	surface.SetWhiteTexture()
+	surface.SetColor(1,0,0,0.5)
+	surface.DrawRect(350, 350, surface.GetTextSize("outline blur text"))
 end)
