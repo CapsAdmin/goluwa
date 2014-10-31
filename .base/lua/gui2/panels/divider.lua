5af15adc153695c1b569309923fbc9116484e57a
CapsAdmin <eliashogstvedt@gmail.com>
2014-10-31 23:04:17 +0100

    added a way to make the divider invisible but still draggable
    Former-commit-id: ec74cfd84ab47401ed400e4d49ec2e8fd9d5104c

--- .base/lua/gui2/panels/divider.lua
+++ .base/lua/gui2/panels/divider.lua
@@ -7,6 +7,7 @@ PANEL.ClassName = "divider"
 
 prototype.GetSet(PANEL, "DividerHeight", 0)
 prototype.GetSet(PANEL, "DividerWidth", S*2)
+prototype.GetSet(PANEL, "HideDivider", false)
 
 local function create_horizontal_divider(self)
 	if self.horizontal_divider then return end
@@ -46,28 +47,36 @@ end
 
 function PANEL:OnLayout()
 	if self.horizontal_divider then
+		self.horizontal_divider:SetNoDraw(self.HideDivider)
+		self.horizontal_divider:BringToFront()
+	
 		self.horizontal_divider:SetSize(Vec2(self.DividerWidth, self.DividerHeight == 0 and self:GetHeight() or self.DividerHeight))
 		
 		if self.left then
-			self.left:SetSize(Vec2(self.horizontal_divider:GetPosition().x, self:GetHeight()))
+			self.left:SetSize(Vec2(self.horizontal_divider:GetX() + (self.HideDivider and self.DividerWidth or 0), self:GetHeight()))
 		end
 		
 		if self.right then
-			self.right:SetX(self.horizontal_divider:GetPosition().x + self.DividerWidth)
-			self.right:SetSize(Vec2(self:GetWidth() - self.horizontal_divider:GetPosition().x - self.DividerWidth,  self:GetHeight()))
+			local offset = self.HideDivider and 0 or self.DividerWidth
+			self.right:SetX(self.horizontal_divider:GetX() + offset)
+			self.right:SetSize(Vec2(self:GetWidth() - self.horizontal_divider:GetX() + offset,  self:GetHeight()))
 		end
 	end
 	
 	if self.vertical_divider then
+		self.vertical_divider:SetNoDraw(self.HideDivider)
+		self.vertical_divider:BringToFront()
+
 		self.vertical_divider:SetSize(Vec2(self.DividerHeight == 0 and self:GetWidth() or self.DividerHeight, self.DividerWidth))
 		
 		if self.top then
-			self.top:SetSize(Vec2(self:GetWidth(), self.vertical_divider:GetPosition().y))
+			self.top:SetSize(Vec2(self:GetWidth(), self.vertical_divider:GetY() + (self.HideDivider and self.DividerWidth or 0)))
 		end
 		
 		if self.bottom then
-			self.bottom:SetY(self.vertical_divider:GetPosition().y + self.DividerWidth)
-			self.bottom:SetSize(Vec2(self:GetWidth(), self:GetHeight() - self.vertical_divider:GetPosition().y - self.DividerWidth))
+			local offset = self.HideDivider and 0 or self.DividerWidth
+			self.bottom:SetY(self.vertical_divider:GetY() + offset)
+			self.bottom:SetSize(Vec2(self:GetWidth(), self:GetHeight() - self.vertical_divider:GetY() - offset))
 		end
 	end
 end
