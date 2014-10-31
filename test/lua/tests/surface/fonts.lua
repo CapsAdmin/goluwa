f49a4ccc0e51c924529418ae26cb2147acb5b069
CapsAdmin <eliashogstvedt@gmail.com>
2014-10-31 01:45:13 +0100

    updated tests
    Former-commit-id: 4a820f4e2f9fb01b1cf8038f983628f2dd92d6cf

--- test/lua/tests/surface/fonts.lua
+++ test/lua/tests/surface/fonts.lua
@@ -6,6 +6,11 @@ surface.CreateFont("lol", {
 event.AddListener("Draw2D", "lol", function()
 	surface.SetColor(1,1,1,1)
 	surface.SetFont("lol")
-	surface.SetTextPos(17, 0)
+	surface.SetTextPos(17, 30)
 	surface.DrawText("empathize foolish self benefit start off preferred occasions")
-end)                 
+	
+	local w, h = surface.GetTextSize("empathize foolish self benefit start off preferred occasions")
+	surface.SetWhiteTexture()
+	surface.SetColor(1,0,0,0.25)
+	surface.DrawRect(17, 30, w, h)
+end)
