73d160d7845130d8a796a322042e108e6b7ee6a7
CapsAdmin <eliashogstvedt@gmail.com>
2014-11-04 11:19:11 +0100

    added prototype.SetupProperty(info) added prototype.GetStorableVariables(meta) changed callback argument to extra_info

--- .base/lua/libraries/prototype/get_is_set.lua
+++ .base/lua/libraries/prototype/get_is_set.lua
@@ -1,78 +1,99 @@
 local prototype = (...) or _G.prototype
 
 local __store = false
+local __meta
 
-function prototype.StartStorable()
+function prototype.StartStorable(meta)
 	__store = true
+	__meta = meta
 end
 
 function prototype.EndStorable()
 	__store = false
+	__meta = nil
 end
 
-function prototype.GetSet(tbl, name, def, callback)
+function prototype.GetStorableVariables(meta)
+	return meta.storable_variables or {}
+end
 
-	if type(def) == "number" then	
+function prototype.SetupProperty(info)
+	local meta = info.meta or __meta
+	local name = info.name
+	local default = info.default
+	local callback = info.callback
+	local set_name = info.set_name .. name
+	local get_name = info.get_name .. name
+	
+	if type(default) == "number" then	
 		if callback then
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) or def self[callback](self) end
+			meta[set_name] = meta[set_name] or function(self, var) self[name] = tonumber(var) or default self[callback](self) end
 		else
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) or def end
+			meta[set_name] = meta[set_name] or function(self, var) self[name] = tonumber(var) or default end
 		end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tonumber(self[name]) or def end
-	elseif type(def) == "string" then
+		meta[get_name] = meta[get_name] or function(self, var) return tonumber(self[name]) or default end
+	elseif type(default) == "string" then
 		if callback then
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tostring(var) self[callback](self) end
+			meta[set_name] = meta[set_name] or function(self, var) self[name] = tostring(var) self[callback](self) end
 		else
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tostring(var) end
+			meta[set_name] = meta[set_name] or function(self, var) self[name] = tostring(var) end
 		end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) return tostring(self[name]) end
+		meta[get_name] = meta[get_name] or function(self, var) return tostring(self[name]) end
 	else
 		if callback then
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) if var == nil then var = def end self[name] = var self[callback](self) end
+			meta[set_name] = meta[set_name] or function(self, var) if var == nil then var = default end self[name] = var self[callback](self) end
 		else
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) if var == nil then var = def end self[name] = var end
+			meta[set_name] = meta[set_name] or function(self, var) if var == nil then var = default end self[name] = var end
 		end
-		tbl["Get" .. name] = tbl["Get" .. name] or function(self, var) if self[name] ~= nil then return self[name] end return def end
+		meta[get_name] = meta[get_name] or function(self, var) if self[name] ~= nil then return self[name] end return default end
 	end
 
-    tbl[name] = def
+    meta[name] = default
 
 	if __store then
-		tbl.storable_variables = tbl.storable_variables or {}
-		table.insert(tbl.storable_variables, name)
+		meta.storable_variables = meta.storable_variables or {}
+		table.insert(meta.storable_variables, info)
 	end
 end
 
-function prototype.IsSet(tbl, name, def, callback)
+function prototype.GetSet(meta, name, default, extra_info)	
 	
-	if type(def) == "number" then
-		if callback then
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) self[callback](self) end
-		else
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = tonumber(var) end
-		end
-	else
-		if callback then
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = var self[callback](self) end
-		else
-			tbl["Set" .. name] = tbl["Set" .. name] or function(self, var) self[name] = var end
-		end
+	local info = {
+		meta = meta, 
+		name = name, 
+		default = default,
+		set_name = "Set",
+		get_name = "Get",
+	}
+	
+	if extra_info then
+		table.merge(info, extra_info)
 	end
 	
-	tbl["Is" .. name] = tbl["Is" .. name] or function(self, var) if self[name] ~= nil then return self[name] end return def end
-
-    tbl[name] = def
-
-	if __store then
-		tbl.storable_variables = tbl.storable_variables or {}
-		table.insert(tbl.storable_variables, name)
-	end
+	prototype.SetupProperty(info)
 end
 
-function prototype.RemoveField(tbl, name)
-	tbl["Set" .. name] = nil
-    tbl["Get" .. name] = nil
-    tbl["Is" .. name] = nil
+function prototype.IsSet(meta, name, default, extra_info)
+	
+	local info = {
+		meta = meta, 
+		name = name, 
+		default = default,
+		set_name = "Set",
+		get_name = "Is",
+	}
+	
+	if extra_info then
+		table.merge(info, extra_info)
+	end
+	
+	prototype.SetupProperty(info)
+end
 
-    tbl[name] = nil
+function prototype.RemoveField(meta, name)
+	meta["Set" .. name] = nil
+    meta["Get" .. name] = nil
+    meta["Is" .. name] = nil
+
+    meta[name] = nil
 end
