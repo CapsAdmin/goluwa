d7ce2ae673f88f142881c1bfa5c9adb57fc1fc5d
CapsAdmin <eliashogstvedt@gmail.com>
2014-11-10 15:47:25 +0100

    refactoring

--- .base/lua/libraries/network/chat.lua
+++ .base/lua/libraries/network/chat.lua
@@ -100,6 +100,219 @@ if CLIENT then
 		if not chat.IsVisible() then return 0, 0 end
 		return chat.panel:GetPosition()
 	end
+	
+	function chat.GetPanel()
+		if chat.panel:IsValid() then return chat.panel end
+			
+		local frame = gui2.CreatePanel("frame")
+		frame:SetTitle("chatbox")
+		frame:SetSize(Vec2(400, 250))
+		frame:SetPosition(Vec2(20, render.GetHeight()-frame:GetHeight()-20))
+		
+		frame:CallOnRemove(chat.Close)
+		
+		local tab = frame:CreatePanel("tab")
+		tab:Dock("fill")
+		frame.tab = tab
+		
+		local page = tab:AddTab("chat")
+		
+		local S = gui2.skin.scale
+		
+		local edit = frame:CreatePanel("text_edit")
+		edit:SetStretchToPanelWidth(frame)
+		edit:SetHeight(10*S)
+		frame.edit = edit
+		
+		local scroll = page:CreatePanel("scroll")
+		scroll:SetXScrollBar(false)
+		page.scroll = scroll
+
+		local text = scroll:SetPanel(gui2.CreatePanel("text"))
+		text:SetPosition(Vec2()+S*2)
+		text.markup:SetLineWrap(true)
+		text:AddEvent("ChatAddText")
+
+		function text:OnChatAddText(args)
+			self.markup:AddFont(gui2.skin.default_font)
+			self.markup:AddTable(args, true)
+			self.markup:AddTagStopper()
+			self.markup:AddString("\n")
+		end
+		
+		function text:OnLayout()
+			self.markup:SetMaxWidth(self.Parent:GetWidth())
+		end
+		
+		text:Layout()
+	
+		edit:RequestFocus()
+		--edit:SetMultiline(true)
+		
+		local i = 1
+		local history = {}
+		local last_history
+		local found_autocomplete
+		
+		-- autocomplete should be done after keys like space and backspace are pressed
+		-- so we can use the string after modifications
+		edit.OnPostKeyInput = function(self, key, press)
+			if not press then return end
+			
+			local str = self:GetText():trim()
+			
+			local scroll = 0
+			 
+			if key == "tab" then
+				scroll = input.IsKeyDown("left_shift") and -1 or 1
+			end
+			
+			found_autocomplete = autocomplete.Query("chatsounds", str, scroll)
+								
+			if key == "tab" and found_autocomplete then
+				edit:SetText(found_autocomplete[1])
+				return false
+			end
+		end
+		
+		edit.OnPreKeyInput = function(self, key, press)	
+			if not press then return end
+			
+			local str = self:GetText()
+			
+			local ctrl = input.IsKeyDown("left_control") or input.IsKeyDown("right_control")
+			
+			if ctrl or str == "" or str == last_history then
+				local browse = false
+				
+				if key == "up" then
+					i = math.clamp(i + 1, 1, #history)
+					browse = true
+				elseif key == "down" then
+					i = math.clamp(i - 1, 1, #history)
+					browse = true
+				end
+				
+				local found = history[i]
+				if browse and found then
+					edit:SetText(found)
+					edit:SetCaretPosition(Vec2(#found, 0))
+					last_history = found
+				end
+			end
+
+			if key == "enter" and not ctrl or key == "escape" then
+			
+				if key ~= "escape" then
+					i = 0
+					if #str > 0 then
+						chat.Say(str)
+						if history[1] ~= str then
+							table.insert(history, 1, str)
+						end
+					end
+				end
+				
+				chat.Close()
+				
+				return
+			end	
+			
+			event.Call("ChatTextChanged", str)
+		end
+			
+		edit.OnTextChanged = function(self, str)
+			event.Call("ChatTextChanged", str)
+			frame:Layout()
+		end
+		
+		edit.OnPostDraw = function()
+			if found_autocomplete and #found_autocomplete > 0 then
+				autocomplete.DrawFound(0, edit:GetHeight(), found_autocomplete, nil, 2) 
+			end
+		end
+		
+		local page = tab:AddTab("console")	
+		page:SetColor(gui2.skin.font_edit_background)
+		
+		local scroll = page:CreatePanel("scroll")
+		scroll:SetXScrollBar(false)
+		page.scroll = scroll
+
+		local text = scroll:SetPanel(gui2.CreatePanel("text"))
+		text:SetPosition(Vec2()+S*2)
+		text.markup:SetLineWrap(true)
+		text:AddEvent("ConsolePrint")
+
+		function text:OnConsolePrint(str)
+			self.markup:AddFont(gui2.skin.default_font)
+			self.markup:AddString(str, true)
+			self.markup:AddTagStopper()
+		end
+		
+		for i, v in pairs(console.history) do
+			text:OnConsolePrint(v)
+		end
+		
+		function text:OnLayout()
+			self.markup:SetMaxWidth(self.Parent:GetWidth())
+		end
+		
+		text:Layout()
+				
+		local old = tab.OnLayout
+		
+		tab.OnLayout = function()
+			old(tab)
+			
+			local page = tab:GetSelectedPage()
+			local scroll = page.scroll
+			
+			edit:SizeToText()
+			edit:SetHeight(math.max(edit:GetHeight(), S*8))
+			edit:SetWidth(page:GetWidth()-S)
+			edit:SetY(frame:GetHeight() - edit:GetHeight()-S)
+			edit:SetX(S)
+			edit.label.markup:SetMaxWidth(page:GetWidth()-S)
+			
+			scroll:SetHeight(page:GetHeight() - edit:GetHeight())
+			scroll:SetWidth(page:GetWidth())
+			scroll.scroll_area:SetScrollFraction(Vec2(0,1))
+		end
+		
+		chat.panel = frame
+		
+		return frame
+	end
+	
+	local old_mouse_trap
+	
+	function chat.Open()
+		old_mouse_trap = window.GetMouseTrapped()
+		
+		local panel = chat.GetPanel()
+		
+		panel.tab:SelectTab("chat")
+		panel:Minimize(true)
+		panel.edit:SetText("")
+		panel.edit:RequestFocus()
+		panel:Layout(true)
+		
+		input.DisableFocus = true
+		window.SetMouseTrapped(false)
+	end
+	
+	function chat.Close()
+		local panel = chat.GetPanel()
+		
+		panel:Minimize(false)		
+		panel.edit:SetText("")
+		panel.edit:Unfocus()
+		
+		input.DisableFocus = false
+		window.SetMouseTrapped(old_mouse_trap) 
+		
+	end
 		
 	--[[event.AddListener("ConsoleLineEntered", "chat", function(line)
 		if not network.IsStarted() then return end
@@ -110,170 +323,12 @@ if CLIENT then
 		
 		return false
 	end)]]
-	
-
-	local i = 1
-	local history = {}
-	local last_history
-	
+		
 	-- this depends on "gui" which is an addon, which may as well be a part of goluwa
  	-- TODO!!
 			
 	console.AddCommand("showchat", function()	
-		local frame =  chat.panel
-		local found_autocomplete = {}
-				
-		if not frame:IsValid() then
-			local old_mouse_trap = window.GetMouseTrapped()
-			
-			frame = gui2.CreatePanel("frame")
-			frame:SetTitle("chatbox")
-			frame:SetSize(Vec2(400, 250))
-			frame:SetColor(gui2.skin.font_edit_background)
-			
-			local S = gui2.skin.scale
-			
-			local edit = frame:CreatePanel("text_edit")
-			edit:SetStretchToPanelWidth(frame)
-			edit:SetHeight(10*S)
-			edit:SetColor(Color(1,1,1,1))
-			edit:SetStyle("frame")
-			edit:SetTextColor(Color(0,0,0,1))
-			frame.edit = edit
-			
-			local scroll = frame:CreatePanel("scroll")
-			scroll:SetXScrollBar(false)
-			frame.scroll = scroll
-
-			local text = scroll:SetPanel(gui2.CreatePanel("text"))
-			text:SetPosition(Vec2()+S*2)
-			text.markup:SetLineWrap(true)
-			text:AddEvent("ChatAddText")
-
-			function text:OnChatAddText(args)
-				self.markup:AddFont(gui2.skin.default_font)
-				self.markup:AddTable(args, true)
-				self.markup:AddTagStopper()
-				self.markup:AddString("\n")
-			end
-			
-			function text:OnLayout()
-				self.markup:SetMaxWidth(self.Parent:GetWidth())
-			end
-			
-			text:Layout()
-		
-			edit:RequestFocus()
-			--edit:SetMultiline(true)
-			
-			-- autocomplete should be done after keys like space and backspace are pressed
-			-- so we can use the string after modifications
-			edit.OnPostKeyInput = function(self, key, press)
-				if not press then return end
-				
-				local str = self:GetText():trim()
-				
-				local scroll = 0
-				 
-				if key == "tab" then
-					scroll = input.IsKeyDown("left_shift") and -1 or 1
-				end
-				
-				found_autocomplete = autocomplete.Query("chatsounds", str, scroll)
-									
-				if key == "tab" and found_autocomplete then
-					edit:SetText(found_autocomplete[1])
-					return false
-				end
-			end
-			
-			edit.OnPreKeyInput = function(self, key, press)	
-				if not press then return end
-				
-				local str = self:GetText()
-				
-				local ctrl = input.IsKeyDown("left_control") or input.IsKeyDown("right_control")
-				
-				if ctrl or str == "" or str == last_history then
-					local browse = false
-					
-					if key == "up" then
-						i = math.clamp(i + 1, 1, #history)
-						browse = true
-					elseif key == "down" then
-						i = math.clamp(i - 1, 1, #history)
-						browse = true
-					end
-					
-					local found = history[i]
-					if browse and found then
-						edit:SetText(found)
-						edit:SetCaretPosition(Vec2(#found, 0))
-						last_history = found
-					end
-				end
-
-				if key == "enter" and not ctrl or key == "escape" then
-				
-					if key ~= "escape" then
-						i = 0
-						if #str > 0 then
-							chat.Say(str)
-							if history[1] ~= str then
-								table.insert(history, 1, str)
-							end
-						end
-					end
-					
-					window.SetMouseTrapped(old_mouse_trap) 
-					
-					edit:SetText("")
-					frame:Minimize()
-					input.DisableFocus = false
-					
-					event.Call("ChatTextChanged", "")
-					
-					return
-				end	
-				
-				event.Call("ChatTextChanged", str)
-			end
-				
-			edit.OnTextChanged = function(self, str)
-				event.Call("ChatTextChanged", str)
-				frame:Layout()
-			end
-			
-			edit.OnLayout = function()
-				edit:SizeToText()
-				edit:SetHeight(math.max(edit:GetHeight(), S*8))
-				edit:SetWidth(frame:GetWidth())
-				edit:SetY(frame:GetHeight() - edit:GetHeight())
-				edit:SetX(0)
-				edit.label.markup:SetMaxWidth(frame:GetWidth())
-				
-				scroll:SetPosition(Vec2(0, 10*S))
-				scroll:SetHeight(frame:GetHeight() - edit:GetHeight() - S*10)
-				scroll:SetWidth(frame:GetWidth())
-				scroll.scroll_area:SetScrollFraction(Vec2(0,1))
-			end
-			
-			edit.OnPostDraw = function()
-				if found_autocomplete and #found_autocomplete > 0 then
-					autocomplete.DrawFound(0, edit:GetHeight(), found_autocomplete, nil, 2) 
-				end
-			end
-				
-			window.SetMouseTrapped(false)
-		end
-				
-		frame:Minimize(true)
-		frame.scroll.scroll_area:SetScrollFraction(Vec2(0,1))
-		frame.edit:SetText("")
-		frame:Layout(true)
-		input.DisableFocus = true
-
-		chat.panel = frame
+		chat.Open()
 	end)
 	
 	input.Bind("y", "showchat")
