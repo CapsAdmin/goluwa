3a667dc9b07c0fc5954a828f13d3f9cc39efd62b
CapsAdmin <eliashogstvedt@gmail.com>
2014-11-17 02:03:22 +0100

    refactored text renderer

---
+++ .base/lua/libraries/graphics/surface/fonts/base_font.lua
@@ -0,0 +1,218 @@
+﻿local META = {}
+
+META.Type = "font"
+META.ClassName = "base"
+
+META.pages = {}
+META.chars = {}
+META.state = "reading"
+
+prototype.GetSet(META, "Path", "")
+prototype.GetSet(META, "Padding", 0)
+prototype.IsSet(META, "Spacing", 1)
+prototype.IsSet(META, "Size", 12)
+prototype.GetSet(META, "Filtering", "linear")
+prototype.GetSet(META, "ShadingInfo")
+prototype.GetSet(META, "FallbackFonts")
+prototype.IsSet(META, "Monospace", false)
+prototype.IsSet(META, "Ready", false)
+prototype.GetSet(META, "LoadSpeed", 10)
+prototype.GetSet(META, "Shadow", 0)
+prototype.GetSet(META, "ShadowColor", Color(0,0,0,1))
+
+function META:GetGlyphData(code)
+	error("not implemented")
+end
+
+function META:CreateTextureAtlas()
+	self.texture_atlas = render.CreateTextureAtlas(512, 512, {
+		min_filter = self.Filtering,
+		mag_filter = self.Filtering,
+	})
+	
+	self.texture_atlas:SetPadding(self.Padding)
+
+	for code in pairs(self.chars) do
+		self.chars[code] = nil
+		self:LoadGlyph(code)
+	end
+	
+	self.texture_atlas:Build()
+	
+	self:Shade()
+end
+
+function META:Shade(source, vars)
+	if source then
+		for _, tex in ipairs(self:GetTextures()) do
+			tex:Shade(source, vars)
+		end
+	elseif self.ShadingInfo then
+		self:CreateTextureAtlas()
+		
+		for _, info in ipairs(self.ShadingInfo) do
+			for _, tex in ipairs(self:GetTextures()) do
+				tex:Shade(info.source, info.vars)
+			end
+		end
+	end
+end
+
+function META:Rebuild()
+	if self.ShadingInfo then
+		self:Shade()
+	else
+		self.texture_atlas:Build()
+	end
+end
+
+function META:LoadGlyph(code)
+	if self.chars[code] then return end
+	
+	local buffer, char = self:GetGlyphData(code)
+	
+	if not buffer and self.FallbackFonts then
+		for i, font in ipairs(self.FallbackFonts) do
+			buffer, char = font:GetGlyphData(code)
+			if buffer then break end
+		end
+	end
+	
+	if buffer then		
+		self.texture_atlas:Insert(code, {		
+			w = char.w, 
+			h = char.h, 
+			buffer = buffer,
+		})
+		
+		self.chars[code] = char
+	end
+end
+
+function META:DrawString(str, x, y)
+	if not self.Ready then return end
+	self.string_cache = self.string_cache or {}
+	
+	if not self.string_cache[str] then
+		
+		local poly
+		local data = {}
+	
+		local X, Y = 0, 0
+		local last_tex
+		
+		local rebuild = false
+		
+		for i = 1, utf8.length(str) do
+			local char = utf8.sub(str, i,i)
+			local ch = self.chars[char]
+			if not ch then
+				self:LoadGlyph(char)
+				rebuild = true
+			end
+		end
+		
+		if rebuild then
+			self:Rebuild()
+		end
+				
+		for i = 1, utf8.length(str) do
+			local char = utf8.sub(str, i,i)
+			local ch = self.chars[char]
+			
+			if char == "\n" then
+				X = x
+				Y = Y + self.Size
+			elseif char == "\t" then
+				X = X + self.Size * 4
+			elseif not ch and char == " " then
+				X = X + self.Size
+			elseif ch then		
+				local texture = self.texture_atlas:GetPageTexture(char)
+				
+				if texture ~= last_tex then
+					poly = surface.CreatePoly(#str)
+					table.insert(data, {poly = poly, texture = texture})
+					last_tex = texture
+				end
+				
+				local x,y, w,h, sx,sy = self.texture_atlas:GetUV(char)
+				poly:SetUV(x,y, w,h, sx,sy) 
+				poly:SetRect(i, X-self.Padding/2, Y+self.Padding/2 + (ch.h - ch.bitmap_top) + self.Size, w, -h)
+				
+				if self.Monospace then 
+					X = X + self.Spacing
+				else
+					X = X + ch.x_advance + self.Spacing
+				end
+			end
+		end
+				
+		self.string_cache[str] = data
+	end
+	
+	surface.PushMatrix(x, y)
+	for i, v in ipairs(self.string_cache[str]) do
+		surface.SetTexture(v.texture)
+		render.SetCullMode("front")
+		if self.Shadow > 0 then
+			local r,g,b,a = surface.SetColor(self.ShadowColor:Unpack())
+			surface.Translate(self.Shadow, self.Shadow)
+			v.poly:Draw()
+			surface.Translate(-self.Shadow, self.Shadow)
+			surface.SetColor(r,g,b,a)
+		end
+		v.poly:Draw()
+		render.SetCullMode("back")
+	end	
+	surface.PopMatrix()
+	
+	if surface.debug_font_size then
+		surface.SetColor(1,0,0,0.25)
+		surface.SetWhiteTexture()
+		surface.DrawRect(x, y, surface.GetTextSize(str))
+	end
+end
+
+function META:GetTextSize(str)
+	if not self.Ready then return 0,0 end
+	
+	local X, Y = 0, self.Size
+	
+	local rebuild = false
+	local length = utf8.length(str)
+	
+	for i = 1, length do
+		local char = utf8.sub(str, i,i)
+		local ch = self.chars[char]
+		if not ch then
+			self:LoadGlyph(char)
+			rebuild = true
+		end
+	end
+	
+	if rebuild then
+		self:Rebuild()
+	end
+	
+	for i = 1, length do
+		local char = utf8.sub(str, i,i)
+		local ch = self.chars[char] 
+		if char == "\n" then
+			Y = Y + self.Size
+		elseif char == "\t" then
+			X = X + self.Size
+		elseif not ch and char == " " then
+			X = X + self.Size
+		elseif ch then
+			if self.Monospace then 
+				X = X + self.Spacing
+			else
+				X = X + ch.x_advance + self.Spacing
+			end
+		end
+	end
+	return X, Y
+end
+
+prototype.Register(META)
---
+++ .base/lua/libraries/graphics/surface/fonts/fonts.lua
@@ -0,0 +1,252 @@
+local surface = (...) or _G.surface
+
+surface.fonts = surface.fonts or {}
+surface.registered_fonts = surface.registered_fonts or {}
+surface.font_dpi = 72
+
+local ready = false
+local queue = {}
+
+function surface.InitializeFonts()
+	local font = surface.CreateFont("default", {path = "fonts/unifont.ttf"})
+	surface.SetFont(font)
+
+	ready = true
+	
+	for k,v in pairs(queue) do
+		surface.CreateFont(unpack(v))
+	end
+end
+
+function surface.CreateFont(name, options, callback)
+	
+	if not ready then
+		table.insert(queue, {name, options, callback})
+		return
+	end
+	
+	local path = options.path or "fonts/unifont.ttf"
+	local size = options.size or 14
+	local padding = options.padding or 1
+	local fallback = options.fallback
+	
+	if fallback then
+		if type(fallback) == "string" then fallback = {options.fallback} end
+		for k,v in ipairs(fallback) do fallback[k] = surface.fonts[v] end
+	end
+	
+	local shader = options.shade
+	
+	if shader then
+		if shader.source then
+			shader = {shader}
+		end
+	end
+	
+	local shadow = options.shadow
+	local shadow_color = options.shadow_color or Color(0,0,0,0.5)
+
+	if shadow and type(shadow) ~= "number" then
+		shadow = size / 10 --???
+	end
+	
+	local monospace = options.monospace
+	local spacing = options.spacing
+	
+	if monospace and not spacing then
+		spacing = size
+	end
+	
+	spacing = spacing or 1
+	
+	surface.fonts[name] = surface.fonts.default
+	
+	for class_name, _ in pairs(surface.registered_fonts) do
+		local self = prototype.CreateDerivedObject("font", class_name)
+		
+		self:SetName(name)
+		self:SetPath(path)
+		self:SetSize(size)
+		self:SetPadding(padding)
+		self:SetFallbackFonts(fallback)
+		self:SetShadingInfo(shader)
+		self:SetShadow(shadow)
+		self:SetShadowColor(shadow_color)
+		self:SetMonospace(monospace)
+		self:SetSpacing(spacing)
+		
+		local ok, err = pcall(self.Initialize, self, function(...)
+			self:SetReady(true)
+			
+			event.Call("FontChanged", name, options)
+			if callback then
+				callback(...)
+			end
+		end)
+		
+		if ok then			
+			surface.fonts[name] = self
+			surface.InvalidateFontSizeCache(name)
+			
+			return self
+		else
+			self:Remove()
+		end
+		
+		if surface.debug then
+			logf("%s failed to load font %s: %s\n", class_name, name, err)
+		end
+	end
+end
+
+function surface.RegisterFont(meta)
+	meta.TypeBase = "base"
+	meta.Type = "font"
+	
+	prototype.Register(meta)
+	
+	surface.registered_fonts[meta.ClassName] = meta
+end
+
+function surface.SetFont(font)
+	surface.current_font = surface.fonts[font] or surface.fonts.default
+end
+
+function surface.GetFont()
+	return surface.current_font
+end
+
+local X, Y = 0, 0
+
+function surface.DrawText(str, x, y)
+	
+	local ux,uy,uw,uh,usx,usy = surface.GetRectUV()
+	local old_tex = surface.GetTexture()
+	local r,g,b,a = surface.GetColor()
+	
+	x = x or X
+	y = y or Y
+	
+	local font = surface.current_font
+	
+	if not font or not font:IsReady() then
+		surface.SetColor(0.8, 0.8, 0.8, 1)
+		surface.SetWhiteTexture()
+		surface.DrawRect(x, y, 32, 32)
+		local deg = 360 / 8
+		for i = 0, 7 do
+			local n=0
+			if math.floor(os.clock()*5)%18>=9 then
+				n = ((math.floor(os.clock()*5) % 9) - i)
+			else
+				n = 1-(((math.floor(os.clock()*5)+9) % 9) - i)
+			end
+			surface.SetColor(n, n, n, 1)
+			local ang = math.rad(deg * i)
+			local X, Y = math.sin(ang), math.cos(ang)
+			surface.DrawLine(X*2+16, Y*2+16, X*16 + 16, Y*16 + 16, 2)
+		end
+	else
+		font:DrawString(str, x, y)
+	end
+	
+	surface.SetRectUV(ux,uy,uw,uh,usx,usy)
+	surface.SetTexture(old_tex)
+	surface.SetColor(r,g,b,a)
+end
+
+function surface.SetTextPosition(x, y)
+	X = x or X
+	Y = y or Y
+end
+
+do
+	local cache = {}
+
+	function surface.GetTextSize(str)
+		local font = surface.current_font
+		
+		if not font then
+			return 0,0
+		end
+		
+		if not font:IsReady() then
+			return font.Size, font.Size
+		end
+		
+		if cache[font] and cache[font][str] then 
+			return cache[font][str][1], cache[font][str][2] 
+		end
+		
+		local x, y = font:GetTextSize(str)
+		
+		cache[font] = cache[font] or {}
+		cache[font][str] = cache[font][str] or {}
+		cache[font][str][1] = x
+		cache[font][str][2] = y
+		
+		return x, y
+	end
+	
+	function surface.InvalidateFontSizeCache(font)
+		if font then
+			cache[font] = nil
+		else
+			cache = {}
+		end
+	end
+end
+
+function surface.WrapString(str, max_width)
+	if not max_width or max_width == 0 then
+		return str:explode("")
+	end
+	
+	local lines = {}
+	
+	local last_pos = 0
+	local line_width = 0
+	local found = false
+
+	local space_pos
+
+	for pos, char in pairs(str:utotable()) do
+		local w, h = surface.GetTextSize(char)
+
+		if char:find("%s") then
+			space_pos = pos
+		end
+
+		if line_width + w >= max_width then
+
+			if space_pos then
+				lines[#lines+1] = str:usub(last_pos+1, space_pos)
+				last_pos = space_pos
+			else
+				lines[#lines+1] = str:usub(last_pos+1, pos)
+				last_pos = pos
+			end
+
+			line_width = 0
+			found = true
+			space_pos = nil
+		else
+			line_width = line_width + w
+		end
+	end
+
+	if found then
+		lines[#lines+1] = str:usub(last_pos+1, pos)
+	else
+		lines[#lines+1] = str
+	end
+
+	return lines
+end
+
+include("base_font.lua")
+include("fonts/*", surface)
+
+if RELOAD then
+	surface.InitializeFonts()
+end
