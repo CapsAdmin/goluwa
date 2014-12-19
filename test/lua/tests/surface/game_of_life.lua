f46f82d6095687575a2b3784163da73f5e52da4b
CapsAdmin <eliashogstvedt@gmail.com>
2014-12-19 03:50:05 +0100

    game of life shader test
    Former-commit-id: 083dbd0c7d1b098b82d260c603eb3831e92fcb45

---
+++ test/lua/tests/surface/game_of_life.lua
@@ -0,0 +1,50 @@
+local start = 0.5
+local tex = Texture({width = render.GetWidth(), height = render.GetHeight(), min_filter = "nearest", mag_filter = "nearest"}):Fill(function() 
+	return math.random()> start and 255 or 0,math.random()> start and 255 or 0,math.random()> start and 255 or 0,math.random()> start and 255 or 0
+end)
+
+event.CreateTimer("update_cells", 0, 0,function()
+render.SetBlendMode2("src_color", "src_color", "add")
+tex:Shade([[
+	vec4 color = texture(self, uv);
+	vec2 uv = gl_FragCoord.xy / size;
+	vec2 uv_unit = 1.0 / size;	
+	
+	{
+		int neighbours = 0;
+		
+		for (float y = -1; y <= 1; y++)
+		{
+			for (float x = -1; x <= 1; x++)
+			{
+				if (texture(self, uv + (uv_unit * vec2(x, y))).a > 0)
+				{
+					neighbours++;
+				}
+			}
+		}
+		
+		if (color.a > 0 && (neighbours-1 < 2 || neighbours-1 > 3))
+		{
+			color.a = 0;
+		}
+		else if (neighbours == 3)
+		{
+			color.a = 1;
+		}
+	}
+		
+	return color;
+]], nil, true)
+
+end)
+
+event.AddListener("Draw2D", "fb", function()
+	surface.SetWhiteTexture()
+	surface.SetColor(0,0,0,1)
+	surface.DrawRect(0, 0, tex.w, tex.h)
+	
+	surface.SetTexture(tex)
+	surface.SetColor(1,1,1,1)
+	surface.DrawRect(0, 0, tex.w, tex.h)
+end)   
