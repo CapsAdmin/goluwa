5cbe22059d967699421ad4a403663c6ab1d4b003
CapsAdmin <eliashogstvedt@gmail.com>
2014-12-22 12:32:20 +0100

    added more tests

---
+++ test/lua/tests/multiple_windows.lua
@@ -0,0 +1,15 @@
+local gl = require("lj-opengl") -- OpenGL
+local wnd = utility.RemoveOldObject(render.CreateWindow(512, 512))
+
+function wnd:OnUpdate(dt)
+	--render.DrawScene(self, dt)
+	--do return end
+	render.Start(self)
+		render.Clear(gl.e.GL_COLOR_BUFFER_BIT, gl.e.GL_DEPTH_BUFFER_BIT)
+		surface.Start()
+			surface.SetWhiteTexture()
+			surface.SetColor(Color():GetRandom())
+			surface.DrawRect(0,0,50,50)
+		surface.End()
+	render.End()
+end
