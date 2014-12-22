94cdc04d216c9747fa1a24c74b174a707a045bc8
CapsAdmin <eliashogstvedt@gmail.com>
2014-12-22 16:50:51 +0100

    broken thread support

--- lovemu/lua/lovemu/love/thread.lua
+++ lovemu/lua/lovemu/love/thread.lua
@@ -3,13 +3,14 @@ local love = (...) or _G.lovemu.love
 love.thread = {}
 
 local threads = {}
+local threads2 = {}
 
 local Thread = {}
 Thread.Type = "Thread"
 
 function Thread:start() end
 function Thread:wait() end
-function Thread:set() end
+function Thread:set(key, val) self.vars[key] = val end
 function Thread:send() end
 function Thread:receive() end
 function Thread:peek() end
@@ -17,17 +18,42 @@ function Thread:kill() end
 function Thread:getName() return self.name end
 function Thread:getKeys() return {} end
 function Thread:get() return end
-function Thread:demand() return end
+function Thread:demand(name) return self.vars[name] end
 
-function love.thread.newThread(name)
+function love.thread.newThread(name, script_path)
 	local self = lovemu.CreateObject(Thread)
 	
+	self.vars = {}
+	
+	local env = getfenv(2)
+	local func = love.filesystem.load(script_path)
+	local thread = utility.CreateThread()
+	function thread:OnStart()
+		setfenv(func, env)
+		self:Sleep()
+		func()
+		self:Sleep()
+	end
+	thread:Start()
+	
+	function thread:OnFinish()
+		logn("[lovemu] thread ", name ," finished")
+	end
+	
+	threads[name] = self
+	threads2[thread.co] = self
+	
 	self.name = name
+	
+	logn("[lovemu] creating thread ", name)
 	
 	return self
 end
 
 function love.thread.getThread(name)
+	if not name then
+		return threads2[coroutine.running()]
+	end
 	return threads[name]
 end
 
