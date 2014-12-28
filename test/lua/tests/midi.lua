8d921c1f505601327024d7d7e0a5a9d1b0970def
CapsAdmin <eliashogstvedt@gmail.com>
2014-12-28 19:44:33 +0100

    updated midi.lua to somewhat use sf2

--- test/lua/tests/midi.lua
+++ test/lua/tests/midi.lua
@@ -1,15 +1,31 @@
-local data = utility.MidiToTable("C:/Windows/Media/flourish.mid")
-print(data)
+local data = utility.MidiToTable("C:/Windows/Media/onestop.mid")
+local sf2 = utility.SF2ToTable("C:/Users/caps/Pictures/zelda.sf2")
+	
+local function create_source(index)
+	local sample = sf2.pdta.shdr[index]
+	
+	local buffer = audio.CreateBuffer()		
+	buffer:SetData(sf2.sdta.data, sf2.sdta.size)
+	buffer:SetLoopPoints(sample.start_loop, sample.stop_loop) 
+	
+	local source = audio.CreateSource()
+	source:SetBuffer(buffer) 
+	source:SetLooping(true)
+	source:SetSampleOffset(sample.start)
+	source.sample_info = sample
+		
+	return source
+end
+
 local clock = -1
 
 event.AddListener("Update", "asdf", function(dt)
 	for _, track in ipairs(data.tracks) do
 	
 		if not track.sound then
-			local sound = audio.CreateSource("sounds/wowozela/sine_880.wav")
+			local sound = create_source(_)
 
 			sound:Play()
-			sound:SetLooping(true)
 			sound:SetGain(0)
 			
 			track.start_i = 1
@@ -27,13 +43,13 @@ event.AddListener("Update", "asdf", function(dt)
 				if v.subtype == "program_change" then
 
 				elseif v.subtype == "note_on" then
-					track.sound:SetPitch((2 ^ ((12+v.note_number)/12)/ 127) + track.pitch_bend)
+					track.sound:SetPitch((2 ^ ((v.note_number + track.sound.sample_info.original_pitch - (12*3))/12)/ 127) + track.pitch_bend)
 					track.sound:SetGain(v.velocity/128)
 					track.pitch_bend = 0
 				elseif v.subtype == "note_off" then 
 					track.sound:SetGain(0)
 				elseif v.subtype == "pitch_bend" then
-					track.pitch_bend = v.value/8000
+					track.pitch_bend = v.value/16000
 				elseif v.subtype == "controller" then
 					
 				else
