a7d7504ad47a4713b80300e5ed8e2d0d40b8797d
CapsAdmin <eliashogstvedt@gmail.com>
2014-12-28 19:44:19 +0100

    added utility.SF2ToTable(path)
    Former-commit-id: f5a201af845521fa5a8c579bb47f7a48274fe1ef

--- .base/lua/libraries/utilities/midi.lua
+++ .base/lua/libraries/utilities/midi.lua
@@ -2,9 +2,9 @@ local utility = _G.utility or ...
 
 do
 	local frame_rate_flags = {
-		[0x00] = 24, 
-		[0x20] = 25, 
-		[0x40] = 29, 
+		[0x00] = 24,
+		[0x20] = 25,
+		[0x40] = 29,
 		[0x60] = 30,
 	}
 
@@ -14,7 +14,7 @@ do
 		local midi = file:ReadStructure([[
 			string header_id[4] = MThd;
 			swap unsigned long header_size = 6;
-			
+
 			swap unsigned short format_type;
 			swap unsigned short track_count;
 			swap unsigned short time_division;
@@ -24,30 +24,30 @@ do
 
 		local last_event_type
 
-		for i = 1, midi.track_count do	
+		for i = 1, midi.track_count do
 			local info = file:ReadStructure([[
 				string track_id[4] = MTrk;
 				swap unsigned long track_size;
 			]])
-			
-			info.track_size = file:GetPosition() + info.track_size 
-			
+
+			info.track_size = file:GetPosition() + info.track_size
+
 			local track = {}
 			track.events = {}
-			
+
 			local time = 0ULL
-				
-			for i = 1, 1000000 do		
+
+			for i = 1, 1000000 do
 				local event = {}
-						
+
 				time = time + file:ReadVarInt()
 				local event_type = file:ReadByte()
-				
+
 				if bit.band(event_type, 0xf0) == 0xf0 then
-					if event_type == 0xff then							
+					if event_type == 0xff then
 						local sub_type = file:ReadByte()
 						local length = file:ReadVarInt()
-									
+
 						if sub_type == 0x00 then
 							track.sequence_number = file:ReadInt()
 						elseif sub_type == 0x01 then
@@ -66,25 +66,25 @@ do
 							track.cueoint = file:ReadBytes(length)
 						elseif sub_type == 0x20 then
 							event.subtype = "midi_channel_prefix"
-							if length ~= 1 then 
-								error("Expected length for midi_channel_prefix event is 1, got " .. length) 
+							if length ~= 1 then
+								error("Expected length for midi_channel_prefix event is 1, got " .. length)
 							end
 							event.channel = file:ReadByte();
 						elseif sub_type == 0x2f then
 							--event.subtype = "end_of_track"
-							if length ~= 0 then 
-								error("Expected length for end_of_track event is 0, got " .. length) 
+							if length ~= 0 then
+								error("Expected length for end_of_track event is 0, got " .. length)
 							end
 							break
 						elseif sub_type == 0x51 then
 							track.set_tempo = bit.lshift(file:ReadByte(), 16) + bit.lshift(file:ReadByte(), 8) + file:ReadByte()
-							if length ~= 3 then 
-								error("Expected length for set_tempo event is 3, got " .. length) 
+							if length ~= 3 then
+								error("Expected length for set_tempo event is 3, got " .. length)
 							end
 						elseif sub_type == 0x54 then
 							track.smpte_offset = {}
-							if length ~= 5 then 
-								error("Expected length for smpte_offset event is 5, got " .. length) 
+							if length ~= 5 then
+								error("Expected length for smpte_offset event is 5, got " .. length)
 							end
 							local hour_byte = file:ReadByte()
 							track.smpte_offset.frame_rate = frame_rate_flags[bit.band(hour_byte, 0x60)]
@@ -95,8 +95,8 @@ do
 							track.smpte_offset.subframe = file:ReadByte()
 						elseif sub_type == 0x58 then
 							track.time_signature = {}
-							if length ~= 4 then 
-								error("Expected length for timeSignature event is 4, got " .. length) 
+							if length ~= 4 then
+								error("Expected length for timeSignature event is 4, got " .. length)
 							end
 							track.time_signature.numerator = file:ReadByte()
 							track.time_signature.denominator = file:ReadByte() ^ 2
@@ -104,8 +104,8 @@ do
 							track.time_signature.thirty_seconds = file:ReadByte()
 						elseif sub_type == 0x59 then
 							track.key_signature = {}
-							if length ~= 2 then 
-								error("Expected length for keySignature event is 2, got " .. length) 
+							if length ~= 2 then
+								error("Expected length for keySignature event is 2, got " .. length)
 							end
 							track.key_signature.key = file:ReadByte(true)
 							track.key_signature.scale = file:ReadByte()
@@ -127,22 +127,22 @@ do
 					end
 				else
 					local param1
-					
+
 					if bit.band(event_type, 0x80) == 0 and last_event_type then
 						param1 = event_type
 						event_type = last_event_type
 					else
 						param1 = file:ReadByte()
 						last_event_type = event_type
-					end		
-					
+					end
+
 					track.channel = bit.band(event_type, 0x0f)
-					
+
 					local event = {}
 					event.time = time
 
 					local event_type = bit.rshift(event_type, 4)
-					
+
 					if event_type == 0x08 then
 						event.subtype = "note_off"
 						event.note_number = param1
@@ -175,19 +175,19 @@ do
 					else
 						error("Unrecognised MIDI event type: " .. event_type)
 					end
-					
+
 					table.insert(track.events, event)
 				end
-				
+
 				if next(event) then
 					events[i] = event
 				end
-				
-				if file:GetPosition() >= info.track_size then 
+
+				if file:GetPosition() >= info.track_size then
 					break
 				end
 			end
-			
+
 			midi.tracks[i] = track
 		end
 
@@ -203,5 +203,196 @@ do
 		end
 
 		return midi
+	end
+end
+
+do
+	local function mod_bits_to_table(mod_bits)
+		return {
+			type = bit.rshift(mod_bits, 10),
+			p = bit.band(bit.rshift(mod_bits, 9), 1),
+			d = bit.band(bit.rshift(mod_bits, 9), 1),
+			cc = bit.band(bit.rshift(mod_bits, 7), 1),
+			index = bit.band(mod_bits, 0x3f),
+		}
+	end
+
+	function utility.SF2ToTable(path)
+		local sf2 = vfs.Open(path)
+
+		local header = sf2:ReadStructure([[
+			string magic[4] = RIFF;
+			unsigned int size;
+			string magic2[4] = sfbk;
+		]])
+
+		local out = {}
+
+		-- list
+		for i = 1, 3 do
+			local header = sf2:ReadStructure([[
+				string magic[4] = LIST;
+				unsigned int size;
+				char type[4];
+			]])
+
+			local the_end = sf2:GetPosition() + header.size - 4
+
+			local chunk = {}
+
+			for i = 1, 1024 do
+				if sf2:GetPosition() >= the_end then break end
+
+				local id = sf2:ReadBytes(4)
+				local size = sf2:ReadLong()
+								
+				local the_end = sf2:GetPosition() + size
+
+				if id == "ifil" then
+					chunk.version = sf2:ReadShort() .. "." .. sf2:ReadShort()
+				elseif id == "INAM" then
+					chunk.name = sf2:ReadBytes(size):removepadding()
+				elseif id == "isng" then
+					chunk.engine = sf2:ReadBytes(size):removepadding()
+				elseif id == "IENG" then
+					chunk.engineers = sf2:ReadBytes(size):removepadding()
+				elseif id == "ISFT" then
+					chunk.tools = sf2:ReadBytes(size):removepadding()
+				elseif id == "ICMT" then
+					chunk.comments = sf2:ReadBytes(size):removepadding()
+				elseif id == "ICOP" then
+					chunk.copyright = sf2:ReadBytes(size):removepadding()
+				elseif id == "smpl" then
+					chunk.data =  ffi.cast("uint8_t *", sf2:ReadBytes(size))
+					chunk.size = size
+				elseif id == "phdr" then
+					local list = {}
+
+					repeat
+						local info = {}
+						info.preset_name = sf2:ReadBytes(20):removepadding()
+						info.preset = sf2:ReadShort()
+						info.bank = sf2:ReadShort()
+						info.preset_bag_index = sf2:ReadShort()
+						info.library = sf2:ReadLong()
+						info.genre = sf2:ReadLong()
+						info.morphology_genre = sf2:ReadLong()
+						table.insert(list, info)
+					until sf2:GetPosition() >= the_end
+
+					chunk.phdr = list
+				elseif id == "pbag" then
+					local list = {}
+
+					repeat
+						local info = {}
+						info.gen_index = sf2:ReadShort()
+						info.mod_index = sf2:ReadShort()
+						table.insert(list, info)
+					until sf2:GetPosition() >= the_end
+										
+					chunk.pbag = list
+				elseif id == "pmod" then
+					sf2:Advance(size)
+				elseif id == "iver" then
+					sf2:Advance(size)
+				elseif id == "IPRD" then
+					sf2:Advance(size)
+				elseif id == "ICRD" then
+					sf2:Advance(size)
+				elseif id == "irom" then
+					sf2:Advance(size)
+				elseif id == "imod" then
+					local list = {}
+
+					repeat
+						local info = {}
+											
+						info.mod_src_oper = mod_bits_to_table(sf2:ReadShort())
+						
+						info.mod_dest_oper = sf2:ReadShort()
+						info.mod_amount = sf2:ReadShort()
+						info.mod_amt_src_oper = mod_bits_to_table(sf2:ReadShort())
+						
+						info.mod_trans_oper = sf2:ReadShort()
+						table.insert(list, info)
+					until sf2:GetPosition() >= the_end
+					
+					chunk.imod = list
+				elseif id == "pgen" then
+					sf2:Advance(size)
+				elseif id == "igen" then
+					local list = {}
+					
+					repeat
+						local gen = {}
+						local gen_operator = sf2:ReadShort()
+						
+						gen.operator = gen_operator
+						
+						if gen_operator == 43 or gen_operator == 44 then
+							gen.lo = sf2:ReadByte()
+							gen.hi = sf2:ReadByte()
+						else
+							gen.amount = sf2:ReadShort()
+						end
+						
+						table.insert(list, gen)
+					until sf2:GetPosition() >= the_end
+
+					chunk.igen = list
+				elseif id == "inst" then
+					local list = {}
+
+					repeat
+						local info = {}
+						info.instrument_name = sf2:ReadBytes(20):removepadding()
+						info.instrument_bag_index = sf2:ReadShort()
+						table.insert(list, info)
+					until sf2:GetPosition() >= the_end
+					
+					chunk.inst = list
+
+				elseif id == "ibag" then
+					local list = {}
+
+					repeat
+						local info = {}
+						info.GenNdx = sf2:ReadShort()
+						info.ModNdx = sf2:ReadShort()
+						table.insert(list, info)
+					until sf2:GetPosition() >= the_end
+
+				   chunk.ibag = list
+				elseif id == "shdr" then
+					local list = {}
+					repeat
+						local info = {}
+						info.sample_name = sf2:ReadBytes(20):removepadding()
+						
+						info.start = sf2:ReadUnsignedLong()
+						info.stop = sf2:ReadUnsignedLong()
+						
+						info.start_loop = sf2:ReadLong()
+						info.stop_loop = sf2:ReadLong()
+						
+						info.sample_rate = sf2:ReadLong()
+						info.original_pitch = sf2:ReadByte()
+						info.pitch_correction = sf2:ReadByte()
+						info.sample_link = sf2:ReadShort()
+						info.sample_type = sf2:ReadShort()
+						table.insert(list, info)
+					until sf2:GetPosition() >= the_end
+
+					chunk.shdr = list
+				else
+					break
+				end
+			end
+
+			out[header.type] = chunk
+		end
+		
+		return out
 	end
 end
