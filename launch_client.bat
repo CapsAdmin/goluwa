87469ac61362d7e07dcfa2d9baa8a55cb6c7e4e9
CapsAdmin <eliashogstvedt@gmail.com>
2015-01-20 23:57:54 +0100

    force set arch to x86 temporarily

--- launch_client.bat
+++ launch_client.bat
@@ -3,5 +3,6 @@ ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
 
 SET CLIENT=1
 SET SERVER=0
+set arch=x86
 cd .base/bin/windows/%arch%/
 start luajit ../../../lua/init.lua %*
