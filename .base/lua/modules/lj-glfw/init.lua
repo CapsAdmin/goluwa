82def62179e00f09de4d4414a28a6fdb8d6e7a18
CapsAdmin <eliashogstvedt@gmail.com>
2015-01-22 01:01:18 +0100

    removed debug print
    Former-commit-id: 154fe14c57d255f64535baaa42522e0ea50a6db5

--- .base/lua/modules/lj-glfw/init.lua
+++ .base/lua/modules/lj-glfw/init.lua
@@ -3,7 +3,7 @@ local header = require("lj-glfw.header")
 
 ffi.cdef(header)
 
-local lib = print(assert(ffi.load(jit.os == "Linux" and "glfw" or "glfw3")))
+local lib = assert(ffi.load(jit.os == "Linux" and "glfw" or "glfw3"))
 
 local glfw = {
 	e = enums,
