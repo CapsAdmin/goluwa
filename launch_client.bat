681301b8b8e6deb8296d0f9edc9693d39ae680a1
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-05 23:55:29 +0100

    since sdl doesn't crash use x64 by default

--- launch_client.bat
+++ launch_client.bat
@@ -3,6 +3,6 @@ ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
 
 SET CLIENT=1
 SET SERVER=0
-set arch=x86
+
 cd .base/bin/windows_%arch%/
 start luajit ../../lua/init.lua %*
