673b7279f5980c49b5d124bcf3701531c20122d5
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-06 14:46:15 +0100

    removed binaries and added a powershell script to automatically download the required binaries
    Former-commit-id: 4e22aefd41cf4188fc9ec47817946740f5cd5fc0

--- launch_server.bat
+++ launch_server.bat
@@ -1,9 +1,5 @@
-cd %~dp0
-ECHO %PROCESSOR_ARCHITECTURE%|FINDSTR AMD64>NUL && SET ARCH=x64|| SET ARCH=x86
-
+@ECHO OFF
 SET CLIENT=0
 SET SERVER=1
 SET ARGS={'host','open steam/friends'}
-
-cd .base/bin/windows_%ARCH%/
-start luajit ../../lua/init.lua %*
+PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dp0.base\bin\launch_windows.ps1'"
