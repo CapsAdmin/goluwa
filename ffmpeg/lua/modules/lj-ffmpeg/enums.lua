88057a5a6893259a52273ac58e12dd8944da817b
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-06 12:04:41 +0100

    be free my child
    Former-commit-id: 9a22ccee6f8465b0784ea068188b0c859291440c

--- ffmpeg/lua/modules/lj-ffmpeg/enums.lua
+++
@@ -1,9 +0,0 @@
-return  
-{
-	AVSEEK_FLAG_ANY = 4,
-	AV_TIME_BASE = 1000000,
-	AV_DICT_IGNORE_SUFFIX = 2,
-	AV_NOPTS_VALUE = ffi.cast("uint64_t", math.huge),
-	AV_TIME_BASE_Q = ffi.new("AVRational", {num = 1, den = 1000000}),
-	CODEC_CAP_DELAY = 0x0020,
-}
