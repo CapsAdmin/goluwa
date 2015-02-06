fdf9836b062a2f96f0cca6cd282bf5af400ef817
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-06 15:28:24 +0100

    fixed not working on ps 2 (windows 7)

--- .base/bin/launch_windows.ps1
+++ .base/bin/launch_windows.ps1
@@ -1,10 +1,15 @@
-if ([System.Environment]::Is64BitProcess){ $arch = "x64" } else { $arch = "x86" }
-$url = "https://github.com/CapsAdmin/goluwa/releases/download/windows-binaries/" + $arch + ".zip"
-$output_folder = $PSScriptRoot + "\windows_" + $arch
+function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
 
-if(!(Test-Path $output_folder))
+if ($ENV:PROCESSOR_ARCHITECTURE -Match "64"){ $arch = "x64" } else { $arch = "x86" }
+$url = "https://github.com/CapsAdmin/goluwa/releases/download/windows-binaries/" + $arch + ".zip"
+$output_folder = $(PSScriptRoot) + "\windows_" + $arch
+
+if(!(Test-Path ($output_folder + "\luajit.exe")))
 {
-	New-Item -ItemType directory -Path $output_folder
+	if (!(Test-Path $output_folder))
+	{
+		New-Item -ItemType directory -Path $output_folder
+	}
 	
 	Write-Output $output_folder
 	
@@ -12,7 +17,16 @@ if(!(Test-Path $output_folder))
 		
 	Remove-Item $download_location -ErrorAction SilentlyContinue -Confirm:$false -Recurse:$true
 	
-	Invoke-WebRequest $url -OutFile $download_location
+	if (Get-Command Invoke-WebRequest -CommandType cmdlet -errorAction SilentlyContinue)
+	{
+		#nicer download
+		Invoke-WebRequest $url -OutFile $download_location
+	}
+	else
+	{
+		$client = new-object System.Net.WebClient
+		$client.DownloadFile( $url, $download_location )	
+	}
 		
 	$shell = new-object -com shell.application
 	$zip = $shell.NameSpace($download_location)
@@ -25,5 +39,5 @@ if(!(Test-Path $output_folder))
 	Remove-Item ($download_location) -ErrorAction SilentlyContinue -Confirm:$false -Recurse:$true
 }
 
-Set-Location ($PSScriptRoot + "\windows_$arch\")
+Set-Location ($(PSScriptRoot) + "\windows_$arch\")
 Start-Process "luajit" "../../lua/init.lua "
