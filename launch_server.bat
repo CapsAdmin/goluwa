05f35b78be96ed72a0ab1247b1ac6ee7061fb8ab
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-07 15:22:02 +0100

    show stdout, stderr and error code if stderr is not empty
    Former-commit-id: 08a5d7718496ed26a451d9f788024db5ada81f67

--- launch_server.bat
+++ launch_server.bat
@@ -2,4 +2,4 @@
 SET CLIENT=0
 SET SERVER=1
 SET ARGS={'host','open steam/friends'}
-PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dp0.base\bin\launch_windows.ps1'"
+PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '%~dp0.base\bin\launch_windows.ps1'"
