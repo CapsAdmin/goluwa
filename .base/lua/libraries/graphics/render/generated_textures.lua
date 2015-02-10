31f2d83ba89ededa6be27fe2cb1995669c10c6ae
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-10 20:51:57 +0100

    loading texture is now generated and animated
    Former-commit-id: 00fdc1e183531270394d5653769da2a7cad06c6d

--- .base/lua/libraries/graphics/render/generated_textures.lua
+++ .base/lua/libraries/graphics/render/generated_textures.lua
@@ -4,8 +4,7 @@ function render.GenerateTextures()
 	render.white_texture = render.CreateTexture(8,8, nil, {no_remove = true}):Fill(function() return 255, 255, 255, 255 end)
 	render.black_texture = render.CreateTexture(8,8, nil, {no_remove = true}):Fill(function() return 0, 0, 0, 255 end)
 	render.grey_texture = render.CreateTexture(8,8, nil, {no_remove = true}):Fill(function() return 127, 127, 127, 255 end)
-	render.noise_texture = render.CreateTexture(512,512, nil, {no_remove = true}):Fill(function() return math.random(255),math.random(255),math.random(255),math.random(255) end)
-
+	render.noise_texture = render.CreateTexture(512,512, nil, {no_remove = true}):Fill(function() return math.random(255),math.random(255),math.random(255),math.random(255) end)	
 	render.error_tex = render.CreateTexture(256, 256, nil, {no_remove = true})
 	local size = 16
 	render.error_tex:Fill(function(x, y)
@@ -16,7 +15,40 @@ function render.GenerateTextures()
 		end
 	end)
 	
-	render.loading_texture = render.CreateTexture("textures/loading.jpg", {no_remove = true})
+	do
+		local center_size = 70
+		local outter_size = 0.9
+		local width = 15
+		local arms = 13
+		local speed = 16
+		local trail_duration = 7
+		local base_arm_brightness = 0.4
+		local loading = render.CreateFrameBuffer(256, 256)
+		
+		event.CreateTimer("update_loading_texture", 1/15, 0, function()
+			if not surface.IsReady() then return end
+			loading:Begin()
+				local time = system.GetElapsedTime()
+				surface.SetColor(0.2, 0.2, 0.2, 1)
+				surface.SetWhiteTexture()
+				surface.DrawRect(0, 0, loading.w, loading.h)
+				local deg = 360 / arms
+				
+				for i = 0, arms do
+					local n = (-((time*speed + i)%arms / arms) + 1) ^ trail_duration
+					
+					surface.SetColor(base_arm_brightness + n, base_arm_brightness + n, base_arm_brightness + n, 1)
+					
+					local ang = math.rad(deg * i)
+					local X, Y = math.sin(ang), math.cos(ang)
+					local W2, H2 = loading.w/2, loading.h/2
+					
+					surface.DrawLine(X*center_size+W2, Y*center_size+H2, X*outter_size*W2 + W2, Y*outter_size*H2 + H2, width)
+				end
+			loading:End()
+		end)
+		render.loading_texture = loading:GetTexture()
+	end
 end
 
 function render.GetWhiteTexture()
@@ -35,7 +67,7 @@ function render.GetErrorTexture()
 	return render.error_tex
 end
 
-function render.GetLoadingTexture()	
+function render.GetLoadingTexture()
 	return render.loading_texture or render.error_tex
 end
 
@@ -44,5 +76,5 @@ function render.GetNoiseTexture()
 end
 
 if RELOAD then
-	render.GenerateTextures()
+	render.GenerateTextures()	
 end
