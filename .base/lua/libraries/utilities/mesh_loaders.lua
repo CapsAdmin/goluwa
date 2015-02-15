ba367b32ed06b072eba2f4b08433f933f441e608
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-15 23:20:09 +0100

    use threads.Sleep and such instead of passing around thread object everywhere

--- .base/lua/libraries/utilities/mesh_loaders.lua
+++ .base/lua/libraries/utilities/mesh_loaders.lua
@@ -1,4 +1,5 @@
 local assimp = requirew("lj-assimp")
+local cb_render 
 
 do -- render model
 	utility.render_model_cache = {}
@@ -70,6 +71,8 @@ do -- render model
 	end
 		
 	local cb = utility.CreateCallbackThing(utility.render_model_cache)
+	
+	cb_render = cb
 
 	function utility.LoadRenderModel(path, callback, callback2, on_fail)
 		check(path, "string")
@@ -98,13 +101,12 @@ do -- render model
 		
 		resource.Download(path, function(full_path)			
 			local out = {}
-						
-			if path:endswith(".mdl") and steam.LoadModel then
-				local thread = threads.CreateThread()
-				thread.debug = true
-				
-				function thread.OnStart()
-					local meshes = {}
+
+			local thread = threads.CreateThread()
+			thread.debug = true
+			
+			function thread:OnStart()							
+				if steam.LoadModel and path:endswith(".mdl") then				
 					steam.LoadModel(path, function(model_data)					
 						local mesh = render.CreateMeshBuilder()
 						
@@ -117,39 +119,24 @@ do -- render model
 						
 						mesh:Upload()
 						cb:callextra(path, "mesh", mesh)
-						table.insert(out, mesh)
-						
-					end, thread)
-				end
-				
-				function thread.OnFinish()
-					cb:stop(path, out)
-				end
-				
-				thread:Start()			
-			elseif path:endswith(".bsp") and steam.LoadMap then
-				steam.LoadMap(path, function(data, thread)
-					for _, mesh in ipairs(data.render_meshes) do 
+						table.insert(out, mesh)					
+					end)
+				elseif steam.LoadMap and path:endswith(".bsp") then
+					for _, mesh in ipairs(steam.LoadMap(path).render_meshes) do 
 						cb:callextra(path, "mesh", mesh)
 						table.insert(out, mesh)
 					end
-					
-					cb:stop(path, out)
-				end)
-			elseif assimp then		
-				local flags = assimp.e.aiProcessPreset_TargetRealtime_Quality
-				--[[
-					bit.bor(
-						assimp.e.aiProcess_CalcTangentSpace, 
-						assimp.e.aiProcess_GenSmoothNormals, 
-						assimp.e.aiProcess_Triangulate,
-						assimp.e.aiProcess_JoinIdenticalVertices			
-					)
-				]]
-				
-				local thread = threads.CreateThread()
+				elseif assimp then		
+					local flags = assimp.e.aiProcessPreset_TargetRealtime_Quality
+					--[[
+						bit.bor(
+							assimp.e.aiProcess_CalcTangentSpace, 
+							assimp.e.aiProcess_GenSmoothNormals, 
+							assimp.e.aiProcess_Triangulate,
+							assimp.e.aiProcess_JoinIdenticalVertices			
+						)
+					]]
 									
-				function thread.OnStart()
 					assimp.ImportFileEx(path, flags, function(model_data, i, total_meshes)
 						if render.debug then logf("[render] %s loading %q %s\n", path, model_data.name, i .. "/" .. total_meshes) end
 						
@@ -166,16 +153,14 @@ do -- render model
 						cb:callextra(path, "mesh", mesh)
 						table.insert(out, mesh)
 					end, true)
+				else
+					cb:callextra(path, "on_fail", "unknown format " .. path)
 				end
 				
-				function thread.OnFinish()
-					cb:stop(path, out)
-				end
-				
-				thread:Start()
-			else
-				cb:callextra(path, "on_fail", "unknown format " .. path)
+				cb:stop(path, out)
 			end
+			
+			thread:Start()
 		end, function(reason)
 			cb:callextra(path, "on_fail", reason)
 		end)
@@ -202,49 +187,64 @@ do -- physics model
 		cb:start(path, callback, {on_fail = on_fail})
 		
 		resource.Download(path, function(full_path)
-			if path:endswith(".bsp") and steam.LoadMap then
-				steam.LoadMap(full_path, function(data, thread)
-					cb:stop(path, data.physics_meshes)
-				end)	
-			elseif assimp then
-				local scene = assimp.ImportFile(full_path, assimp.e.aiProcessPreset_TargetRealtime_Quality)
+			local thread = threads.CreateThread()
+			thread.debug = true
+			
+			function thread:OnStart()
+				if steam.LoadMap and path:endswith(".bsp") then
 				
-				if scene.mMeshes[0].mNumVertices == 0 then
-					return nil, "no vertices found in " .. path
-				end
-									
-				local vertices = ffi.new("float[?]", scene.mMeshes[0].mNumVertices  * 3)
-				local triangles = ffi.new("unsigned int[?]", scene.mMeshes[0].mNumFaces * 3)
-				
-				ffi.copy(vertices, scene.mMeshes[0].mVertices, ffi.sizeof(vertices))
-
-				local i = 0
-				for j = 0, scene.mMeshes[0].mNumFaces - 1 do
-					for k = 0, scene.mMeshes[0].mFaces[j].mNumIndices - 1 do
-						triangles[i] = scene.mMeshes[0].mFaces[j].mIndices[k]
-						i = i + 1 
+					-- :(
+					if GRAPHICS and cb_render and cb_render:get(path) and cb_render:get(path).callback then
+						repeat 
+							threads.Report("waiting for render mesh to finish loading")
+							threads.Sleep()
+						until not cb_render:get(path) or not cb_render:get(path).callback
 					end
+					-- :(
+					
+					cb:stop(path, steam.LoadMap(full_path).physics_meshes)
+				elseif assimp then
+					local scene = assimp.ImportFile(full_path, assimp.e.aiProcessPreset_TargetRealtime_Quality)
+					
+					if scene.mMeshes[0].mNumVertices == 0 then
+						return nil, "no vertices found in " .. path
+					end
+										
+					local vertices = ffi.new("float[?]", scene.mMeshes[0].mNumVertices  * 3)
+					local triangles = ffi.new("unsigned int[?]", scene.mMeshes[0].mNumFaces * 3)
+					
+					ffi.copy(vertices, scene.mMeshes[0].mVertices, ffi.sizeof(vertices))
+
+					local i = 0
+					for j = 0, scene.mMeshes[0].mNumFaces - 1 do
+						for k = 0, scene.mMeshes[0].mFaces[j].mNumIndices - 1 do
+							triangles[i] = scene.mMeshes[0].mFaces[j].mIndices[k]
+							i = i + 1 
+						end
+					end
+								
+					local mesh = {	
+						triangles = {
+							count = tonumber(scene.mMeshes[0].mNumFaces), 
+							pointer = triangles, 
+							stride = ffi.sizeof("unsigned int") * 3, 
+						},					
+						vertices = {
+							count = tonumber(scene.mMeshes[0].mNumVertices),  
+							pointer = vertices, 
+							stride = ffi.sizeof("float") * 3,
+						},
+					}
+					
+					cb:stop(path, {mesh})
+					
+					assimp.ReleaseImport(scene)
+				else
+					cb:callextra(path, "on_fail", "unknown format " .. path)
 				end
-							
-				local mesh = {	
-					triangles = {
-						count = tonumber(scene.mMeshes[0].mNumFaces), 
-						pointer = triangles, 
-						stride = ffi.sizeof("unsigned int") * 3, 
-					},					
-					vertices = {
-						count = tonumber(scene.mMeshes[0].mNumVertices),  
-						pointer = vertices, 
-						stride = ffi.sizeof("float") * 3,
-					},
-				}
-				
-				cb:stop(path, {mesh})
-				
-				assimp.ReleaseImport(scene)
-			else
-				cb:callextra(path, "on_fail", "unknown format " .. path)
 			end
+			
+			thread:Start()
 		end, function(reason)
 			cb:callextra(path, "on_fail", reason)
 		end)
