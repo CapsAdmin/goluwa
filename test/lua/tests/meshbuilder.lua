65cd9133b293319c8d70bc7e12894da9c93a285a
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-17 22:41:28 +0100

    center heightmap

--- test/lua/tests/meshbuilder.lua
+++ test/lua/tests/meshbuilder.lua
@@ -1,11 +1,12 @@
-local heightmap = Texture("https://github.com/meetar/heightmap-demos/raw/gh-pages/SRTM_US_scaled_1024.jpg")   
+local heightmap = Texture("https://dl.dropboxusercontent.com/u/244444/heightmaps/HeightMap_Projects/HeightMap6.jpg")
+local diffuse = Texture("https://dl.dropboxusercontent.com/u/244444/heightmaps/HeightMap_Projects/HeightMap_6BaseTexture.jpg")
 
 local model = render.CreateMeshBuilder()
-model:LoadHeightmap(heightmap, heightmap:GetSize(), Vec2(256, 256), -100)
+model:LoadHeightmap(heightmap, heightmap:GetSize()/5, Vec2(128, 128), -200)
 model:SmoothNormals()
 model:Upload()
 
 local ent = utility.RemoveOldObject(entities.CreateEntity("visual"), "lol")
 ent:SetCull(false)
 ent:AddMesh(model)  
-ent:SetDiffuseTexture(heightmap)
+ent:SetDiffuseTexture(diffuse)
