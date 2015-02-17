dd006e489d2fa69becd8d9387d0cff273642f0f5
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-17 19:57:57 +0100

    fixed mesh_builder:LoadHeightmap()

--- test/lua/tests/meshbuilder.lua
+++ test/lua/tests/meshbuilder.lua
@@ -1,11 +1,11 @@
-local heightmap = Texture("textures/gui/skins/dark.png") 
+local heightmap = Texture("http://i.imgur.com/Sc2wB.jpg")  
 
 local model = render.CreateMeshBuilder()
-model:LoadHeightmap(heightmap)
+model:LoadHeightmap(heightmap, nil, 256)
 model:Upload()
 
 local ent = utility.RemoveOldObject(entities.CreateEntity("visual"), "lol")
 ent:SetCull(false)
 ent:AddMesh(model)
 ent:SetDiffuseTexture(heightmap)
-print(model)
+print(model)      
