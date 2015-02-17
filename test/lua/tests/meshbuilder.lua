f993e50f73f973202a92de22d99f51dbb38c32fc
CapsAdmin <eliashogstvedt@gmail.com>
2015-02-17 20:41:48 +0100

    use vec2 sizes

--- test/lua/tests/meshbuilder.lua
+++ test/lua/tests/meshbuilder.lua
@@ -1,11 +1,11 @@
-local heightmap = Texture("http://i.imgur.com/Sc2wB.jpg")  
+local heightmap = Texture("https://github.com/meetar/heightmap-demos/raw/gh-pages/SRTM_US_scaled_1024.jpg")   
 
 local model = render.CreateMeshBuilder()
-model:LoadHeightmap(heightmap, nil, 256)
+model:LoadHeightmap(heightmap, heightmap:GetSize(), Vec2(256, 256), -100)
+model:SmoothNormals()
 model:Upload()
 
 local ent = utility.RemoveOldObject(entities.CreateEntity("visual"), "lol")
 ent:SetCull(false)
-ent:AddMesh(model)
+ent:AddMesh(model)  
 ent:SetDiffuseTexture(heightmap)
-print(model)      
