3a53f3c6c4cb1e4aa3bde1f70cf5addbb240a095
CapsAdmin <eliashogstvedt@gmail.com>
2015-03-04 04:22:10 +0100

    wip tangent and binormal
    Former-commit-id: 889dae42e793f8b52266d20e05ea4b288de55dab

--- .base/lua/libraries/graphics/render/mesh_builder.lua
+++ .base/lua/libraries/graphics/render/mesh_builder.lua
@@ -131,6 +131,53 @@ do -- helpers
 			self.Vertices[ai].normal = normal
 			self.Vertices[bi].normal = normal
 			self.Vertices[ci].normal = normal
+			--[[
+			-- This is a triangle from your vertices  
+			local v1 = self.Vertices[ai].pos;  
+			local v2 = self.Vertices[bi].pos;  
+			local v3 = self.Vertices[ci].pos;  
+			 
+			-- These are the texture coordinate of the triangle  
+			local w1 = self.Vertices[ai].uv;  
+			local w2 = self.Vertices[bi].uv;  
+			local w3 = self.Vertices[ci].uv;  
+			 
+			local x1 = v2.x - v1.x
+			local x2 = v3.x - v1.x
+			local y1 = v2.y - v1.y
+			local y2 = v3.y - v1.y
+			local z1 = v2.z - v1.z
+			local z2 = v3.z - v1.z
+			 
+			local s1 = w2.x - w1.x
+			local s2 = w3.x - w1.x
+			local t1 = w2.y - w1.y
+			local t2 = w3.y - w1.y
+			 
+			local r = 1 / (s1 * t2 - s2 * t1)
+			local sdir = Vec3((t2 * x1 - t1 * x2) * r, (t2 * y1 - t1 * y2) * r, (t2 * z1 - t1 * z2) * r) 
+			local tdir = Vec3((s1 * x2 - s2 * x1) * r, (s1 * y2 - s2 * y1) * r, (s1 * z2 - s2 * z1) * r) 
+			 
+			-- Gram-Schmidt orthogonalize  
+			local tangent = sdir - normal * normal:GetDot(sdir)
+			tangent:Normalize()
+			 
+			-- Calculate handedness (here maybe you need to switch >= with <= depend on the geometry winding order)  
+			local tangentdir = normal:Cross(sdir):GetDot(tdir) >= 0 and 1 or -1  
+			local binormal = normal:Cross(tangent) * tangentdir
+			
+			self.Vertices[ai].normal = normal
+			self.Vertices[ai].tangent = tangent
+			self.Vertices[ai].binormal = binormal
+			
+			self.Vertices[bi].normal = normal
+			self.Vertices[bi].tangent = tangent
+			self.Vertices[bi].binormal = binormal
+			
+			self.Vertices[ci].normal = normal
+			self.Vertices[ci].tangent = tangent
+			self.Vertices[ci].binormal = binormal]]
+			
 			threads.Sleep()
 		end
 	end
