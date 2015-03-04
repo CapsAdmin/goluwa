2a4260305f3e16649670bf7e817448912b725014
CapsAdmin <eliashogstvedt@gmail.com>
2015-03-04 22:59:54 +0100

    updated tests
    Former-commit-id: 097c9aefafc1db66a3d635e8f1d17adb7cd01d75

--- test/lua/tests/code_statistics.lua
+++ test/lua/tests/code_statistics.lua
@@ -6,26 +6,42 @@ local data = {
 	files = {},
 }
 
+local blacklist = {
+	"header.lua",
+	"enums.lua",
+	"utf8data.lua",
+	"/modules/",
+	"_emotes.lua",
+	"icons.lua",
+}
+
 local words = {}
 
 for _, path in ipairs(vfs.Search("lua/", ".lua")) do
-	if not path:find("modules") or (path:find("lj-", nil, true) and (not path:find("header.lua") and not path:find("enums"))) then
-		local str = vfs.Read(path)
-		if str then
-			local lines = str:count("\n")
-			data.total_lines = data.total_lines + lines
-			str = str:gsub("%s+", " ")
-			data.total_words = data.total_words + str:count(" ")
-			data.total_chars = data.total_chars + #str
-			
-			for i, word in ipairs(str:explode(" ")) do
-				words[word] = (words[word] or 0) + 1
-			end
-			table.insert(data.files, {path = path, lines = lines})
-		else
-			print(path)
+	for i,v in ipairs(blacklist) do
+		if path:find(v) then 
+			goto continue
 		end
 	end
+	
+	local str = vfs.Read(path)
+	
+	if str then
+		local lines = str:count("\n")
+		data.total_lines = data.total_lines + lines
+		str = str:gsub("%s+", " ")
+		data.total_words = data.total_words + str:count(" ")
+		data.total_chars = data.total_chars + #str
+		
+		for i, word in ipairs(str:explode(" ")) do
+			words[word] = (words[word] or 0) + 1
+		end
+		table.insert(data.files, {path = path, lines = lines})
+	else
+		print(path)
+	end
+	
+	::continue::
 end
 
 data.total_chars = data.total_chars - data.total_words
