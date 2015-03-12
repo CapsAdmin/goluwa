a4afd4f9ecce9ea8dd94ca717639e8755cdeca28
CapsAdmin <eliashogstvedt@gmail.com>
2015-03-12 22:40:22 +0100

    added google.SayTTS(lang, str)
    Former-commit-id: c4dd3755a0025f06ce7c70570f1d0259eba44b7d

--- .base/lua/autorun/google.lua
+++ .base/lua/autorun/google.lua
@@ -1,27 +1,40 @@
 google = {}
 
-local base_url = "http://translate.google.com/translate_a/t?client=t&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&q=%s"
+do
+	local base_url = "http://translate.google.com/translate_a/t?client=t&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&q=%s"
 
-function google.Translate(from, to, str, callback)
-	from = from or "auto"
-	to = to or "en"
-	str = str or ""
+	function google.Translate(from, to, str, callback)
+		from = from or "auto"
+		to = to or "en"
+		str = str or ""
 
-	local url = base_url:format(from, to, sockets.EscapeURL(str))
+		local url = base_url:format(from, to, sockets.EscapeURL(str))
 
-	sockets.Get(url, function(data)
-		local out = {translated = "", transliteration = "", from = ""}
-		local content = data.content:match(".-%[(%b[])"):sub(2, -2)
-		
-		for part in content:gmatch("(%b[])") do
-			local to, from, trl = part:match("%[(%b\"\"),(%b\"\"),(%b\"\")")
-			out.translated = out.translated .. to:sub(2,-2)
-			out.from = out.from .. from:sub(2,-2)
-			out.transliteration = out.transliteration .. trl:sub(2,-2)
-		end
-		
-		callback(out)
-	end)
+		sockets.Get(url, function(data)
+			local out = {translated = "", transliteration = "", from = ""}
+			local content = data.content:match(".-%[(%b[])"):sub(2, -2)
+			
+			for part in content:gmatch("(%b[])") do
+				local to, from, trl = part:match("%[(%b\"\"),(%b\"\"),(%b\"\")")
+				out.translated = out.translated .. to:sub(2,-2)
+				out.from = out.from .. from:sub(2,-2)
+				out.transliteration = out.transliteration .. trl:sub(2,-2)
+			end
+			
+			callback(out)
+		end)
+	end
+end
+
+do
+	local base_url = "http://translate.google.com/translate_tts?tl=%s&q=%s"
+	
+	function google.SayTTS(lang, str)
+		local url = base_url:format(lang, sockets.EscapeURL(str))
+
+		local source = audio.CreateSource(url)
+		source:Play()
+	end
 end
 
 function google.AutoComplete(question, callback)
