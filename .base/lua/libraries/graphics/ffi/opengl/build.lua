d6743e77543fc7ba73605ef146376520505ea12c
CapsAdmin <eliashogstvedt@gmail.com>
2015-03-27 19:26:02 +0100

    added post fixes to be automatically done in build.lua

--- .base/lua/libraries/graphics/ffi/opengl/build.lua
+++ .base/lua/libraries/graphics/ffi/opengl/build.lua
@@ -56,7 +56,7 @@ for str in xml:gmatch("<command>(.-)</command>") do
 	local args = {}
 	local i = 1
 		
-	local cast_str = (str:match("<proto>(.-)<name>") or str:match("<ptype>(.-)</ptype>")) .. "(*)("
+	local cast_str = (str:match("<proto.->(.-)<name>") or str:match("<ptype.->(.-)</ptype>")) .. "(*)("
 	
 	if cast_str:find("ptype", nil, true) then
 		cast_str = cast_str:gsub("<ptype>", "")
@@ -65,7 +65,10 @@ for str in xml:gmatch("<command>(.-)</command>") do
 	
 	for param in str:gmatch("<param.-</param>") do
 		local name = param:match("<name>(.-)</name>")
-		local type = param:match("<ptype>(.-)</ptype>")
+		local type = param:match("<param.->(.-)<name>")
+		type = type:gsub("<ptype>", "")
+		type = type:gsub("</ptype>", "")
+		type = type:trim()
 		local group = param:match("group=\"(.-)\"")
 		
 		if not group then
@@ -75,16 +78,7 @@ for str in xml:gmatch("<command>(.-)</command>") do
 				end
 			end
 		end
-		
-		if type then
-			local extra = param:match("</ptype> (.-)<name>")
-			if extra then
-				type = type .. extra
-			end
-		else
-			type = param:match(">(.-)<name>")
-		end
-		
+				
 		if name == "end" then name = "_end" end
 		if name == "in" then name = "_in" end
 				
@@ -97,12 +91,19 @@ for str in xml:gmatch("<command>(.-)</command>") do
 				
 		cast_str = cast_str .. type .. ", "
 				
-		args[i] = {type = type, group = group, group_name = group_name, name = name}
+		args[i] = {	
+			type = type, 
+			group = group, 
+			group_name = group_name, 
+			name = name
+		}
 		
 		i = i + 1
 	end
 	
 	cast_str = cast_str:sub(0,-3) .. ")"
+	
+	if cast_str:endswith("(*)") then cast_str = cast_str .. "()" end
 	
 	local get_function
 	
@@ -110,7 +111,14 @@ for str in xml:gmatch("<command>(.-)</command>") do
 		get_function = true
 	end
 	
-	functions[func_name] = {args = args, type = type, group = group, name = name, cast_str = cast_str, get_function = get_function}
+	functions[func_name] = {
+		args = args, 
+		type = type, 
+		group = group, 
+		name = name, 
+		cast_str = cast_str, 
+		get_function = get_function,
+	}
 end
 
 local objects = {}
@@ -166,16 +174,23 @@ local i = 1
 local insert = function(s) lua[i] = s i=i+1 end
 
 insert"local gl = {}"
-insert"local get_proc_address = ... or function() end"
 insert""
 insert"ffi.cdef[["
 
-for line in xml:match("<types>.-</types>"):gmatch("<type>(.-)</type>") do
+local done = {}
+
+for line in xml:match("<types>.-</types>"):gmatch("<type(.-)</type>") do
 	local cdef = line:match("(typedef.+;)")
-	cdef = cdef:gsub("<name>", "")
-	cdef = cdef:gsub("</name>", "")
-	cdef = cdef:gsub("<apientry/>", "")
-	insert(cdef)
+	if cdef and not cdef:find("\n") and not cdef:find("khronos_") then
+		cdef = cdef:gsub("<name>", "")
+		cdef = cdef:gsub("</name>", "")
+		cdef = cdef:gsub("<apientry/>", "")
+		
+		if not done[cdef] then
+			insert(cdef)
+			done[cdef] = true
+		end
+	end
 end
 
 insert("typedef enum GL_LUA_ENUMS {")
@@ -196,6 +211,11 @@ insert("} GL_LUA_ENUMS;")
 
 insert"]]"
 
+insert"function gl.Initialize(get_proc_address)"
+insert"\tif type(get_proc_address) == \"function\" then"
+insert"\t\tgl.GetProcAddress = get_proc_address"
+insert"\tend"
+
 for k, func_info in pairs(functions) do
 	local nice = func_info.name:sub(3)
 	
@@ -212,21 +232,21 @@ for k, func_info in pairs(functions) do
 		end
 	end
 	
-	insert"do"
-	insert("\tlocal func = get_proc_address(\""..func_info.name.."\")")
-	insert"\tif func ~= nil then"
-	insert("\t\tlocal ok, func = pcall(ffi.cast, '"..func_info.cast_str.."', func)")
-	insert"\t\tif ok then"
+	insert"\tdo"
+	insert("\t\tlocal func = gl.GetProcAddress(\""..func_info.name.."\")")
+	insert"\t\tif func ~= nil then"
+	insert("\t\t\tlocal ok, func = pcall(ffi.cast, '"..func_info.cast_str.."', func)")
+	insert"\t\t\tif ok then"
 		
-	insert("\t\tgl." .. nice .. " = func")
+	insert("\t\t\t\tgl." .. nice .. " = func")
 	
 	if func_info.name:find("Gen%a-s$") then
-		insert("\t\tgl." .. nice:sub(0,-2) .. " = function() local id = ffi.new('GLint[1]') func(1, id) return id[0] end")
+		insert("\t\t\tgl." .. nice:sub(0,-2) .. " = function() local id = ffi.new('GLint[1]') func(1, id) return id[0] end")
 	end
 	
+	insert"\t\t\tend"
 	insert"\t\tend"
 	insert"\tend"
-	insert"end"
 	
 	func_info.arg_line = arg_line
 end
@@ -236,52 +256,51 @@ for name, object_functions in pairs(objects) do
 	local delete = functions["glDelete" .. name .. "s"]
 	
 	if create and delete then
-		insert("do -- " .. name)
-		insert"\tlocal META = {}"
-		insert"\tMETA.__index = META"
+		insert("\tdo -- " .. name)
+		insert"\t\tlocal META = {}"
+		insert"\t\tMETA.__index = META"
 		
 		for friendly, info in pairs(object_functions) do
 			local arg_line = info.arg_line:match(".-, (.+)") or ""
-			insert("\tfunction META:" .. friendly .. "(" .. arg_line .. ")")
+			insert("\t\tfunction META:" .. friendly .. "(" .. arg_line .. ")")
 				if arg_line ~= "" then arg_line = ", " .. arg_line end
-				insert("\t\treturn gl." .. info.name:sub(3) .. "(self.id" .. arg_line .. ")")
-			insert"\tend"
+				insert("\t\t\treturn gl." .. info.name:sub(3) .. "(self.id" .. arg_line .. ")")
+			insert"\t\tend"
 		end
 		
 		
-		insert("\tlocal ctype = ffi.typeof('struct { int id; }')")
-		insert"\tffi.metatype(ctype, META)"
+		insert("\t\tlocal ctype = ffi.typeof('struct { int id; }')")
+		insert"\t\tffi.metatype(ctype, META)"
 		
-		insert("\tlocal temp = ffi.new('GLuint[1]')")
+		insert("\t\tlocal temp = ffi.new('GLuint[1]')")
 		
-		insert"\tfunction META:Delete()"
-		insert"\t\ttemp[0] = self.id"
-		insert("\t\tgl." .. delete.name:sub(3) .. "(1, temp)")
-		insert"\tend"
+		insert"\t\tfunction META:Delete()"
+		insert"\t\t\ttemp[0] = self.id"
+		insert("\t\t\tgl." .. delete.name:sub(3) .. "(1, temp)")
+		insert"\t\tend"
 		
 		local arg_line = create.arg_line:match("(.+),.-,+") or ""
 		
-		insert("\tfunction gl.Create" .. name .. "(" .. arg_line .. ")")
+		insert("\t\tfunction gl.Create" .. name .. "(" .. arg_line .. ")")
 		if arg_line ~= "" then arg_line = arg_line .. ", " end
-		insert("\t\tgl." .. create.name:sub(3) .. "(" .. arg_line .. "1, temp)")
-		insert"\t\tlocal self = ffi.new(ctype)"
-		insert"\t\tself.id = temp[0]"
-		insert"\t\treturn self"
-		insert"\tend"
+		insert("\t\t\tgl." .. create.name:sub(3) .. "(" .. arg_line .. "1, temp)")
+		insert"\t\t\tlocal self = ffi.new(ctype)"
+		insert"\t\t\tself.id = temp[0]"
+		insert"\t\t\treturn self"
+		insert"\t\tend"
 		
-		insert"end"
+		insert"\tend"
 	end
 end
+
+insert("end")
+
+insert("gl.e = setmetatable({}, {__index = function(_, key) return tonumber(ffi.cast(\"GL_LUA_ENUMS\", key)) end})")
 
 insert("return gl")
 --collectgarbage()
 local code = table.concat(lua, "\n")
-vfs.Write("gl.lua", code)
-local gl = include("gl.lua", gl.GetProcAddress)
+vfs.Write(R"lua/libraries/graphics/ffi/opengl/init.lua", code)
 
-print(gl)
-
-local tex = gl.CreateTexture("GL_TEXTURE_2D")
-tex:SetParameterf("GL_TEXTURE_WIDTH", 1337)
-tex:GetParameterfv("GL_TEXTURE_WIDTH")
-print(tex)
+package.loaded["graphics.ffi.opengl"] = nil
+require("graphics.ffi.opengl")
