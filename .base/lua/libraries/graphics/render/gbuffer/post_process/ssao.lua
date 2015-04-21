cd9d0dfa39530062dbce068af91626a2ddca8eef
Leandro Fonseca <leandro-456@live.com>
2015-04-21 04:23:13 -0300

    48 pass SSAO

--- .base/lua/libraries/graphics/render/gbuffer/post_process/ssao.lua
+++ .base/lua/libraries/graphics/render/gbuffer/post_process/ssao.lua
@@ -5,8 +5,8 @@ PASS.Default = true
 
 PASS.Source = [[	
 	float compare_depths( in float depth1, in float depth2 ) {
-		float diff = (depth2)-(depth1-0.001);
-		diff = clamp(diff *= 30000, 0, 0.25);
+		float diff = (depth2)-(depth1-0.000001);
+		diff = clamp(diff *= 2048.0, 0, 0.25);
 						
 		return diff;
 	}
@@ -16,19 +16,19 @@ PASS.Source = [[
 
 		float depth = get_depth(uv);
 		
-		if (depth > 0.05) return 1;
+		if (depth > 1.0) return 1;
 		
 		float pw = 1.0 / g_screen_size.x;
 		float ph = 1.0 / g_screen_size.y;
 
-		float ao = 0;
+		float ao = 8.0;
 		
-		float aoscale = 1.2;
+		float aoscale = 1.1;
 		
 		pw /= aoscale;
 		ph /= aoscale;
 		
-		for (int i = 1; i < 16; i++)
+		for (int i = 1; i < 48; i++)
 		{					
 			ao += compare_depths(depth, get_depth(vec2(uv.x+pw,uv.y+ph)));
 			ao += compare_depths(depth, get_depth(vec2(uv.x-pw,uv.y+ph)));
@@ -39,9 +39,9 @@ PASS.Source = [[
 			ph *= aoscale;
 		}			 
 	 
-		ao/=16.0;
+		ao/=48.0;
 	 
-		return 0.5+clamp(ao, 0, 1)*0.5;
+		return 0.5+clamp(ao, 0.0, 1.0)*0.5;
 	}
 	out vec4 out_color;
 
