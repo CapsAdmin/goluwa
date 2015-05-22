47785045968e63206369497ef43e8f2d3b3e22f6
CapsAdmin <eliashogstvedt@gmail.com>
2015-05-23 01:35:21 +0200

    improved string.randomtext

--- .base/lua/libraries/extensions/string.lua
+++ .base/lua/libraries/extensions/string.lua
@@ -1,82 +1,65 @@
 include("string_anime.lua")
 
-function string.randomtext(word_count)
-	word_count = word_count or 8
-	
-	local vowels = {
-		"e", 
-		"a", 
-		"o", 
-		"i", 
-		"u", 
-		"y"
-	}
-	
-	local consonants = {
-		"t", 
-		"n", 
-		"s", 
-		"h", 
-		"r", 
-		"d", 
-		"l", 
-		"c", 
-		"m", 
-		"w", 
-		"f", 
-		"g", 
-		"p", 
-		"b", 
-		"v", 
-		"k", 
-		"j", 
-		"x", 
-		"q", 
-		"z",
-	}
-	
-	local text = {}
-	
-	local last_punctation = 1
-	local capitalize = true
-	
-	for i = 1, word_count do
-		math.randomseed(i)
-		local word = {}
+do
+	local vowels = {"e", "a", "o", "i", "u", "y"}	
+	local consonants = {"t", "n", "s", "h", "r", "d", "l", "c", "m", "w", "f", "g", "p", "b", "v", "k", "j", "x", "q", "z"}
+	local first_letters = {"t", "a", "s", "h", "w", "i", "o", "b", "m", "f", "c", "l", "d", "p", "n", "e", "g", "r", "y", "u", "v", "j", "k", "q", "z", "x"}
+
+	function string.randomtext(word_count)
+		word_count = word_count or 8
 		
-		local consonant_start = math.random() > 0.5 and 1 or 0
+		local text = {}
 		
-		for i = 1, math.ceil((math.random()^3)*8) + math.random(1, 3) do
-			if i%2 == consonant_start then
-				word[i] = consonants[math.floor((math.random()^3) * #consonants) + 1]
-			else
-				word[i] = vowels[math.floor((math.random()^3) * #vowels) + 1]
+		local last_punctation = 1
+		local capitalize = true
+		
+		for i = 1, word_count do
+			math.randomseed(i)
+			local word = ""
+			
+			local consonant_start = 1
+			
+			local length = math.ceil((math.random()^3)*8) + math.random(2, 3)
+
+			for i = 1, length do
+				if i == 1 then 
+					word = word .. first_letters[math.floor((math.random()^3) * #first_letters) + 1]
+					if table.hasvalue(vowels, word[i]) then
+						consonant_start = 0
+					end
+				elseif i%2 == consonant_start then
+					word = word .. consonants[math.floor((math.random()^4) * #consonants) + 1]
+				else
+					if i ~= length or math.random() < 0.25 then
+						word = word .. vowels[math.floor((math.random()^3) * #vowels) + 1]
+					end
+				end
+				
+				if capitalize then
+					word = word:upper()
+					capitalize =  false
+				end
 			end
 			
-			if capitalize then
-				word[i] = word[i]:upper()
-				capitalize =  false
+			text[i] = word
+			
+			last_punctation = last_punctation + 1
+			
+			if last_punctation > math.random(4,16) then
+				if math.random() > 0.9 then
+					text[i] = text[i] .. ","
+				else
+					text[i] = text[i] .. "."
+					capitalize = true
+				end
+				last_punctation = 1
 			end
+			
+			text[i] = text[i]  .. " "
 		end
 		
-		text[i] = table.concat(word)
-		
-		last_punctation = last_punctation + 1
-		
-		if last_punctation > math.random(4,16) then
-			if math.random() > 0.9 then
-				text[i] = text[i] .. ","
-			else
-				text[i] = text[i] .. "."
-				capitalize = true
-			end
-			last_punctation = 1
-		end
-		
-		text[i] = text[i]  .. " "
+		return table.concat(text)
 	end
-	
-	return table.concat(text)
 end
 
 function string.readablehex(str)
