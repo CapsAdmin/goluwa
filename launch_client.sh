96d3b9fa86649d0cec5b79ff9411e2f0cff46947
capsadmin <capsadmin@capsadmin.laptop>
2015-06-07 01:30:18 +0200

    cleanup in launch scripts

--- launch_client.sh
+++ launch_client.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export SERVER=0
+export CLIENT=1
+
 cd core/bin
 bash launch_linux.sh
--- launch_server.sh
+++ launch_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SERVER=1
+export CLIENT=0
 export ARGS="{'host'}"
 
 cd core/bin
