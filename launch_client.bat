69e3ab633dade36845825dcf4b6d7070733a9f31
capsadmin <capsadmin@capsadmin.laptop>
2015-06-07 23:44:22 +0200

    windows fix

--- launch_client.bat
+++ launch_client.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET CLIENT=1
 SET SERVER=0
-PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '%~dp0.base\bin\launch_windows.ps1'"
+PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '%~dp0core\bin\launch_windows.ps1'"
