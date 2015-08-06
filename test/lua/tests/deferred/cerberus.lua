237870ac966f47963576fd8f515f9cb6de4890a1
CapsAdmin <eliashogstvedt@gmail.com>
2015-08-06 16:46:43 +0200

    more pbr tests

--- test/lua/tests/deferred/cerberus.lua
+++ test/lua/tests/deferred/cerberus.lua
@@ -1,41 +1,43 @@
 for k,v in pairs(entities.GetAll()) do v:Remove() end
 
-render.camera_3d:SetPosition(Vec3(2.4, 0, -0))
-render.camera_3d:SetAngles(Ang3(0, math.pi, 0))
+render.camera_3d:SetPosition(Vec3(2.1400938034058, -3.7667279243469, 2.0517530441284))
+render.camera_3d:SetAngles(Ang3(0.51163148880005, 2.1237199306488, 0))
+
 
 local ent = entities.CreateEntity("visual")
-ent:SetModelPath("models/cube.obj")
-ent:SetSize(-1)
+ent:SetModelPath("models/cerebus/Cerberus_LP.FBX")
+ent:SetSize(-0.05)
+ent:SetPosition(Vec3(2,0,0))
+ent:SetCull(false)
 
-local max = 5
+local max = 8
 local lights = {}
 
 for i = 1, max do
 	local light = entities.CreateEntity("light")
 	light:SetColor(HSVToColor(i/max, 0.5, 1))
-	light:SetSize(1)
+	light:SetSize(4)
 	light:SetIntensity(2.5)
 	light.seed = math.random()*math.pi
+	light.dir = Vec3():GetRandom()
 	table.insert(lights, light)
 end
 
 event.AddListener("Update", "test", function()
-	local time = system.GetElapsedTime() / 10
+	local time = system.GetElapsedTime() 
 	for i, light in ipairs(lights) do
 		i = i / max
 		i = i * math.pi * 2
 		time = time + light.seed
-		light:SetPosition(Vec3(1.05, math.sin(time + i) * math.cos(time/2), math.cos(time + i) * math.sin(time/2)))
+		light:SetPosition((light.dir:GetRotated(Vec3(1,0,0), math.cos(time)) + light.dir:GetRotated(Vec3(0,1,0), math.sin(time))) * 4)
 	end
 	--ent:SetAngles(Ang3(time,time,0))
 end)
 
 local mat = render.CreateMaterial("model")
 mat:SetDiffuseTexture(Texture("textures/Cerberus_A.tga"))
-mat:SetNormalTexture(Texture("textures/Cerberus_N.tga"))
+mat:SetNormalTexture(Texture("textures/Cerberus_N.tga")) 
 mat:SetRoughnessTexture(Texture("textures/Cerberus_R.tga"))
 mat:SetMetallicTexture(Texture("textures/Cerberus_M.tga"))
 
 ent:SetMaterialOverride(mat)
-
-pbr_test = ent
---
+++ test/lua/tests/deferred/normals.lua
@@ -0,0 +1,41 @@
+for k,v in pairs(entities.GetAll()) do v:Remove() end
+
+render.camera_3d:SetPosition(Vec3(2.4, 0, -0))
+render.camera_3d:SetAngles(Ang3(0, math.pi, 0))
+
+local ent = entities.CreateEntity("visual")
+ent:SetModelPath("models/cube.obj")
+ent:SetSize(-1)
+
+local max = 5
+local lights = {}
+
+for i = 1, max do
+	local light = entities.CreateEntity("light")
+	light:SetColor(HSVToColor(i/max, 0.5, 1))
+	light:SetSize(1)
+	light:SetIntensity(2.5)
+	light.seed = math.random()*math.pi
+	table.insert(lights, light)
+end
+
+event.AddListener("Update", "test", function()
+	local time = system.GetElapsedTime() / 10
+	for i, light in ipairs(lights) do
+		i = i / max
+		i = i * math.pi * 2
+		time = time + light.seed
+		light:SetPosition(Vec3(1.05, math.sin(time + i) * math.cos(time/2), math.cos(time + i) * math.sin(time/2)))
+	end
+	--ent:SetAngles(Ang3(time,time,0))
+end)
+
+local mat = render.CreateMaterial("model")
+mat:SetDiffuseTexture(Texture("textures/Cerberus_A.tga"))
+mat:SetNormalTexture(Texture("textures/Cerberus_N.tga")) 
+mat:SetRoughnessTexture(Texture("textures/Cerberus_R.tga"))
+mat:SetMetallicTexture(Texture("textures/Cerberus_M.tga"))
+
+ent:SetMaterialOverride(mat)
+
+pbr_test = ent
