e79a5b18f193833beb4830a09dafeda52e4d84b3
CapsAdmin <eliashogstvedt@gmail.com>
2015-08-12 19:09:54 +0200

    added material entity

---
+++ src/lua/libraries/entities/components/material.lua
@@ -0,0 +1,52 @@
+local COMPONENT = {}
+
+COMPONENT.Name = "material"
+COMPONENT.Icon = "textures/silkicons/palette.png"
+
+function COMPONENT:OnAdd(ent)
+	local ent = ent:GetParent()
+	if ent:IsValid() then
+		self.mat = render.CreateMaterial(self.material_type)
+		local mdl = ent:GetComponent("model")
+		if mdl:IsValid() then
+			self.prev_mat = mdl:GetMaterialOverride()
+			mdl:SetMaterialOverride(self.mat)
+		end
+	end
+end
+
+function COMPONENT:OnRemove(ent)
+	local ent = ent:GetParent()
+	if ent:IsValid() then
+		if self.prev_mat then
+			local mdl = ent:GetComponent("model")
+			if mdl:IsValid() then
+				mdl:SetMaterialOverride(self.prev_mat)
+			end
+			self.prev_mat = nil
+		end
+		self.mat:Remove()
+	end
+end
+
+prototype.RegisterComponent(COMPONENT)
+
+event.AddListener("GBufferInitialized", "register_material_components", function()
+	for name, meta in pairs(prototype.GetRegisteredSubTypes("material")) do
+		local COMPONENT = {}
+
+		COMPONENT.Name = name .. "_material"
+		COMPONENT.Base = "material"
+		COMPONENT.Icon = "textures/silkicons/palette.png"
+
+		COMPONENT.material_type = name
+
+		prototype.StartStorable()
+			prototype.DelegateProperties(COMPONENT, meta, "mat")
+		prototype.EndStorable()
+		
+		prototype.RegisterComponent(COMPONENT)
+		
+		prototype.SetupComponents(COMPONENT.Name, {COMPONENT.Name}, COMPONENT.Icon)
+	end  
+end)
