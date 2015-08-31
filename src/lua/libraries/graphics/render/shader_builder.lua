3cb3bded8368d1242765366214f88cbe638b9e0e
CapsAdmin <eliashogstvedt@gmail.com>
2015-08-31 18:47:58 +0200

    whoopsie: ARB_enhanced_layouts > GL_ARB_enhanced_layouts

--- src/lua/libraries/graphics/render/shader_builder.lua
+++ src/lua/libraries/graphics/render/shader_builder.lua
@@ -229,7 +229,7 @@ local function variables_to_string(type, variables, prepend, macro, array)
 			if data.type == "sampler2D" or data.type == "samplerCube" then
 				local layout = ("layout(binding = %i)"):format(texture_channel)
 				
-				if not render.IsExtensionSupported("ARB_enhanced_layouts") then
+				if not render.IsExtensionSupported("GL_ARB_enhanced_layouts") then
 					layout = ""
 				end
 				
