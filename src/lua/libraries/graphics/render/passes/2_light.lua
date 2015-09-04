6fb611fa13eb1d316a3e0d3a2cd04ec93361f351
CapsAdmin <eliashogstvedt@gmail.com>
2015-09-04 16:58:22 +0200

    lighting optimizations

--- src/lua/libraries/graphics/render/passes/2_light.lua
+++ src/lua/libraries/graphics/render/passes/2_light.lua
@@ -159,48 +159,35 @@ PASS.Shader = {
 				
 				return vec3(0,0,0);
 			}
-			
-			const float e = 2.71828182845904523536028747135;
-			const float pi = 3.1415926535897932384626433832;
-			
-			float beckmannDistribution(float x, float roughness) {
-			  float NdotH = max(x, 0.0001);
-			  float cos2Alpha = NdotH * NdotH;
-			  float tan2Alpha = (cos2Alpha - 1.0) / cos2Alpha;
-			  float roughness2 = roughness;
-			  float denom = 3.141592653589793 * roughness2 * cos2Alpha * cos2Alpha;
-			  return exp(tan2Alpha / roughness2) / denom;
-			}
-			
+						
 			float get_specular(vec3 lightDirection, vec3 viewDirection, vec3 surfaceNormal, float roughness, float fresnel) 
-			{
-			  float VdotN = max(dot(viewDirection, surfaceNormal), 0.0);
-			  float LdotN = max(dot(lightDirection, surfaceNormal), 0.0);
-
-			  //Half angle vector
-			  vec3 H = normalize(lightDirection + viewDirection);
-
-			  //Geometric term
-			  float NdotH = max(dot(surfaceNormal, H), 0.0);
-			  float VdotH = max(dot(viewDirection, H), 0.000001);
-			  float LdotH = max(dot(lightDirection, H), 0.000001);
-			  float G1 = (2.0 * NdotH * VdotN) / VdotH;
-			  float G2 = (2.0 * NdotH * LdotN) / LdotH;
-			  float G = min(1.0, min(G1, G2));
+			{			
+				float VdotN = max(dot(viewDirection, surfaceNormal), 0.0);
+				vec3 H = normalize(lightDirection + viewDirection);
+				float NdotH = max(dot(surfaceNormal, H), 0.0001);
 			  
-			  //Distribution term
-			  float D = beckmannDistribution(NdotH, roughness);
-
-			  //Fresnel term
-			  float F = (1.0 - VdotN) * fresnel*4;
-
-			  //Multiply terms and done
-			  return  G * F * D / max(3.14159265 * VdotN, 0.000001);
+				float cos2Alpha = NdotH * NdotH;
+				float beck_dist = exp((cos2Alpha - 1.0) / cos2Alpha / roughness) / (3.141592653589793 * roughness * cos2Alpha);
+			
+				return 
+				min(
+					1.0, 
+					min(
+						(2.0 * NdotH * VdotN) / max(dot(viewDirection, H), 0.000001), 
+						(2.0 * NdotH * max(dot(lightDirection, surfaceNormal), 0.0)) / 
+						max(dot(lightDirection, H), 0.000001)
+					)
+				) * 
+				(1.0 - VdotN) * 
+				fresnel * 
+				4 * 
+				beck_dist / 
+				max(3.14159265 * VdotN, 0.000001);
 			}
-				
+			
 			void main()
 			{		
-				//{out_color.rgb = vec3(0.1); out_color.a = 1; return;}
+				//{out_color.rgb = vec3(0.01); out_color.a = 1; return;}
 			
 				vec2 uv = get_screen_uv();					
 				vec3 view_pos = get_view_pos(uv);
@@ -213,12 +200,12 @@ PASS.Shader = {
 				float roughness = get_roughness(uv);
 				
 				vec3 reflection = texture(tex_reflection, uv).rgb;
-				vec3 specular = vec3(get_specular(normalize(view_pos - light_view_pos), normalize(view_pos), -normal, (pow(roughness, 3)) + 0.0005, metallic));
-				
+				float specular = get_specular(normalize(view_pos - light_view_pos), normalize(view_pos), -normal, (roughness * roughness * roughness) + 0.0005, metallic);
 				
 				out_color.rgb = diffuse * mix(vec3(1,1,1), reflection, metallic);
-				out_color.rgb += specular * attenuate;
+				out_color.rgb += specular.rrr * attenuate;
 				out_color.rgb *= ambient + attenuate;
+				
 			
 			
 				out_color.a = 1;
