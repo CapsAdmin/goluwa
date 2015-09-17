1d07b2fa3d1fd465aa186a0b279e0a40c7d1d91a
CapsAdmin <eliashogstvedt@gmail.com>
2015-09-17 19:39:46 +0200

    more post process stuff

---
+++ src/lua/libraries/graphics/render/post_process/chromatic_abberation.lua
@@ -0,0 +1,31 @@
+local PASS = {}
+
+PASS.Name = FILE_NAME
+PASS.Default = false
+
+PASS.Source = [[
+out vec3 fragColor;
+
+void main()
+{    
+    // maximum aberration in number of pixels at uv.x == 0 or 1 (left or right edge)
+    const float redAberration = 10.0;
+    const float greenAberration =0.0;
+    const float blueAberration = -10.0;
+
+    float pctEffect = (uv.x - 0.5) * 2.0;
+
+    vec3 aberration = vec3(redAberration / g_screen_size.x, greenAberration / g_screen_size.x, blueAberration / g_screen_size.x);
+    aberration *= pctEffect;
+
+    vec3 col;
+	
+    col.r = texture(self, vec2(uv.x+aberration.x,uv.y)).x;
+    col.g = texture(self, vec2(uv.x+aberration.y,uv.y)).y;
+    col.b = texture(self, vec2(uv.x+aberration.z,uv.y)).z;
+
+    fragColor = col;
+}
+]]
+
+render.AddGBufferShader(PASS)
