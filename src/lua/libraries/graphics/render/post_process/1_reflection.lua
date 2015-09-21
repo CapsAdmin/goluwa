f9bdfd3282fef390c1e51bd00c402af1e68d514f
CapsAdmin <eliashogstvedt@gmail.com>
2015-09-21 02:46:14 +0200

    removed unused code

--- src/lua/libraries/graphics/render/post_process/1_reflection.lua
+++ src/lua/libraries/graphics/render/post_process/1_reflection.lua
@@ -74,8 +74,6 @@ table.insert(PASS.Source, {
 ]]
 })
 
-if true then
-
 for x = -1, 1 do
 	for y = -1, 1 do
 		if x == 0 and y == 0 then goto continue end
@@ -119,91 +117,6 @@ for x = -1, 1 do
 		::continue::
 	end
 end 
-
-elseif false then
-
-for x = -2, 2 do
-	for y = -2, 2 do
-		if x == 0 and y == 0 then goto continue end
-		table.insert(PASS.Source, {
-			buffer = {
-				size_divider = 1,
-				internal_format = "rgb8",
-			},
-			source = [[
-				out vec3 out_color;
-				
-				vec3 blur(vec2 tex, vec2 dir)
-				{
-					if (get_view_normal(tex) == vec3(0,0,0))
-						return vec3(1);
-				
-					float weights[9] =
-					{
-						0.013519569015984728,
-						0.047662179108871855,
-						0.11723004402070096,
-						0.20116755999375591,
-						0.240841295721373,
-						0.20116755999375591,
-						0.11723004402070096,
-						0.047662179108871855,
-						0.013519569015984728
-					};
-
-					const float indices[9] = {-4, -3, -2, -1, 0, +1, +2, +3, +4};
-
-					vec2 step = dir/g_screen_size.xy;
-
-					vec3 normal[9];
-
-					]] ..(function()
-						local str = ""
-						for i = 0, 8 do
-							str = str .. "normal["..i.."] = get_view_normal(tex + indices["..i.."]*step).xyz;\n"
-						end
-						return str
-					end)()..[[
-
-					float total_weight = 1.0;
-					float discard_threshold = 0.85;
-
-					int i;
-
-					for( i=0; i<9; ++i )
-					{
-						if( dot(normal[i].xyz, normal[4].xyz) < discard_threshold/* || abs(normal[i].w - normal[4].w) > 0.0001*/ )
-						{
-							total_weight -= weights[i];
-							weights[i] = 0;
-						}
-					}
-
-					//
-
-					vec3 res = vec3(0);
-
-					for( i=0; i<9; ++i )
-					{
-						res += texture(tex_stage_]]..#PASS.Source..[[, tex + indices[i]*step).rgb * weights[i];
-					}
-
-					res /= total_weight;
-
-					return res;
-				}
-				
-				void main()
-				{					 
-					out_color = blur(uv, vec2(]]..x..","..y..[[) * pow(get_roughness(uv) * 1.75, 4));
-				}
-			]]
-		})
-		::continue::
-	end
-end 
-
-end
 
 table.insert(PASS.Source, {
 	source = [[
