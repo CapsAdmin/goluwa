9474039787427be92744f53d0b72e81c59dfdb21
CapsAdmin <eliashogstvedt@gmail.com>
2015-09-21 19:33:20 +0200

    tweak jit options
    maxtrace needs to be higher or everything goes to hell

--- src/lua/libraries/system.lua
+++ src/lua/libraries/system.lua
@@ -606,11 +606,11 @@ end
 
 do
 	local current = {
-		maxtrace = 1000, -- Max. number of traces in the cache						default = 1000						
-		maxrecord = 4000, -- Max. number of recorded IR instructions                default = 4000
-		maxirconst = 500, -- Max. number of IR constants of a trace                default = 500
-		maxside = 100*2, -- Max. number of side traces of a root trace                default = 100
-		maxsnap = 500*4, -- Max. number of snapshots for a trace                     default = 500
+		maxtrace = 1000*5, -- Max. number of traces in the cache						default = 1000						
+		maxrecord = 4000*5, -- Max. number of recorded IR instructions                default = 4000
+		maxirconst = 500*5, -- Max. number of IR constants of a trace                default = 500
+		maxside = 100, -- Max. number of side traces of a root trace                default = 100
+		maxsnap = 500, -- Max. number of snapshots for a trace                     default = 500
 		minstitch = 0, -- Min. # of IR ins for a stitched trace.					default = 0
 		hotloop = 56, -- Number of iterations to detect a hot loop or hot call     default = 56
 		hotexit = 10, -- Number of taken exits to start a side trace                 default = 10
