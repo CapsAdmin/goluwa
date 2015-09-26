91f9c96393af5958bf2b4aba65a6781f39ff9df3
CapsAdmin <eliashogstvedt@gmail.com>
2015-09-26 10:23:28 +0200

    remove 'open steam/friends' from ARGS

--- launch_server.bat
+++ launch_server.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET CLIENT=0
 SET SERVER=1
-SET ARGS={'host','open steam/friends'}
+SET ARGS={'host'}
 PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "& '%~dp0src\launch_windows.ps1'"
