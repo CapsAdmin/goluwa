6cbf1e5fd46fcaaab4f3dcd70f31f87ee2580f1d
CapsAdmin <eliashogstvedt@gmail.com>
2015-09-28 17:28:31 +0200

    reduce some jit option settings to reduce chance of crashing

--- src/lua/libraries/system.lua
+++ src/lua/libraries/system.lua
@@ -607,10 +607,10 @@ end
 do
 	local current = {
 		maxtrace = 1000*10, -- Max. number of traces in the cache						default = 1000		min = 1	 max = 65535
-		maxrecord = 4000*5, -- Max. number of recorded IR instructions                default = 4000
-		maxirconst = 500*5, -- Max. number of IR constants of a trace                default = 500
+		maxrecord = 4000*2, -- Max. number of recorded IR instructions                default = 4000
+		maxirconst = 500*2, -- Max. number of IR constants of a trace                default = 500
 		maxside = 100, -- Max. number of side traces of a root trace                default = 100
-		maxsnap = 500*2, -- Max. number of snapshots for a trace                     default = 500
+		maxsnap = 500, -- Max. number of snapshots for a trace                     default = 500
 		minstitch = 0, -- Min. # of IR ins for a stitched trace.					default = 0
 		hotloop = 56*10, -- Number of iterations to detect a hot loop or hot call     default = 56
 		hotexit = 10, -- Number of taken exits to start a side trace                 default = 10
