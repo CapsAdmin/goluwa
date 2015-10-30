c7d13f9e357e1daf3a998cf96a901cefd67bcf7e
CapsAdmin <eliashogstvedt@gmail.com>
2015-10-30 15:17:56 +0100

    more work on resource browser

--- src/lua/tests/resource_browser.lua
+++ src/lua/tests/resource_browser.lua
@@ -1,3 +1,5 @@
+console.RunString("mount gmod")
+
 local frame = gui.CreatePanel("frame", nil, "lol")
 frame:SetSize(Vec2()+512)
 frame:CenterSimple()
@@ -26,18 +28,58 @@ tree:SetSize(Vec2() + 20000)
 
 bottom:SetDividerPosition(200)
 
---[[
-render.InitializeGBuffer()
-local ent = entities.CreateEntity("visual")
-ent:SetModel("models/cube.obj")
-local light = entities.CreateEntity("light")
+local ent = entities.CreateEntity("visual", entities.GetWorld())
+ent:SetModelPath("models/cube.obj")
+ent:SetVisible(false)
+local light = entities.CreateEntity("light", entities.GetWorld())
 light:SetSize(100)
-local function draw_scene(mat)
-	light:SetPosition(Vec3() + 2)
+local function draw_scene(mat, pos, rot, fov, w,h)
+	local old_view = render.camera_3d:GetView()
+	local old_projection = render.camera_3d:GetProjection()
+	local old_viewport = render.camera_3d:GetViewport()
+
+
+
+	local projection = Matrix44()
+	projection:Perspective(fov, render.camera_3d.FarZ, render.camera_3d.NearZ, w / h)
+
+	local view = Matrix44()
+	view:SetRotation(rot)
+	view:Translate(pos.y, pos.x, pos.z)
+	render.camera_3d:SetProjection(projection)
+	render.camera_3d:SetView(view)
+
+	render.camera_3d:SetViewport(Rect(0,0,w,h))
+
+local lol = render.active_framebuffer
 	ent:SetMaterialOverride(mat)
-	ent:Draw()
+	ent:SetVisible(true)
+	render.DrawGBuffer()
+	ent:SetVisible(false)
+
+print(render.active_framebuffer == lol)
+
+	render.camera_3d:SetViewport(old_viewport)
+	render.camera_3d:SetView(old_view)
+	render.camera_3d:SetProjection(old_projection)
 end
-]]
+
+
+local file_types = {
+	material = {"vmt"},
+	image = {"png", "jpeg", "dds", "vtf", "bmp", "tga"},
+	sound = {"wav", "ogg"},
+}
+
+local function get_file_type(path)
+	for file_type, extensions in pairs(file_types) do
+		for i, extension in ipairs(extensions) do
+			if path:endswith("." .. extension) then
+				return file_type
+			end
+		end
+	end
+end
 
 local function add_icon(full_path)
 	local dir, name = full_path:match("(.+)/(.+)")
@@ -52,19 +94,29 @@ local function add_icon(full_path)
 	label:SetText(name)
 	label:SetupLayout("bottom", "center_x_simple")
 
+	local file_type = get_file_type(full_path)
+
 	local icon = area:CreatePanel("base")
 	icon:SetSize(Vec2() + 128)
-	icon.OnMouseEnter = function()
-		if full_path:endswith(".vmt") then
-			--local mat = render.CreateMaterial("model")
-			--steam.LoadMaterial(full_path, mat)
-			--draw_scene(mat)
-		else
+	icon:SetColor(Color(1,1,1,0.5))
+
+	if file_type == "material" then
+		icon:SetCachedRendering(true)
+		local mat = render.CreateMaterial("model")
+		steam.LoadMaterial(full_path, mat)
+		icon.OnDraw = function() draw_scene(mat, Vec3()+10, QuatDeg3(45,45,0), 90, icon:GetWidth(), icon:GetHeight()) end
+	elseif file_type == "image" then
+		icon:SetTexture(Texture("loading"))
+		icon.OnMouseEnter = function()
 			local tex = Texture(full_path)
 			icon:SetTexture(tex)
 			icon:SetSize((Vec2() + 100) * tex:GetSize().x/tex:GetSize().y)
 			icon:SetupLayout("center_simple")
 		end
+	elseif file_type == "sound" then
+
+	else
+
 	end
 end
 
@@ -79,8 +131,6 @@ local function populate_icons(full_path)
 end
 
 local function populate(dir, node)
-	local folders = false
-
 	for _, full_path in pairs(vfs.Find(dir, nil, true)) do
 		local dir, name = full_path:match("(.+)/(.+)")
 		name = name or full_path
@@ -89,14 +139,15 @@ local function populate(dir, node)
 		if vfs.IsFolder(full_path) then
 			local node = node:AddNode(name, gui.skin.icons.folder)
 			node:SetExpandCallback(function(b)
-				if not populate(full_path .. "/", node) then
-					node.expand:SetVisible(false)
-				end
+				populate(full_path .. "/", node)
 			end)
 			node.OnSelect = function()
 				populate_icons(full_path)
 			end
-			folders = true
+
+			if #vfs.Find(full_path .. "/") == 0 then
+				node.expand:SetVisible(false)
+			end
 		else
 
 		end
@@ -104,11 +155,9 @@ local function populate(dir, node)
 	tree:SizeToChildrenHeight()
 
 	tree:Layout()
-
-	return folders
 end
 
-local where = "textures/"
+local where = "materials/"
 
 populate(where, tree)
 
