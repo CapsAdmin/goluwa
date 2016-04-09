98978aaa0849e74483d294c5a4c6830d15e2df61
CapsAdmin <eliashogstvedt@gmail.com>
2016-04-10 01:57:04 +0200

    added beizer curve object
    wip

---
+++ src/lua/examples/2d/beizer_curve.lua
@@ -0,0 +1,28 @@
+local tex = render.CreateTextureFromPath("http://bgfons.com/upload/rope_texture2073.jpg")
+
+local curve = surface.CreateQuadricBeizerCurve()
+--curve:Add(Vec2(114 , 90 ), Vec2(183 , 25))
+--curve:Add(Vec2(278 , 104), Vec2(322 , 161))
+--curve:Add(Vec2(287 , 241), Vec2(275 , 375))
+--curve:Add(Vec2(445 , 350), Vec2(560 , 312))
+--curve:Add(Vec2(621 , 416))
+
+curve:Add(Vec2(200,50))
+curve:Add(Vec2(400,50))
+curve:Add(Vec2(500,300))
+curve:Add(Vec2(100,300))
+
+
+local poly = curve:ConstructPoly(40, 1, 10)
+poly.mesh:SetMode("triangle_strip")
+
+event.AddListener("PostDrawMenu", "lol", function()
+	surface.SetColor(1,1,1,1)
+	surface.SetTexture(tex)
+
+	--surface.DrawRect(0,0,tex:GetSize():Unpack())
+
+	poly:Draw()
+
+	surface.SetWhiteTexture()
+end)
--- src/lua/libraries/graphics/surface/poly.lua
+++ src/lua/libraries/graphics/surface/poly.lua
@@ -327,3 +327,112 @@ function surface.CreatePoly(vertex_count)
 
 	return self
 end
+
+do
+	local META = prototype.CreateTemplate("quadric_beizer_curve")
+
+	META:GetSet("JoinLast", true)
+
+	function surface.CreateQuadricBeizerCurve()
+		local self = prototype.CreateObject(META)
+
+		self.nodes = {}
+
+		return self
+	end
+
+	function META:Add(point, control)
+		table.insert(self.nodes, {point = point, control = control})
+	end
+
+	local function quadratic_bezier(a, b, control, t)
+		return (1 - t) * (1 - t) * a + (2 - 2 * t) * t * control + b * t * t
+	end
+
+	function META:ConvertToPoints(quality)
+		quality = quality or 60
+
+		local points = {}
+		local precision = 1 / quality
+
+		for i, current in ipairs(self.nodes) do
+			local next = self.nodes[i + 1]
+			if self.JoinLast then
+				if not next then
+					next = self.nodes[1]
+				end
+			else
+				if not next then
+					break
+				end
+			end
+			local current_control = current.control or current.point:GetLerped(0.5, next.point)
+
+			for step = 0, 1, precision do
+				table.insert(points, quadratic_bezier(current.point, next.point, current_control, step))
+			end
+		end
+
+		return points
+	end
+
+	local function line_segment_normal(a, b)
+		return Vec2(b.y - a.y, a.x - b.x):Normalize()
+	end
+
+	function META:CreateOffsetedCurve(offset)
+		local offseted = surface.CreateQuadricBeizerCurve()
+
+		for i, current in ipairs(self.nodes) do
+			local next = self.nodes[i+1]
+			if self.JoinLast then
+				if not next then
+					next = self.nodes[1]
+				end
+			else
+				if not next then
+					break
+				end
+			end
+			local prev = self.nodes[i-1] or self.nodes[#self.nodes]
+			local current_control = current.control or current.point:GetLerped(0.5, next.point)
+			local prev_control = prev.control or prev.point:GetLerped(0.5, current.point)
+
+			local normal = line_segment_normal(current.point, current_control)
+			normal = normal + line_segment_normal(prev_control, current.point)
+			normal:Normalize()
+
+			local surface_normal = line_segment_normal(current.point, next.point)
+
+			offseted:Add(current.point + normal * offset, current_control + surface_normal * offset)
+		end
+
+		return offseted
+	end
+
+	function META:ConstructPoly(width, quality, stretch)
+		width = width or 30
+		stretch = stretch or 1
+
+		local negative_points = self:CreateOffsetedCurve(-width):ConvertToPoints(quality)
+		local positive_points = self:CreateOffsetedCurve(width):ConvertToPoints(quality)
+
+		local poly = surface.CreatePoly(#positive_points * 2)
+		local distance_positive = 0
+		local distance_negative = 0
+
+		for i, positive in ipairs(positive_points) do
+			if i > 1 then
+				distance_positive = distance_positive +
+				(negative_points[i - 1]:Distance(negative_points[i]) + positive_points[i - 1]:Distance(positive_points[i])) / stretch / 2
+			end
+
+			poly:SetVertex((i - 1) * 2, negative_points[i].x, negative_points[i].y, distance_positive, 0)
+			poly:SetVertex((i - 1) * 2 + 1, positive_points[i].x, positive_points[i].y, distance_positive, 1)
+		end
+
+		return poly
+	end
+
+	META:Register()
+end
