ccb38efa0a570d87cd132ea6c6f6f0bf7962aeb1
CapsAdmin <eliashogstvedt@gmail.com>
2016-04-24 15:10:52 +0200

    updated strung

--- src/lua/modules/strung/strung.lua
+++ src/lua/modules/strung/strung.lua
@@ -655,7 +655,7 @@ struct M {
 }]] local M = new"struct M" -- fields of the "_M_atchers" table.
 
 function compile (pat, mode) -- local, declared above
-  local anchored = (pat:sub(1,1) == "^")
+  local anchored = (pat:sub(1,1) == "^") and mode ~= "gmatch"
   local caps, sets = {open = 0, type={}}, {}
   local data = {}
   local buf = {
@@ -729,7 +729,7 @@ function compile (pat, mode) -- local, declared above
 
   -- load the source
   local source = t_concat(buf)
-  local loader, err = loadstring(source, mode .. " " .. pat)
+  local loader, err = loadstring(source)
   if not loader then error(source.."\nERROR:"..err) end
   local code = loader(bittest, charsets, capsptr, constchar, expose)
   return {code,   source,   ncaps,   capsptr, anchored}
@@ -965,8 +965,7 @@ local gsub do
     end
   end
 
-  local function select_handler (ncaps, repl)
-    t = type(repl)
+  local function select_handler (ncaps, repl, t)
     if t == "string" then
       if repl:find("%%") then
         return string_with_captures_handler, string_with_captures_handler
@@ -978,7 +977,7 @@ local gsub do
     elseif t == "function" then
       return function_handler, producers[ncaps]
     else
-      error("Bad replacement type for GSUB TODO IMPROVE MESSAGE.")
+      error("bad argument #3 to 'strung.gsub' (string/function/table expected)")
     end
   end
 
@@ -990,7 +989,13 @@ local gsub do
 
     if not success then return subj, 0 end
 
-    local handler, producer = select_handler(c[M.NCAPS], repl)
+    local t = type(repl)
+
+    if t == "number" then
+      repl, t = tostring(repl), "string"
+    end
+
+    local handler, producer = select_handler(c[M.NCAPS], repl, t)
 
     -- Anchored patterns should not be matched more than once.
     if c[M.ANCHORED] then n = 1 end
