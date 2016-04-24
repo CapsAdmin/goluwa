ce1e30c9f954e9b2b578547343a784548c6b8f7c
CapsAdmin <eliashogstvedt@gmail.com>
2016-04-25 01:40:34 +0200

    add table.reverse(tbl)

--- src/lua/libraries/extensions/table.lua
+++ src/lua/libraries/extensions/table.lua
@@ -43,6 +43,12 @@ function table.isarray(t)
 	return true
 end
 
+function table.reverse(tbl)
+	for i = 1, math.floor(#tbl / 2) do
+		tbl[i], tbl[#tbl - i + 1] = tbl[#tbl - i + 1], tbl[i]
+	end
+end
+
 -- 12:34 - <mniip> http://codepad.org/cLaX7lVn
 function table.multiremove(tbl, locations)
 
