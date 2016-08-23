69b8c6ac6605486b94274150e105556cf712b477
CapsAdmin <eliashogstvedt@gmail.com>
2016-08-23 13:48:06 +0200

    optimizations

--- src/lua/libraries/system.lua
+++ src/lua/libraries/system.lua
@@ -92,11 +92,11 @@ do
 	function system.UpdateTitlebarFPS(dt)
 		if not show:Get() then return end
 
-		total = total + 1/dt
+		total = total + dt
 		count = count + 1
 
 		if wait(1) then
-			system.SetConsoleTitle(("FPS: %i"):format(total / count), "fps")
+			system.SetConsoleTitle(("FPS: %i"):format(1/(total / count)), "fps")
 			system.SetConsoleTitle(("GARBAGE: %s"):format(utility.FormatFileSize(collectgarbage("count") * 1024)), "garbage")
 
 			if GRAPHICS then
