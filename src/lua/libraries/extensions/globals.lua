a1702a58131011c73de509917b0c8fa1854e30da
CapsAdmin <eliashogstvedt@gmail.com>
2016-08-27 13:37:54 +0200

    fix trace abort when using setmetatable

--- src/lua/libraries/extensions/globals.lua
+++ src/lua/libraries/extensions/globals.lua
@@ -3,24 +3,29 @@ do
 	local rawget = rawget
 	local getmetatable = getmetatable
 	local newproxy = newproxy
+
+	local function gc(s)
+		local tbl = getmetatable(s).__div
+		rawset(tbl, "__gc_proxy", nil)
+
+		local new_meta = getmetatable(tbl)
+
+		if new_meta then
+			local __gc = rawget(new_meta, "__gc")
+			if __gc then
+				__gc(tbl)
+			end
+		end
+	end
+
 	-- 52 compat
 	function setmetatable(tbl, meta)
 		if meta and rawget(meta, "__gc") and not rawget(tbl, "__gc_proxy") then
 			local proxy = newproxy(true)
 			rawset(tbl, "__gc_proxy", proxy)
 
-			getmetatable(proxy).__gc = function()
-				rawset(tbl, "__gc_proxy", nil)
-
-				local new_meta = getmetatable(tbl)
-
-				if new_meta then
-					local __gc = rawget(new_meta, "__gc")
-					if __gc then
-						__gc(tbl)
-					end
-				end
-			end
+			getmetatable(proxy).__div = tbl
+			getmetatable(proxy).__gc = gc
 		end
 
 		return _OLD_G.setmetatable(tbl, meta)
