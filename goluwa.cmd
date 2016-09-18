9095ef7efbfd52ec8f073cae427f80c08818d0bb
CapsAdmin <eliashogstvedt@gmail.com>
2016-09-18 14:37:19 -0700

    rewrote windows launch scripts

--- goluwa.cmd
+++ goluwa.cmd
@@ -1,3 +1,2 @@
-@ECHO OFF
-SET IDE=1
-PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dp0src\cli\windows.ps1'"
+@echo off
+PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0src\cli\windows.ps1" ide
--- src/cli/windows.ps1
+++ src/cli/windows.ps1
@@ -1,110 +1,99 @@
-function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
+$arg=$args[0]
+$ROOT_DIR = $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$ROOT_DIR = $([System.IO.Path]::GetFullPath("$ROOT_DIR\..\..\data"))
 
-function download($url, $loc)
-{
-	Write-Output "downloading binaries from $url to $loc"
-	
-	#remove any previous attempted downloads
-	Remove-Item "$loc.zip" -ErrorAction SilentlyContinue -Confirm:$false -Recurse:$true
+New-Item -ItemType Directory -Force -Path $ROOT_DIR | Out-Null
+Set-Location $ROOT_DIR
 
-	
-	if (Get-Module -ListAvailable -Name BitsTransfer) {
-		Import-Module BitsTransfer
-		Start-BitsTransfer -Source $url -Destination $loc
-		echo "LOL!!!!"
+if ($ENV:PROCESSOR_ARCHITEW6432 -Match "64") { 
+	$ARCH = "x64" 
+} else { 
+	$ARCH = "x86" 
+}
+
+$ide_url = "https://github.com/pkulchenko/ZeroBraneStudio/archive/master.zip"
+$bin_url = "https://github.com/CapsAdmin/goluwa/releases/download/windows-binaries/$ARCH.zip"
+
+function Remove($path) {
+	if(Test-Path "$path" -PathType Container) {
+		Write-Host -NoNewline "removing directory: '$ROOT_DIR\$path' ... "
+		Get-ChildItem -Path "$path\\*" -Recurse -Force | Remove-Item -Force -Recurse
+		Remove-Item $path -Recurse -Force
+		Write-Host "OK"
+	} elseif(Test-Path "$path" -PathType Leaf) {
+		Write-Host -NoNewline "removing file: '$ROOT_DIR\$path' ... "
+		Remove-Item -Force "$path"
+		Write-Host "OK"
 	} else {
-		#(New-Object System.Net.WebClient).DownloadFile($url, $loc)
+		Write-Host "could not find: $path"
 	}
 }
 
-function extract([string] $source, [string] $dir, [string] $zip_dir)
-{
-	Write-Output "unzipping files from $source/$zip_dir to $dir"
-	$shell = New-Object -ComObject Shell.Application
+function Download($url, $location) {
+	if(!(Test-Path "$location")) {
+		Write-Host -NoNewline "'$url' >> '$ROOT_DIR\$location' ... "
+		#if (Get-Module -ListAvailable -Name BitsTransfer) {
+		#	Import-Module BitsTransfer
+		#	Start-BitsTransfer -Source $url -Destination $location
+		#} else {
+			(New-Object System.Net.WebClient).DownloadFile($url, "$pwd\$location")
+		#}
+		Write-Host "OK"
+	} else {
+		Write-Host "'$ROOT_DIR\$location' already exists"
+	}
+}
 
-	$zip = $shell.NameSpace("$source\$zip_dir")
+function Extract($file, $location, $move_files) {
+	Write-Host -NoNewline "$file >> '$location' ... "
+
+	$shell = New-Object -Com Shell.Application
 	
-	foreach($item in $zip.items())
+	$zip = $shell.NameSpace($([System.IO.Path]::GetFullPath("$pwd\$file")))
+	
+	if (!$zip) {
+		Write-Error "could not extract $ROOT_DIR\$file!"
+	}
+	
+	if (!(Test-Path $location)) {
+		New-Item -ItemType directory -Path $location | Out-Null
+	}
+
+	foreach($item in $zip.items()) {
+		$shell.Namespace("$pwd\$location").CopyHere($item, 0x14)
+	}
+	
+	if ($move_files)
 	{
-		$shell.Namespace($dir).copyhere($item)
+		Move-Item -Confirm:$false -Force -Path "$location\*\*" -Destination "$location"
+	}
+
+	Write-Host "OK"
+}
+
+if($arg -eq "ide") {	
+	if (!(Test-Path ("ide\zbstudio.exe"))) {
+		Download $ide_url temp.zip
+		Extract temp.zip ide $true
+		Remove temp.zip
 	}
 	
-	Remove-Item $source -ErrorAction SilentlyContinue -Confirm:$false -Recurse:$true
+	Set-Location "ide"
+	.\zbstudio.exe -cfg ../../src/lua/zerobrane/config.lua
 }
 
-$src = @'
-    [DllImport("Kernel32.dll")]
-    public static extern IntPtr GetConsoleWindow();
-    [DllImport("User32.dll")]
-    public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'@
-
-Add-Type -Name ConsoleUtils -Namespace Foo -MemberDefinition $src
-$hWnd = [Foo.ConsoleUtils]::GetConsoleWindow()
-
-if ($ENV:PROCESSOR_ARCHITECTURE -Match "64"){ $arch = "x64" } else { $arch = "x86" }
-
-$binaries_url = "https://github.com/CapsAdmin/goluwa/releases/download/windows-binaries/" + $arch + ".zip"
-$binaries_dir = [IO.Path]::GetFullPath($(PSScriptRoot) + "\..\..\data\bin\windows_" + $arch)
-
-if (!(Test-Path $binaries_dir)) 
-{
-	New-Item -path $binaries_dir -type directory
-}
-
-if(!(Test-Path ($binaries_dir + "\luajit.exe")))
-{
-	[Foo.ConsoleUtils]::ShowWindow($hWnd, 1)
-	
-	download $binaries_url "$binaries_dir.zip"
-	extract "$binaries_dir.zip" $binaries_dir
-}
-
-if ($env:IDE)
-{
-	$ide_url = "https://github.com/pkulchenko/ZeroBraneStudio/archive/master.zip"
-	$ide_dir = [IO.Path]::GetFullPath($(PSScriptRoot) + "\..\..\data\ide")
-
-	if (!(Test-Path $ide_dir)) 
-	{
-		New-Item -path $ide_dir -type directory
-	}
-	
-	if(!(Test-Path ($ide_dir + "\zbstudio.exe")))
-	{
-		download $ide_url "$ide_dir.zip"
-		extract "$ide_dir.zip" $ide_dir "ZeroBraneStudio-master"	
-	}
-	
-	cd $ide_dir
-	Start-Process -FilePath "zbstudio.exe" -ArgumentList "-cfg ../../src/lua/zerobrane/config.lua"
-}
-else
-{
+if ($arg -eq "launch" -Or $arg -eq "") {
 	Write-Output "launching"
+	
+	$bin_dir = "bin\windows_$ARCH"
 
-	[Foo.ConsoleUtils]::ShowWindow($hWnd, 0)
-
-	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-	$pinfo.FileName = $binaries_dir + "\luajit.exe"
-	$pinfo.WorkingDirectory = $binaries_dir + "\"
-	$pinfo.RedirectStandardError = $true
-	$pinfo.RedirectStandardOutput = $true
-	$pinfo.UseShellExecute = $false
-	$pinfo.Arguments = "../../../src/lua/init.lua"
-	$p = New-Object System.Diagnostics.Process
-	$p.StartInfo = $pinfo
-	$p.Start() | Out-Null
-	#$p.WaitForExit()
-	$stdout = $p.StandardOutput.ReadToEnd()
-	$stderr = $p.StandardError.ReadToEnd()
-
-	Write-Host "======== stdout ========`n$stdout========================`n`n"
-	Write-Host "======== stderr ========`n$stderr========================`n`n"
-	Write-Host "exit code: " $p.ExitCode "`n`n"
-
-	if ($stderr -or ($p.ExitCode -ne 0))
-	{
-		[Foo.ConsoleUtils]::ShowWindow($hWnd, 1)
+	if (!(Test-Path "$bin_dir\luajit.exe")) {	
+		Download $bin_url temp.zip
+		Extract temp.zip $bin_dir
+		Remove temp.zip
 	}
+
+	Set-Location $pwd\$bin_dir\
+	
+	.\luajit.exe ../../../src/lua/init.lua
 }
