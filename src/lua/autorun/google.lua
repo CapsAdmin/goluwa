710c2a1dbe9b6b55031d9664b582e3168b596ac5
CapsAdmin <eliashogstvedt@gmail.com>
2016-09-29 00:02:03 +0200

    some work on google translate

--- src/lua/autorun/google.lua
+++ src/lua/autorun/google.lua
@@ -1,16 +1,106 @@
 google = {}
 
 do
-	local base_url = "http://translate.google.com/translate_a/t?client=t&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&q=%s"
+	local base_url = "https://translate.google.com/translate_a/single?client=t&sl={FROM}&tl={TO}&hl={FROM}&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=2&rom=1&ssel=0&tsel=0&kc=3&tk={TOKEN}&q="
+
+	local xr = function(a, chunk)
+		for offset = 0, #chunk - 3, 3 do
+			local b = chunk:byte(offset + 3)
+			b = ("a"):byte() <= b and b - 87 or b
+			b = "+" == chunk:byte(offset + 2) and bit.rshift(a, b) or bit.lshift(a, b)
+			a = "+" == chunk:byte(offset + 1) and a + bit.band(b, 4294967295) or bit.bxor(a, b)
+		end
+
+		return a
+	end
+
+	function gen_token(input)
+		local b = {}
+		local bi = 0
+		for i = 0, #input - 1 do
+			local frame = input:byte(i+1)
+			if 128 > frame then
+				b[bi] = frame
+				bi = bi + 1
+			else
+				if 2048 > frame then
+					b[bi] = bit.bor(bit.rshift(frame, 6), 192)
+					bi = bi + 1
+				else
+					if 55296 == bit.band(frame, 64512) and (i + 1 < #input and 56320 == bit.band(input:byte(i + 1), 64512)) then
+						frame = 65536 + bit.lshift(bit.band(frame, 1023), 10) + bit.band(input:byte(i + 1), 1023)
+
+						b[bi] = bit.bor(bit.rshift(frame, 18), 240)
+						bi = bi + 1
+						b[bi] = bit.bor(bit.band(bit.rshift(frame, 12), 63), 128)
+						bi = bi + 1
+					else
+						b[bi] = bit.bor(bit.rshfit(frame, 12), 224)
+						bi = bi + 1
+					end
+
+					b[bi] = bit.bor(bit.band(bit.rshift(frame, 6), 63), 128)
+					bi = bi + 1
+				end
+
+				b[bi] = bit.bor(bit.band(frame, 63), 128)
+				bi = bi + 1
+			end
+		end
+
+		local out = 0
+
+		for bi = 0, #b - 1 do
+			out = out + b[bi]
+			out = xr(out, "+-a^+6")
+		end
+		out = xr(out, "+-3^+b+-f")
+		out = bit.bxor(out, base_token[1]) or 0
+		if 0 > out then
+			out = bit.band(out, 2147483647) + 2147483648
+		end
+		out = out % 1E6
+		return out .. "." .. bit.bxor(out, base_token[2])
+	end
+
+	--[[local eval_str = "('((function(){var a\x3d2504491494;var b\x3d-797604507;return 409749+\x27.\x27+(a+b)})())')"
+	local a = eval_str:match("var a=(.-);")
+	local b = eval_str:match("var b=(.-);")
+	local c = eval_str:match("return (%d+)")
+
+	base_token = c .. "." .. (a+b)
+	base_token = base_token:split(".")]]
 
 	function google.Translate(from, to, str, callback)
+
+		if not base_token then
+			-- i get 403 for some reason but if i can get past that getting the base token should work
+			sockets.Get("http://translate.google.com", function(data)
+				local eval_str = data.content:match("TKK=eval(%b())")
+
+				local a = eval_str:match("var a=(.-);")
+				local b = eval_str:match("var b=(.-);")
+				local c = eval_str:match("return (%d+)")
+
+				base_token = c .. "." .. (a+b)
+				base_token = base_token:split(".")
+			end)
+			return
+		end
+
 		from = from or "auto"
 		to = to or "en"
 		str = str or ""
 
-		local url = base_url:format(from, to, sockets.EscapeURL(str))
+
+		local url = base_url
+		url = url:gsub("{FROM}", from)
+		url = url:gsub("{TO}", to)
+		url = url:gsub("{TOKEN}", gen_token(str)) -- untested, copied from js source
+		url = url .. sockets.EscapeURL(str)
 
 		sockets.Get(url, function(data)
+			table.print(data)
 			local out = {translated = "", transliteration = "", from = ""}
 			local content = data.content:match(".-%[(%b[])"):sub(2, -2)
 
