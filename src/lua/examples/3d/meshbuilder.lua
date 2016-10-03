36601cec2510b6e768f125cb95a16eb2a33b7366
CapsAdmin <eliashogstvedt@gmail.com>
2016-10-03 02:08:02 +0200

    fixed some examples

--- src/lua/examples/3d/meshbuilder.lua
+++ src/lua/examples/3d/meshbuilder.lua
@@ -1,5 +1,5 @@
-local heightmap = render.CreateTextureFromPath("https://dl.dropboxusercontent.com/u/244444/heightmaps/HeightMap_Projects/HeightMap6.jpg")
-local diffuse = render.CreateTextureFromPath("https://dl.dropboxusercontent.com/u/244444/heightmaps/HeightMap_Projects/HeightMap_6BaseTexture.jpg")
+local heightmap = render.CreateTextureFromPath("textures/heightmap/HeightMap6.jpg")
+local diffuse = render.CreateTextureFromPath("textures/heightmap/HeightMap_6BaseTexture.jpg")
 
 local model = render.CreateMeshBuilder()
 model:LoadHeightmap(heightmap, heightmap:GetSize()/5, Vec2(128, 128), -200)
@@ -9,4 +9,7 @@ model:Upload()
 local ent = utility.RemoveOldObject(entities.CreateEntity("visual"), "lol")
 ent:SetCull(false)
 ent:AddMesh(model)
-ent:SetAlbedoTexture(diffuse)
+
+local mat = render.CreateMaterial("model")
+mat:SetAlbedoTexture(diffuse)
+ent:SetMaterialOverride(mat)
