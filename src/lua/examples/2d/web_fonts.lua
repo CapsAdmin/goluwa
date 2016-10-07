5c74252b1b9174d28d76bb95c0dc2bd5e6cb0a04
CapsAdmin <eliashogstvedt@gmail.com>
2016-10-08 00:24:07 +0200

    updated examples

--- src/lua/examples/2d/web_fonts.lua
+++ src/lua/examples/2d/web_fonts.lua
@@ -1,12 +1,64 @@
-ï»¿local font = surface.CreateFont({
-	path = "Aladin",
-	size = 50,
-})
+local fonts = {
+	surface.CreateFont({
+		path = "aladin",
+		size = 54,
+	}),
+	surface.CreateFont({
+		path = "angeline vintage",
+		size = 30,
+	}),
+	surface.CreateFont({
+		path = "roboto bold",
+		size = 60,
+	}),
+	surface.CreateFont({
+		path = "roboto italic",
+		size = 60,
+	}),
+	surface.CreateFont({
+		path = "Ruslan Display",
+		size = 60,
+	}),
+	surface.CreateFont({
+		path = "arial",
+		size = 60,
+	}),
+	surface.CreateFont({
+		path = "courier new",
+		size = 40,
+	}),
+	surface.CreateFont({
+		path = "tahoma",
+		size = 40,
+	}),
+	surface.CreateFont({
+		path = "helvetica",
+		size = 40,
+	}),
+	--[[surface.CreateFont({
+		path = "fonts/resource_imagefont1.png",
+		glyphs = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]\"",
+	}),
+	surface.CreateFont({
+		path = "fonts/boldfont.png",
+		glyphs = " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!'-:*@<>+/_$&?",
+	}),]]
+}
 
 event.AddListener("PreDrawGUI", "lol", function()
-	surface.SetColor(1,1,1,1)
+	local y = 0
+	for _, font in ipairs(fonts) do
+		local str = font:GetName()
+		local size = Vec2(font:GetTextSize(str))
+		surface.SetColor(1,1,1,1)
+		surface.SetFont(font)
+		surface.SetTextPosition(30, 30 + y)
+		surface.DrawText(str)
 
-	surface.SetFont(font)
-	surface.SetTextPosition(10, 10)
-	surface.DrawText("Aladin ã…åœ‹åœ‹ã„´ã…‡ ã…ã…ã…‡ >> í¼‡í¾¿Ñ¾ << no aliens in unifont :(")
+		surface.SetWhiteTexture()
+		surface.SetColor(1,0,0,0.25)
+		surface.DrawRect(30, 30 + y, size.x, size.y)
+
+		y = y + size.y + 10
+	end
 end)
--- src/lua/examples/code_statistics.lua
+++ src/lua/examples/code_statistics.lua
@@ -12,24 +12,36 @@ local blacklist = {
 	"header.lua",
 	"enums.lua",
 	"utf8data.lua",
-	"/modules/",
 	"_emotes.lua",
 	"icons.lua",
+	"capsadmin",
+}
+
+local blacklist_dir = {
+	"/modules",
+	"/capsdamin",
+	"/gmod",
+	"/lovemu",
+	"/build",
+	"/repo",
 }
 
 local words = {}
+local done = {}
 
-for _, path in ipairs(vfs.Search("lua/", ".lua")) do
+for _, path in ipairs(vfs.Search("os:/media/caps/ssd_840_120gb/goluwa/src/lua/", ".lua", nil, blacklist_dir)) do
 	for i,v in ipairs(blacklist) do
 		if path:find(v) then
 			goto continue
 		end
 	end
 
+	if done[path] then goto continue end
+
 	local str = vfs.Read(path)
 
 	if str then
-		local lines = str:count("\n")
+		local lines = str:gsub("[\n]+", "\n"):count("\n")
 		data.total_lines = data.total_lines + lines
 		str = str:gsub("%s+", " ")
 		data.total_words = data.total_words + str:count(" ")
@@ -38,10 +50,13 @@ for _, path in ipairs(vfs.Search("lua/", ".lua")) do
 		for i, word in ipairs(str:split(" ")) do
 			words[word] = (words[word] or 0) + 1
 		end
+
 		table.insert(data.files, {path = path, lines = lines})
 	else
 		print(path)
 	end
+
+	done[path] = true
 
 	::continue::
 end
@@ -56,7 +71,7 @@ end
 
 table.sort(data.words, function(a, b) return a.count > b.count end)
 
-for i = 20 + 1, #data.words do
+for i = 50 + 1, #data.words do
 	data.words[i] = nil
 	data.files[i] = nil
 end
