c6532552cf8c1721005d2a3188924c1392ceca2f
CapsAdmin <eliashogstvedt@gmail.com>
2016-10-13 02:57:25 +0200

    __gc: use table.gcnew() if exists

--- src/lua/libraries/extensions/globals.lua
+++ src/lua/libraries/extensions/globals.lua
@@ -4,31 +4,59 @@ do
 	local getmetatable = getmetatable
 	local newproxy = newproxy
 
-	local function gc(s)
-		local tbl = getmetatable(s).__div
-		rawset(tbl, "__gc_proxy", nil)
+	if pcall(require, "table.gcnew") then
+		local function gc(s)
+			local tbl = s.tbl
+			rawset(tbl, "__gc_proxy", nil)
 
-		local new_meta = getmetatable(tbl)
+			local new_meta = getmetatable(tbl)
 
-		if new_meta then
-			local __gc = rawget(new_meta, "__gc")
-			if __gc then
-				__gc(tbl)
+			if new_meta then
+				local __gc = rawget(new_meta, "__gc")
+				if __gc then
+					__gc(tbl)
+				end
 			end
 		end
-	end
 
-	-- 52 compat
-	function setmetatable(tbl, meta)
-		if meta and rawget(meta, "__gc") and not rawget(tbl, "__gc_proxy") then
-			local proxy = newproxy(true)
-			rawset(tbl, "__gc_proxy", proxy)
+		-- 52 compat
+		function setmetatable(tbl, meta)
+			if meta and rawget(meta, "__gc") and not rawget(tbl, "__gc_proxy") then
+				local proxy = _OLD_G.setmetatable(table.gcnew(), {__gc = gc})
+				proxy.tbl = tbl
 
-			getmetatable(proxy).__div = tbl
-			getmetatable(proxy).__gc = gc
+				rawset(tbl, "__gc_proxy", proxy)
+			end
+
+			return _OLD_G.setmetatable(tbl, meta)
+		end
+	else
+		local function gc(s)
+			local tbl = getmetatable(s).__div
+			rawset(tbl, "__gc_proxy", nil)
+
+			local new_meta = getmetatable(tbl)
+
+			if new_meta then
+				local __gc = rawget(new_meta, "__gc")
+				if __gc then
+					__gc(tbl)
+				end
+			end
 		end
 
-		return _OLD_G.setmetatable(tbl, meta)
+		-- 52 compat
+		function setmetatable(tbl, meta)
+			if meta and rawget(meta, "__gc") and not rawget(tbl, "__gc_proxy") then
+				local proxy = newproxy(true)
+				rawset(tbl, "__gc_proxy", proxy)
+
+				getmetatable(proxy).__div = tbl
+				getmetatable(proxy).__gc = gc
+			end
+
+			return _OLD_G.setmetatable(tbl, meta)
+		end
 	end
 end
 do -- logging
