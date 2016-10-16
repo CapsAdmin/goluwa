e0244c008bb95d31bb62673dbd95a9ffa1c6dd20
CapsAdmin <eliashogstvedt@gmail.com>
2016-10-16 18:41:06 +0200

    update tests

--- src/lua/examples/2d/beizer_curve.lua
+++ src/lua/examples/2d/beizer_curve.lua
@@ -7,22 +7,28 @@ local curve = gfx.CreateQuadricBezierCurve()
 --curve:Add(Vec2(445 , 350), Vec2(560 , 312))
 --curve:Add(Vec2(621 , 416))
 
-curve:Add(Vec2(200,50))
-curve:Add(Vec2(400,50))
-curve:Add(Vec2(500,300))
-curve:Add(Vec2(100,300))
+curve:Add(Vec2(0,0))
+curve:Add(Vec2(1,0))
+curve:Add(Vec2(1,1))
+curve:Add(Vec2(0,1))
 
 
-local poly = curve:ConstructPoly(40, 1, 10)
+local poly = curve:ConstructPoly(Vec2(-0.1, 0), 4, 5)
 poly.mesh:SetMode("triangle_strip")
 
 event.AddListener("PostDrawGUI", "lol", function()
+	--surface.DrawRect(0,0,tex:GetSize():Unpack())
+
+	surface.SetWhiteTexture()
+	surface.DrawRect(50,50,500,500)
+
 	surface.SetColor(1,1,1,1)
 	surface.SetTexture(tex)
 
-	--surface.DrawRect(0,0,tex:GetSize():Unpack())
+	surface.PushMatrix(50, 50, 500, 500)
+		poly:Draw()
+	surface.PopMatrix()
 
-	poly:Draw()
 
 	surface.SetWhiteTexture()
 end)
