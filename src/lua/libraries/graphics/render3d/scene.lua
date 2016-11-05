535ad000eec918c2c0ff094f8f8fa6b5dff9aa6d
CapsAdmin <eliashogstvedt@gmail.com>
2016-11-05 20:16:21 +0100

    added distance sorted scene

--- src/lua/libraries/graphics/render3d/scene.lua
+++ src/lua/libraries/graphics/render3d/scene.lua
@@ -3,6 +3,9 @@ local render3d = (...) or _G.render3d
 render3d.scene = render3d.scene or {}
 local scene_keyval = utility.CreateWeakTable()
 
+render3d.scene_dist = render3d.scene_dist or {}
+local scene_keyval_dist = utility.CreateWeakTable()
+
 local needs_sorting = true
 
 function render3d.AddModel(model)
@@ -11,12 +14,19 @@ function render3d.AddModel(model)
 		needs_sorting = true
 		scene_keyval[model] = model
 	end
+	if not scene_keyval_dist[model] then
+		table.insert(render3d.scene_dist, model)
+		scene_keyval_dist[model] = model
+	end
 end
 
 function render3d.RemoveModel(model)
 	if scene_keyval[model] then
 		table.removevalue(render3d.scene, model)
 		needs_sorting = true
+	end
+	if scene_keyval_dist[model] then
+		table.removevalue(render3d.scene_dist, model)
 	end
 end
 
@@ -32,6 +42,18 @@ function render3d.SortScene()
 	end)
 end
 
+function render3d.SortDistanceScene(reverse)
+	if reverse then
+		table.sort(render3d.scene_dist, function(a, b)
+			return a.tr:GetCameraDistance() > b.tr:GetCameraDistance()
+		end)
+	else
+		table.sort(render3d.scene_dist, function(a, b)
+			return a.tr:GetCameraDistance() < b.tr:GetCameraDistance()
+		end)
+	end
+end
+
 function render3d.DrawScene(what)
 	event.Call("DrawScene")
 
@@ -43,4 +65,12 @@ function render3d.DrawScene(what)
 	for _, model in ipairs(render3d.scene) do
 		model:Draw(what)
 	end
+end
+
+function render3d.GetScene()
+	return render3d.scene
+end
+
+function render3d.GetDistanceSortedScene()
+	return render3d.scene_dist
 end
