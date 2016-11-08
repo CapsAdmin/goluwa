fbcfde02799dc6eb7e5f748286e3b424b6054285
CapsAdmin <eliashogstvedt@gmail.com>
2016-11-08 08:10:34 +0000

    cubemap example

---
+++ src/lua/examples/3d/cubemap.lua
@@ -0,0 +1,40 @@
+steam.MountSourceGame("csgo")
+
+local tex = render.CreateTexture("cube_map")
+tex:SetSize(Vec2() + 1024)
+tex:SetMinFilter("linear")
+tex:SetMagFilter("linear")
+tex:SetWrapS("clamp_to_edge")
+tex:SetWrapT("clamp_to_edge")
+tex:SetWrapR("clamp_to_edge")
+tex:SetSeamlessCubemap(true)
+tex:SetupStorage()
+tex:LoadCubemap("materials/skybox/vietnam.vtf")
+
+local shader = render.CreateShader({
+	name = "cubemap",
+	fragment = {
+		mesh_layout = {
+			{uv = "vec2"},
+		},
+		variables = {
+			cubemap = tex,
+		},
+		source = [[
+			vec4 out_color;
+			void main()
+			{
+				out_color = vec4(1,0,1,1);//texture(cubemap, get_camera_dir(uv));
+			}
+		]],
+	},
+})
+
+event.AddListener("PreDrawGUI", "lol", function()
+	render.SetBlendMode()
+
+	render2d.PushMatrix(0, 0, render2d.GetSize())
+		shader:Bind()
+		render2d.rectangle:Draw()
+	render2d.PopMatrix()
+end)
