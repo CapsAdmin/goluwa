4b3cc7b3d7d8471d40f671fe01f0f99d276c6ac3
CapsAdmin <eliashogstvedt@gmail.com>
2016-11-14 01:34:52 +0100

    sky tweaks

--- src/lua/libraries/graphics/render3d/gbuffer_shaders/test2.lua
+++ src/lua/libraries/graphics/render3d/gbuffer_shaders/test2.lua
@@ -94,9 +94,10 @@ vec3 atmosphere(vec3 r, vec3 r0, vec3 pSun, float iSun, float rPlanet, float rAt
         iTime += iStepSize;
 
     }
+	vec3 influence = texture(lua[sky_tex = steam.GetSkyTexture()], r).rgb;
 
     // Calculate and return the final color.
-    return iSun * (pRlh * kRlh * totalRlh + pMie * kMie * totalMie);
+    return iSun * (pRlh * influence * kRlh * totalRlh + pMie * kMie * totalMie);
 }
 
 vec3 gbuffer_compute_sky(vec3 ray, float depth)
@@ -105,15 +106,9 @@ vec3 gbuffer_compute_sky(vec3 ray, float depth)
 	vec3 sun_direction = lua[(vec3)render3d.GetShaderSunDirection];
 	float intensity = lua[world_sun_intensity = 1];
 
-	vec3 sky_color = lua[world_sky_color = Vec3(0.18867780436772762, 0.4978442963618773, 0.6616065586417131)];
-	vec3 influence = texture(lua[sky_tex = steam.GetSkyTexture()], ray).rgb*depth;
-	sky_color *= influence;
-
-	//{return textureLatLon(lua[nightsky_tex = render.CreateTextureFromPath("textures/skybox/street.jpg")], reflect(ray, sun_direction)).rgb;};
-
 	vec3 stars = textureLatLon(lua[nightsky_tex = render.CreateTextureFromPath("textures/skybox/milkyway.jpg")], reflect(ray, sun_direction)).rgb;
 	stars += pow(stars*1.25, vec3(1.5));
-	stars *= depth * 0.05;
+	stars *= depth * 0.005;
 
 	return depth*max(atmosphere(
 		normalize(ray),         		// normalized ray direction
