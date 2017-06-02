f76b16760780376f1f458f5516e7a7461b7389c9
CapsAdmin <eliashogstvedt@gmail.com>
2017-06-02 12:58:23 +0200

    don't store cookie
    fix extra newline on chat

--- src/lua/autorun/alan_chatbot.lua
+++ src/lua/autorun/alan_chatbot.lua
@@ -3,12 +3,12 @@ bot:SetNick("Alan")
 bot.gender = "male"
 
 function bot:Ask(question, cb, noprint)
-	self.cookie = self.cookie or vfs.Read("data/alan_cookie")
+	--self.cookie = self.cookie or vfs.Read("data/alan_cookie")
 
 	sockets.Request({
 		url = "http://www.a-i.com/alan1/webface1_ctrl.asp?gender="..self.gender.."&name="..self:GetNick().."&question="..question.."",
 		callback = function(data)
-			local answer =  data.content:match("<option>answer = (.-)<option>")
+			local answer =  data.content:match("<option>answer = (.-)\n")
 
 			answer = answer:gsub("notreallyanemailaddress", clients.GetLocalClient():GetNick())
 			answer = answer:gsub("notreallyalastname ", "")
@@ -16,7 +16,7 @@ function bot:Ask(question, cb, noprint)
 			if answer then
 				if not self.cookie then
 					self.cookie = data.header["set-cookie"]
-					vfs.Write("data/alan_cookie", self.cookie)
+					--vfs.Write("data/alan_cookie", self.cookie)
 				end
 				if not noprint then
 					chat.ClientSay(bot, answer)
