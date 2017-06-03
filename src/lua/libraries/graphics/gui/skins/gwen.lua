cad96ca92e2d8021c4091d8e77645ea477e491cd
CapsAdmin <eliashogstvedt@gmail.com>
2017-06-03 09:38:12 +0200

    turn off filtering for default gui font

--- src/lua/libraries/graphics/gui/skins/gwen.lua
+++ src/lua/libraries/graphics/gui/skins/gwen.lua
@@ -37,6 +37,7 @@ for i, sub_skin in ipairs(skins) do
 			path = "Roboto",
 			fallback = gfx.GetDefaultFont(),
 			size = 5.5*scale,
+			filtering = "none",
 		})
 
 		local texture = render.CreateTextureFromPath("textures/gui/skins/" .. sub_skin)
