9b10a7064df44f77ff1cdb0cc966072ff1aa8ecc
CapsAdmin <eliashogstvedt@gmail.com>
2017-07-19 14:06:24 +0200

    fix vfs.GetExtensionFromPath not being correct in some cases

--- src/lua/libraries/filesystem/path_utilities.lua
+++ src/lua/libraries/filesystem/path_utilities.lua
@@ -31,7 +31,7 @@ function vfs.RemoveExtensionFromPath(str)
 end
 
 function vfs.GetExtensionFromPath(str)
-	return str:match(".+%.(%a+)")
+	return vfs.GetFileNameFromPath(str):match(".+%.(%a+)")
 end
 
 function vfs.GetFolderFromPath(str)
