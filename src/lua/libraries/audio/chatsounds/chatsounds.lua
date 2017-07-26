97562f50adfd34534e6f063063d0bc6c0f0c63f4
CapsAdmin <eliashogstvedt@gmail.com>
2017-07-26 17:48:27 +0200

    remove unused code

--- src/lua/libraries/audio/chatsounds/chatsounds.lua
+++ src/lua/libraries/audio/chatsounds/chatsounds.lua
@@ -593,11 +593,6 @@ function chatsounds.PlayScript(script)
 
 			sound.duration = sound.duration or sound.snd:GetDuration()
 
-			-- let it be able to think once first so we can modify duration and such when changing pitch
-			if sound.think then
-				sound:think()
-			end
-
 			-- init modifiers
 			if sound.modifiers then
 				for mod, data in pairs(sound.modifiers) do
@@ -730,20 +725,5 @@ function chatsounds.Shutdown()
 	autocomplete.RemoveList("chatsounds")
 	event.RemoveListener("Update", "chatsounds")
 end
-
-chatsounds.Say([[
-	nice of the princess to invite us over for a picnic eh luigi
-	(cdi%130--1 cdi%50--1 cdi%180--1 cdi%115--1 cdi%170--1)*5
-	i hope she made lots of spaghetti--10*20 spaghetti
-	cdi%150--4 cdi%130--4 cdi%120--4 cdi%50--4
-	luigi look its from | bowser bowser^50 bowser^25 bowser^12 bowser^5
-	dear pesky plumbers--50 plumbers%150--50 plumbers%200--50 plumbers%250--50 plumbers%400--50 plumbers%700--50 plumbers%1000--50 plumbers%2000--50 plumbers%5000--50
-	the koopalings and i have taken over the mushroom kingdom^1000
-	the princess is now a permanent	guest%150 at one of my seven (koopa hotels):pitch(50)
-	i dare you to find her if you can
-	we gotta find the princess
-	and you gotta help us if you need instructions on how to get through
-	the hotels check out the:choose(4, cdihm_mario) enclosed instruction book
-]])
 
 return chatsounds
