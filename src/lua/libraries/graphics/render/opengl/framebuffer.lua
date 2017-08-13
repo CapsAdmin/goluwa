e20c59de402ad39e1a1e475bb5756564033c12fc
CapsAdmin <eliashogstvedt@gmail.com>
2017-08-13 11:20:45 +0200

    cleanup

--- src/lua/libraries/graphics/render/opengl/framebuffer.lua
+++ src/lua/libraries/graphics/render/opengl/framebuffer.lua
@@ -5,70 +5,40 @@ if not render.IsExtensionSupported("GL_ARB_framebuffer_object") then
 
 	local META = prototype.GetRegistered("framebuffer")
 
-	function render._CreateFrameBuffer(self, id_override)
-		self.textures = {}
-	end
-
-	function META:OnRemove()
-	end
-
-	function META:_Bind()
-	end
-
-	function META:SetTextureLayer(pos, tex, layer)
-		self.textures[pos] = tex
-	end
-
-	function META:SetTexture(pos, tex, mode, uid, face)
-		self.textures[pos] = tex
-	end
-
-	function META:GetTexture(pos)
-		pos = pos or 1
-
-		if self.textures[pos] then
-			return self.textures[pos]
-		end
-
-		return render.GetErrorTexture()
-	end
-
-	function META:SetWrite(pos, b)
-	end
-
-	function META:WriteThese(str)
-	end
-
-	function META:SaveDrawBuffers()
-	end
-
-	function META:RestoreDrawBuffers()
-	end
-
 	function META:ClearAll(r,g,b,a, d,s)
+		gl.ClearColor(r,g,b,a)
+		gl.ClearDepth(d)
+		gl.ClearStencil(s)
 		gl.Clear(bit.bor(gl.e.GL_COLOR_BUFFER_BIT, gl.e.GL_DEPTH_BUFFER_BIT, gl.e.GL_STENCIL_BUFFER_BIT))
 	end
 
 	function META:ClearColor(r,g,b,a)
+		gl.ClearColor(r,g,b,a)
 		gl.Clear(gl.e.GL_COLOR_BUFFER_BIT)
 	end
 
 	function META:ClearDepth(d)
+		gl.ClearDepth(d)
 		gl.Clear(gl.e.GL_DEPTH_BUFFER_BIT)
 	end
 
 	function META:ClearStencil(s)
+		gl.ClearStencil(s)
 		gl.Clear(gl.e.GL_STENCIL_BUFFER_BIT)
 	end
 
 	function META:ClearDepthStencil(d, s)
+		gl.ClearDepth(d)
+		gl.ClearStencil(s)
+		gl.Clear(bit.bor(gl.e.GL_STENCIL_BUFFER_BIT, gl.e.GL_DEPTH_BUFFER_BIT))
 	end
 
 	function META:ClearTexture(i, r,g,b,a)
-		gl.Clear(gl.e.GL_COLOR_BUFFER_BIT)
+		self:ClearColor(r,g,b,a)
 	end
 
 	prototype.Register(META)
+
 	return
 end
 
