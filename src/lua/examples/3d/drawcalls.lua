3293c922f44ad4a85995a7229fc338aeb0fbcfce
CapsAdmin <eliashogstvedt@gmail.com>
2017-08-16 13:01:41 +0200

    cleanup drawcall test

--- src/lua/examples/3d/drawcalls.lua
+++ src/lua/examples/3d/drawcalls.lua
@@ -1,59 +1,34 @@
+render3d.Initialize()
 entities.Panic()
 
-commands.RunString("mount gmod")
-
 local materials = {}
-
-for i = 1, 1 do
-	materials[i] = render.CreateBlankTexture(Vec2() + 32, "return vec4(random(uv*1), random(uv*2), random(uv*3), random(uv*4));")
+local function get_random_texture()
+	return render.CreateBlankTexture(Vec2() + 32, "return vec4(random(uv*"..math.random().."), random(uv*"..math.random().."), random(uv*"..math.random().."), random(uv*"..math.random().."));")
 end
 
+for i = 1, 64 do
+	local mat = render.CreateMaterial("model")
 
-do
-	local parent = parent
+	mat:SetAlbedoTexture(get_random_texture())
+	mat:SetNormalTexture(get_random_texture())
+	mat:SetRoughnessTexture(get_random_texture())
+	mat:SetMetallicTexture(get_random_texture())
 
-	for x = 0, 20 do
+	materials[i] = mat
+end
+
+local i = 1
+
+for x = 0, 20 do
 	for y = 0, 20 do
-	for z = 0, 20 do
+		for z = 0, 20 do
 
-		local child = entities.CreateEntity("visual")
-		child:SetPosition(Vec3(x, y, z)*2)
-		child:SetAngles(Ang3(0,0,0))
-		child:SetScale(Vec3(1, 1, 1))
-		child:SetModelPath("models/cube.obj")
-
-	--[[local light = entities.CreateEntity("light", child)
-		light:SetColor(Color(1,0,0,1))
-		light:SetSize(100)
-		light:SetIntensity(10)]]
-
-		--[[local mat = render.CreateMaterial("model")
-
-		mat:SetAlbedoTexture(table.random(materials))
-		mat:SetNormalTexture(table.random(materials))
-		mat:SetRoughnessTexture(table.random(materials))
-		mat:SetMetallicTexture(table.random(materials))
-
-		child:SetMaterialOverride(mat)]]
-
-
-		parent = child
+			local child = entities.CreateEntity("visual")
+			child:SetPosition(Vec3(x, y, z)*5)
+			--child:SetAngles(Ang3(0,0,0):GetRandom())
+			--child:SetScale(Vec3(1, 1, 1) * math.randomf(1, 1.5))
+			child:SetModelPath("models/cube.obj")
+			child:SetMaterialOverride(table.random(materials))
+		end
 	end
-	end
-	end
-end
-do return end
-
-local start = system.GetElapsedTime()
-
-parent:BuildChildrenList()
-
-function goluwa.Update()
-	local t = (system.GetElapsedTime() - start) / 10
-
-	for _, child in ipairs(parent:GetChildrenList()) do
-		child:SetAngles(Ang3(t,t,-t))
-		t = t * 1.001
-	end
-
 end
