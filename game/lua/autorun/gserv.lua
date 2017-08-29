879c9d8db1edb191c6076139a59e8a296532aadf
CapsAdmin <eliashogstvedt@gmail.com>
2017-08-30 00:50:11 +0200

    also support numbers as steamworks id

--- game/lua/autorun/gserv.lua
+++ game/lua/autorun/gserv.lua
@@ -366,9 +366,9 @@ do -- addons
 				type = "git",
 				name = name_override or url:match(".+/(.+)%.git"):lower(),
 			}
-		elseif url:find("steamcommunity") and url:find("id=%d+") then
+		elseif url:find("steamcommunity") and url:find("id=%d+") or tonumber(url) then
 			info = {
-				id = url:match("id=(%d+)"),
+				id = url:match("id=(%d+)") or tonumber(url),
 				type = "workshop",
 				name = name_override or url:match("id=(%d+)"),
 			}
