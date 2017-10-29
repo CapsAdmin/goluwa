46946a2aba2268333ba7a1d405ca54eb9ba74bc1
CapsAdmin <eliashogstvedt@gmail.com>
2017-10-29 16:13:02 +0100

    tweaks

--- game/lua/examples/gui/material_design.lua
+++ game/lua/examples/gui/material_design.lua
@@ -1,6 +1,3 @@
--- scale translation somehow
-
-
 local base = gui.CreatePanel("base", nil, "lol")
 base:SetSize(window.GetSize())
 base:CenterSimple()
@@ -9,16 +6,15 @@ base:SetDraggable(true)
 base:SetColor(ColorBytes(238, 238, 238))
 base:SetMargin(Rect()+16)
 base:SetName("base")
---[[
+
 local btn = base:CreatePanel("image")
 --btn:SetResizable(true)
 btn:SetDraggable(true)
-btn:SetPath("https://openclipart.org/image/2400px/svg_to_png/252869/Prismatic-Abstract-Star-Motif-2.png")
+--btn:SetPath("http://www.pngmart.com/files/5/Marketplace-Transparent-PNG.png")
 
 btn:SetSize(Vec2()+160)
 btn:CenterSimple()
 btn:SetColor(ColorBytes(255, 255, 255))
-]]
 
 base:SetStack(true)
 
@@ -31,52 +27,42 @@ for i = 0, 0 do
 	btn:SetSize(Vec2()+128)
 	btn:SetPadding(Rect()+32)
 	btn:SetColor(ColorHSV(math.random(),0.75,1))
+	btn:SetZ((i/15) * 45)
 	--btn:CenterSimple()
 	--btn:SetPosition(Vec2(i*170,0))
-
-
-	btn.OnPostDraw = function()  end
-
 end
 
-local shadow_size = window.GetSize()/2
+local shadow_size = Vec2() + 1024
 local shadow_texture = render.CreateTexture("2d_multisample")
 shadow_texture:SetSize(shadow_size)
-shadow_texture:SetMultisample(4)
+shadow_texture:SetMultisample(8)
 shadow_texture:SetInternalFormat("rgba8")
-shadow_texture:SetMipMapLevels(1)
 shadow_texture:SetupStorage()
 
 local shadow_framebuffer = render.CreateFrameBuffer()
 shadow_framebuffer:SetTexture(1, shadow_texture)
 shadow_texture.fb = shadow_framebuffer
 
+local resolve_tex = render.CreateBlankTexture(shadow_size)
+
 function base:DrawChild(child)
 	child.lol = child.lol or math.random()*100
-	child.Z = 0--45*(math.sin(5*system.GetElapsedTime()+child.lol)*0.5+0.5)
+	child.Z = 45*(math.sin(5*system.GetElapsedTime()+child.lol)*0.5+0.5)
 --	for _, child in ipairs(self:GetChildren()) do
 	--	if child:IsVisible() then
 			child:InvalidateMatrix()
 			child:RebuildMatrix(true)
 
-			local m = child:GetSize():GetLength() * 0.002
+			local m = child:GetSize():GetLength() * 0.0025
 
 			render2d.SetWorldMatrix(child.Matrix)
 
 			shadow_framebuffer:Begin()
 			shadow_framebuffer:ClearColor(0,0,0,0)
 
-
-			render2d.PushMatrix()
-				render2d.Translatef(math.sin(os.clock()) * 40,math.cos(os.clock()) * 40)
-				render2d.Scale(50,50)
-				render2d.BindShader()
-				render2d.rectangle:Draw()
-			render2d.PopMatrix()
-
 			-- trail and error math
 			render2d.PushMatrix()
-			render2d.Translatef(0,child.Z*m/3 + 10 + math.sin(os.clock()*10)*5)
+			render2d.Translatef(0,child.Z*m / window.GetSize().y * shadow_size.y / 2.5)
 			render2d.Scale(1/self.Size.x * shadow_size.x,1/self.Size.y * shadow_size.y)
 			render2d.Translatef(-child.Position.x * (self.Size.x / shadow_size.x) + child.Position.x, -child.Position.y * (self.Size.y / shadow_size.y) + child.Position.y)
 				child:OnPreDraw()
@@ -86,12 +72,25 @@ function base:DrawChild(child)
 
 			shadow_framebuffer:End()
 
+			resolve_tex:Clear()
+			resolve_tex:Shade([[
+				vec4 color = vec4(0);
+
+				for (int i = 0; i < samples; i++)
+				{
+					color += texelFetch(msaa_tex, ivec2(uv * textureSize(msaa_tex)), i);
+				}
+
+				return color / samples;
+			]], {samples = shadow_texture:GetMultisample(), msaa_tex = shadow_texture})
+
+
 			local iterations = 2
 			local size = child.Z * m ^ 2.75
 
 			for i = 1, iterations do
 				for _, dir in ipairs({Vec2(1, 0), Vec2(0, 1)}) do
-					shadow_texture:Shade([[
+					resolve_tex:Shade([[
 						vec4 sum = vec4(0.0);
 
 						vec2 blur = radius/resolution;
@@ -113,7 +112,7 @@ function base:DrawChild(child)
 						return sum;
 					]], {
 						radius = size/i,
-						resolution = shadow_size,
+						resolution = window.GetSize(),
 						dir = dir,
 					}, "none")
 
@@ -125,7 +124,7 @@ function base:DrawChild(child)
 			self:InvalidateMatrix()
 			self:RebuildMatrix()
 			render2d.SetWorldMatrix(self.Matrix)
-			gfx.DrawRect(0, 0, self.Size.x, self.Size.y, shadow_texture, 1,1,1,0.75)
+			gfx.DrawRect(0, 0, self.Size.x, self.Size.y, resolve_tex, 1,1,1,0.75)
 			render.SetPresetBlendMode("alpha")
 		--end
 	--end
