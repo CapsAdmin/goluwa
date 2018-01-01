0c7420c09434ee86fe5196dd1e6de2970142a17b
CapsAdmin <eliashogstvedt@gmail.com>
2018-01-01 23:37:44 +0100

    disable bot activity by default

--- game/lua/autorun/server/bot_activity.lua
+++ game/lua/autorun/server/bot_activity.lua
@@ -1,34 +1,36 @@
-event.Timer("bot_activity", 0.25, 0, function()
-	if math.random() > 0.99 then
-		local bot = table.random(clients.GetAll())
-		if bot and bot:IsBot() then
-			bot:Kick(string.randomwords(2, math.random()))
-		end
-	end
-
-	if math.random() > 0.9 and #clients.GetAll() < 32 then
-		clients.CreateBot()
-	end
-
-	if math.random() > 0.999 then
-		for k,v in pairs(clients.GetAll()) do
-			if v:IsBot() then
-				v:Kick("LOL")
+commands.Add("bot_activity", function()
+	event.Timer("bot_activity", 0.25, 0, function()
+		if math.random() > 0.99 then
+			local bot = table.random(clients.GetAll())
+			if bot and bot:IsBot() then
+				bot:Kick(string.randomwords(2, math.random()))
 			end
 		end
-	end
 
-	for _, bot in ipairs(clients.GetAll()) do
-		if bot:IsBot() then
-			if not bot.do_soemthing or bot.do_soemthing < os.clock() then
+		if math.random() > 0.9 and #clients.GetAll() < 32 then
+			clients.CreateBot()
+		end
 
-				if math.random() > 0.5 then
-					local sentence = string.randomwords(math.random(20), math.random())
-					chat.ClientSay(bot, sentence)
+		if math.random() > 0.999 then
+			for k,v in pairs(clients.GetAll()) do
+				if v:IsBot() then
+					v:Kick("LOL")
 				end
-
-				bot.do_soemthing = (os.clock() + math.random()*30)
 			end
 		end
-	end
+
+		for _, bot in ipairs(clients.GetAll()) do
+			if bot:IsBot() then
+				if not bot.do_soemthing or bot.do_soemthing < os.clock() then
+
+					if math.random() > 0.5 then
+						local sentence = string.randomwords(math.random(20), math.random())
+						chat.ClientSay(bot, sentence)
+					end
+
+					bot.do_soemthing = (os.clock() + math.random()*30)
+				end
+			end
+		end
+	end)
 end)
