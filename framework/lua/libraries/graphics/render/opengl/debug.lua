15d7a49cf001179016c424bd5a76995f09741134
CapsAdmin <eliashogstvedt@gmail.com>
2018-04-10 18:09:39 +0200

    fix error when debug.getlocal returns cdata

--- framework/lua/libraries/graphics/render/opengl/debug.lua
+++ framework/lua/libraries/graphics/render/opengl/debug.lua
@@ -49,7 +49,7 @@ function render.SetDebug(b)
 						obj = nil
 					end
 
-					if obj and obj.GetDebugTrace and obj:GetDebugTrace() ~= "" then
+					if obj and hasindex(obj) and obj.GetDebugTrace and obj:GetDebugTrace() ~= "" then
 						logn(obj:GetDebugTrace())
 					end
 
