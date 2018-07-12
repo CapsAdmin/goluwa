8d9d6743dd2d2a901031fe425e123c9ab308528c
CapsAdmin <eliashogstvedt@gmail.com>
2018-07-12 13:38:54 +0200

    add swizzle support for vectors and share some code between vec2 and vec3

--- framework/lua/libraries/structs/vec3.lua
+++ framework/lua/libraries/structs/vec3.lua
@@ -3,121 +3,25 @@ local structs = (...) or _G.structs
 local META = prototype.CreateTemplate("Vec3")
 
 META.NumberType = "double"
-META.Args = {"x", "y", "z"}
+META.Args = {{"x", "r", "p"}, {"y", "g", "y"},  {"z", "b", "r"}}
 
 structs.AddAllOperators(META)
+structs.AddOperator(META, "generic_vector")
 
--- length stuff
-do
-	function META:GetLengthSquared()
-		return self.x * self.x + self.y * self.y + self.z * self.z
-	end
+structs.Swizzle(META)
+structs.Swizzle(META, 2, "structs.Vec2")
 
-	function META:SetLength(num)
-		if num == 0 then
-			self.x = 0
-			self.y = 0
-			self.z = 0
-			return
-		end
-
-		local scale = math.sqrt(self:GetLengthSquared()) * num
-
-		self.x = self.x / scale
-		self.y = self.y / scale
-		self.z = self.z / scale
-
-		return self
-	end
-
-	function META:GetLength()
-		return math.sqrt(self:GetLengthSquared())
-	end
-
-	META.__len = META.GetLength
-
-	function META.__lt(a, b)
-		if structs.IsType(a, b) and type(b) == "number" then
-			return a:GetLength() < b
-		elseif structs.IsType(b, a) and type(a) == "number" then
-			return b:GetLength() < a
-		end
-	end
-
-	function META.__le(a, b)
-		if structs.IsType(a, b) and type(b) == "number" then
-			return a:GetLength() <= b
-		elseif structs.IsType(b, a) and type(a) == "number" then
-			return b:GetLength() <= a
-		end
-	end
-
-	function META:SetMaxLength(num)
-		local length = self:GetLengthSquared()
-
-		if length * length > num then
-			local scale = math.sqrt(length) * num
-
-			self.x = self.x / scale
-			self.y = self.y / scale
-			self.z = self.z / scale
-		end
-
-		return self
-	end
-
-	function META.Distance(a, b)
-		return (a - b):GetLength()
-	end
-end
-
-function META.Lerp(a, mult, b)
-
-	a.x = (b.x - a.x) * mult + a.x
-	a.y = (b.y - a.y) * mult + a.y
-	a.z = (b.z - a.z) * mult + a.z
-
-	return a
-end
-
-structs.AddGetFunc(META, "Lerp", "Lerped")
-
-function META:Normalize()
-	local sqr = self:GetLengthSquared()
-
-	if sqr == 0 then return self end
-
-	local len = math.sqrt(sqr)
-
-	self.x = self.x / len
-	self.y = self.y / len
-	self.z = self.z / len
-
-	return self
-end
-
-structs.AddGetFunc(META, "Normalize", "Normalized")
 
 function META.Cross(a, b)
 	local x, y, z = a.x, a.y, a.z
+
 	a.x = y * b.z - z * b.y
 	a.y = z * b.x - x * b.z
 	a.z = x * b.y - y * b.x
+
 	return a
 end
-
 structs.AddGetFunc(META, "Cross")
-
-function META.GetDot(a, b)
-	return
-		a.x * b.x +
-		a.y * b.y +
-		a.z * b.z
-end
-
-function META:GetVolume()
-	return self.x * self.y * self.z
-end
 
 function META:GetAngles()
 	local n = self:GetNormalized()
--- framework/lua/libraries/structs/vec4.lua
+++ framework/lua/libraries/structs/vec4.lua
@@ -3,104 +3,14 @@ local structs = (...) or _G.structs
 local META = prototype.CreateTemplate("Vec4")
 
 META.NumberType = "double"
-META.Args = {"x", "y", "z", "w"}
+META.Args = {{"x", "r"}, {"y", "g"}, {"z", "b"}, {"w", "a"}}
 
 structs.AddAllOperators(META)
 
--- length stuff
-do
-	function META:GetLengthSquared()
-		return self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w
-	end
-
-	function META:SetLength(num)
-		if num == 0 then
-			self.x = 0
-			self.y = 0
-			self.z = 0
-			self.w = 0
-			return
-		end
-
-		local scale = math.sqrt(self:GetLengthSquared()) * num
-
-		self.x = self.x / scale
-		self.y = self.y / scale
-		self.z = self.z / scale
-		self.w = self.w / scale
-
-		return self
-	end
-
-	function META:GetLength()
-		return math.sqrt(self:GetLengthSquared())
-	end
-
-	META.__len = META.GetLength
-
-	function META.__lt(a, b)
-		if structs.IsType(a, b) and type(b) == "number" then
-			return a:GetLength() < b
-		elseif structs.IsType(b, a) and type(a) == "number" then
-			return b:GetLength() < a
-		end
-	end
-
-	function META.__le(a, b)
-		if structs.IsType(a, b) and type(b) == "number" then
-			return a:GetLength() <= b
-		elseif structs.IsType(b, a) and type(a) == "number" then
-			return b:GetLength() <= a
-		end
-	end
-
-	function META:SetMaxLength(num)
-		local length = self:GetLengthSquared()
-
-		if length * length > num then
-			local scale = math.sqrt(length) * num
-
-			self.x = self.x / scale
-			self.y = self.y / scale
-			self.z = self.z / scale
-			self.w = self.w / scale
-		end
-
-		return self
-	end
-
-	function META.Distance(a, b)
-		return (a - b):GetLength()
-	end
-end
-
-function META.Lerp(a, mult, b)
-
-	a.x = (b.x - a.x) * mult + a.x
-	a.y = (b.y - a.y) * mult + a.y
-	a.z = (b.z - a.z) * mult + a.z
-
-	return a
-end
-
-structs.AddGetFunc(META, "Lerp", "Lerped")
-
-function META:Normalize()
-	local sqr = self:GetLengthSquared()
-
-	if sqr == 0 then return self end
-
-	local len = math.sqrt(sqr)
-
-	self.x = self.x / len
-	self.y = self.y / len
-	self.z = self.z / len
-	self.w = self.w / len
-
-	return self
-end
-
-structs.AddGetFunc(META, "Normalize", "Normalized")
+structs.AddOperator(META, "generic_vector")
+structs.Swizzle(META)
+structs.Swizzle(META, 3, "structs.Vec3")
+structs.Swizzle(META, 2, "structs.Vec2")
 
 structs.Register(META)
 
