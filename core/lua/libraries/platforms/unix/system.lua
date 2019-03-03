c56112c1959e1955d2294fefbc57cee4b6562c36
Elias Hogstvedt <eliashogstvedt@gmail.com>
2019-03-03 22:38:38 +0100

    fix system.GetTime on osx

--- core/lua/libraries/platforms/unix/system.lua
+++ core/lua/libraries/platforms/unix/system.lua
@@ -33,7 +33,28 @@ do
 	end
 end
 
-do
+if ffi.os == "OSX" then
+	ffi.cdef([[
+		struct mach_timebase_info {
+			uint32_t	numer;
+			uint32_t	denom;
+		};
+		int mach_timebase_info(struct mach_timebase_info *info);
+		uint64_t mach_absolute_time(void);
+	]])
+
+	local tb = ffi.new("struct mach_timebase_info")
+	ffi.C.mach_timebase_info(tb)
+	local orwl_timebase = tb.numer
+	local orwl_timebase = tb.denom
+	local orwl_timestart = ffi.C.mach_absolute_time()
+
+	function system.GetTime()
+		local diff = (ffi.C.mach_absolute_time() - orwl_timestart) * orwl_timebase
+		diff = tonumber(diff) / 1000000000
+		return diff
+	end
+else
 	ffi.cdef([[
 		struct timespec {
 			long int tv_sec;
