f22c60a3dfa9b87ea15ff4c129a27cc3b1bf673f
Elias Hogstvedt <eliashogstvedt@gmail.com>
2019-05-04 17:23:52 +0200

    more work on javascript transpiler

--- core/lua/libraries/oh/lua/js_code_emitter.lua
+++ core/lua/libraries/oh/lua/js_code_emitter.lua
@@ -6,6 +6,44 @@ local table_remove = table.remove
 local ipairs = ipairs
 
 local META = oh.BaseEmitter
+function META:ExpressionFromSuffixes(node)
+
+	local linear = {node.value}
+
+	for _, suffix in ipairs(node.suffixes) do
+		table.insert(linear, 1, suffix)
+		table.insert(linear, 1, suffix)
+	end
+
+	local i = 1
+
+	local function get_token()
+		return linear[i]
+	end
+
+	local function binary()
+		local val = {value = get_token()}
+		i = i + 1
+
+		while get_token() do
+			local op = get_token()
+			if not op then break end
+			i = i + 1
+
+			local right = binary()
+			local left = val
+
+			val = {}
+			val.op = op
+			val.left = left
+			val.right = right
+		end
+
+		return val
+	end
+
+	return binary()
+end
 
 function META:Expression(v)
 	if v.tokens["left("] then
@@ -16,6 +54,23 @@ function META:Expression(v)
 
 	if v.type == "operator" then
 		local func_name = lua.syntax.GetFunctionForOperator(v.tokens["operator"])
+
+		if v.tokens["operator"].value == "+" then
+			func_name = "$O.add"
+		elseif v.tokens["operator"].value == "-" then
+			func_name = "$O.sub"
+		elseif v.tokens["operator"].value == "*" then
+			func_name = "$O.mul"
+		elseif v.tokens["operator"].value == "/" then
+			func_name = "$O.div"
+		elseif v.tokens["operator"].value == "%" then
+			func_name = "$O.mod"
+		elseif v.tokens["operator"].value == "^" then
+			func_name = "$O.pow"
+		elseif v.tokens["operator"].value == ".." then
+			func_name = "$O.concat"
+		end
+
 		if func_name then
 			self:Emit(" " .. func_name .. "(")
 			if v.left then self:Expression(v.left) end
@@ -34,7 +89,54 @@ function META:Expression(v)
 	elseif v.type == "unary" then
 		self:Unary(v)
 	elseif v.type == "value" then
-		self:EmitToken(v.value)
+		if v.value.value == "..." then
+			self:EmitToken(v.value, "...args")
+		elseif v.value.value == "nil" then
+			self:EmitToken(v.value, "undefined")
+		elseif v.suffixes then
+
+			local exp = self:ExpressionFromSuffixes(v)
+
+			local function go(node)
+				if node.value then
+					local node = node.value
+
+					if node.type == "letter" then
+						self:EmitToken(node)
+					elseif node.type == "index_expression" then
+						self:Expression(node.value)
+					elseif node.type == "self_index" or node.type == "index" then
+						self:Emit('"')
+						self:EmitToken(node.value.value)
+						self:Emit('"')
+						if node.type == "self_index" then
+							self.SELFINDEX = true
+						end
+					elseif node.type == "call" then
+						self:ExpressionList(node.arguments)
+					end
+					return
+				end
+				if self.NEWINDEX then
+					self.NEWINDEX = nil
+					go(node.right)
+					self:Emit(",")
+					go(node.left)
+				else
+					self:Emit("$O.")
+					self:Emit(node.op.type)
+					self:Emit("(")
+					go(node.right)
+					self:Emit(",")
+					go(node.left)
+					self:Emit(")")
+				end
+			end
+
+			go(exp)
+		else
+			self:EmitToken(v.value)
+		end
 
 		if v.data_type then
 			--print(v)
@@ -49,31 +151,6 @@ function META:Expression(v)
 			self:EmitToken(v)
 		end
 	end
-
-	if v.suffixes then
-		for _, node in ipairs(v.suffixes) do
-			if node.type == "index" then
-				self:EmitToken(node.tokens["."])
-				self:EmitToken(node.value.value)
-			elseif node.type == "self_index" then
-				self:EmitToken(node.tokens[":"])
-				self:EmitToken(node.value.value)
-			elseif node.type == "index_expression" then
-				self:EmitToken(node.tokens["["])
-				self:Expression(node.value)
-				self:EmitToken(node.tokens["]"])
-			elseif node.type == "call" then
-				if node.tokens["call("] then
-					self:EmitToken(node.tokens["call("])
-				end
-				self:ExpressionList(node.arguments)
-
-				if node.tokens["call)"] then
-					self:EmitToken(node.tokens["call)"])
-				end
-			end
-		end
-	end
 end
 
 function META:Operator(v)
@@ -85,7 +162,10 @@ function META:Operator(v)
 		translate = "&&"
 	elseif v.tokens.operator.value == "or" then
 		translate = "||"
+	elseif v.tokens.operator.value == "~=" then
+		translate = "!="
 	end
+
 	self:EmitToken(v.tokens.operator, translate)
 end
 
@@ -94,25 +174,35 @@ function META:Function(v)
 
 	if v.is_local then
 		self:EmitToken(v.tokens["local"], "let")
+	else
+		self:EmitToken(v.tokens["function"], "/*function*/")
 	end
-
-	--self:EmitToken(v.tokens["function"])
 	--self:Whitespace(" ")
 
 	if v.value then
+		self.NEWINDEX = true
+		self:Emit("$O.newindex(")
 		self:Expression(v.value)
+		self:Emit(",")
 	end
 
 	if v.is_local then
-		self:Emit(" = ")
+		self:Emit("=")
 	end
 
 	self:EmitToken(v.tokens["func("])
+	if self.SELFINDEX then
+		self.SELFINDEX = nil
+		self:Emit("self")
+		if v.arguments[1] then
+			self:Emit(",")
+		end
+	end
 	self:ExpressionList(v.arguments)
 	self:EmitToken(v.tokens["func)"])
 
-	self:Emit(" => ")
-	
+	self:Emit("=>")
+
 	if v.return_types then
 		for i,args in ipairs(v.return_types) do
 			for i,v in ipairs(args) do
@@ -123,7 +213,7 @@ function META:Function(v)
 		end
 	end
 
-	self:Emit(" {")
+	self:Emit("{")
 
 	self:Whitespace("\n")
 		self:Whitespace("\t+")
@@ -132,11 +222,16 @@ function META:Function(v)
 	self:Whitespace("\t")
 
 	self:EmitToken(v.tokens["end"], "}")
+
+	if v.value then
+		self:Emit(")")
+	end
 end
 
 function META:Table(v)
 	if not v.children[1] then
-		self:EmitToken(v.tokens["{"])self:EmitToken(v.tokens["}"])
+		self:EmitToken(v.tokens["{"])
+		self:EmitToken(v.tokens["}"])
 	else
 		local guess = "array"
 
@@ -193,7 +288,7 @@ function META:Table(v)
 			end
 			self:Whitespace("\t-")
 		self:Whitespace("\t")
-		
+
 		if guess == "table" then
 			self:EmitToken(v.tokens["}"])
 		else
@@ -204,6 +299,15 @@ end
 
 function META:Unary(v)
 	local func_name = lua.syntax.GetFunctionForUnaryOperator(v.tokens["operator"])
+
+	if v.tokens["operator"].value == "#" then
+		func_name = "$O.len"
+	elseif v.tokens["operator"].value == "-" then
+		func_name = "$O.unm"
+	elseif v.tokens["operator"].value == "~" then
+		func_name = "$O.bnot"
+	end
+
 	if func_name then
 		self:Emit(" " .. func_name .. "(")
 		self:Expression(v.expression)
@@ -255,22 +359,32 @@ function META:Block(block)
 		if data.type == "if" then
 			for i,v in ipairs(data.clauses) do
 
-				self:Whitespace("\t")self:EmitToken(v.tokens["if/else/elseif"], v.tokens["if/else/elseif"].value == "elseif" and "else if" or nil) 
-					if v.condition then 
-						self:Emit("(")
-						self:Expression(v.condition) 
-						self:Emit(")")
-						self:Whitespace(" ") 
-						self:EmitToken(v.tokens["then"], "{") 
-					else
-						self:Emit("{") 
-					end
+				self:Whitespace("\t")
+
+				if v.tokens["if/else/elseif"].value == "if" then
+					self:EmitToken(v.tokens["if/else/elseif"])
+				elseif v.tokens["if/else/elseif"].value == "elseif" then
+					self:EmitToken(v.tokens["if/else/elseif"], "} else if")
+				else
+					self:EmitToken(v.tokens["if/else/elseif"], "} else {")
+				end
+
+				if v.condition then
+					self:Emit("(")
+					self:Expression(v.condition)
+					self:Emit(")")
+					self:Whitespace(" ")
+					self:EmitToken(v.tokens["then"], "{")
+				end
+
 				self:Whitespace("\n")
 				self:Whitespace("\t+")
 				self:Block(v.block)
 				self:Whitespace("\t-")
-				
-				self:Emit("}") 
+
+				if v.tokens["end"] then
+					self:EmitToken(v.tokens["end"], "}")
+				end
 			end
 		elseif data.type == "goto" then
 			self:Whitespace("\t") self:EmitToken(data.tokens["goto"]) self:Whitespace(" ") self:Expression(data.label)
@@ -301,7 +415,7 @@ function META:Block(block)
 			self:Whitespace("\t")self:EmitToken(data.tokens["break"])
 		elseif data.type == "return" then
 			self:Whitespace("\t")self:Whitespace("?")self:EmitToken(data.tokens["return"])
-
+			self:Emit(" ")
 			if data.expressions then
 				self:ExpressionList(data.expressions)
 			end
@@ -346,22 +460,28 @@ function META:Block(block)
 			self:Whitespace("\t")self:EmitToken(data.tokens["end"], "}")
 
 		elseif data.type == "do" then
-			self:Whitespace("\t")self:EmitToken(data.tokens["do"])self:Whitespace("\n")
+			self:Whitespace("\t")self:EmitToken(data.tokens["do"], "{")self:Whitespace("\n")
 				self:Whitespace("\t+")
 					self:Block(data.block)
 				self:Whitespace("\t-")
-			self:Whitespace("\t")self:EmitToken(data.tokens["end"])
+			self:Whitespace("\t")self:EmitToken(data.tokens["end"], "}")
 		elseif data.type == "assignment" then
 			self:Whitespace("\t") if data.is_local then self:EmitToken(data.tokens["local"], "let")self:Whitespace(" ") end
+
+			if not data.is_local then
+			end
 
 			for i,v in ipairs(data.lvalues) do
 				if data.is_local then
 					self:EmitToken(v.value)
 				else
+					self.NEWINDEX = true
+					self:EmitToken(v.value, "$O.newindex(")
 					self:Expression(v)
 				end
 				if data.lvalues[2] and i ~= #data.lvalues then
-					self:EmitToken(v.tokens[","])self:Whitespace(" ")
+					self:EmitToken(v.tokens[","])
+					self:Whitespace(" ")
 				end
 
 				if v.value_type then
@@ -372,16 +492,28 @@ function META:Block(block)
 			end
 
 			if data.rvalues then
-				self:Whitespace(" ")self:EmitToken(data.tokens["="])self:Whitespace(" ")
+				self:Whitespace(" ")
+				self:EmitToken(data.tokens["="], not data.is_local and "," or nil)
+				self:Whitespace(" ")
 
 				for i,v in ipairs(data.rvalues) do
 					self:Expression(v)
 
 					if data.rvalues[2] and i ~= #data.rvalues then
-						self:EmitToken(v.tokens[","])self:Whitespace(" ")
+						self:EmitToken(v.tokens[","])
+						self:Whitespace(" ")
 					end
 				end
+			else
+				self:Emit(", undefined")
 			end
+
+			if not data.is_local then
+				self:Emit(")")
+			end
+
+			self.NEWINDEX = false
+			self:Emit(";")
 		elseif data.type == "function" then
 			self:Function(data)
 		elseif data.type == "expression" then
@@ -396,7 +528,7 @@ function META:Block(block)
 			self:EmitToken(data.tokens["shebang"])
 		elseif data.type == "interface" then
 			self:Emit("-- interface TODO")
-		elseif data.type == "compiler_option" then
+		elseif data.type == "compiler$Otion" then
 			self:Emit("--" .. data.lua)
 
 			if data.lua:startswith("E:") then
