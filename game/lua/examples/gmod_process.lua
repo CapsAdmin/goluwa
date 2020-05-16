69688caa5b1b4b14335c489f03ad83db2975c422
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-05-16 11:27:12 +0200

    more examples

---
+++ game/lua/examples/gmod_process.lua
@@ -0,0 +1,31 @@
+local ffi = require("ffi")
+ffi.cdef("int ioctl(int fd, unsigned long request, ...);")
+
+local p = assert(io.popen("sleep 1; echo 'aaa'"))
+local fd = ffi.C.fileno(p)
+local int = ffi.new("size_t[1]")
+
+event.AddListener("Update", "", function()
+    if ffi.C.ioctl(fd, 21531, int) == 0 and int[0] > 0 then
+        print(p:read(tonumber(int[0])))
+    end
+end)
+LOL = p
+
+local p = assert(io.popen([[read -p "input:" test; sleep 1; echo "INPUT IS: $test"]], "w"))
+event.Delay(1, function() p:write("test!\n") end)
+local fd = ffi.C.fileno(p)
+
+local int = ffi.new("size_t[1]")
+event.AddListener("Update", "", function()
+    if ffi.C.ioctl(fd, 21531, int) == 0 and int[0] > 0 then
+        print("|" .. p:read(tonumber(int[0])) .. "|")
+    end
+end)
+LOL2 = p
+
+
+
+--local gmod = assert(steam.FindSourceGame("gmod"))
+
+  --  print(gmod.game_dir)
---
+++ game/lua/examples/sockets/harvest_lua.lua
@@ -0,0 +1,53 @@
+local function fetch_repos(page_num)
+    page_num = page_num or 1
+    http.GET(
+        "https://github.com/search?l=Lua&o=desc&p="..page_num.."&q=extension%3Alua&s=forks&type=Repositories"
+    )
+
+    :Then(function(content)
+        for url in content:gmatch("Repository&quot;,&quot;url&quot;:&quot;(.-)&quot;") do
+            serializer.StoreInFile("luadata", "lua_harvest", url, true)
+        end
+    end)
+end
+
+
+--fetch_repos(2)
+
+local git = system.GetCLICommand("git")
+
+fs.CreateDirectory("harvest_lua")
+
+fs.PushWorkingDirectory("harvest_lua")
+
+for url in pairs(serializer.GetKeyValuesInFile("luadata", "lua_harvest")) do
+    local name = url:match(".+/(.+)")
+    if fs.get_type(name) ~= "directory" then
+        git.clone(url, "--depth 1", "&")
+    end
+end
+
+fs.PopWorkingDirectory()
+
+fs.CreateDirectory("harvest_lua/flat")
+
+for path in io.popen("locate .lua"):lines() do
+    if path:endswith(".lua") then
+        local f, err = io.open(path)
+        if f then
+            local content = f:read("*all") f:close()
+            if content and loadstring(content) then
+                vfs.Write("os:" .. e.ROOT_FOLDER .. "harvest_lua/flat/" .. crypto.CRC32(content) .. ".lua", content)
+            end
+        end
+    end
+end
+
+for _, path in ipairs(fs.get_files_recursive("harvest_lua")) do
+    if path:endswith(".lua") then
+        local f = assert(io.open(path)) local content = f:read("*all") f:close()
+        if content then
+            vfs.CopyFile("os:" .. e.ROOT_FOLDER .. path, "os:" .. e.ROOT_FOLDER .. "harvest_lua/flat/" .. crypto.CRC32(content) .. ".lua")
+        end
+    end
+end
