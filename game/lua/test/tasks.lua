e48f9db8a929367250b4e27859c71fb67e88b5e4
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-05-16 11:28:43 +0200

    more tests

---
+++ game/lua/test/tasks.lua
@@ -0,0 +1,33 @@
+local function promise(func)
+    return func
+end
+
+local function await(func, ...)
+    assert(coroutine.running())
+
+    local ret = nil
+
+    func(function(...) ret = {...} end)
+
+    while ret == nil do
+        tasks.Wait()
+    end
+
+    return unpack(ret)
+end
+
+foo = promise(function(resolve, ...)
+    return (function(someval)
+        local val = nil
+
+        event.Delay(5, function()
+            resolve(someval)
+        end)
+    end)(...)
+end)
+
+tasks.enabled = true
+
+tasks.CreateTask(function()
+    local val = await(foo, 5)
+end)
