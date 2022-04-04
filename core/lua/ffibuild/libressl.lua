1d0e5e1612556ed2159b0988b2fb1ec8d87bc05d
CapsAdmin <eliashogstvedt@gmail.com>
2022-04-05 01:04:31 +0200

    faster build

--- core/lua/ffibuild/libressl.lua
+++ core/lua/ffibuild/libressl.lua
@@ -1,7 +1,7 @@
 ffibuild.Build({
 	name = "libtls",
 	url = "https://github.com/libressl-portable/portable.git",
-	cmd = "./autogen.sh && ./configure && make",
+	cmd = "./autogen.sh && ./configure && make -j32",
 	addon = vfs.GetAddonFromPath(SCRIPT_PATH),
 
 	c_source = [[
